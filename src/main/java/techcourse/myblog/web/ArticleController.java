027057905885a1b462ab15cc5964ebd8ef11df5f
Latera_ <bluesky0255@gmail.com>
2019-07-09 14:41:50 +0900

    Implement article publish request

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -3,7 +3,13 @@ package techcourse.myblog.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import techcourse.myblog.domain.ArticleRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+import java.io.IOException;
 
 @Controller
 public class ArticleController {
@@ -13,5 +19,10 @@ public class ArticleController {
     @GetMapping("/writing")
     public String editArticle() {
         return "article-edit";
+    }
+
+    @PostMapping("/articles")
+    public void publishArticle(String title, String backgroundURL, String content, HttpServletResponse response) throws IOException {
+        response.sendRedirect("/");
     }
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,11 +1,16 @@
 package techcourse.myblog.web;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,4 +32,21 @@ public class ArticleControllerTests {
                 .expectStatus().isOk();
     }
 
+    @Test
+    void createArticlePost() {
+        Map<String,String> requestMap = new HashMap<>();
+        String title = "hello";
+        String backgroundURL = "http://asdf.com";
+        String content = "helloContent";
+
+        requestMap.put("title", title);
+        requestMap.put("backgroundURL", backgroundURL);
+        requestMap.put("content",content);
+        webTestClient.post()
+            .uri("/articles")
+            .body(Mono.just(requestMap), Map.class)
+            .exchange()
+            .expectStatus().is3xxRedirection();
+
+    }
 }
