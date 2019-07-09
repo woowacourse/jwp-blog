d6004e1c53ceb7da6bab59f124b59ed89bf7bc17
Soojin Roh <sooreal91@gmail.com>
2019-07-09 14:59:18 +0900

    feat : 게시물의 정보를 담고 있는 bean 객체인 article class 구현

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,43 @@
 package techcourse.myblog.domain;
 
 public class Article {
+    private final Long articleId;
+    private final String title;
+    private final String content;
+    private final String writer;
+    private final String pictureSource;
+    private final String writeDate;
+
+    public Article(Long articleId, String title, String content, String writer, String pictureSource, String writeDate) {
+        this.articleId = articleId;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.pictureSource = pictureSource;
+        this.writeDate = writeDate;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getPictureSource() {
+        return pictureSource;
+    }
+
+    public String getWriteDate() {
+        return writeDate;
+    }
 }
