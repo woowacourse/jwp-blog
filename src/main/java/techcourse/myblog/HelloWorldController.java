170b1d15c11808c448cc4503e0c0ed7c4efe9926
Jo Jinsik <kunggom@gmail.com>
2019-07-09 12:36:15 +0900

    Test 통과 실습 완료

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,8 +1,19 @@
 package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
+    @ResponseBody
+    @GetMapping("/helloworld")
+    public String getHelloWorld(@RequestParam("blogName") final String blogName) {
+        return blogName;
+    }
 
+    @ResponseBody
+    @PostMapping("/helloworld")
+    public String postHelloWorld(@RequestBody String blogName) {
+        return blogName;
+    }
 }
