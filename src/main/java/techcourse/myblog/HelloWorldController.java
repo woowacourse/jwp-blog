b5227b624248527c973f7e36c8ba5de5f04e3183
yunheehyeon <ok94life@daum.net>
2019-07-09 14:13:03 +0900

    실습

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,8 +1,29 @@
 package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
+
+    @ResponseBody
+    @GetMapping("/helloworld")
+    public String helloWorld(String blogName) {
+        return blogName;
+    }
+
+    @ResponseBody
+    @PostMapping("/helloworld")
+    public String helloWord(@RequestBody String blogName) {
+        return blogName;
+    }
+
+
+    @GetMapping("/hello")
+    public String helloWorld2(String blogName, Model model) {
+        model.addAttribute("blogName", blogName);
+        return "index";
+    }
 
 }
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,27 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Article> articles = articleRepository.findAll();
+
+        return "index.html";
+    }
+
+    @GetMapping("/articles/new")
+    public String getNewArticle() {
+        return "article-edit.html";
+    }
 }
