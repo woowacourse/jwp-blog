77395761840cbbb970996160e64cdf80e9988916
yunheehyeon <ok94life@daum.net>
2019-07-09 16:25:08 +0900

    feat : 게시글 작성 및 리스트에 저장

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,11 +1,14 @@
 package techcourse.myblog.web;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import techcourse.myblog.domain.Article;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,24 +17,37 @@ public class ArticleControllerTests {
     private WebTestClient webTestClient;
 
     @Test
-    void index() {
+    public void index() {
         webTestClient.get().uri("/")
                 .exchange()
                 .expectStatus().isOk();
     }
 
     @Test
-    void showWritingPage() {
+    public void showWritingPage() {
         webTestClient.get().uri("/writing")
                 .exchange()
                 .expectStatus().isOk();
     }
 
     @Test
-    void articleForm() {
+    public void articleForm() {
         webTestClient.get().uri("/articles/new")
                 .exchange()
                 .expectStatus().isOk();
     }
 
+    @Test
+    public void addArticle() {
+        String title = "TEST";
+        String postUrl = "aaa";
+        String articleContents = "testtest";
+        Article article = new Article(0L, title, postUrl, articleContents);
+
+        webTestClient.post()
+                .uri("/write")
+                .body(Mono.just(article), Article.class)
+                .exchange()
+                .expectStatus().isOk();
+    }
 }
