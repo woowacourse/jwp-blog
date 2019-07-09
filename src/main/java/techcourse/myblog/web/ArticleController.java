eb2882db4e131545da28facac8589721ce082d8a
김성훈 <seonghun127@gmail.com>
2019-07-09 14:31:24 +0900

    setting: mission complete

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,22 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/articles/new")
+    public String getNewArticle() {
+        return "article-edit";
+    }
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -26,5 +26,4 @@ public class ArticleControllerTests {
                 .exchange()
                 .expectStatus().isOk();
     }
-
 }
