5d78b0f1d83d394771e26cfae8fb65d4959dce23
yejilee <yjll1019@naver.com>
2019-07-09 14:01:14 +0900

    feat: get/post practice

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,28 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
-    @Autowired
-    private ArticleRepository articleRepository;
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@ResponseBody
+	@GetMapping("/articles/new")
+	public String createArticle() {
+		return "article-edit";
+	}
 }
+
+
+
+
