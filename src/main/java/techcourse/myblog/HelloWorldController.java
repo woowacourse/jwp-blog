2cf6ea5a0a600ab7f153dbb8a2cba90168398c84
Latera_ <bluesky0255@gmail.com>
2019-07-09 14:12:24 +0900

    Add example code

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,8 +1,24 @@
 package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    @ResponseBody
+    public String helloWorldGet(String blogName) {
+        return blogName;
+    }
+
+    @PostMapping("/helloworld")
+    @ResponseBody
+    public String helloWorldPost(@RequestBody String blogName) {
+        return blogName;
+    }
 
 }
