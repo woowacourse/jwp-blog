4f8fe2db007663dace5103288d430c842f37342f
yejilee <yjll1019@naver.com>
2019-07-09 14:45:38 +0900

    feat: click the button to move article-edit page

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,24 +1,22 @@
 package techcourse.myblog.web;
 
+import techcourse.myblog.domain.ArticleRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
 	@Autowired
 	private ArticleRepository articleRepository;
 
-	@GetMapping("/")
+/*	@GetMapping("/")
 	public String index() {
 		return "index";
-	}
+	}*/
 
-	@ResponseBody
-	@GetMapping("/articles/new")
+	@GetMapping("writing")
 	public String createArticle() {
 		return "article-edit";
 	}
