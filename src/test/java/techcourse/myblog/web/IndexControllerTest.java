96e911e5a41211a0d2d314e19a26652d09d3ea13
deocksoo <dusol92@snu.ac.kr>
2019-07-10 16:57:33 +0900

    [ADD] IndexController를 추가합니다
    index 페이지를 thymeleaf 템플릿 엔진에서 templates 디렉토리를 base로 하여 찾아줄 수
    있도록 수정하였습니다.

---
+++ src/test/java/techcourse/myblog/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package techcourse.myblog.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @DisplayName("index.html을 제대로 되돌려준다.")
+    void indexTest() {
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
