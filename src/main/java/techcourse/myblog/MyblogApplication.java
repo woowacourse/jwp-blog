15e8eee1aa5a087d45172a86103f01f65c6f52b6
kangminkim <kangmin789@naver.com>
2019-07-10 13:47:52 +0900

    docs : README 작성

--- src/main/java/techcourse/myblog/MyblogApplication.java
+++ src/main/java/techcourse/myblog/MyblogApplication.java
@@ -2,12 +2,18 @@ package techcourse.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class MyblogApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled","false");
+        System.setProperty("spring.devtools.livereload.enabled","true");
         SpringApplication.run(MyblogApplication.class, args);
     }
-
 }
