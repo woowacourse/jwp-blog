fbb978dfdbacf9f9fffec0a3bb8695cbcccd1488
starkim06 <starkim06@naver.com>
2019-07-10 15:21:34 +0900

    feat: Test 주석추가

--- src/test/java/techcourse/myblog/HelloWorldControllerTests.java
+++ src/test/java/techcourse/myblog/HelloWorldControllerTests.java
@@ -13,7 +13,12 @@ import reactor.core.publisher.Mono;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloWorldControllerTests {
 
+    //테스트를 하면 수동으로 하는 것에 비해 시간이 많이 단축된다.
+    //webtestclient 를 검색해서 이것저것 해보자.
+    //ex) webtestclient post
+
     @Autowired
+    //얘가 하는 역할은 서버를 먼저 띄운다. (?)
     private WebTestClient webTestClient;
 
     @Test
@@ -28,6 +33,8 @@ public class HelloWorldControllerTests {
 
     }
 
+    //아래를 String 대신에 Map을 보내보자
+    //Map 보다는 Map과 같은 클래스를 만들어서 보내는 것을 지향한다
     @Test
     void passParamWithPost() {
         String blogName = "helloWrold";
