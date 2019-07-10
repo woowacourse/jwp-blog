fe6f83c06364bba0cc8518203ae807c9baf2f15f
deocksoo <dusol92@snu.ac.kr>
2019-07-10 14:19:56 +0900

    [TEST] ArticleRepository 테스트에 주석을 추가합니다

--- src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
@@ -9,12 +9,18 @@ class ArticleRepositoryTest {
     @Test
     @DisplayName("게시물 등록하는 테스트")
     void save() {
+        // Given
         ArticleRepository articleRepository = new ArticleRepository();
         Article article = new Article();
+
         article.setTitle("test title");
         article.setContent("test content");
         article.setCoverUrl("test url");
+
+        // When
         articleRepository.save(article);
+
+        // Then
         assertThat(articleRepository.findAll()).contains(article);
     }
 }
