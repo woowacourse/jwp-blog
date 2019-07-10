eeb56443a93104f129603ff0f673e88dcefd04ae
Latera_ <bluesky0255@gmail.com>
2019-07-10 17:55:55 +0900

    Add test code follow redirection

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -86,20 +90,52 @@ public class ArticleControllerTests {
         requestMap.put("backgroundURL", newArticle.getCoverUrl());
         requestMap.put("content", "changed contents");
         webTestClient.put()
-            .uri("/articles/" + newArticle.getId() )
+            .uri("/articles/" + newArticle.getId())
             .body(Mono.just(requestMap), Map.class)
             .exchange()
             .expectStatus().isOk();
     }
 
     @Test
-    void deleteArticle(){
+    void deleteArticle() {
         Article newArticle = Article.of("my article", "http://image.com/", "origin contents");
         articleRepository.addArticle(newArticle);
 
         webTestClient.delete()
-            .uri("/articles/" + newArticle.getId() )
+            .uri("/articles/" + newArticle.getId())
             .exchange()
             .expectStatus().isOk();
+    }
+
+    @Test
+    void create_article() {
+        String title = "목적의식 있는 연습을 통한 효과적인 학습";
+        String coverUrl = "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/5tdm/image/7OdaODfUPkDqDYIQKXk_ET3pfKo.jpeg";
+        String contents = "나는 우아한형제들에서 우아한테크코스 교육 과정을 진행하고 있다. 우테코를 설계하면서 고민스러웠던 부분 중의 하나는 '선발 과정을 어떻게 하면 의미 있는 시간으로 만들 것인가?'였다.";
+
+        webTestClient.post()
+            .uri("/articles")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(BodyInserters
+                .fromFormData("title", title)
+                .with("coverUrl", coverUrl)
+                .with("contents", contents))
+            .exchange()
+            .expectStatus().is3xxRedirection()
+            .expectBody()
+            .consumeWith(response -> {
+                webTestClient.get().uri(response.getResponseHeaders().get("Location").get(0))
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody()
+                    .consumeWith(res -> {
+                        String body = new String(res.getResponseBody());
+                        assertThat(body.contains(title)).isTrue();
+                        assertThat(body.contains(coverUrl)).isTrue();
+                        // This assertion can fail when length of contents over 100 because of excerpt.
+//                        assertThat(body.contains(contents)).isTrue();
+                    });
+
+            });
     }
 }
