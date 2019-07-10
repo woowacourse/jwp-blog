7c195291e646db389bde6f143be98c0739db68d4
DaejunBae <dpudpu11@gmail.com>
2019-07-10 14:32:07 +0900

    feat: apply thymeleaf-dialect

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -3,15 +3,21 @@ package techcourse.myblog.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import techcourse.myblog.domain.ArticleRepository;
 
-@Controller
+@Controller("/articles")
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
-    @GetMapping("/articles/new")
+    @GetMapping("/new")
     public String articleForm(){
         return "article-edit";
+    }
+
+    @PostMapping
+    public String write(){
+        return "index";
     }
 }
