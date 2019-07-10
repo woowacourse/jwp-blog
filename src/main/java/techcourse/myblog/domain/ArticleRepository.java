d1748675c14d0d51d044e41f5501cde74a4faaf1
kangminkim <kangmin789@naver.com>
2019-07-10 16:25:11 +0900

    feat : sample 게시글 구현

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -8,6 +8,13 @@ import java.util.List;
 @Repository
 public class ArticleRepository {
     private List<Article> articles = new ArrayList<>();
+
+    public ArticleRepository() {
+        articles.add(new Article("aaaaa", "https://previews.123rf.com/images/romeolu/romeolu1601/romeolu160100122/50594417-%EB%88%88-%EB%B0%B0%EA%B2%BD.jpg", "ccccc"));
+        articles.add(new Article("title222", "https://previews.123rf.com/images/romeolu/romeolu1601/romeolu160100122/50594417-%EB%88%88-%EB%B0%B0%EA%B2%BD.jpg", "content2222"));
+        articles.add(new Article("333333", "https://previews.123rf.com/images/romeolu/romeolu1601/romeolu160100122/50594417-%EB%88%88-%EB%B0%B0%EA%B2%BD.jpg", "3333"));
+    }
+
     // TODO: 2019-07-10 고치기....
     private int lastId = 1;
 
