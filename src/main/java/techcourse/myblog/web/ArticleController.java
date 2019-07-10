32396780d9cf747e6d1e12259e11ea651092b4a5
yejilee <yjll1019@naver.com>
2019-07-10 15:35:47 +0900

    feat: write the article

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,10 +1,13 @@
 package techcourse.myblog.web;
 
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -17,7 +20,8 @@ public class ArticleController {
 	}
 
 	@PostMapping("articles")
-	public String saveArticle(@ModelAttribute Article article) {
+	public String saveArticle(Article article, Model model) {
+		model.addAttribute(article);
 		articleRepository.save(article);
 		return "article";
 	}
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -2,31 +2,61 @@ package techcourse.myblog.web;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
-    @Autowired
-    private WebTestClient webTestClient;
+	@Autowired
+	private WebTestClient webTestClient;
 
-    @Test
-    void index() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
-    }
+	@Test
+	void index() {
+		webTestClient.get().uri("/")
+				.exchange()
+				.expectStatus().isOk();
+	}
 
-    @Test
-    void articleForm() {
-        webTestClient.get().uri("/writing")
-                .exchange()
-                .expectStatus().isOk();
-    }
+	@Test
+	void articleForm() {
+		webTestClient.get().uri("/writing")
+				.exchange()
+				.expectStatus().isOk();
+	}
 
+	@Test
+	void saveArticle() {
+		String title = "제목";
+		String contents = "contents";
+		String coverUrl = "https://image-notepet.akamaized.net/resize/620x-/seimage/20190222%2F88df4645d7d2a4d2ed42628d30cd83d0.jpg";
+
+		webTestClient.post()
+				.uri("/articles")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.body(BodyInserters
+						.fromFormData("title", title)
+						.with("coverUrl", coverUrl)
+						.with("contents", contents))
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.consumeWith(response -> {
+					String body = new String(response.getResponseBody());
+					System.out.println(new String(response.getResponseBody()));
+					assertThat(body.contains(title)).isTrue();
+					assertThat(body.contains(coverUrl)).isTrue();
+					assertThat(body.contains(contents)).isTrue();
+				});
+	}
 }
