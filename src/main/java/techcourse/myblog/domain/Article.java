9af8d03e2d06277715952047b7557c3e59e104c9
DaejunBae <dpudpu11@gmail.com>
2019-07-11 20:45:19 +0900

    refactor: articleRepository.class Optinal+테스트 추가

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,10 +1,25 @@
 package techcourse.myblog.domain;
 
+import java.util.Objects;
+
 public class Article {
     private Long id;
     private String title;
     private String coverUrl;
     private String contents;
+
+    public Article(final String title, final String coverUrl, final String contents) {
+        this.title = title;
+        this.coverUrl = coverUrl;
+        this.contents = contents;
+    }
+
+    public Article(final Long id, final String title, final String coverUrl, final String contents) {
+        this.id = id;
+        this.title = title;
+        this.coverUrl = coverUrl;
+        this.contents = contents;
+    }
 
     public long getId() {
         return id;
@@ -46,6 +61,22 @@ public class Article {
                 ", coverUrl='" + coverUrl + '\'' +
                 ", contents='" + contents + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Article article = (Article) o;
+        return Objects.equals(id, article.id) &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(coverUrl, article.coverUrl) &&
+                Objects.equals(contents, article.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, coverUrl, contents);
     }
 }
 
