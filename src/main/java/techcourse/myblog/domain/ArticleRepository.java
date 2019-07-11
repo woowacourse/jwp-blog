10354386eb153d395e15b26a0063bf1fbe732b28
starkim06 <starkim06@naver.com>
2019-07-11 17:50:21 +0900

    refactor: 코드 리팩토링

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -10,21 +10,15 @@ public class ArticleRepository {
     private List<Article> articles = new ArrayList<>();
 
     public List<Article> findAll() {
-        return articles;
+        return new ArrayList<>(articles);
     }
 
     public void add(Article article) {
+        article.setId(issuedId());
         articles.add(article);
     }
 
-    public Article findById(int id) {
-        return articles.stream()
-                .filter(x -> x.getId() == id)
-                .findFirst()
-                .get();
-    }
-
-    public int issuedId() {
+    private int issuedId() {
         int size = articles.size();
         if (size == 0) {
             return 0;
@@ -32,6 +26,14 @@ public class ArticleRepository {
         return articles.get(size - 1).getId() + 1;
     }
 
+    public Article findArticleById(int id) {
+        return articles.stream()
+                .filter(x -> x.getId() == id)
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException("해당하는 ID의 기사가 없습니다."));
+    }
+
+    //TODO 삭제하고 새로 추가하는게 나은가 아니면 수정하는게 더 나을까?
     public void updateArticle(int id, Article article) {
         int index = 0;
         for (int i = 0; i < articles.size(); i++) {
@@ -39,11 +41,10 @@ public class ArticleRepository {
                 index = i;
             }
         }
-
         articles.set(index, article);
     }
 
     public void deleteById(int id) {
-        articles.remove(findById(id));
+        articles.remove(findArticleById(id));
     }
 }
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,7 +1,6 @@
 package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,14 +9,19 @@ import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
-    @Autowired
-    private ArticleRepository articleRepository;
+
+    private final ArticleRepository articleRepository;
 
     @Autowired
-    ApplicationContext applicationContext;
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @GetMapping("/")
     public ModelAndView nameChange(String blogName, ModelAndView modelAndView) {
+        if (blogName == null) {
+            blogName = "누구게?";
+        }
         modelAndView.setViewName("index");
         modelAndView.addObject("blogName", blogName);
         modelAndView.addObject("articleRepository", articleRepository);
@@ -29,12 +33,10 @@ public class ArticleController {
         return "article-edit";
     }
 
-    //여기는 annotation은 어딧지? @ResponseBody는 언제 붙이고 이때는 왜 생략해야될까?
+    //TODO 여기는 annotation은 어딧지? @ResponseBody는 언제 붙이고 이때는 왜 생략해야될까?
     @PostMapping("/articles")
     public ModelAndView writeArticle(Article article, ModelAndView modelAndView) {
-        article.setId(articleRepository.issuedId());
         articleRepository.add(article);
-        System.out.println("이번 기사의 ID : " + article.getId());
         modelAndView.setViewName("article");
         modelAndView.addObject("article", article);
         return modelAndView;
@@ -42,9 +44,7 @@ public class ArticleController {
 
     @GetMapping("/article/{articleId}")
     public ModelAndView showArticle(@PathVariable String articleId, ModelAndView modelAndView) {
-        System.out.println("articleId : " + articleId);
-        Article article = articleRepository.findById(Integer.parseInt(articleId));
-        System.out.println("Article + " + article);
+        Article article = articleRepository.findArticleById(Integer.parseInt(articleId));
         modelAndView.setViewName("article");
         modelAndView.addObject("article", article);
         return modelAndView;
@@ -53,9 +53,7 @@ public class ArticleController {
     @GetMapping("/articles/{articleId}/edit")
     public ModelAndView editArticle(@PathVariable String articleId, ModelAndView modelAndView) {
         modelAndView.setViewName("article-edit");
-        System.out.println("ID : : : : : " + articleId);
-        Article article = articleRepository.findById(Integer.parseInt(articleId));
-        System.out.println("Article : " + article);
+        Article article = articleRepository.findArticleById(Integer.parseInt(articleId));
         modelAndView.addObject("article", article);
         return modelAndView;
     }
