c6cc934edb9d7823e0a54b77d38283e57f5be8ee
Soojin Roh <sooreal91@gmail.com>
2019-07-11 14:47:10 +0900

    feat: 게시글 상세 조회 기능 구현
    1. articleId에 해당하는 게시글 상세 조회 메서드 구현
    2. articleId에 해당하는 게시글을 가져와 Web UI에 구현

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
@@ -12,6 +15,20 @@ public class ArticleController {
     // 필드에서 애노테이션을 해주기보다는 생성자에서 해주는 것을 추천함
     @Autowired
     private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String articleCreateForm() {
+        return "article-edit";
+    }
+
+    @PostMapping("/write")
+    public ModelAndView writeArticle(final Article article) {
+        Long latestId = articleRepository.generateNewId();
+        article.setArticleId(latestId);
+        articleRepository.addArticle(article);
+
+        return new ModelAndView("redirect:/articles/" + latestId);
+    }
 
     @GetMapping("/articles/{articleId}")
     public String findArticleById(@PathVariable Long articleId, Model model) {
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -3,15 +3,26 @@ package techcourse.myblog.web;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
+
     @Autowired
     private WebTestClient webTestClient;
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @Test
     void index() {
@@ -27,4 +38,61 @@ public class ArticleControllerTests {
                 .expectStatus().isOk();
     }
 
+    // 아래의 테스트와 같은 기능, 삭제 가능
+    @Test
+    void 작성된_게시글을_리스트에_등록하는지_테스트() {
+        String title = "안녕슬로스";
+        String contents = "쩌싀워더펑여우";
+        String coverUrl = "/images/article/sloth.jpg";
+
+        webTestClient.post()
+                .uri("/write")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("contents", contents))
+                .exchange();
+
+        Long latestId = articleRepository.generateNewId() - 1;
+
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContents(contents);
+        article.setCoverUrl(coverUrl);
+        article.setArticleId(latestId);
+
+        assertThat(articleRepository.findAll().contains(article)).isTrue();
+    }
+
+    @Test
+    void 게시물_작성_테스트() {
+        String title = "안녕슬로스";
+        String contents = "쩌싀워더펑여우";
+        String coverUrl = "/images/article/sloth.jpg";
+
+        webTestClient.post()
+                .uri("/write")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("contents", contents))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody()
+                .consumeWith(response -> {
+                    webTestClient.get().uri(response.getResponseHeaders().getLocation())
+                            .exchange()
+                            .expectStatus()
+                            .isOk()
+                            .expectBody()
+                            .consumeWith(res -> {
+                                String body = new String(res.getResponseBody());
+                                assertThat(body.contains(title)).isTrue();
+                                assertThat(body.contains(coverUrl)).isTrue();
+                                assertThat(body.contains(contents)).isTrue();
+                            });
+                });
+    }
 }
