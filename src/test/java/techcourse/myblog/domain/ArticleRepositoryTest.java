3e454ec0839683488adf2c8e5e4bcd4ebf4e499d
imkimheejoo <jujudamdadi@naver.com>
2019-07-11 21:55:08 +0900

    style:안쓰는 코드 제거

--- src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
