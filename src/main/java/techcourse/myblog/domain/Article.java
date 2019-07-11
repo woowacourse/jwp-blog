5282c411d04490b1a5345b1083b60a2e802c3e30
codeanddonuts <kkw200@naver.com>
2019-07-11 11:16:19 +0900

    feat: Article POJO

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,44 @@
 package techcourse.myblog.domain;
 
-public class Article {
+public class Article implements Comparable<Article> {
+    private int number;
+    private String title;
+    private String coverUrl;
+    private String contents;
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCoverUrl() {
+        return this.coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public String getContents() {
+        return this.contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public int compareTo(Article rhs) {
+        return this.number - rhs.number;
+    }
 }
