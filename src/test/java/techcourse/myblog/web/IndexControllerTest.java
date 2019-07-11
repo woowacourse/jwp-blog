b44c7bd506c410a0fb06e2e4fb99dab961f9ed88
deocksoo <dusol92@snu.ac.kr>
2019-07-11 11:32:06 +0900

    [TEST] IndexController에서 사용하지 않는 라이브러리를 제거합니다

--- src/test/java/techcourse/myblog/web/IndexControllerTest.java
+++ src/test/java/techcourse/myblog/web/IndexControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
