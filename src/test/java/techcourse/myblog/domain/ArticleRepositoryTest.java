0ab19c803c1f796adc93517188c3bb17b571a63c
SunHo Van <vsh123@naver.com>
2019-07-11 11:44:44 +0900

    refactor : 테스트 코드 중복 로직 제거

--- src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
@@ -1,5 +1,6 @@
 package techcourse.myblog.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -7,43 +8,49 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArticleRepositoryTest {
+    private ArticleRepository articleRepository;
+    private Article newArticle;
+
+    @BeforeEach
+    void setUp() {
+        articleRepository = new ArticleRepository();
+        newArticle = Article.of("title", "http://background.com", "가나다라마바사");
+    }
 
     @Test
     void addArticle() {
-        ArticleRepository articleRepository = new ArticleRepository();
-        articleRepository.addArticle(Article.of("title", "http://background.com", "가나다라마바사"));
+        articleRepository.addArticle(newArticle);
+
         assertThat(articleRepository.findAll()).hasSize(1);
     }
 
     @Test
     void findById() {
-        ArticleRepository articleRepository = new ArticleRepository();
-        Article newArticle = Article.of("title", "http://background.com", "가나다라마바사");
         articleRepository.addArticle(newArticle);
         Optional<Article> maybeArticle = articleRepository.findById(newArticle.getId());
+
         assertThat(maybeArticle.isPresent()).isTrue();
         assertThat(maybeArticle.get()).isEqualTo(newArticle);
     }
 
     @Test
-    void update() {
-        ArticleRepository articleRepository = new ArticleRepository();
-        Article newArticle = Article.of("title", "http://background.com", "가나다라마바사");
+    void 수정_테스트() {
         articleRepository.addArticle(newArticle);
         Article articleFound = articleRepository.findById(newArticle.getId())
                 .orElseThrow(IllegalStateException::new);
+
         articleFound.setTitle("changed title");
         articleFound = articleRepository.findById(newArticle.getId())
                 .orElseThrow(IllegalStateException::new);
+
         assertThat(articleFound.getTitle()).isEqualTo("changed title");
     }
 
     @Test
     void delete() {
-        ArticleRepository articleRepository = new ArticleRepository();
-        Article newArticle = Article.of("title", "http://background.com", "가나다라마바사");
         articleRepository.addArticle(newArticle);
         articleRepository.deleteById(newArticle.getId());
+
         assertThat(articleRepository.findAll()).hasSize(0);
     }
 }
