12dc2d1cfe058f6fc9a81a47e34ce6b4db6cf15b
yejilee <yjll1019@naver.com>
2019-07-12 13:52:56 +0900

    refactor: modify annotation

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,23 +1,19 @@
 package techcourse.myblog;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class HelloWorldController {
-	@ResponseBody
 	@GetMapping("/helloworld")
 	public String getBlogNameUsingGet(String blogName) {
 		return blogName;
 	}
 
-	@ResponseBody
 	@PostMapping("/helloworld")
 	public String getBlogNameUsingPost(@RequestBody String blogName) {
 		return blogName;
 	}
 
-	@ResponseBody
 	@GetMapping("/{blogName}")
 	public String changeBlogName(@PathVariable("blogName") String name) { //@PathVariable
 		return name;
