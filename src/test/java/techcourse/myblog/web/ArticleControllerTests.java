b9b78ce50dc896287b0573b2770574aaba1c69fa
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-12 18:17:58 +0900

    test : ArticleController test code 추가

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,20 +1,40 @@
 package techcourse.myblog.web;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleRepository;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
     @Autowired
     private WebTestClient webTestClient;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    private Article article;
+    private Article deleteArticle;
+
+    @BeforeEach
+    void setUp() {
+        article = new Article("title1", "url1", "content1");
+        deleteArticle = new Article("title2", "url2", "content2");
+        articleRepository.saveArticle(article);
+        articleRepository.saveArticle(deleteArticle);
+        articleRepository.saveArticle(new Article("title3", "url3", "content3"));
+    }
 
     @Test
     void index() {
@@ -25,28 +45,62 @@ public class ArticleControllerTests {
 
     @Test
     void articleForm() {
-        webTestClient.get().uri("/articles/new")
+        webTestClient.get().uri("/writing")
                 .exchange()
                 .expectStatus().isOk();
     }
 
     @Test
-    void write_POST() {
-        // TODO: 2019-07-10 test...
-        String title = "title";
-        String content = "contents";
-        String coverUrl = "https://abc.com";
-
-        Article article = new Article(title, content, coverUrl);
-
-
-        webTestClient.post()
-                .uri("/articles")
-                .body(Mono.just(article), Article.class)
+    void createArticle() {
+        webTestClient.post().uri("/articles")
+                .body(fromFormData("", "").with("", "").with("", ""))
                 .exchange()
-                .expectStatus()
-                .isOk();
+                .expectStatus().isFound()
+                .expectHeader().valueMatches("location", ".*/articles/(\\d)*");
+    }
 
+    @Test
+    void goDetailArticle() {
+        webTestClient.get().uri("/articles/" + article.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(Objects.requireNonNull(response.getResponseBody()));
+                    assertThat(body.contains(article.getTitle())).isTrue();
+                    assertThat(body.contains(article.getCoverUrl())).isTrue();
+                    assertThat(body.contains(article.getContents())).isTrue();
 
+                });
+    }
+
+    @Test
+    void deleteArticle() {
+        webTestClient.delete().uri("/articles/" + deleteArticle.getId())
+                .exchange()
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void updateArticle() {
+        Article updateArticle = new Article("update title", "update url", "update content");
+        updateArticle.setId(article.getId());
+        webTestClient.put().uri("/articles/" + article.getId())
+                .body(fromFormData("title", updateArticle.getTitle())
+                        .with("coverUrl", updateArticle.getCoverUrl())
+                        .with("contents", updateArticle.getContents()))
+                .exchange()
+                .expectStatus().is3xxRedirection();
+
+        assertThat(updateArticle.getTitle()).isEqualTo(articleRepository.getArticleById(article.getId()).getTitle());
+        assertThat(updateArticle.getCoverUrl()).isEqualTo(articleRepository.getArticleById(article.getId()).getCoverUrl());
+        assertThat(updateArticle.getContents()).isEqualTo(articleRepository.getArticleById(article.getId()).getContents());
+    }
+
+    @Test
+    void editArticle() {
+        webTestClient.get().uri("/articles/" + article.getId() + "/edit")
+                .exchange()
+                .expectStatus().isOk();
     }
 }
