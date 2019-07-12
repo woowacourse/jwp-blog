0042d5feeb4a02491ff893c9ce4e8f9e1211029d
ddu0422 <43198414+ddu0422@users.noreply.github.com>
2019-07-13 08:53:52 +0900

    [게시글 생성/조회/수정/삭제] 미르 미션 제출합니다. (#32)
    * [ADD] Add README
    
    * [Fix] Code Formatting
    
    * [Fix] Pass ArticleControllerTest, HelloWorldControllerTest
    
    * [ADD] Move to writing page
    
    * [ADD] Create article logic
    
    * [ADD] Show article lists
    
    * [ADD] Show selected article
    
    * [ADD] Springboot Devtools (auto reload)
    
    * [ADD] Remove duplication body code of index, article, article-edit HTML
    
    - using Thymeleaf template layout
    
    * [Fix] create_article Test
    
    * [ADD] Remove duplicate head code of index, article, article-edit HTML
    
    * [ADD] Modify article function
    
    * [ADD] Remove article function
    
    * [REF] Fix ArticleControllerTest
    
    * [ADD] Redirect to article
    
    * [REF] Add this keyword
    
    * [REF] Extract magic number as constant variable
    
    * [REF] Add matchId method in Article
    
    * [ADD] ArticleRepository, ArticleController Test
    
    * [REF] change from field injection to constructor injection
    
    * [REF] add article constructor
    
    * Revert "[REF] add article constructor"
    
    This reverts commit fd354e2c1d8e9207522f2d93d35d34e27872d10a.
    
    * Revert "[REF] change from field injection to constructor injection"
    
    This reverts commit e2d48c8f68051f3c2e77ffb6411bdc5cf3761a36.
    
    * [REF] add article constructor
    
    * [REF] change from field injection to constructor injection
    
    * [REF] eliminate repetitive request response

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,38 @@
 package techcourse.myblog.domain;
 
 public class Article {
+    private int id;
+    private String title;
+    private String coverUrl;
+    private String contents;
+    
+    public Article(String title, String coverUrl, String contents) {
+        this.title = title;
+        this.coverUrl = coverUrl;
+        this.contents = contents;
+    }
+
+    public boolean matchId(int id) {
+        return this.id == id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+    
+    public String getContents() {
+        return contents;
+    }
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,30 +1,77 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
+
+@AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
+    private static final int ARTICLE_ID = 0;
+    private static final String LOCATION = "location";
+    
     @Autowired
     private WebTestClient webTestClient;
-
-    @Test
-    void index() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
+    
+    @BeforeEach
+    void 게시글_작성_확인() {
+        checkRedirect(HttpMethod.POST, "/articles", HttpStatus.FOUND,
+                LOCATION, ".*articles.*");
     }
-
+    
     @Test
-    void articleForm() {
-        webTestClient.get().uri("/articles/new")
-                .exchange()
-                .expectStatus().isOk();
+    void index_페이지_조회() {
+        check(HttpMethod.GET, "/", HttpStatus.OK);
     }
-
+    
+    @Test
+    void 게시글_작성_페이지_확인() {
+        check(HttpMethod.GET, "/writing", HttpStatus.OK);
+    }
+    
+    @Test
+    void 게시글_조회() {
+        check(HttpMethod.GET, "/articles/" + ARTICLE_ID, HttpStatus.OK);
+    }
+    
+    @Test
+    void 게시글_수정_페이지_확인() {
+        check(HttpMethod.GET, "/articles/" + ARTICLE_ID + "/edit", HttpStatus.OK);
+    }
+    
+    @Test
+    void 게시글_수정_확인() {
+        checkRedirect(HttpMethod.PUT, "/articles/" + ARTICLE_ID, HttpStatus.FOUND,
+                LOCATION, ".*articles.*");
+    }
+    
+    @AfterEach
+    void 게시글_삭제_확인() {
+        checkRedirect(HttpMethod.DELETE, "/articles/" + ARTICLE_ID, HttpStatus.FOUND,
+                LOCATION, ".*/");
+    }
+    
+    private ResponseSpec check(HttpMethod httpMethod, String uri, HttpStatus httpStatus) {
+        return webTestClient.method(httpMethod)
+                .uri(uri)
+                .exchange()
+                .expectStatus().isEqualTo(httpStatus);
+    }
+    
+    private ResponseSpec checkRedirect(HttpMethod httpMethod, String uri,
+                                       HttpStatus httpStatus, String name, String pattern) {
+        return check(httpMethod, uri, httpStatus)
+                .expectHeader().valueMatches(name, pattern);
+    }
 }
