383ab85e29239196cd4c2b047605a36f4152b4d4
deocksoo <dusol92@snu.ac.kr>
2019-07-12 19:03:27 +0900

    [REFACTOR] ArticleController가 의존성을 주입받는 위치를 변경합니다
    1. ArticleRepository를 필드변수로 주입받던 기존 방식에서 생성자 주입 방식으로
    변경하였습니다.
    2. 필드 변수의 불필요한 접근제어자를 변경하였습니다.

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,6 +1,5 @@
 package techcourse.myblog.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +8,13 @@ import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
-    public static final int INCREMENT_AMOUNT = 1;
-    @Autowired
+    private static final int INCREMENT_AMOUNT = 1;
+
     private ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @GetMapping("/articles/new")
     public String articleCreationPage(Model model) {
