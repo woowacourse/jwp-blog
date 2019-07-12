daaecef6cfd6af71fc23e0551560f105357a0bd9
강연욱 <fiasboc23@gmail.com>
2019-07-12 14:16:15 +0900

    refactor : remove Article set method

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -3,66 +3,60 @@ package techcourse.myblog.domain;
 import java.util.Objects;
 
 public class Article {
-	private int id;
-	private String title;
-	private String contents;
-	private String coverUrl;
+    private int id;
+    private String title;
+    private String contents;
+    private String coverUrl;
 
-	public Article(String title, String contents, String coverUrl) {
-		this.title = title;
-		this.contents = contents;
-		this.coverUrl = coverUrl;
-	}
+    public Article(String title, String contents, String coverUrl) {
+        this.title = title;
+        this.contents = contents;
+        this.coverUrl = coverUrl;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public String getCoverUrl() {
+        return coverUrl;
+    }
 
-	public String getCoverUrl() {
-		return coverUrl;
-	}
+    public String getContents() {
+        return contents;
+    }
 
-	public void setCoverUrl(String coverUrl) {
-		this.coverUrl = coverUrl;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getContents() {
-		return contents;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
+    public void update(Article modifiedArticle) {
+        this.title = modifiedArticle.title;
+        this.contents = modifiedArticle.contents;
+        this.coverUrl = modifiedArticle.coverUrl;
+    }
 
-	public int getId() {
-		return id;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Article)) {
+            return false;
+        }
+        final Article article = (Article) o;
+        return getId() == article.getId() &&
+                Objects.equals(getTitle(), article.getTitle()) &&
+                Objects.equals(getContents(), article.getContents()) &&
+                Objects.equals(getCoverUrl(), article.getCoverUrl());
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof Article)) {
-			return false;
-		}
-		final Article article = (Article) o;
-		return getId() == article.getId() &&
-				Objects.equals(getTitle(), article.getTitle()) &&
-				Objects.equals(getContents(), article.getContents()) &&
-				Objects.equals(getCoverUrl(), article.getCoverUrl());
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(getId(), getTitle(), getContents(), getCoverUrl());
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getContents(), getCoverUrl());
+    }
 }
--- src/main/java/techcourse/myblog/repository/ArticleRepository.java
+++ src/main/java/techcourse/myblog/repository/ArticleRepository.java
@@ -9,36 +9,36 @@ import techcourse.myblog.domain.Article;
 
 @Repository
 public class ArticleRepository {
-	private static int articleCount = 0;
-	private List<Article> articles = new ArrayList<>();
+    private static int articleCount = 0;
+    private List<Article> articles = new ArrayList<>();
 
-	public List<Article> findAll() {
-		return articles;
-	}
+    public List<Article> findAll() {
+        return articles;
+    }
 
-	public Article save(Article article) {
-		article.setId(++articleCount);
-		articles.add(article);
-		return findById(articleCount);
-	}
+    public Article save(Article article) {
+        article.setId(++articleCount);
+        articles.add(article);
+        return findById(articleCount);
+    }
 
-	public Article findById(int id) {
-		return articles.stream()
-				.filter(article -> article.getId() == id)
-				.findFirst()
-				.orElseThrow(IllegalArgumentException::new)
-				;
-	}
+    public Article findById(int id) {
+        return articles.stream()
+                .filter(article -> article.getId() == id)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                ;
+    }
 
-	public void update(Article modifiedArticle) {
-		Article originArticle = findById(modifiedArticle.getId());
-		originArticle.setCoverUrl(modifiedArticle.getCoverUrl());
-		originArticle.setTitle(modifiedArticle.getTitle());
-		originArticle.setContents(modifiedArticle.getContents());
-	}
+    public void update(Article modifiedArticle) {
+        findArticle(modifiedArticle).update(modifiedArticle);
+    }
 
-	public void deleteById(int articleId) {
-		Article article = findById(articleId);
-		articles.remove(article);
-	}
+    private Article findArticle(Article article) {
+        return findById(article.getId());
+    }
+
+    public void deleteById(int articleId) {
+        articles.remove(findById(articleId));
+    }
 }
