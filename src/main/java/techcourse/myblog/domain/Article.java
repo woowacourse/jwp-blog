403e1a3e42f13bb625327ff7fd643842fa9d0e20
박우빈 (Park WooBeen) <pwb0127@gmail.com>
2019-07-12 15:11:31 +0900

    [게시글 생성/조회] 닉 미션 제출합니다. (#5)
    * feat: GET /writing 요청 구현
    
    * feat: POST /articles 요청 구현
    
    * feat: GET / 요청에서 article 목록 표시 구현
    
    * feat: GET /articles/{articleId} 요청 구현
    
    * feat: GET /articles/{articleId}/edit 요청 구현
    
    * feat: DELETE /articles/{articleId} 요청 구현
    
    * feat: static 파일 경로 변경
    
    * feat: liveReload 기능 dependency 추가
    
    * feat: HTML 중복 제거
    
    * test: webTestClient의 비동기성 요청으로 인한 문제 수정
    
    * feat: coverUrl이 없을 경우 기본 이미지를 띄워주도록 하는 로직 추가
    
    * refactor: 사용하지 않는 클래스 삭제 및 코드 정리
    
    * refactor: index.html에서 coverUrl의 존재유무를 확인하는 방법 변경
    
    * fix: template 관련 issue 수정
    
    * refactor: Article의 articleId를 id로 변경
    
    * refactor: HiddenHttpMethodFilter는 SpringBoot에 기본적으로 등록되어 있기에 코드레벨에서 삭제
    
    * refactor: article의 id 자료형을 int로 통일
    
    * refactor: ArticleRepository에 Articles 일급콜렉션 적용
    
    * refactor: ArticleController의 articleRepository의 주입 방식을 field에서 constructor로 변경
    
    * refactor: Articles를 삭제하고 ArticleRepository에서 findAll()을 호출 시 새로운 list를 반환하도록 변경

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,56 @@
 package techcourse.myblog.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Article {
+    private int id;
+    private String title;
+    private String coverUrl;
+    private String contents;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public boolean isCoverUrl() {
+        return StringUtils.isNotBlank(coverUrl);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", coverUrl='" + coverUrl + '\'' +
+                ", contents='" + contents + '\'' +
+                '}';
+    }
 }
