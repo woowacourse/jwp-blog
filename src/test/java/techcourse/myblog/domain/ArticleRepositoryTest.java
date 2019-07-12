b0a5d31858b5b2c83a88a022f5ef9bdab93b1234
deocksoo <dusol92@snu.ac.kr>
2019-07-12 17:38:38 +0900

    [REFACTOR] ArticleRepositoryTest를 리펙토링 합니다
    1. 필드 상수명을 변경하였습니다.
    2. 메서드 순서를 정리하였습니다.
    3. 수정, 삭제할 때 id가 없는 경우에 대한 테스트 케이스들을 추가하였습니다.

--- src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
@@ -3,13 +3,15 @@ package techcourse.myblog.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import techcourse.myblog.domain.validator.CouldNotFindArticleIdException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArticleRepositoryTest {
-    private static final int ARTICLE_TEST_ID = 1;
-    private static final int ARTICLE_ID_NOT_IN_REPO = 2;
+    private static final int TEST_ARTICLE_ID = 1;
+    private static final int TEST_ARTICLE_ID_NOT_IN_REPO = 2;
+
     private ArticleRepository articleRepository;
     private Article article;
 
@@ -17,46 +19,33 @@ class ArticleRepositoryTest {
     void setUp() {
         articleRepository = new ArticleRepository();
         article = new Article(
-                ARTICLE_TEST_ID,
+                TEST_ARTICLE_ID,
                 "test title",
-                "test contents",
-                "test Url"
+                "test coverUrl",
+                "test contents"
         );
+
+        articleRepository.save(article);
     }
 
     @Test
-    @DisplayName("게시물 등록하는 테스트")
-    void save() {
-        // When
-        articleRepository.save(article);
+    @DisplayName("게시물을 저장한다.")
+    void saveTest() {
+        Article savingArticle = new Article(
+                0,
+                "saveTest title",
+                "saveTest coverUrl",
+                "saveTest contents"
+        );
+        articleRepository.save(savingArticle);
 
-        // Then
-        assertThat(articleRepository.findAll()).contains(article);
+        assertThat(articleRepository.findAll()).contains(savingArticle);
     }
 
     @Test
     @DisplayName("게시물 id로 게시물을 조회한다.")
     void findTest() {
-        articleRepository.save(article);
-
-        assertThat(articleRepository.find(ARTICLE_TEST_ID)).isEqualTo(article);
-    }
-
-    @Test
-    @DisplayName("게시물 id가 없는 경우 예외를 던져준다.")
-    void notFindTest() {
-        articleRepository.save(article);
-
-        assertThrows(IllegalArgumentException.class, () -> articleRepository.find(ARTICLE_ID_NOT_IN_REPO));
-    }
-
-    @Test
-    @DisplayName("게시물 id를 이용해 해당 게시물을 지운다.")
-    void deleteByIdTest() {
-        articleRepository.save(article);
-
-        articleRepository.delete(ARTICLE_TEST_ID);
-        assertThrows(IllegalArgumentException.class, () -> articleRepository.find(ARTICLE_TEST_ID));
+        assertThat(articleRepository.find(TEST_ARTICLE_ID)).isEqualTo(article);
     }
 
     @Test
@@ -64,10 +53,9 @@ class ArticleRepositoryTest {
     void updateTitleTest() {
         String updatedTitle = "updated title";
 
-        articleRepository.save(article);
-        articleRepository.updateTitle(ARTICLE_TEST_ID, updatedTitle);
+        articleRepository.updateTitle(TEST_ARTICLE_ID, updatedTitle);
 
-        Article updatedArticle = articleRepository.find(ARTICLE_TEST_ID);
+        Article updatedArticle = articleRepository.find(TEST_ARTICLE_ID);
         assertThat(updatedArticle.getTitle()).isEqualTo(updatedTitle);
     }
 
@@ -76,10 +64,9 @@ class ArticleRepositoryTest {
     void updateCoverUrlTest() {
         String updateCoverUrl = "updated coverUrl";
 
-        articleRepository.save(article);
-        articleRepository.updateCoverUrl(ARTICLE_TEST_ID, updateCoverUrl);
+        articleRepository.updateCoverUrl(TEST_ARTICLE_ID, updateCoverUrl);
 
-        Article updatedArticle = articleRepository.find(ARTICLE_TEST_ID);
+        Article updatedArticle = articleRepository.find(TEST_ARTICLE_ID);
         assertThat(updatedArticle.getCoverUrl()).isEqualTo(updateCoverUrl);
     }
 
@@ -88,10 +75,52 @@ class ArticleRepositoryTest {
     void updateContentsTest() {
         String updateContents = "updated contents";
 
-        articleRepository.save(article);
-        articleRepository.updateContents(ARTICLE_TEST_ID, updateContents);
+        articleRepository.updateContents(TEST_ARTICLE_ID, updateContents);
 
-        Article updatedArticle = articleRepository.find(ARTICLE_TEST_ID);
+        Article updatedArticle = articleRepository.find(TEST_ARTICLE_ID);
         assertThat(updatedArticle.getContents()).isEqualTo(updateContents);
+    }
+
+    @Test
+    @DisplayName("게시물 id를 이용해 해당 게시물을 지운다.")
+    void deleteByIdTest() {
+        articleRepository.delete(TEST_ARTICLE_ID);
+        assertThrows(CouldNotFindArticleIdException.class, () -> articleRepository.find(TEST_ARTICLE_ID));
+    }
+
+    @Test
+    @DisplayName("Repository에 없는 id를 조회하는 경우 예외를 던져준다.")
+    void findFailTest() {
+        assertThrows(CouldNotFindArticleIdException.class, () -> articleRepository.find(TEST_ARTICLE_ID_NOT_IN_REPO));
+    }
+
+    @Test
+    @DisplayName("Repository에 없는 id의 타이틀을 수정 시도하는 경우 예외를 던져준다.")
+    void updateTitleFailTest() {
+        assertThrows(CouldNotFindArticleIdException.class, () -> articleRepository.updateContents(
+                TEST_ARTICLE_ID_NOT_IN_REPO, "new contents"
+        ));
+    }
+
+    @Test
+    @DisplayName("Repository에 없는 id의 CoverUrl을 수정 시도하는 경우 예외를 던져준다.")
+    void updateCoverUrlFailTest() {
+        assertThrows(CouldNotFindArticleIdException.class, () -> articleRepository.updateContents(
+                TEST_ARTICLE_ID_NOT_IN_REPO, "new contents"
+        ));
+    }
+
+    @Test
+    @DisplayName("Repository에 없는 id의 컨텐츠를 수정 시도하는 경우 예외를 던져준다.")
+    void updateContentsFailTest() {
+        assertThrows(CouldNotFindArticleIdException.class, () -> articleRepository.updateContents(
+                TEST_ARTICLE_ID_NOT_IN_REPO, "new contents"
+        ));
+    }
+
+    @Test
+    @DisplayName("Repository에 없는 id를 조회하는 경우 예외를 던져준다.")
+    void deleteFailTest() {
+        assertThrows(CouldNotFindArticleIdException.class, () -> articleRepository.delete(TEST_ARTICLE_ID_NOT_IN_REPO));
     }
 }
