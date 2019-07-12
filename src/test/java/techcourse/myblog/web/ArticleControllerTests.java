61a0a64fc5ff458268e0b827ce9e3f78c14d7703
박우빈 (Park WooBeen) <pwb0127@gmail.com>
2019-07-12 15:11:31 +0900

    [게시글 생성/조회] 닉 미션 제출합니다. (#5)
    * feat: GET /writing 요청 구현
    
    * feat: POST /articles 요청 구현
    
    * feat: GET / 요청에서 article 목록 표시 구현
    
    * feat: GET /articles/{articleId} 요청 구현
    
    * feat: GET /articles/{articleId}/edit 요청 구현
    
    * feat: DELETE /articles/{articleId} 요청 구현
    
    * feat: static 파일 경로 변경
    
    * feat: liveReload 기능 dependency 추가
    
    * feat: HTML 중복 제거
    
    * test: webTestClient의 비동기성 요청으로 인한 문제 수정
    
    * feat: coverUrl이 없을 경우 기본 이미지를 띄워주도록 하는 로직 추가
    
    * refactor: 사용하지 않는 클래스 삭제 및 코드 정리
    
    * refactor: index.html에서 coverUrl의 존재유무를 확인하는 방법 변경
    
    * fix: template 관련 issue 수정
    
    * refactor: Article의 articleId를 id로 변경
    
    * refactor: HiddenHttpMethodFilter는 SpringBoot에 기본적으로 등록되어 있기에 코드레벨에서 삭제
    
    * refactor: article의 id 자료형을 int로 통일
    
    * refactor: ArticleRepository에 Articles 일급콜렉션 적용
    
    * refactor: ArticleController의 articleRepository의 주입 방식을 field에서 constructor로 변경
    
    * refactor: Articles를 삭제하고 ArticleRepository에서 findAll()을 호출 시 새로운 list를 반환하도록 변경

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -3,12 +3,16 @@ package techcourse.myblog.web;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import techcourse.myblog.domain.Article;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class ArticleControllerTests {
     @Autowired
     private WebTestClient webTestClient;
@@ -27,4 +31,91 @@ public class ArticleControllerTests {
                 .expectStatus().isOk();
     }
 
+    @Test
+    void writeArticleForm() {
+        webTestClient.get().uri("/writing")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void saveArticle() {
+        Article article = new Article();
+        article.setTitle("제목");
+        article.setCoverUrl("http");
+        article.setContents("내용");
+
+        webTestClient.post().uri("/articles")
+                .body(Mono.just(article), Article.class)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void fetchArticle() {
+        Article article = new Article();
+        article.setTitle("제목");
+        article.setCoverUrl("http");
+        article.setContents("내용");
+
+        webTestClient.post().uri("/articles")
+                .body(Mono.just(article), Article.class)
+                .exchange();
+
+        webTestClient.get().uri("/articles/1")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void editArticle() {
+        Article article = new Article();
+        article.setTitle("제목");
+        article.setCoverUrl("http");
+        article.setContents("내용");
+
+        webTestClient.post().uri("/articles")
+                .body(Mono.just(article), Article.class)
+                .exchange();
+
+        webTestClient.get().uri("/articles/1/edit")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void saveEditedArticle() {
+        Article article = new Article();
+        article.setTitle("제목");
+        article.setCoverUrl("http");
+        article.setContents("내용");
+
+        webTestClient.post().uri("/articles")
+                .body(Mono.just(article), Article.class)
+                .exchange();
+
+        webTestClient.put().uri("/articles/1")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void deleteArticle() {
+        Article article = new Article();
+        article.setTitle("제목");
+        article.setCoverUrl("http");
+        article.setContents("내용");
+
+        webTestClient.post().uri("/articles")
+                .body(Mono.just(article), Article.class)
+                .exchange();
+
+        webTestClient.post().uri("/articles")
+                .body(Mono.just(article), Article.class)
+                .exchange();
+
+        webTestClient.delete().uri("/articles/2")
+                .exchange()
+                .expectStatus().isFound();
+    }
 }
