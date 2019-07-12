3125088eadd373bea4143a4028f0af384826c11b
강연욱 <fiasboc23@gmail.com>
2019-07-12 17:03:15 +0900

    test: articleRepository delete

--- src/test/java/techcourse/myblog/repository/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/repository/ArticleRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import techcourse.myblog.domain.Article;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArticleRepositoryTest {
     private ArticleRepository articleRepository;
@@ -22,8 +23,17 @@ public class ArticleRepositoryTest {
     }
 
     @Test
-    void findById() {
+    void findByIdTest() {
         Article savedArticle = articleRepository.save(article);
         assertThat(articleRepository.findById(article.getId())).isEqualTo(savedArticle);
+    }
+
+    @Test
+    void deleteTest() {
+        Article savedArticle = articleRepository.save(article);
+        articleRepository.deleteById(savedArticle.getId());
+        assertThrows(IllegalArgumentException.class, () -> {
+            articleRepository.findById(savedArticle.getId());
+        });
     }
 }
