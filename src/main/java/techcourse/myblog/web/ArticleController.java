f6b80992c423261b165e8c23fd298f3370e31522
hyojaekim <hj00686@naver.com>
2019-07-12 19:34:13 +0900

    [REFACTOR] 사용하지 않는 라이브러리를 삭제합니다

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,6 +1,5 @@
 package techcourse.myblog.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
