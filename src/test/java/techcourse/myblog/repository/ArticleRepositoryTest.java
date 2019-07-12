b05b64a72bc42993a0475cc0d9afb3db55760462
강연욱 <fiasboc23@gmail.com>
2019-07-12 16:58:58 +0900

    test: articleRepository findById

--- src/test/java/techcourse/myblog/repository/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/repository/ArticleRepositoryTest.java
@@ -1,15 +1,29 @@
 package techcourse.myblog.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import techcourse.myblog.domain.Article;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArticleRepositoryTest {
+    private ArticleRepository articleRepository;
+    private Article article = new Article("title", "contents", "coverUrl");
+
+    @BeforeEach
+    void setUp() {
+        articleRepository = new ArticleRepository();
+    }
+
     @Test
     void saveTest() {
-        ArticleRepository articleRepository = new ArticleRepository();
-        Article article = articleRepository.save(new Article("title", "contents", "coverUrl"));
-        assertThat(articleRepository.findAll().get(0)).isEqualTo(article);
+        Article savedArticle = articleRepository.save(article);
+        assertThat(articleRepository.findAll().get(0)).isEqualTo(savedArticle);
+    }
+
+    @Test
+    void findById() {
+        Article savedArticle = articleRepository.save(article);
+        assertThat(articleRepository.findById(article.getId())).isEqualTo(savedArticle);
     }
 }
