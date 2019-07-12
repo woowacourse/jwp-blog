14b2f68c43f42e0e54310d71cde479f352ca7994
DaejunBae <dpudpu11@gmail.com>
2019-07-12 15:50:33 +0900

    test: ArticleControllerTests.write() 추가

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,17 +1,14 @@
 package techcourse.myblog.web;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
-import techcourse.myblog.domain.Article;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @AutoConfigureWebTestClient
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
     @Autowired
@@ -33,14 +30,21 @@ public class ArticleControllerTests {
 
     @Test
     void write() {
-        Article article = new Article(1L,"title", "coverUrl", "content");
+        String title = "목적의식 있는 연습을 통한 효과적인 학습";
+        String coverUrl = "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/5tdm/image/7OdaODfUPkDqDYIQKXk_ET3pfKo.jpeg";
+        String content = "나는 우아한형제들에서 우아한테크코스 교육 과정을 진행하고 있다. 우테코를 설계하면서 고민스러웠던 부분 중의 하나는 '선발 과정을 어떻게 하면 의미 있는 시간으로 만들 것인가?'였다.";
 
         webTestClient.post()
                 .uri("/articles")
-                .body(Mono.just(article), Article.class)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("content", content))
                 .exchange()
                 .expectStatus().is3xxRedirection();
     }
+
 
     @Test
     void delete(){
