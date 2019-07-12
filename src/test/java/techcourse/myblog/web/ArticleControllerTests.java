e4003a5d24001eb54fdaa09e883a8e16e99cb36f
jinwook <jin942002@naver.com>
2019-07-12 15:50:22 +0900

    test: 게시글 수정 페이지 응답 코드 확인하는 테스트 작성

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -97,6 +97,21 @@ public class ArticleControllerTests {
 
     @Test
     void 게시글_수정_응답() {
+        String title = "스파이더맨 보고싶다";
+        String coverUrl = "https://pgnqdrjultom1827145.cdn.ntruss.com/img/bc/30/bc30f170793e5342c4ca6cca771da57f922f8a9a25fa09eb2b672962cda1ea92_v1.jpg";
+        String contents = "스파이더맨과 미슽테리우스";
+
+        Article article = new Article();
+        article.setTitle(title);
+        article.setCoverUrl(coverUrl);
+        article.setContents(contents);
+
+        articleRepository.save(article);
+
+        webTestClient.get().uri("/articles/0/edit")
+                .exchange()
+                .expectStatus().isOk();
+
 
     }
 
@@ -106,7 +121,7 @@ public class ArticleControllerTests {
     }
 
     @Test
-    void 인덱스_화면_응답() {
+    void 인덱스_페이지_응답() {
 
     }
 
