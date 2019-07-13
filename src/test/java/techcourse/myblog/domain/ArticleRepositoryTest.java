9ac166a79924d8948c6fedce28a0fcf095c0cfb4
CODEMCD <psbum1609@gmail.com>
2019-07-13 17:07:52 +0900

    refactor: 게시글 생성 중복 제거

--- src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
@@ -13,51 +13,46 @@ class ArticleRepositoryTest {
     @BeforeEach
     void setUp() {
         articleRepository = new ArticleRepository();
+        articleRepository.insert(new Article(0, "title", "url", "contents"));
     }
 
     @Test
     void 게시글_정상_추가() {
-        Article article = new Article(0, "title", "url", "contents");
+        int id = articleRepository.nextId();
+        Article article = new Article(id, "title2", "url2", "contents2");
         articleRepository.insert(article);
         assertThat(articleRepository.findAll()).contains(article);
     }
 
     @Test
     void 게시글_정상_검색() {
-        Article article = new Article(0, "title", "url", "contents");
-        articleRepository.insert(article);
-        assertThat(articleRepository.findById(0)).isEqualTo(article);
+        assertDoesNotThrow(() -> articleRepository.findById(0));
     }
 
     @Test
     void 존재하지_않는_게시글_검색_에러() {
-        assertThrows(ArticleNotFoundException.class, () ->
-                articleRepository.findById(0));
+        assertThrows(ArticleNotFoundException.class, () -> articleRepository.findById(1));
     }
 
     @Test
     void 존재하는_게시글_정상_수정() {
-        int id = articleRepository.nextId();
-        articleRepository.insert(new Article(id, "title", "url", "contents"));
         assertDoesNotThrow(() ->
-                articleRepository.update(new Article(id, "a", "a", "a")));
+                articleRepository.update(new Article(0, "title2", "url2", "contents2")));
     }
 
     @Test
     void 존재하지않는_게시글_수정_시도_에러() {
         assertThrows(ArticleNotFoundException.class, () ->
-                articleRepository.update(new Article(articleRepository.nextId(), "a", "a", "a")));
+                articleRepository.update(new Article(articleRepository.nextId(), "title2", "url2", "contents2")));
     }
 
     @Test
     void 존재하는_게시글_정상_삭제() {
-        int id = articleRepository.nextId();
-        articleRepository.insert(new Article(id, "title", "url", "contents"));
-        assertDoesNotThrow(() -> articleRepository.remove(id));
+        assertDoesNotThrow(() -> articleRepository.remove(0));
     }
 
     @Test
     void 존재하지않는_게시글_삭제_시도_에러() {
-        assertThrows(ArticleNotFoundException.class, () -> articleRepository.remove(0));
+        assertThrows(ArticleNotFoundException.class, () -> articleRepository.remove(articleRepository.nextId()));
     }
 }
