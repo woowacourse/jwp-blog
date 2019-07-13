9886beed1f19cc1d36a5ad430749c66adc6ac482
CODEMCD <psbum1609@gmail.com>
2019-07-13 19:16:15 +0900

    refactor: 게시글 생성 및 삭제 중복 제거

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,5 +1,7 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,23 @@ public class ArticleControllerTests {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @BeforeEach
+    void setUp() {
+        String title = "titleTest";
+        String coverUrl = "coverUrlTest";
+        String contents = "contentsTest";
+        webTestClient.post()
+                .uri("/write")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("contents", contents))
+                .exchange()
+                .expectStatus().isFound()
+        ;
+    }
+
     @Test
     void index() {
         webTestClient.get().uri("/")
@@ -39,105 +58,38 @@ public class ArticleControllerTests {
     }
 
     @Test
-    void create_article() {
-        String title = "목적의식 있는 연습을 통한 효과적인 학습";
-        String coverUrl = "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/5tdm/image/7OdaODfUPkDqDYIQKXk_ET3pfKo.jpeg";
-        String contents = "나는 우아한형제들에서 우아한테크코스 교육 과정을 진행하고 있다. 우테코를 설계하면서 고민스러웠던 부분 중의 하나는 선발 과정을 어떻게 하면 의미 있는 시간으로 만들 것인가였다.";
-
-        webTestClient.post()
-                .uri("/write")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("title", title)
-                        .with("coverUrl", coverUrl)
-                        .with("contents", contents))
-                .exchange()
-                .expectStatus().isFound()
-                .expectBody()
-                .consumeWith(response -> {
-                    webTestClient.get()
-                            .uri(response.getRequestHeaders().getLocation())
-                            .exchange()
-                            .expectStatus().isOk()
-                            .expectBody()
-                            .consumeWith(response2 -> {
-                                String body = new String(response2.getResponseBody());
-                                assertThat(body.contains(title)).isTrue();
-                                assertThat(body.contains(coverUrl)).isTrue();
-                                assertThat(body.contains(contents)).isTrue();
-                            });
-                });
-    }
-
-    @Test
-    void create_article_en() {
-        String title = "titleTest";
-        String coverUrl = "urlTest";
-        String contents = "contentsTest";
-        webTestClient.post()
-                .uri("/write")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("title", title)
-                        .with("coverUrl", coverUrl)
-                        .with("contents", contents))
-                .exchange()
-                .expectStatus().isFound()
-                .expectBody()
-                .consumeWith(response -> {
-                    webTestClient.get()
-                            .uri(response.getRequestHeaders().getLocation())
-                            .exchange()
-                            .expectStatus().isOk()
-                            .expectBody()
-                            .consumeWith(response2 -> {
-                                String body = new String(response2.getResponseBody());
-                                assertThat(body.contains(title)).isTrue();
-                                assertThat(body.contains(coverUrl)).isTrue();
-                                assertThat(body.contains(contents)).isTrue();
-                            });
-                });
-    }
-
-    @Test
     void 게시글_페이지_정상_조회() {
-        int id = articleRepository.nextId();
         String title = "titleTest";
-        String coverUrl = "urlTest";
+        String coverUrl = "coverUrlTest";
         String contents = "contentsTest";
-        articleRepository.insert(new Article(id, title, coverUrl, contents));
 
         webTestClient.get()
-                .uri("/articles/" + id)
+                .uri("/articles/" + (articleRepository.nextId() - 1))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .consumeWith(response -> {
-                    String body = new String(response.getResponseBody());
+                .consumeWith(response2 -> {
+                    String body = new String(response2.getResponseBody());
                     assertThat(body.contains(title)).isTrue();
                     assertThat(body.contains(coverUrl)).isTrue();
                     assertThat(body.contains(contents)).isTrue();
-                });
+                })
+        ;
     }
 
     @Test
     void 존재하지_않는_게시글_조회_에러() {
         webTestClient.get()
-                .uri("/articles/" + articleRepository.nextId())
+                .uri("/articles/" + ((articleRepository.nextId() - 1) + 1))
                 .exchange()
-                .expectStatus().is5xxServerError();
+                .expectStatus().is5xxServerError()
+        ;
     }
 
     @Test
     void 게시글_수정페이지_이동() {
-        int id = articleRepository.nextId();
-        String title = "titleTest";
-        String coverUrl = "urlTest";
-        String contents = "contentsTest";
-        articleRepository.insert(new Article(id, title, coverUrl, contents));
-
         webTestClient.get()
-                .uri("/articles/" + id + "/edit")
+                .uri("/articles/" + (articleRepository.nextId() - 1) + "/edit")
                 .exchange()
                 .expectStatus().isOk()
         ;
@@ -145,41 +97,33 @@ public class ArticleControllerTests {
 
     @Test
     void 게시글_수정() {
-        int id = articleRepository.nextId();
-        String newTitle = "newTile";
-        String title = "title";
-        String coverUrl = "coverUrl";
-        String contents = "contents";
-        articleRepository.insert(new Article(id, title, coverUrl, contents));
+        String newTitle = "newTileTest";
+        String newCoverUrl = "newCoverUrlTest";
+        String newContents = "newContentsTest";
 
         webTestClient.put()
-                .uri("/articles/" + id)
+                .uri("/articles/" + (articleRepository.nextId() - 1))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters
                         .fromFormData("title", newTitle)
-                        .with("coverUrl", coverUrl)
-                        .with("contents", contents))
+                        .with("coverUrl", newCoverUrl)
+                        .with("contents", newContents))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .consumeWith(response -> {
-                    String body = new String(response.getResponseBody());
+                .consumeWith(response2 -> {
+                    String body = new String(response2.getResponseBody());
                     assertThat(body.contains(newTitle)).isTrue();
-                    assertThat(body.contains(coverUrl)).isTrue();
-                    assertThat(body.contains(contents)).isTrue();
-                });
+                    assertThat(body.contains(newCoverUrl)).isTrue();
+                    assertThat(body.contains(newContents)).isTrue();
+                })
+        ;
     }
 
-    @Test
+    @AfterEach
     void 게시글_삭제() {
-        int id = articleRepository.nextId();
-        String title = "title";
-        String coverUrl = "coverUrl";
-        String contents = "contents";
-        articleRepository.insert(new Article(id, title, coverUrl, contents));
-
         webTestClient.delete()
-                .uri("/articles/" + id)
+                .uri("/articles/" + (articleRepository.nextId() - 1))
                 .exchange()
                 .expectStatus().isFound()
         ;
