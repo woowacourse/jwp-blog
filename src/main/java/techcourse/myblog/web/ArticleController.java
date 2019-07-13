07dbc576e25184ba580ac2d53c5257fea35abbfe
yejilee <yjll1019@naver.com>
2019-07-13 18:18:02 +0900

    refactor: for code convention

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -3,7 +3,6 @@ package techcourse.myblog.web;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.repository.ArticleRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
