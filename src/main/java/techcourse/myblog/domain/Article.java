97823ac206deef2567385a29ae3e6570d47fc887
ddu0422 <ddu0422@naver.com>
2019-07-13 12:26:46 +0900

    [REF] add article edit method / equals & hashcode

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,5 +1,7 @@
 package techcourse.myblog.domain;
 
+import java.util.Objects;
+
 public class Article {
     private int id;
     private String title;
@@ -14,6 +16,12 @@ public class Article {
 
     public boolean matchId(int id) {
         return this.id == id;
+    }
+    
+    public void edit(Article article) {
+        this.title = article.title;
+        this.coverUrl = article.coverUrl;
+        this.contents = article.contents;
     }
 
     public int getId() {
@@ -34,5 +42,21 @@ public class Article {
     
     public String getContents() {
         return contents;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id == article.id &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(coverUrl, article.coverUrl) &&
+                Objects.equals(contents, article.contents);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, coverUrl, contents);
     }
 }
