a3455209fd76ad1b44eb04d77f0df690cc5c8c59
deocksoo <dusol92@snu.ac.kr>
2019-07-13 22:14:55 +0900

    [REFACTOR] CouldNotFindArticleIdException이 RuntimeException을 상속하도록 수정합니다

--- src/main/java/techcourse/myblog/domain/validator/CouldNotFindArticleIdException.java
+++ src/main/java/techcourse/myblog/domain/validator/CouldNotFindArticleIdException.java
@@ -1,6 +1,6 @@
 package techcourse.myblog.domain.validator;
 
-public class CouldNotFindArticleIdException extends IllegalArgumentException {
+public class CouldNotFindArticleIdException extends RuntimeException {
     public CouldNotFindArticleIdException() {
         super("ID를 찾을 수 없습니다");
     }
