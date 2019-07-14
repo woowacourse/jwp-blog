b6374df1a52e61da08d96c79616edc3dd858fb6e
kmdngyu <kmdngyu@gmail.com>
2019-07-14 12:47:56 +0900

    refactor index 생성자 주입

--- src/main/java/techcourse/myblog/web/IndexController.java
+++ src/main/java/techcourse/myblog/web/IndexController.java
@@ -1,6 +1,5 @@
 package techcourse.myblog.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +10,11 @@ import java.util.List;
 
 @Controller
 public class IndexController {
-    @Autowired
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
+
+    public IndexController(final ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
