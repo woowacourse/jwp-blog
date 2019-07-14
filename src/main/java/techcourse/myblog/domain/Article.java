13fefbb30f9daf349eb9c6fe5d80c95781e49834
hyperpace <cnk9109@gmail.com>
2019-07-14 22:30:39 +0900

    feat(Article): remove setter

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,31 +1,42 @@
 package techcourse.myblog.domain;
 
+import java.util.Objects;
+
 public class Article {
     private String title;
     private String contents;
     private String coverUrl;
 
-    public String getTitle() {
-        return title;
+    public Article(String title, String contents, String coverUrl) {
+        this.title = title;
+        this.contents = contents;
+        this.coverUrl = coverUrl;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String getTitle() {
+        return title;
     }
 
     public String getContents() {
         return contents;
     }
 
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
     public String getCoverUrl() {
         return coverUrl;
     }
 
-    public void setCoverUrl(String coverUrl) {
-        this.coverUrl = coverUrl;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return Objects.equals(getTitle(), article.getTitle()) &&
+                Objects.equals(getContents(), article.getContents()) &&
+                Objects.equals(getCoverUrl(), article.getCoverUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getContents(), getCoverUrl());
     }
 }
