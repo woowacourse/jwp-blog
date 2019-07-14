055253b5fcdde0a6333de89b3a684da8e8b8ea59
deocksoo <dusol92@snu.ac.kr>
2019-07-15 04:43:30 +0900

    [REFACTOR] ArticleRepository의 변수명들을 변경합니다.
    setup된 article의 id를 상수로 관리하도록 하지 않고, setup될 때 필드 변수로 입력받아
    사용하도록 수정하였습니다.

--- src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/domain/ArticleRepositoryTest.java
@@ -10,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ArticleRepositoryTest {
-    private static final int TEST_ARTICLE_ID = 1;
-    private static final int TEST_ARTICLE_ID_NOT_IN_REPO = 2;
+    private static final int ARTICLE_ID_NOT_IN_REPO = 2;
 
     private ArticleRepository articleRepository;
     private ArticleDto articleDto;
+    private int setUpArticleId;
 
     @BeforeEach
     void setUp() {
@@ -26,6 +26,7 @@ class ArticleRepositoryTest {
         );
 
         articleRepository.save(articleDto);
+        setUpArticleId = articleRepository.getLastGeneratedId();
     }
 
     @Test
@@ -53,7 +54,7 @@ class ArticleRepositoryTest {
     @Test
     @DisplayName("게시물 id로 게시물을 조회한다.")
     void findTest() {
-        Article article = articleRepository.findBy(TEST_ARTICLE_ID);
+        Article article = articleRepository.findBy(setUpArticleId);
 
         assertThat(article.getTitle()).isEqualTo(articleDto.getTitle());
         assertThat(article.getCoverUrl()).isEqualTo(articleDto.getCoverUrl());
@@ -71,10 +72,10 @@ class ArticleRepositoryTest {
         );
 
         // When
-        articleRepository.updateBy(TEST_ARTICLE_ID, updatedArticleDto);
+        articleRepository.updateBy(setUpArticleId, updatedArticleDto);
 
         // Then
-        Article resultArticle = articleRepository.findBy(TEST_ARTICLE_ID);
+        Article resultArticle = articleRepository.findBy(setUpArticleId);
 
         assertThat(resultArticle.getTitle()).isEqualTo(updatedArticleDto.getTitle());
         assertThat(resultArticle.getCoverUrl()).isEqualTo(updatedArticleDto.getCoverUrl());
@@ -84,22 +85,22 @@ class ArticleRepositoryTest {
     @Test
     @DisplayName("게시물 id를 이용해 해당 게시물을 지운다.")
     void deleteByIdTest() {
-        articleRepository.deleteBy(TEST_ARTICLE_ID);
-        assertThatThrownBy(() -> articleRepository.findBy(TEST_ARTICLE_ID))
+        articleRepository.deleteBy(setUpArticleId);
+        assertThatThrownBy(() -> articleRepository.findBy(setUpArticleId))
                 .isInstanceOf(CouldNotFindArticleIdException.class);
     }
 
     @Test
     @DisplayName("Repository에 없는 id를 조회하는 경우 예외를 던져준다.")
     void findFailTest() {
-        assertThatThrownBy(() -> articleRepository.findBy(TEST_ARTICLE_ID_NOT_IN_REPO))
+        assertThatThrownBy(() -> articleRepository.findBy(ARTICLE_ID_NOT_IN_REPO))
                 .isInstanceOf(CouldNotFindArticleIdException.class);
     }
 
     @Test
     @DisplayName("Repository에 없는 id를 삭제하는 경우 예외를 던져준다.")
     void deleteFailTest() {
-        assertThatThrownBy(() -> articleRepository.deleteBy(TEST_ARTICLE_ID_NOT_IN_REPO)).
+        assertThatThrownBy(() -> articleRepository.deleteBy(ARTICLE_ID_NOT_IN_REPO)).
                 isInstanceOf(CouldNotFindArticleIdException.class);
     }
 
