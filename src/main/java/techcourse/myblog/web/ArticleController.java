742f55cdbbee5c4335e67cb8e57504e8c60e3ea9
starkim06 <starkim06@naver.com>
2019-07-15 15:30:40 +0900

    refactor: 리뷰 반영, controller ModelAnvView 메서드 내부에서 수정

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,5 +1,6 @@
 package techcourse.myblog.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,29 +12,19 @@ public class ArticleController {
 
     private ArticleRepository articleRepository;
 
+    @Autowired
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
 
-    //TODO ModelAndView와 Model로 하고 String을 넘길때는 어떤 차이가 있나?
-    //차이가 없고, 다만 ModelAndView가 옛날거다.
-    //명확해서 쓰기는 쓰시지만 팀이나 취향에 따라 다르다.
-    //파라미터에서 받지 말고 안에서 새로 생성하자.
-    //파라미터에서 모델엔뷰 안에 모델이 있어서 작동은 하는데,
-    //정상적인 케이스는 안에서 새로 만들어 주는게 더 낫다.
     @GetMapping("/")
     public ModelAndView index(String blogName) {
         if (blogName == null) {
             blogName = "누구게?";
         }
-        //아래와 같이 하지말고 findAll()을 한 뒤에 던져주자.
-//        modelAndView.setViewName("index");
-//        modelAndView.addObject("blogName", blogName);
-//        modelAndView.addObject("articleRepository", articleRepository);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
         modelAndView.addObject("blogName", blogName);
-//        List<Article> articles = articleRepository.findAll();
         modelAndView.addObject("articles", articleRepository.findAll());
         return modelAndView;
     }
@@ -43,33 +34,33 @@ public class ArticleController {
         return "article-edit";
     }
 
-    //TODO 여기는 annotation은 어딧지? @ResponseBody는 언제 붙이고 이때는 왜 생략해야될까?
     @PostMapping("/articles")
-    public ModelAndView save(Article article, ModelAndView modelAndView) {
+    public ModelAndView save(Article article) {
         articleRepository.save(article);
+        ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("article");
         modelAndView.addObject("article", article);
         return modelAndView;
     }
 
     @GetMapping("/article/{articleId}")
-    public ModelAndView show(@PathVariable String articleId, ModelAndView modelAndView) {
+    public ModelAndView show(@PathVariable String articleId) {
         Article article = articleRepository.findArticleById(Integer.parseInt(articleId));
+        ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("article");
         modelAndView.addObject("article", article);
         return modelAndView;
     }
 
     @GetMapping("/articles/{articleId}/edit")
-    public ModelAndView writeForm(@PathVariable String articleId, ModelAndView modelAndView) {
+    public ModelAndView writeForm(@PathVariable String articleId) {
+        ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("article-edit");
         Article article = articleRepository.findArticleById(Integer.parseInt(articleId));
         modelAndView.addObject("article", article);
         return modelAndView;
     }
 
-    //path에 담는 이유는 REstful한 무엇인가를 하게 보이기 위한? 그런 느낌이지 않으띾?
-    //이렇게 담을건지 안담을 건지는 취향차이이다.
     @PutMapping("/articles/{articleId}")
     public String update(@PathVariable String articleId, Article newArticle) {
         Article article = articleRepository.findArticleById(Integer.parseInt(articleId));
