18f18f1c93b967e4890b3d803d955dc2d30056b4
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-07-15 13:13:36 +0900

    make setup method for test code of ArticleController

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,5 +1,6 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +19,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
     private WebTestClient webTestClient;
-
-    @Autowired
-    public ArticleControllerTests(WebTestClient webTestClient) {
-        this.webTestClient = webTestClient;
-        saveTestArticle();
-    }
-
     private static String testTitle = "목적의식 있는 연습을 통한 효과적인 학습";;
     private static String testCoverUrl = "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/5tdm/image/7OdaODfUPkDqDYIQKXk_ET3pfKo.jpeg";
     private static String testContents = "나는 우아한형제들에서 우아한테크코스 교육 과정을 진행하고 있다. 우테코를 설계하면서 고민스러웠던 부분 중의 하나는 '선발 과정을 어떻게 하면 의미 있는 시간으로 만들 것인가?'였다.";
     private static String testUniContents = StringEscapeUtils.escapeJava(testContents);
+
+    @Autowired
+    public ArticleControllerTests(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
+
+    @BeforeEach
+    void setUp() {
+        saveTestArticle();
+    }
 
     @Test
     void showArticleWritingPageTest() {
