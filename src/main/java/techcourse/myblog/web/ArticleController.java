1d4e8cfdf771268f89ceec42d396edc248b9c2fb
kimjungmin-developer <50177674+kimjungmin-developer@users.noreply.github.com>
2019-07-15 10:46:38 +0900

    [게시글 생성/조회/수정/삭제] 제이엠 미션 제출합니다. (#21)
    * [ADD] Add README
    
    * [Fix] Code Formatting
    
    * [Fix] Pass ArticleControllerTest, HelloWorldControllerTest
    
    * [ADD] Move to writing page
    
    * [ADD] Create article logic
    
    * [ADD] Show article lists
    
    * [ADD] Show selected article
    
    * [ADD] Springboot Devtools (auto reload)
    
    * [ADD] Remove duplication body code of index, article, article-edit HTML
    
    - using Thymeleaf template layout
    
    * [Fix] create_article Test
    
    * [ADD] Remove duplicate head code of index, article, article-edit HTML
    
    * [ADD] Modify article function
    
    * [ADD] Remove article function
    
    * [REF] Fix ArticleControllerTest
    
    * [ADD] Redirect to article
    
    * [REF] Add this keyword
    
    * [REF] Extract magic number as constant variable
    
    * [REF] Add matchId method in Article
    
    * [ADD] ArticleRepository, ArticleController Test
    
    * [DELETE] Delte this from ArticelRepository
    
    * [DELETE] Delete setter methods
    
    * [Refactor] Fix test codes
    
    * [Refactor] Remove duplicate codes in article/article-edit.html via thymeleaf
    
    * [Refactor] Fix bugs
    
    * [Refactor] Fix bugs
    
    * [Refactor] Implement ArticleDto class
    
    * [Delete] Delte unnecessary codes

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,63 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
+import techcourse.myblog.domain.dto.ArticleDto;
 
 @Controller
 public class ArticleController {
+    private final ArticleRepository articleRepository;
+
     @Autowired
-    private ArticleRepository articleRepository;
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("articles", articleRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/writing")
+    public String createArticleForm() {
+        return "article-edit";
+    }
+
+    @PostMapping("/articles")
+    public String createArticle(ArticleDto articleDto, Model model) {
+        Article article = new Article(articleRepository.newArticleId(), articleDto);
+        model.addAttribute("article", article);
+        articleRepository.add(article);
+        return "redirect:/articles/" + article.getId();
+    }
+
+    @GetMapping("/articles/{articleId}")
+    public String selectArticle(@PathVariable int articleId, Model model) {
+        model.addAttribute("article", this.articleRepository.findById(articleId));
+        return "article";
+    }
+
+    @GetMapping("/articles/{articleId}/edit")
+    public String updateArticleForm(@PathVariable int articleId, Model model) {
+        model.addAttribute("article", this.articleRepository.findById(articleId));
+        return "article-edit";
+    }
+
+    @PutMapping("/articles/{articleId}")
+    public String updateArticle(@PathVariable int articleId, ArticleDto articleDto, Model model) {
+        articleRepository.update(articleId, articleDto);
+        Article updatedArticle = articleRepository.findById(articleId);
+        model.addAttribute("article", updatedArticle);
+        return "redirect:/articles/" + updatedArticle.getId();
+    }
+
+    @DeleteMapping("/articles/{articleId}")
+    public String deleteArticle(@PathVariable int articleId) {
+        articleRepository.remove(articleId);
+        return "redirect:/";
+    }
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,30 +1,109 @@
 package techcourse.myblog.web;
 
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
+
+@AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
     @Autowired
     private WebTestClient webTestClient;
 
+    private static final int ARTICLE_ID = 0;
+
+    @BeforeEach
+    void 게시글_작성_확인() {
+        webTestClient.post()
+                .uri("/articles")
+                .body(fromFormData("title", "test-title").with("coverUrl", "test-coverUrl").with("contents", "test-contents"))
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().valueMatches("location", ".*articles.*");
+    }
+
     @Test
-    void index() {
+    void index_페이지_조회() {
         webTestClient.get().uri("/")
                 .exchange()
                 .expectStatus().isOk();
     }
 
     @Test
-    void articleForm() {
-        webTestClient.get().uri("/articles/new")
+    void 게시글_작성_페이지_확인() {
+        webTestClient.get().uri("/writing")
                 .exchange()
                 .expectStatus().isOk();
     }
 
+    @Test
+    void 게시글_조회() {
+        webTestClient.get()
+                .uri("/articles/" + ARTICLE_ID)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertThat(body.contains("test-title")).isTrue();
+                    assertThat(body.contains("test-coverUrl")).isTrue();
+                    assertThat(body.contains("test-contents")).isTrue();
+                });
+    }
+
+    @Test
+    void 게시글_수정_페이지_확인() {
+        webTestClient.get()
+                .uri("/articles/" + ARTICLE_ID)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void 게시글_수정_확인() {
+        String newTitle = "newTitle";
+        String newCoverUrl = "newUrl";
+        String newContents = "newContents";
+        webTestClient.put()
+                .uri("/articles/" + ARTICLE_ID)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData("title", newTitle).with("coverUrl", newCoverUrl).with("contents", newContents))
+                .exchange()
+                .expectStatus().isFound()
+                .expectBody()
+                .consumeWith((response -> {
+                    webTestClient.get()
+                            .uri(response.getRequestHeaders().getLocation())
+                            .exchange()
+                            .expectStatus().isOk()
+                            .expectBody()
+                            .consumeWith(response2 -> {
+                                String body = new String(response2.getResponseBody());
+                                assertThat(body.contains("newTitle")).isTrue();
+                                assertThat(body.contains("newUrl")).isTrue();
+                                assertThat(body.contains("newContents")).isTrue();
+                            });
+                }));
+    }
+
+    @AfterEach
+    void 게시글_삭제_확인() {
+        webTestClient.delete()
+                .uri("/articles/" + ARTICLE_ID)
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().valueMatches("location", ".*/");
+    }
 }
