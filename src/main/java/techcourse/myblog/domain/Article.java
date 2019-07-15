e18c2182b96fd952fc298050a7f029d872a057e0
starkim06 <starkim06@naver.com>
2019-07-15 15:08:33 +0900

    refactor: 리뷰 반영 : setter 메서드명 번경

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -3,22 +3,10 @@ package techcourse.myblog.domain;
 import java.util.Objects;
 
 public class Article {
-    //식별성이 있는건 가변
-    //없는건 불변이다.
-    //articel은 값 객체가 아니다.
-    //ID마다 서로 다른 객체라서.
-    //무수히 많은 애들이 생길거라서,
-    //보통은 이런 애들은 Entity라고 부른다.
-    //그리고 Entity는 이렇게 final이 아니고 바끼는게 당연하다
-    //setter 대신에 메서드이름은 change로 바꾸는.....
-    //set, chage라는 어감에 대해 느껴보자
-    //chage는 기존에 있는데 바꾸는 어감이고 set은 처음에 없어도 주입(?)을 하는 그런 느낌이다.
-    //id는 초기에 빌 수 있으니깐 set도 괜찮지 않을까?
-
     private int id;
-    private final String title;
-    private final String coverUrl;
-    private final String contents;
+    private String title;
+    private String coverUrl;
+    private String contents;
 
     public Article(String title, String coverUrl, String contents) {
         this.title = title;
@@ -28,6 +16,18 @@ public class Article {
 
     public boolean checkId(int id) {
         return this.id == id;
+    }
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public void changeContents(String contents) {
+        this.contents = contents;
     }
 
     public void setId(int id) {
