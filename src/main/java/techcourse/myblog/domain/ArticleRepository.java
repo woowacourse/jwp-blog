bdc743ccafe08ccfcd6580f37096133ffeac8b45
iamju <35550012+imkimheejoo@users.noreply.github.com>
2019-07-16 00:27:16 +0900

    [게시글 생성/조회/수정/삭제] 버디 미션 제출합니다. (#26)
    * create README
    
    * feat : 게시글 작성 페이지 이동
    
    * feat: 게시글 작성 기능
    
    게시글 작성 기능 구현
    contents 마크다운으로 보여주도록
    
    * feat: 게시글 목록 조회 기능
    
    * feat: 게시글 조회 기능추가
    
    * refactor: 리팩토링 및 테스트 구현
    
    * style: 불필요한 코드 제거
    
    * refactor: submit_article redirect로 변경 및 테스트코드 수정
    
    * feat: Article 수정 메서드 및 테스트 구현
    
    * refactor: Article,ArticleRepository 리팩토링
    
    * feat:게시글 삭제 구현 및 테스트 추가
    
    * refactor: view의 중복코드 제거(thymeleaf)
    
    * refactor: view의 article page 중복코드 제거(thymeleaf)
    
    * style:안쓰는 코드 제거
    
    * refactor: 마크다운 문법 수정 적용으로 수정
    
    * style: 컨벤션 수정
    
    * fix:DI 생성자 수정
    
    * refactor:ArticleControllerTest 중복코드 리팩토링
    
    * refactor:ArticleRepositoryTest 리팩토링
    
    * refactor: 피드백 수정
    
    * feat: ArticleTest 추가
    
    * fix: Test코드 추상화 및 Article class의 incresement변수 동기화문제 리팩토링
    
    * refactor: 필요없는 sout문 삭제
    
    * fix: ArticleControllerTest의 필드주입을 생성자 주입으로 변경

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -10,6 +10,32 @@ public class ArticleRepository {
     private List<Article> articles = new ArrayList<>();
 
     public List<Article> findAll() {
-        return articles;
+        return new ArrayList<>(articles);
+    }
+
+    public void save(Article article) {
+        articles.add(article);
+    }
+
+    public Article findById(Long articleId) {
+        System.out.println(articles.toString());
+
+        return articles.stream()
+                .filter(article -> article.isEqualToArticleId(articleId))
+                .findAny().orElseThrow(() -> new IllegalArgumentException("해당 인스턴스가 없습니다."));
+    }
+
+    public int count() {
+        return articles.size();
+    }
+
+    public void update(Long articleId, ArticleVO articleVO) {
+        Article article = findById(articleId);
+        article.update(articleVO);
+    }
+
+    public void delete(Long articleId) {
+        Article article = findById(articleId);
+        articles.remove(article);
     }
 }
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,11 +1,64 @@
 package techcourse.myblog.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
+import techcourse.myblog.domain.ArticleVO;
 
 @Controller
 public class ArticleController {
-    @Autowired
-    private ArticleRepository articleRepository;
+
+    private final ArticleRepository articleRepository;
+
+    ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("articles", articleRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/writing")
+    public String writeArticle() {
+        return "article-edit";
+    }
+
+    @PostMapping("/articles")
+    public String createArticle(ArticleVO articleVO, Model model) {
+        Article article = new Article(articleVO.getTitle(), articleVO.getCoverUrl(), articleVO.getContents());
+        articleRepository.save(article);
+        model.addAttribute("article", article);
+        return "redirect:/articles/" + article.getArticleId();
+    }
+
+    @GetMapping("/articles/{articleId}")
+    public String showArticle(@PathVariable Long articleId, Model model) {
+        Article article = articleRepository.findById(articleId);
+        model.addAttribute("article", article);
+        return "article";
+    }
+
+    @GetMapping("/articles/{articleId}/edit")
+    public String updateArticle(@PathVariable Long articleId, Model model) {
+        Article article = articleRepository.findById(articleId);
+        model.addAttribute("article", article);
+        return "article-edit";
+    }
+
+    @PutMapping("/articles/{articleId}")
+    public String showUpdatedArticle(@PathVariable Long articleId, ArticleVO updatedArticle, Model model) {
+        articleRepository.update(articleId, updatedArticle);
+        model.addAttribute("article", updatedArticle);
+        return "redirect:/articles/" + articleId;
+    }
+
+    @DeleteMapping("delete/articles/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleRepository.delete(articleId);
+        return "redirect:/";
+    }
 }
