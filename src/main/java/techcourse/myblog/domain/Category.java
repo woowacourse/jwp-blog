3134e8a71072592294d25d8fd65dec774766ab07
SunHo Van <vsh123@naver.com>
2019-07-15 16:19:32 +0900

    feat : 카테고리 도메인 생성

---
+++ src/main/java/techcourse/myblog/domain/Category.java
@@ -0,0 +1,24 @@
+package techcourse.myblog.domain;
+
+import java.util.Objects;
+
+public class Category {
+    private String category;
+
+    public Category(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category1 = (Category) o;
+        return Objects.equals(category, category1.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category);
+    }
+}
