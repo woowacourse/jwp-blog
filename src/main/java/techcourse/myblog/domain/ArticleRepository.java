236f44d7623ca370a790cda0e0fb30c62a11090b
Doughnut <48856468+codeanddonuts@users.noreply.github.com>
2019-07-15 21:32:42 +0900

    [게시글 생성/조회/수정/삭제] 도넛 미션 제출합니다. (#11)
    * init
    
    * feat: Article POJO
    
    * feat: Article repository
    
    * feat: router
    
    * fix: article delete
    
    * fix: put method
    
    * test: controller tests
    
    * test: repo tests
    
    * feat: live reloading and auto refresh support
    
    * fix : layout
    
    * doc: README.md
    
    * refactor: constructor added
    
    * refactor: validations refactored
    
    * refactor: ModelAndView to RedirectView
    
    * fix: ArticleRepo test
    
    * refactor: moved application properties settings from main method to a separate file
    
    * refactor: tests
    
    * refactor: numbering method
    
    * refactor: no sending http method from the servre explicitly for branching
    
    * refactor: uri routing

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -7,9 +7,50 @@ import java.util.List;
 
 @Repository
 public class ArticleRepository {
-    private List<Article> articles = new ArrayList<>();
+    private static final String defaultCoverUrl = "images/pages/index/study.jpg";
+
+    private final List<Article> articles = new ArrayList<>();
+    private int nextArticleNumber = 0;
+
+    public Article write(final Article article) {
+        if (!article.validateCoverUrl()) {
+            article.setCoverUrl(defaultCoverUrl);
+        }
+        article.numbering(this);
+        this.articles.add(article);
+        return article;
+    }
+
+    public Article find(final int articleId) {
+        return this.articles.get(findIndexOfArticleById(articleId));
+    }
+
+    private int findIndexOfArticleById(final int articleId) {
+        int index = Math.min(articleId, this.articles.size()) - 1;
+        for (; index >= 0; index--) {
+            if (this.articles.get(index).getNumber() == articleId) {
+                break;
+            }
+        }
+        if (index < 0) {
+            throw new NoArticleFoundException();
+        }
+        return index;
+    }
 
     public List<Article> findAll() {
-        return articles;
+        return this.articles;
+    }
+
+    public int nextArticleNumber() {
+        return ++nextArticleNumber;
+    }
+
+    public Article edit(final Article edited, final int articleId) {
+        return find(articleId).update(edited);
+    }
+
+    public void delete(final int articleId) {
+        this.articles.remove(findIndexOfArticleById(articleId));
     }
 }
