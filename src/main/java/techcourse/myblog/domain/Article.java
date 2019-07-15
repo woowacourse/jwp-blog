2309871e6c1833bd23abf1b602855bfc2d40334b
Yumin Kim (김유민) <green4469@gmail.com>
2019-07-15 18:20:50 +0900

    [게시글 생성/조회] 무민 미션 제출합니다. (#10)
    * ADD : 실습미션
    
    * ADD : 제약조건 (HTML중복제거, livereload)
    
    * ADD : 게시글 작성 페이지
    
    * ADD : 게시글 작성
    
    * ADD : 게시글 목록 조회
    
    * ADD : 게시글 조회
    
    * ADD : 게시글 조회 수정
    
    * MOD : 게시글 쓰기 TDD 수정
    
    * ADD : 게시글 수정 페이지 이동
    
    * ADD : 게시글 수정
    
    * ADD : 게시글 삭제
    
    * MOD : Refactor
    
    * rename test
    
    * do not use setter anymore
    
    * add tests for Article class
    
    * remove duplication in tests for ArticleRepository class
    
    * refactor test codes
    
    * use only necessary annotations from lombok
    
    * remove duplication in htmls
    
    cannot put reference to "mypage.css" in common header (css crash)
    
    * remove duplication in html, by common js references
    
    * make setup method for test code of ArticleController
    
    * do not copy when find all articles from repo

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,36 @@
 package techcourse.myblog.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
 public class Article {
+    private static int currentId = 1;
+
+    private final int id;
+    private String title;
+    private String contents;
+    private String coverUrl;
+
+    public Article(String title, String contents, String coverUrl) {
+        this(currentId++, title, contents, coverUrl);
+    }
+
+    private Article(int id, String title, String contents, String coverUrl) {
+        this.title = title;
+        this.contents = contents;
+        this.coverUrl = coverUrl;
+        this.id = id;
+    }
+
+    public Article getCopy() {
+        return new Article(id, title, contents, coverUrl);
+    }
+
+    public void update(Article article) {
+        this.title = article.title;
+        this.contents = article.contents;
+        this.coverUrl = article.coverUrl;
+    }
 }
