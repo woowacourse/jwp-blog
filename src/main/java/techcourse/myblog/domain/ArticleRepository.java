285f0d3d909346d9567fd8a2571b5fcc9726df0b
Ko eunsuk <eunsukko.128@gmail.com>
2019-07-15 15:23:11 +0900

    [REFACTOR] Fix ArticleRepository for concurrent access

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -6,10 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Repository
 public class ArticleRepository {
-    private int newArticleId = 0;
+    private final AtomicInteger newArticleId = new AtomicInteger(0);
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     private Map<Integer, Article> articles = new TreeMap<>();
 
@@ -18,22 +21,37 @@ public class ArticleRepository {
     }
 
     public int insertArticle(Article article) {
-        int id = newArticleId++;
+        int id = nextId();
         article.setId(id);
 
+        lock.writeLock().lock();
         articles.put(id, article);
+        lock.writeLock().unlock();
+
         return id;
     }
 
+    private int nextId() {
+        return newArticleId.getAndIncrement();
+    }
+
     public Article findById(int articleId) {
-        return articles.get(articleId);
+        lock.readLock().lock();
+        Article article = articles.get(articleId);
+        lock.readLock().unlock();
+
+        return article;
     }
 
     public void updateArticle(Article article) {
+        lock.writeLock().lock();
         articles.put(article.getId(), article);
+        lock.writeLock().unlock();
     }
 
     public void deleteArticle(int articleId) {
+        lock.writeLock().lock();
         articles.remove(articleId);
+        lock.writeLock().unlock();
     }
 }
