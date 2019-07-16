a4107a6334c522d6e5c125ea955450f9f5a8e6af
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-16 17:45:53 +0900

    feat(ArticleController): create article

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,7 +1,10 @@
 package techcourse.myblog.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
@@ -16,5 +19,17 @@ public class ArticleController {
     @GetMapping("/")
     public String goIndex() {
         return "index";
+    }
+
+    @GetMapping("/articles")
+    public String goArticles() {
+        return "article-edit";
+    }
+
+    @PostMapping("/articles")
+    public String saveArticle(Article article, Model model) {
+        articleRepository.save(article);
+        model.addAttribute("article", article);
+        return "article";
     }
 }
