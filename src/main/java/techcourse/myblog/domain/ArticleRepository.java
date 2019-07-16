cef345689372a2185ef4d61dcea780edd6231f85
Jo Jinsik <kunggom@gmail.com>
2019-07-16 17:44:54 +0900

    JPA를 통한 기본적인 CRUD 구현 및 테스트 통과

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -1,54 +1,7 @@
 package techcourse.myblog.domain;
 
-import org.springframework.stereotype.Repository;
-import techcourse.myblog.dto.ArticleDto;
+//import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository
-public class ArticleRepository {
-    private static final String NO_SUCH_ARTICLE = "그런 게시물을 찾지 못했습니다.";
-
-    private final List<Article> articles = new ArrayList<>();
-    private int index = 1;
-
-    public List<Article> getAll() {
-        return articles;
-    }
-
-    public int addArticle(final ArticleDto articleDTO) {
-        final Article article = new Article(
-                this.index,
-                articleDTO.getTitle(),
-                articleDTO.getCoverUrl(),
-                articleDTO.getContents()
-        );
-        articles.add(article);
-        this.index++;
-        return article.getId();
-    }
-
-    public Article findById(final int articleId) {
-        return articles.stream()
-                .filter(article -> article.isSameId(articleId))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException(NO_SUCH_ARTICLE));
-    }
-
-    public void updateById(final int articleId, final ArticleDto newArticleDto) {
-        final int index = articles.indexOf(findById(articleId));
-        final Article newArticle = new Article(
-                articleId,
-                newArticleDto.getTitle(),
-                newArticleDto.getCoverUrl(),
-                newArticleDto.getContents()
-        );
-        articles.set(index, newArticle);
-    }
-
-    public void deleteById(final int articleId) {
-        final Article article = findById(articleId);
-        articles.remove(article);
-    }
-}
+//@Repository
+public interface ArticleRepository extends CrudRepository<Article, Long> {}
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,19 +1,19 @@
 package techcourse.myblog.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 import techcourse.myblog.dto.ArticleDto;
 
 @Controller
 public class ArticleController {
+    @Autowired
     private ArticleRepository articleRepository;
-
-    public ArticleController(final ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
-    }
 
 //    @Autowired
 //    void setArticleRepository(final ArticleRepository articleRepository) {
@@ -26,31 +26,35 @@ public class ArticleController {
     }
 
     @PostMapping("/articles")
-    public RedirectView writeNewArticle(@ModelAttribute("article") final ArticleDto articleDTO) {
-        final int index = articleRepository.addArticle(articleDTO);
-        return new RedirectView("/articles/" + index);
-    }
+    public String createArticle(@ModelAttribute("article") final ArticleDto articleDto) {
+        final Article article = new Article(articleDto);
+        articleRepository.save(article);
+        return "redirect:/articles/" + article.getId();
+    } // 현재 이후 라인의 코드는 작동 여부 보증 안 됨.
 
-    @GetMapping("/articles/{articleId}")
-    public String showArticle(final Model model, @PathVariable final int articleId) {
-        model.addAttribute("article", articleRepository.findById(articleId));
-        return "article";
+    @GetMapping("/articles/{id}")
+    public ModelAndView showArticle(@PathVariable final Long id) {
+        final ModelAndView mav = new ModelAndView("article");
+        mav.addObject("article", articleRepository.findById(id).get());
+        return mav;
     }
 
     @GetMapping("/articles/{articleId}/edit")
-    public String editArticlePage(final Model model, @PathVariable final int articleId) {
-        model.addAttribute("article", articleRepository.findById(articleId));
+    public String editArticlePage(final Model model, @PathVariable final Long articleId) {
+        model.addAttribute("article", articleRepository.findById(articleId).get());
         return "article-edit";
     }
 
     @PutMapping("/articles/{articleId}")
-    public RedirectView editArticle(@PathVariable final int articleId, @ModelAttribute("article") final ArticleDto articleDTO) {
-        articleRepository.updateById(articleId, articleDTO);
-        return new RedirectView("/articles/" + articleId);
+    public RedirectView editArticle(@PathVariable final Long articleId, @ModelAttribute("article") final ArticleDto articleDto) {
+        final Article article = articleRepository.findById(articleId).get();
+        article.changeArticle(articleDto);
+        articleRepository.save(article);
+        return new RedirectView("/articles/" + article.getId());
     }
 
     @DeleteMapping("/articles/{articleId}")
-    public String deleteArticle(@PathVariable final int articleId) {
+    public String deleteArticle(@PathVariable final Long articleId) {
         articleRepository.deleteById(articleId);
         return "redirect:/";
     }
