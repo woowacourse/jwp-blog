72e0dc065eb968f0299090f558f2cc6859792626
hyunssooo <jhs695@naver.com>
2019-07-16 09:49:45 +0900

    [게시글 생성/조회/수정/삭제] 지노 미션 제출합니다. (#45)
    * feat: thymeleaf 추가, html 파일 templates 디렉토리로 이동
    
    * feat: Test 주석추가
    
    * feat: ArticleController 추가
    
    * feat: Article 클래스 추가
    
    * feat: 게시글 수정, 삭제 기능 구현
    
    * refactor: 코드 리팩토링
    
    * refactor: 함수명 수정
    
    * refactor: Controller 메서드명 수정
    
    * refactor: 코드 리팩토링
    
    * refactor: 매서드 위치 수정
    
    * refactor: 리뷰 반영
    
    * refactor: 리뷰 반영 : setter 메서드명 번경
    
    * refactor: 리뷰 반영, ArticleRepository.class update 메서드 삭제
    
    * refactor: 리뷰 반영, ArticleRepository.class 수정에 따른 테스트 코드 삭제
    
    * refactor: 리뷰 반영, controller ModelAnvView 메서드 내부에서 수정
    
    * refactor: 테스트 코드 수정
    
    * refactor 피드백 반영
    
    * refactor 피드백 반영

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
+
     @Autowired
     private WebTestClient webTestClient;
+    private WebTestClient.ResponseSpec responseSpec;
 
     @Test
     void index() {
@@ -21,10 +24,47 @@ public class ArticleControllerTests {
     }
 
     @Test
-    void articleForm() {
-        webTestClient.get().uri("/articles/new")
+    void writeForm_test() {
+        webTestClient.get().uri("/writing")
                 .exchange()
                 .expectStatus().isOk();
     }
 
+    @Test
+    void save_test() {
+        insertArticle();
+
+        responseSpec
+                .expectStatus()
+                .isOk();
+    }
+
+    @Test
+    void update_test() {
+        insertArticle();
+
+        webTestClient.put().uri("/articles/1")
+                .body(BodyInserters.fromFormData("title", "title")
+                        .with("coverUrl", "coverUrl").with("contents", "contents"))
+                .exchange()
+                .expectHeader().valueMatches("Location","http://localhost:[0-9]+/$")
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void delete_test() {
+        insertArticle();
+
+        webTestClient.delete().uri("/articles/1")
+                .exchange()
+                .expectStatus()
+                .is3xxRedirection();
+    }
+
+    private void insertArticle() {
+        responseSpec = webTestClient.post().uri("/articles")
+                .body(BodyInserters.fromFormData("title", "title")
+                        .with("coverUrl", "coverUrl").with("contents", "contents"))
+                .exchange();
+    }
 }
