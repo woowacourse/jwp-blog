ad0eb33f903f8e3db7dbe4cc92ede344843fc576
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-16 14:50:01 +0900

    feat: init JPA mission

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,35 +1,4 @@
 package techcourse.myblog.domain;
 
 public class Article {
-    private Long id;
-    private String title;
-    private String coverUrl;
-    private String contents;
-
-    public Article(Long id, String title, String coverUrl, String contents) {
-        this.id = id;
-        this.title = title;
-        this.coverUrl = coverUrl;
-        this.contents = contents;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCoverUrl() {
-        return coverUrl;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public boolean isSameId(long id) {
-        return this.id == id;
-    }
 }
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,16 +1,10 @@
 package techcourse.myblog.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import techcourse.myblog.domain.Article;
+import org.springframework.web.bind.annotation.GetMapping;
 import techcourse.myblog.domain.ArticleRepository;
-import techcourse.myblog.web.dto.ArticleDto;
-
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @Controller
-@RequestMapping("/articles")
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
@@ -19,36 +13,8 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @PostMapping
-    public String createArticle(ArticleDto article) {
-        return "redirect:/articles/" + articleRepository.saveArticle(article);
-    }
-
-    @GetMapping("/{id}")
-    public String goDetailArticle(@PathVariable long id, Model model) {
-        model.addAttribute("article",
-                articleRepository.getArticleById(id)
-                        .orElseThrow(IllegalArgumentException::new));
-        return "article";
-    }
-
-    @PutMapping("/{id}")
-    public String updateArticle(@PathVariable long id, Article updatedArticle) {
-        articleRepository.updateArticle(id, updatedArticle);
-        return "redirect:/articles/" + id;
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteArticle(@PathVariable long id) {
-        articleRepository.removeArticleById(id);
-        return "redirect:/";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String editArticle(@PathVariable long id, Model model) {
-        model.addAttribute("article",
-                articleRepository.getArticleById(id).orElseThrow(IllegalArgumentException::new));
-        model.addAttribute("method", PUT);
-        return "article-edit";
+    @GetMapping("/")
+    public String goIndex() {
+        return "index";
     }
 }
