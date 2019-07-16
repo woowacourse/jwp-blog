4f0707e8fadcfb2e26be069777c58b9aa970eff1
codeanddonuts <kkw200@naver.com>
2019-07-16 15:30:10 +0900

    feat: JPA applied.

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -6,9 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
-import techcourse.myblog.dto.ArticleDto;
-
-import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -17,8 +14,7 @@ public class ArticleController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Article> articles = articleRepository.findAll();
-        model.addAttribute("articles", articles);
+        model.addAttribute("Articles", articleRepository.findAll());
         return "index";
     }
 
@@ -28,36 +24,33 @@ public class ArticleController {
     }
 
     @PostMapping("/articles")
-    public String createArticle(ArticleDto.Create articleDto) {
-        Article article = new Article(articleDto.getTitle(), articleDto.getCoverUrl(), articleDto.getContents());
-        long articleId = articleRepository.save(article);
-        return "redirect:/articles/" + articleId;
+    public String createArticle(Article article) {
+        Article written = articleRepository.save(article);
+        return "redirect:/articles/" + written.getId();
     }
 
     @GetMapping("/articles/{articleId}")
     public String readArticle(@PathVariable long articleId, Model model) {
-        Article article = articleRepository.find(articleId);
-        model.addAttribute("article", article);
+        model.addAttribute("Article", articleRepository.findById(articleId).get());
         return "article";
     }
 
     @GetMapping("/articles/{articleId}/edit")
     public String renderUpdatePage(@PathVariable long articleId, Model model) {
-        Article article = articleRepository.find(articleId);
-        model.addAttribute("article", article);
+        model.addAttribute("Article", articleRepository.findById(articleId).get());
         return "article-edit";
     }
 
     @PutMapping("/articles/{articleId}")
-    public String updateArticle(@PathVariable long articleId, ArticleDto.Update articleDto) {
-        Article article = new Article(articleId, articleDto.getTitle(), articleDto.getCoverUrl(), articleDto.getContents());
-        articleRepository.update(article);
+    public String updateArticle(@PathVariable long articleId, Article article) {
+        article.setId(articleId);
+        articleRepository.save(article);
         return "redirect:/articles/" + articleId;
     }
 
     @DeleteMapping("/articles/{articleId}")
     public String deleteArticle(@PathVariable long articleId) {
-        articleRepository.delete(articleId);
+        articleRepository.deleteById(articleId);
         return "redirect:/";
     }
 }
