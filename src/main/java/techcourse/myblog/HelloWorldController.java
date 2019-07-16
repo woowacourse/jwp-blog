dbadeb4923877af09fe8060ee6d0e7abd77f1138
김민준 <starkim06@naver.com>
2019-07-16 09:51:10 +0900

    [게시글 생성/조회/수정/삭제] 뚱이 미션 제출합니다. (#43)
    * feat: thymeleaf 추가, html 파일 templates 디렉토리로 이동
    
    * feat: Test 주석추가
    
    * feat: ArticleController 추가
    
    * feat: Article 클래스 추가
    
    * feat: 게시글 수정, 삭제 기능 구현
    
    * refactor: 코드 리팩토링
    
    * refactor: 함수명 수정
    
    * refactor: Controller 메서드명 수정
    
    * refactor: 코드 리팩토링
    
    * refactor: 매서드 위치 수정
    
    * refactor: 리뷰 반영
    
    * refactor: 리뷰 반영 : setter 메서드명 번경
    
    * refactor: 리뷰 반영, ArticleRepository.class update 메서드 삭제
    
    * refactor: 리뷰 반영, ArticleRepository.class 수정에 따른 테스트 코드 삭제
    
    * refactor: 리뷰 반영, controller ModelAnvView 메서드 내부에서 수정
    
    * refactor: 테스트 코드 수정
    
    * refactor: delete 테스트 코드 수정
    
    * refactor: delete 테스트 코드 수정
    
    * refactor: 테스트 코드, 메서드 분리

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,8 +1,19 @@
 package techcourse.myblog;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
+    @GetMapping("/helloworld")
+    public String temp(String blogName) {
+        return blogName;
+    }
 
+    @PostMapping("/helloworld")
+    public String asd(@RequestBody String blogName) {
+        return blogName;
+    }
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
+
     @Autowired
     private WebTestClient webTestClient;
 
@@ -21,10 +23,44 @@ public class ArticleControllerTests {
     }
 
     @Test
-    void articleForm() {
-        webTestClient.get().uri("/articles/new")
+    void writeForm_test() {
+        webTestClient.get().uri("/writing")
                 .exchange()
                 .expectStatus().isOk();
     }
 
+    @Test
+    void save_test() {
+        webTestClient.post().uri("/articles")
+                .body(BodyInserters.fromFormData("title", "title")
+                        .with("coverUrl", "coverUrl").with("contents", "contents"))
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void delete_test() {
+        addArticle();
+        webTestClient.delete().uri("/articles/1")
+                .exchange()
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void modify_test() {
+        addArticle();
+        webTestClient.put().uri("/articles/1")
+                .body(BodyInserters.fromFormData("title", "title")
+                        .with("coverUrl", "coverUrl").with("contents", "contents"))
+                .exchange()
+                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/$")
+                .expectStatus().is3xxRedirection();
+    }
+
+    private void addArticle() {
+        webTestClient.post().uri("/articles")
+                .body(BodyInserters.fromFormData("title", "title")
+                        .with("coverUrl", "coverUrl").with("contents", "contents"))
+                .exchange();
+    }
 }
