3e11b7cd459601799ec91e358c41b8c9ad015e17
men7627 <men1219@hanmail.net>
2019-07-16 09:31:07 +0900

    fix : way of put method using

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
@@ -13,51 +13,53 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    //홈 이동
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("articles", articleRepository.findAll());
         return "index";
     }
 
+    //글작성 페이지 이동
     @GetMapping("/writing")
     public String writeArticle(final Model model) {
-        model.addAttribute("article", new Article());
-        model.addAttribute("url", "/articles");
-        model.addAttribute("method", "post");
         return "article-edit";
     }
 
+    //글작성
     @PostMapping("/articles")
-    public ModelAndView confirmWrite(final Article article) {
+    public RedirectView confirmWrite(final Article article) {
         articleRepository.write(article);
-        return new ModelAndView("redirect:/articles/" + article.getId() + "/");
+        return new RedirectView("/articles/" + article.getId());
     }
 
-    @GetMapping("/articles/{articleId}/")
+    //글 보기
+    @GetMapping("/articles/{articleId}")
     public String viewArticle(@PathVariable final int articleId, final Model model) {
-        model.addAttribute("article", articleRepository.find(articleId).orElseThrow(IllegalArgumentException::new));
+        model.addAttribute("article", articleRepository.find(articleId));
         return "article";
     }
 
-    @GetMapping("/articles/{articleId}/edit/")
+    //글 수정 페이지
+    @GetMapping("/articles/{articleId}/edit")
     public String editArticle(@PathVariable final int articleId, final Model model) {
-        model.addAttribute("article", articleRepository.find(articleId).orElseThrow(IllegalArgumentException::new));
-        model.addAttribute("url", "../");
+        model.addAttribute("article", articleRepository.find(articleId));
         model.addAttribute("method", "put");
         return "article-edit";
     }
 
+    //글 수정
     @ResponseBody
-    @PutMapping("/articles/{articleId}/")
-    public ModelAndView confirmEdit(@PathVariable final int articleId, final Article article) {
+    @PutMapping("/articles/{articleId}")
+    public RedirectView confirmEdit(@PathVariable final int articleId, final Article article) {
         articleRepository.edit(article, articleId);
-        return new ModelAndView("redirect:/articles/" + articleId + "/");
+        return new RedirectView("/articles/" + articleId);
     }
 
     @ResponseBody
-    @DeleteMapping("/articles/{articleId}/")
-    public ModelAndView deleteArticle(@PathVariable final int articleId) {
+    @DeleteMapping("/articles/{articleId}")
+    public RedirectView deleteArticle(@PathVariable final int articleId) {
         articleRepository.delete(articleId);
-        return new ModelAndView("redirect:/");
+        return new RedirectView("/");
     }
 }
