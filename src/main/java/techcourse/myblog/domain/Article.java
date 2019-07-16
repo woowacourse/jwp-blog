bf4ee104f7d72307a34e1714c03975a9f6a9c8c0
박효진 <hgenie14@gmail.com>
2019-07-16 15:28:00 +0900

    feat: Article 클래스 구현

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,37 @@
 package techcourse.myblog.domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long articleId;
+
+    private String title;
+    private String coverUrl;
+    private String contents;
+
+    public Article(String title, String coverUrl, String contents) {
+        this.title = title;
+        this.coverUrl = coverUrl;
+        this.contents = contents;
+    }
+
+    public long getArticleId() {
+        return articleId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public String getContents() {
+        return contents;
+    }
 }
