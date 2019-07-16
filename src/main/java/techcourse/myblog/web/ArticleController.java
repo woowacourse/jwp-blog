a949b5e648d3c220f7da3149fc5b47cab58cc212
박효진 <hgenie14@gmail.com>
2019-07-16 18:00:18 +0900

    feat: ArticleController 클래스에 index 페이지 매핑 기능 구현

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,28 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleAssembler;
+import techcourse.myblog.domain.ArticleDto;
 import techcourse.myblog.domain.ArticleRepository;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
+    private final ArticleRepository articleRepository;
+
     @Autowired
-    private ArticleRepository articleRepository;
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping("/")
+    public String showIndex(Model model) {
+        List<ArticleDto> articleDtos = new ArticleAssembler().writeDtos(articleRepository.findAll());
+        model.addAttribute("articles", articleDtos);
+        return "index";
+    }
 }
