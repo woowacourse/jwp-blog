93a4b606d5d680c82d7c0480eb8e7b1581b60480
Do Won Lee <gch01410@gmail.com>
2019-07-16 20:16:15 +0900

    [게시글 생성/조회/수정/삭제] 던 미션 제출합니다. (#40)
    * feat: Set gradle and create templates folder
    
    * feat: Map article-edit.html on writing url
    
    * feat: Add the function of writing articles
    
    * refactor: Change the setting from the relative path to the absolute path
    
    * feat: Add the function of checking the list of articles
    
    * feat: Map each article on permalink
    
    * refactor: Eliminate duplication of html files
    
    * refactor: Modify path and optimize the name of variable
    
    * feat: Add function of editing articles
    
    * feat: Add function of deleting articles
    
    * refactor: Remove duplication of html
    
    * refactor: Merge MainController to ArticleController
    
    * refactor: Create articleDTO
    
    * refactor: Create exception handler of not finding to article
    
    * refactor: Separate the functions into service layers from controller layers
    
    * refactor: Remove MainControllerTest
    
    * refactor: Fix the name of method to apply method naming convention
    
    * refactor: Remove the setter method of DTO
    
    * refactor: Create assembler of Article to convert to dto and entity
    
    * refactor: Move latest id of article to ArticleRepository
    
    * refactor: Customize the exception about Article
    
    * refactor: Replace redirect string with RedirectView
    
    * refactor: Add ResponseStatus annotation to exception
    
    * refactor: Rename package of exception

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,69 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import techcourse.myblog.domain.ArticleRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import techcourse.myblog.dto.ArticleDto;
+import techcourse.myblog.service.ArticleService;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
+    private ArticleService articleService;
+
     @Autowired
-    private ArticleRepository articleRepository;
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/")
+    public String showMain(Model model) {
+        List<ArticleDto> articleDtos = articleService.findAll();
+        model.addAttribute("articleDTOs", articleDtos);
+        return "index";
+    }
+
+    @GetMapping("/writing")
+    public String showWritingPage() {
+        return "article-edit";
+    }
+
+    @PostMapping("/articles")
+    public ModelAndView createArticle(ArticleDto articleDTO) {
+        ArticleDto persistArticle = articleService.save(articleDTO);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setView(new RedirectView("/articles/" + persistArticle.getId()));
+        return modelAndView;
+    }
+
+    @GetMapping("/articles/{id}")
+    public String showArticle(@PathVariable int id, Model model) {
+        model.addAttribute("articleDTO", articleService.findById(id));
+        return "article";
+    }
+
+    @PutMapping("/articles/{id}")
+    public ModelAndView updateArticle(@PathVariable int id, ArticleDto articleDTO) {
+        articleService.update(id, articleDTO);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setView(new RedirectView("/articles/" + id));
+        return modelAndView;
+    }
+
+    @DeleteMapping("/articles/{id}")
+    public ModelAndView deleteArticle(@PathVariable int id) {
+        articleService.delete(id);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setView(new RedirectView("/"));
+        return modelAndView;
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String showEditPage(@PathVariable int id, Model model) {
+        model.addAttribute("articleDTO", articleService.findById(id));
+        return "article-edit";
+    }
 }
