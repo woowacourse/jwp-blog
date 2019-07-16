20ede201c6d9eddc2998c8ab75cba3a3190fbe03
KimHyoGeon <32132938+men7627@users.noreply.github.com>
2019-07-16 09:55:05 +0900

    [게시글 생성/조회/수정/삭제] 쿠기 미션 제출합니다. (#12)
    * init
    
    * feat: Article POJO
    
    * feat: Article repository
    
    * feat: router
    
    * fix: article delete
    
    * fix: put method
    
    * test: controller tests
    
    * test: repo tests
    
    * feat: live reloading and auto refresh support
    
    * fix : layout
    
    * doc: README.md
    
    * fix : ArticleControllerTest
    
    * fix : ArticleRepositoryTest
    
    * fix : ArticleControllerTest
    
    * fix : ArticleControllerTest
    
    * fix : way of put method using
    
    * fix : modify url

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,64 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
+    //홈 이동
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("articles", articleRepository.findAll());
+        return "index";
+    }
+
+    //글작성 페이지 이동
+    @GetMapping("/writing")
+    public String writeArticle(final Model model) {
+        return "article-edit";
+    }
+
+    //글작성
+    @PostMapping("/articles")
+    public RedirectView confirmWrite(final Article article) {
+        articleRepository.write(article);
+        return new RedirectView("/articles/" + article.getId());
+    }
+
+    //글 보기
+    @GetMapping("/articles/{articleId}")
+    public String viewArticle(@PathVariable final int articleId, final Model model) {
+        model.addAttribute("article", articleRepository.find(articleId));
+        return "article";
+    }
+
+    //글 수정 페이지
+    @GetMapping("/articles/{articleId}/edit")
+    public String editArticle(@PathVariable final int articleId, final Model model) {
+        model.addAttribute("article", articleRepository.find(articleId));
+        model.addAttribute("method", "put");
+        return "article-edit";
+    }
+
+    //글 수정
+    @ResponseBody
+    @PutMapping("/articles/{articleId}")
+    public RedirectView confirmEdit(@PathVariable final int articleId, final Article article) {
+        articleRepository.edit(article, articleId);
+        return new RedirectView("/articles/" + articleId);
+    }
+
+    @ResponseBody
+    @DeleteMapping("/articles/{articleId}")
+    public RedirectView deleteArticle(@PathVariable final int articleId) {
+        articleRepository.delete(articleId);
+        return new RedirectView("/");
+    }
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,30 +1,97 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleRepository;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
-    @Autowired
-    private WebTestClient webTestClient;
+        @Autowired
+        private WebTestClient webTestClient;
 
-    @Test
-    void index() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
-    }
+        @Autowired
+        private ArticleRepository articleRepository;
 
-    @Test
-    void articleForm() {
-        webTestClient.get().uri("/articles/new")
-                .exchange()
-                .expectStatus().isOk();
-    }
+        @BeforeEach
+        void setUp() {
+                articleRepository.deleteAll();
+        }
 
+        @Test
+        void indexTest() {
+                webTestClient.get().uri("/")
+                    .exchange()
+                    .expectStatus().isOk();
+        }
+
+        @Test
+        void writeArticleTest() {
+                webTestClient.get().uri("/writing")
+                    .exchange()
+                    .expectStatus().isOk();
+        }
+
+        @Test
+        void confirmWriteTest() {
+                String title = "제목title";
+                String coverUrl = "링크link";
+                String contents = "내용content";
+
+                webTestClient.post().uri("/articles")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(BodyInserters.fromFormData("title", title).with("coverUrl", coverUrl).with("contents", contents))
+                    .exchange()
+                    .expectStatus().isFound().expectHeader().valueMatches("location", "(.)*(/articles/)" + articleRepository.getLastArticleId());
+        }
+
+        @Test
+        void viewArticleTest() {
+                articleRepository.write(new Article());
+                webTestClient.get().uri("/articles/" + articleRepository.getLastArticleId())
+                    .exchange()
+                    .expectStatus().isOk();
+        }
+
+        @Test
+        void editArticleTest() {
+                articleRepository.write(new Article());
+                webTestClient.get().uri("/articles/" + articleRepository.getLastArticleId() + "/edit")
+                    .exchange()
+                    .expectStatus().isOk();
+        }
+
+        @Test
+        void confirmEditTest() {
+                String title = "제목title";
+                String coverUrl = "링크link";
+                String contents = "내용content";
+
+                Article article = new Article(title, coverUrl, contents);
+                articleRepository.write(article);
+                webTestClient.put().uri("/articles/" + articleRepository.getLastArticleId())
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(BodyInserters.fromFormData("title", title).with("coverUrl", coverUrl).with("contents", contents))
+                    .exchange()
+                    .expectStatus().isFound().expectHeader().valueMatches("location", "(.)*(/articles/)" + articleRepository.getLastArticleId());
+        }
+
+        @Test
+        void deleteArticleTest() {
+                articleRepository.write(new Article());
+                webTestClient.delete().uri("/articles/" + articleRepository.getLastArticleId())
+                    .exchange()
+                    .expectStatus().is3xxRedirection();
+                webTestClient.get().uri("/articles/" + articleRepository.getLastArticleId())
+                    .exchange()
+                    .expectStatus().is5xxServerError();
+        }
 }
