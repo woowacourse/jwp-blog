6eaa4bf2602bb0ee79d9ee5607820d0564032d29
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-07-16 10:02:15 +0900

    [게시글 생성/조회/수정/삭제] 르윈 미션 제출합니다. (#8)
    * 실습
    
    * docs : 1단계 기능목록 정리
    
    * feat : 게시글 작성 페이지 이동
    
    * feat : 게시글 작성 및 리스트에 저장
    
    * feat : 게시글 목록 -> model
    
    * feat : 게시글 조회 페이지 이동
    
    * feat : html, templates 폴더 이동, static 경로 설정
    
    * feat : 게시글 작성 후 게시글 노출
    
    * feat : 게시글 작성 후 해당 게시글 조회로 이동
    
    * feat : 게시글 목록 조회 및 단일 게시글 조회
    
    * docs : 2단계 기능 목록 정리
    
    * feat : 게시글 수정
    
    * feat : 게시글 삭제
    
    * html : html 중복 제거
    
    * refactor : 컨벤션에 맞게 리팩토링
    
    * refactor : ArticleControllerTest.class 리팩토링
    
    * html. HTML 공통 부분 제거
    
    * test. ArticleControllerTest.class 상수 추가
    
    * mod. URL "/write" -> "/articles" 로 수정
    
    * configuration. HTML 변경시 자동으로 Reload 설정
    
    * hotfix. Test클래스 수정 및 findAll() 복사 반환
    
    * style. app.css 수정
    
    * feedback. 피드백 반영해 코드 수정
    
    * mod. Dto클래스 추가 후 구조 변경
    
    * mod. Article.class settter, getter 삭제
    
    * test. ArticleTest.class 추가
    
    * mod. ArticleRepository.findArticleById() Optional<Article> 반환하도록 수정
    
    * mod. Article.clss의 toDto() -> ArticleDto.class의 toDto()
    
    * test. ArticleDtoTest.class 추가
    
    * mod. ArticleRepository.class 생성자 추가

---
+++ src/test/java/techcourse/myblog/domain/ArticleTest.java
@@ -0,0 +1,52 @@
+package techcourse.myblog.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArticleTest {
+
+    private static final long ID = 1L;
+    private static final String TITLE = "TITLE";
+    private static final String COVER_URL = "https://url.com";
+    private static final String CONTENTS = "CONTENTS";
+    private static final Article SAMPLE_ARTICLE = new Article(ID, TITLE, COVER_URL, CONTENTS);
+
+    @Test
+    public void 동시성_테스트() {
+        Article expected = SAMPLE_ARTICLE;
+        Article actual = new Article(ID, TITLE, COVER_URL, CONTENTS);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void 동시성_테스트_다를때() {
+        long diffId = 3L;
+        Article expected = SAMPLE_ARTICLE;
+        Article actual = new Article(diffId, TITLE, COVER_URL, CONTENTS);
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void ID가_같을때_True를_반환하는지_테스트() {
+        assertTrue(SAMPLE_ARTICLE.isSameId(ID));
+    }
+
+    @Test
+    public void ID가_다를때_False를_반환하는지_테스트() {
+        long id = 2L;
+        assertFalse(SAMPLE_ARTICLE.isSameId(id));
+    }
+
+    @Test
+    @DisplayName("Article에_대한_변경_테스트")
+    public void update() {
+        Article expected = SAMPLE_ARTICLE;
+        Article actual = new Article(2L, "NEED CHANGE", "http://urlurl.com"
+                , "Content need change");
+        actual.update(expected);
+
+        assertEquals(expected, actual);
+    }
+}
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,30 +1,133 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.StringUtils;
+import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.domain.ArticleRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
+    private static final String ROOT_URL = "/";
+    private static final String EDIT_URL = "/articles/1/edit";
+    private static final String WRITING_URL = "/writing";
+    private static final String ARTICLE_URL = "/articles";
+    private static final String SPECIFIC_ARTICLE_URL = "/articles/1";
+
+    private static final String ARTICLE_DELIMITER
+            = "<div role=\"tabpanel\" class=\"tab-pane fade in active\" id=\"tab-centered-1\">";
+
+    private static final String TITLE_NAME = "title";
+    private static final String COVER_URL_NAME = "coverUrl";
+    private static final String CONTENTS_NAME = "contents";
+
+    private static final String TITLE_VALUE = "TEST";
+    private static final String COVER_URL_VALUE = "https://img.com";
+    private static final String CONTENTS_VALUE = "TEST_CONTENTS";
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @Autowired
     private WebTestClient webTestClient;
 
-    @Test
-    void index() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
+    @BeforeEach
+    void setUp() {
+        articleRepository.deleteAll();
     }
 
     @Test
-    void articleForm() {
-        webTestClient.get().uri("/articles/new")
+    @DisplayName("새로운글을_쓸때_테스트")
+    public void showWritingPage() {
+        webTestClient.get()
+                .uri(WRITING_URL)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus()
+                .isOk();
     }
 
+    @Test
+    @DisplayName("Article을_추가할때_redirect하는지_테스트")
+    public void addArticleTest() {
+        webTestClient.post()
+                .uri(ARTICLE_URL)
+                .body(BodyInserters
+                        .fromFormData(TITLE_NAME, TITLE_VALUE)
+                        .with(COVER_URL_NAME, COVER_URL_VALUE)
+                        .with(CONTENTS_NAME, CONTENTS_VALUE))
+                .exchange()
+                .expectStatus()
+                .isFound();
+    }
+
+    @Test
+    @DisplayName("Article의_목록을_조회하는지_테스트")
+    public void indexTest() {
+        final int count = 3;
+        addArticleTest();
+        addArticleTest();
+        addArticleTest();
+
+        webTestClient.get()
+                .uri(ROOT_URL)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertEquals(count, StringUtils.countOccurrencesOf(body, ARTICLE_DELIMITER));
+                });
+    }
+
+    @Test
+    @DisplayName("ArticleId에_맞는_Article을_조회할때_테스트")
+    public void showArticleById() {
+        addArticleTest();
+
+        webTestClient.get()
+                .uri(SPECIFIC_ARTICLE_URL)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @Test
+    @DisplayName("ArticleId에_맞는_Article을_변경하는지_테스트")
+    public void updateArticleById() {
+        addArticleTest();
+
+        webTestClient.get()
+                .uri(EDIT_URL)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertThat(body.contains(TITLE_VALUE)).isTrue();
+                    assertThat(body.contains(COVER_URL_VALUE)).isTrue();
+                    assertThat(body.contains(CONTENTS_VALUE)).isTrue();
+                });
+    }
+
+    @Test
+    @DisplayName("Article을_삭제할때_redirect하는지_테스트")
+    public void deleteArticle() {
+        addArticleTest();
+
+        webTestClient.delete()
+                .uri(SPECIFIC_ARTICLE_URL)
+                .exchange()
+                .expectStatus()
+                .isFound();
+    }
 }
