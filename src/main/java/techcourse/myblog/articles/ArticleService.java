908ed8893fa567e334c8928085f4bfec5a7d4cdb
SunHo Van <vsh123@naver.com>
2019-07-17 12:43:26 +0900

    refactor: @transactional 추가

--- src/main/java/techcourse/myblog/articles/ArticleService.java
+++ src/main/java/techcourse/myblog/articles/ArticleService.java
@@ -2,10 +2,12 @@ package techcourse.myblog.articles;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
@@ -14,6 +16,7 @@ public class ArticleService {
         return articleRepository.save(article);
     }
 
+    @Transactional(readOnly = true)
     public Article findById(Long id) {
         return articleRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Can't find Article : " + id));
@@ -24,13 +27,14 @@ public class ArticleService {
 
         article.update(editedArticle.getTitle(), editedArticle.getCoverUrl(), editedArticle.getContents());
 
-        return articleRepository.save(article);
+        return article;
     }
 
     public void deleteById(Long id) {
         articleRepository.deleteById(id);
     }
 
+    @Transactional(readOnly = true)
     public List<Article> findAll() {
         return articleRepository.findAll();
     }
