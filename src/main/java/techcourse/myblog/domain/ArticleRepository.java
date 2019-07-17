369fcd17d30ae35cc13f48013a52f8b306ed7b06
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-07-17 15:57:59 +0900

    [FEATURE] Apply JPA
    Repository에 CrudRepository 상속받도록 적용.

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -1,49 +1,7 @@
 package techcourse.myblog.domain;
 
-import org.springframework.stereotype.Repository;
-import techcourse.myblog.web.exception.NotExistEntityException;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicLong;
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 
-@Repository
-public class ArticleRepository {
-    private static final Long AUTO_INCREMENT_START_ID = 1L;
-
-    private AtomicLong newArticleId = new AtomicLong(AUTO_INCREMENT_START_ID);
-
-    private Map<Long, Article> articles = new TreeMap<>();
-
-    public List<Article> findAll() {
-        return new ArrayList<>(articles.values());
-    }
-
-    public Long saveArticle(Article article) {
-        Long id = newArticleId.getAndIncrement();
-        article.setId(id);
-
-        articles.put(id, article);
-        return id;
-    }
-
-    public Article findById(Long articleId) {
-        checkExistArticleId(articleId);
-
-        return articles.get(articleId);
-    }
-
-    public void removeArticle(Long articleId) {
-        checkExistArticleId(articleId);
-
-        articles.remove(articleId);
-    }
-
-    private void checkExistArticleId(Long articleId) {
-        if (!articles.containsKey(articleId)) {
-            throw new NotExistEntityException();
-        }
-    }
 }
