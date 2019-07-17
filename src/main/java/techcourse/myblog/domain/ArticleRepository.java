7de831915aaba8266a5c526eb49056944e6efaee
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-17 17:53:49 +0900

    feat(ArticleRepository): init

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package techcourse.myblog.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
