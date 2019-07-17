3974c2a131b694e660682612c972fe4a09f17551
codeanddonuts <kkw200@naver.com>
2019-07-17 15:48:51 +0900

    feat: login page

--- src/main/java/techcourse/myblog/web/UserController.java
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 import techcourse.myblog.domain.ArticleRepository;
+import techcourse.myblog.domain.User;
 
 @Controller
 public class UserController {
@@ -14,5 +17,15 @@ public class UserController {
     @GetMapping("/signup")
     public String showSignupForm(Model model) {
         return "signup";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/users")
+    public RedirectView registerUser(User user) {
+        return new RedirectView("/login");
     }
 }
