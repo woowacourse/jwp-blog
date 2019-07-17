e7791f782fe8d9f0d4e782ea0eba254417441475
codeanddonuts <kkw200@naver.com>
2019-07-17 17:20:26 +0900

    feat: signup and validation

--- src/main/java/techcourse/myblog/domain/UserRepository.java
+++ src/main/java/techcourse/myblog/domain/UserRepository.java
@@ -2,5 +2,9 @@ package techcourse.myblog.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/techcourse/myblog/web/UserController.java
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import techcourse.myblog.domain.ArticleRepository;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.domain.UserRepository;
 
@@ -17,7 +18,8 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/signup")
-    public String showSignupForm(Model model) {
+    public String showSignupForm(@RequestParam(required = false) String errorMessage, Model model) {
+        model.addAttribute("errorMessage", errorMessage);
         return "signup";
     }
 
@@ -33,7 +35,13 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public RedirectView registerUser(User user) {
-        return new RedirectView("/login");
+    public RedirectView registerUser(User user, RedirectAttributes redirectAttributes) {
+        return userRepository.findByEmail(user.getEmail()).map(u -> {
+            redirectAttributes.addAttribute("errorMessage", "이미 존재하는 이메일입니다.");
+            return new RedirectView("/signup");
+        }).orElseGet(() -> {
+            userRepository.save(user);
+            return new RedirectView("/login");
+        });
     }
 }
