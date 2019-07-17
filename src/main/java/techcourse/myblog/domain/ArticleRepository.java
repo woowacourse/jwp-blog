7a7dc2ccc04489bec56430a6deb8f45425a34621
ddu0422 <ddu0422@naver.com>
2019-07-17 14:58:11 +0900

    refactor: @Transactional 추가

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -1,11 +1,12 @@
 package techcourse.myblog.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Transactional
     default void update(Long id, ArticleDto articleDto) {
         Article article = findById(id).get();
         article.update(articleDto.toArticle());
-        save(article);
     }
 }
