d6ca9985611afd715a66f8d4c1d235b06ff602f4
mac <kangmin46@naver.com>
2019-07-18 16:36:30 +0900

    feat : ServiceTest추가

---
+++ src/test/java/techcourse/myblog/service/UserServiceTests.java
@@ -0,0 +1,44 @@
+package techcourse.myblog.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserServiceTests {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save_Test() {
+        User user = User.builder().name("김강민")
+                .email("kangmin789@naver.com")
+                .password("asdASD12!@")
+                .build();
+        userService.save(user);
+        assertThat(userRepository.findById((long)1)).isNotNull();
+    }
+
+    @Test
+    void uthenticate_Test(){
+        User user = User.builder().name("김강민")
+                .email("2@naver.com")
+                .password("asdASD12!@")
+                .build();
+        userService.save(user);
+        Optional<User> maybeUser = userService.authenticate("2@naver.com","asdASD12!@");
+        assertThat(maybeUser.isPresent()).isTrue();
+    }
+}
