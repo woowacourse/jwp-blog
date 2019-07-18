e9ec82b782ae7cd337026ba5cd30962b483db709
박효진 <hgenie14@gmail.com>
2019-07-18 14:27:46 +0900

    feat: 회원가입 기능 구현

--- src/main/java/techcourse/myblog/web/UserController.java
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -2,9 +2,19 @@ package techcourse.myblog.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import techcourse.myblog.dto.UserDto;
+import techcourse.myblog.service.ArticleService;
+import techcourse.myblog.service.UserService;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/signup")
     public String showSignup() {
         return "signup";
@@ -13,5 +23,11 @@ public class UserController {
     @GetMapping("/login")
     public String showLogin() {
         return "login";
+    }
+
+    @PostMapping("/users")
+    public String enrollUser(UserDto userDto) {
+        userService.createUser(userDto);
+        return "redirect:/login";
     }
 }
--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,5 +28,26 @@ class UserControllerTest {
         webTestClient.get().uri("/login")
                 .exchange()
                 .expectStatus().isOk();
+    }
+
+    @Test
+    public void 회원가입_테스트() {
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("name", "name")
+                        .with("email", "email")
+                        .with("password", "password"))
+                .exchange()
+                .expectStatus().isFound()
+                .expectBody()
+                .consumeWith(response -> {
+                    webTestClient.get()
+                            .uri(response.getResponseHeaders().getLocation())
+                            .exchange()
+                            .expectStatus()
+                            .isOk();
+                });
     }
 }
