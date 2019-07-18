d14d91479976b81402615ea98581e225304f72b1
yejilee <yjll1019@naver.com>
2019-07-19 02:54:59 +0900

    test: 회원등록 기능 테스트 구현

--- src/test/java/techcourse/myblog/web/UsersControllerTest.java
+++ src/test/java/techcourse/myblog/web/UsersControllerTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
@@ -20,6 +22,74 @@ class UsersControllerTest {
 	void getSignUpPage() {
 		webTestClient.get().uri("/signup")
 				.exchange()
-				.expectStatus().isOk();
+				.expectStatus()
+				.isOk();
+	}
+
+	@Test
+	void login() {
+		webTestClient.get()
+				.uri("/login")
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
+
+	@Test
+	void joinSuccess() {
+		webTestClient.post()
+				.uri("/users")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.body(BodyInserters
+						.fromFormData("username", "tiber")
+						.with("email", "tiber@naver.com")
+						.with("password", "asdfASDF1@"))
+				.exchange()
+				.expectStatus()
+				.isFound()
+				.expectHeader()
+				.valueMatches("Location", ".+/login");
+	}
+
+	@Test
+	void joinFailureDueToUsernameValue() {
+		webTestClient.post()
+				.uri("/users")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.body(BodyInserters
+						.fromFormData("username", "t")
+						.with("email", "tiber@naver.com")
+						.with("password", "asdfASDF1@"))
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
+
+	@Test
+	void joinFailureDueToEmailValue() {
+		webTestClient.post()
+				.uri("/users")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.body(BodyInserters
+						.fromFormData("username", "tiber")
+						.with("email", "tibernaver.com")
+						.with("password", "asdfASDF1@"))
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
+
+	@Test
+	void joinFailureDueToPasswordValue() {
+		webTestClient.post()
+				.uri("/users")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.body(BodyInserters
+						.fromFormData("username", "tiber")
+						.with("email", "tiber@naver.com")
+						.with("password", "aB1"))
+				.exchange()
+				.expectStatus()
+				.isOk();
 	}
 }
