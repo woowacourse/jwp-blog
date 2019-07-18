538a4d3b1fccac19b5dc814062b1c6bea25ef696
andole87 <andole87@gmail.com>
2019-07-18 18:00:15 +0900

    feat: LoginInterceptor 생성
    1. LoginInterceptorConfig 생성
    2. 인터셉터 제외 URL 추가.

---
+++ src/main/java/techcourse/myblog/interceptor/LoginInterceptor.java
@@ -0,0 +1,27 @@
+package techcourse.myblog.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import techcourse.myblog.domain.UserException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (request.getSession().getAttribute("userName") == null) {
+            return sendRedirect(response);
+        }
+        return true;
+    }
+
+    private boolean sendRedirect(HttpServletResponse response) {
+        try {
+            response.sendRedirect("/login");
+            return false;
+        } catch (IOException e) {
+            throw new UserException();
+        }
+    }
+}
