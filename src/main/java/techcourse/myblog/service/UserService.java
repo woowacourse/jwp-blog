d2a105d9903dcb06b8a2db98d700d89f4277a26e
yunheehyeon <ok94life@daum.net>
2019-07-18 20:26:45 +0900

    refactor : user service 분리

---
+++ src/main/java/techcourse/myblog/service/UserService.java
@@ -0,0 +1,64 @@
+package techcourse.myblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import techcourse.myblog.domain.SnsInfoRepository;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.domain.UserDto;
+import techcourse.myblog.domain.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SnsInfoRepository snsInfoRepository;
+
+    public Optional<UserDto> findUser(UserDto userDto) {
+        Optional<User> maybeUser = userRepository.findByEmailAndPassword(userDto.getEmail(), userDto.getPassword());
+        return maybeUser.map(UserDto::from);
+    }
+
+    public UserDto create(UserDto userDto) {
+        User user = userRepository.save(userDto.toEntity());
+        return UserDto.from(user);
+    }
+
+    public List<UserDto> readAll() {
+        List<UserDto> users = new ArrayList<>();
+        userRepository.findAll().forEach(user -> users.add(UserDto.from(user)));
+
+        return users;
+    }
+
+    public Optional<UserDto> readWithoutPasswordById(long id) {
+        return userRepository.findById(id).map(UserDto::fromWithoutPassword);
+    }
+
+    public Optional<UserDto> readById(long id) {
+        return userRepository.findById(id).map(UserDto::from);
+    }
+
+    public UserDto updateUser(long id, UserDto userDto) {
+        Optional<UserDto> maybeFindUserDto = readById(id);
+        if (maybeFindUserDto.isPresent()) {
+            maybeFindUserDto.get().updateUserInfo(userDto);
+
+            snsInfoRepository.deleteByUserId(id);
+            User user = userRepository.save(maybeFindUserDto.get().toEntity());
+
+            return UserDto.fromWithoutPassword(user);
+        }
+        throw new IllegalArgumentException("업데이트 할 수 없습니다.");
+    }
+
+    public void deleteById(long id) {
+        snsInfoRepository.deleteByUserId(id);
+        userRepository.deleteById(id);
+    }
+}
