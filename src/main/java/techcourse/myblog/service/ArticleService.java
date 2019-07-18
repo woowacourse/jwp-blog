f865afd19ca52e50e80ba6b6cc81fbf06e852fe0
yunheehyeon <ok94life@daum.net>
2019-07-18 19:46:37 +0900

    feat : article service 분리

---
+++ src/main/java/techcourse/myblog/service/ArticleService.java
@@ -0,0 +1,40 @@
+package techcourse.myblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleDto;
+import techcourse.myblog.domain.ArticleRepository;
+
+import java.util.Optional;
+
+@Service
+public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public long createArticle(ArticleDto articleDto) {
+        Article article = articleRepository.save(articleDto.toArticle());
+        return article.getId();
+    }
+
+    public Optional<ArticleDto> readById(long articleId) {
+        Optional<Article> maybeArticle = articleRepository.findById(articleId);
+        return maybeArticle.map(article -> ArticleDto.from(article));
+    }
+
+    public Article updateByArticle(long articleId, ArticleDto articleDto) {
+        Optional<Article> maybeArticle = articleRepository.findById(articleId);
+        if (maybeArticle.isPresent()) {
+            ArticleDto findArticleDto = ArticleDto.from(maybeArticle.get());
+            articleDto.setId(findArticleDto.getId());
+
+            return articleRepository.save(articleDto.toArticle());
+        }
+        throw new IllegalArgumentException("업데이트 할 수 없습니다.");
+    }
+
+    public void deleteById(long articleId) {
+        articleRepository.deleteById(articleId);
+    }
+}
