8db00dd3eff7a1403956ca7cfd68c081e5a8f3b3
backlo <hjun4512@naver.com>
2019-07-18 14:17:31 +0900

    fix : redirect when email repetition error occur and show alert at client

--- src/main/java/techcourse/myblog/controller/UserController.java
+++ src/main/java/techcourse/myblog/controller/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import techcourse.myblog.controller.dto.UserDTO;
+import techcourse.myblog.exception.EmailRepetitionException;
 import techcourse.myblog.service.UserService;
 
 @Controller
@@ -20,14 +22,21 @@ public class UserController {
     }
 
     @GetMapping("/signup")
-    public String signUpForm(){
+    public String signUpForm(String signUpStatus, Model model){
+        model.addAttribute("signUpStatus", signUpStatus);
         return "signup";
     }
 
     @PostMapping
-    public String create(@ModelAttribute UserDTO userDTO) {
-        userService.save(userDTO);
-        return "redirect:/users/login";
+    public String create(@ModelAttribute UserDTO userDTO, RedirectAttributes redirectAttributes) {
+        try {
+            userService.save(userDTO);
+            return "redirect:/users/login";
+        } catch (EmailRepetitionException e) {
+            redirectAttributes.addAttribute("signUpStatus", e.getMessage());
+            return "redirect:/users/signup";
+        }
+
     }
 
     @GetMapping
