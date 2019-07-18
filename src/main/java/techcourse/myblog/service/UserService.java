57615096353b9847217763a546bb502154a02d9b
gch01410 <gch01410@naver.com>
2019-07-18 17:58:54 +0900

    refactor: Add duplicated email exception

--- src/main/java/techcourse/myblog/service/UserService.java
+++ src/main/java/techcourse/myblog/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.domain.UserRepository;
 import techcourse.myblog.dto.UserRequestDto;
+import techcourse.myblog.exception.DuplicatedEmailException;
 
 import static techcourse.myblog.service.UserAssembler.convertToEntity;
 
@@ -19,6 +20,10 @@ public class UserService {
 
     public void save(UserRequestDto userRequestDto) {
         User user = convertToEntity(userRequestDto);
+        String email = user.getEmail();
+        if (userRepository.findById(email).isPresent()) {
+            throw new DuplicatedEmailException("이메일이 중복됩니다.");
+        }
         userRepository.save(user);
     }
 }
