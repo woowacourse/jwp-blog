dd5d6d99c4762f37a9d1d18bd844501b50139d58
gch01410 <gch01410@naver.com>
2019-07-18 11:06:11 +0900

    delete: Delete ArticleRepository memory version

---
+++ src/main/java/techcourse/myblog/domain/ArticleRepository2.java
@@ -0,0 +1,6 @@
+package techcourse.myblog.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository2 extends JpaRepository<Article, Integer> {
+}
