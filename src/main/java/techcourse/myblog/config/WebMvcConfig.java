3335f42ea8b38c6a5bd7106b0e7938e71dbf47a9
Latera_ <bluesky0255@gmail.com>
2019-07-18 10:36:32 +0900

    Add interceptor for logging

---
+++ src/main/java/techcourse/myblog/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package techcourse.myblog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.myblog.web.interceptor.LoggingInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public WebMvcConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+            .excludePathPatterns("/h2-console")
+            .excludePathPatterns("/css/**")
+            .excludePathPatterns("/js/**")
+            .excludePathPatterns("/images/**"); // Exclude h2 console path from interceptor
+    }
+}
