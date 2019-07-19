5a319a71ab8aa2425dff828cc9e1dc999a559dcd
hyojaekim <hj00686@naver.com>
2019-07-19 21:58:10 +0900

    [ADD] 회원가입 요청 시 이메일 중복을 확인하는 기능을 추가하였습니다.

--- src/main/java/techcourse/myblog/web/UserController.java
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -1,7 +1,6 @@
 package techcourse.myblog.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.domain.UserRepository;
@@ -19,19 +18,23 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String sighUpUser(@Valid UserDto userDto, BindingResult bindingResult, HttpSession session) {
+    public String sighUpUser(@Valid UserDto userDto, HttpSession session) {
         try {
             if (session.getAttribute("user") != null) {
                 return "redirect:/";
             }
-            if (bindingResult.hasErrors()) {
-                throw new InvalidSighUpException("올바르지 않은 입력 값 입니다.");
-            }
+            checkDuplicateEmail(userDto.getEmail());
             User user = User.of(userDto.getName(), userDto.getEmail(), userDto.getPassword());
             userRepository.save(user);
             return "redirect:/login";
         }catch (InvalidSighUpException e) {
             return "sighup";
+        }
+    }
+
+    private void checkDuplicateEmail(String email) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new InvalidSighUpException("해당 이메일은 존재하는 이메일 입니다.");
         }
     }
 }
