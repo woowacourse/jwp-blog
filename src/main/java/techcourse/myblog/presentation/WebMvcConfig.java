50f321e33d8cc5b1a0b08474594dbcff69f93ee9
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-07-19 15:47:22 +0900

    [FEATURE] Implement interceptor for session

---
+++ src/main/java/techcourse/myblog/presentation/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package techcourse.myblog.presentation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.myblog.presentation.interceptor.SessionInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor())
+                .addPathPatterns("/mypage")
+                .addPathPatterns("/mypage/edit"); //로그인 쪽은 예외처리를 한다.
+    }
+}
---
+++ src/main/java/techcourse/myblog/presentation/interceptor/SessionInterceptor.java
@@ -0,0 +1,20 @@
+package techcourse.myblog.presentation.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getSession().getAttribute("email") == null){
+            response.sendRedirect("/login");
+
+            return false;
+        }
+
+        return true;
+    }
+}
