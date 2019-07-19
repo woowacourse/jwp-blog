718e6772918ff17aadf34df5fe0835fe724b3e44
starkim06 <starkim06@naver.com>
2019-07-19 18:19:22 +0900

    refactor: ArticleRepository , 메서드 추가

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -3,4 +3,5 @@ package techcourse.myblog.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    Article findArticleByTitle(String title);
 }
