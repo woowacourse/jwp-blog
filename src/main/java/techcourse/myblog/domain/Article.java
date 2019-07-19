99102a53905deb1fd952adcae9b088ab595d6237
hyperpace <cnk9109@gmail.com>
2019-07-19 22:54:15 +0900

    Revert "[게시글 생성/조회/수정/삭제] 에이든 미션 제출합니다. (#28)"
    This reverts commit b7349040712fe8a76f400945becab6da893c4867.

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,42 +1,4 @@
 package techcourse.myblog.domain;
 
-import java.util.Objects;
-
 public class Article {
-    private String title;
-    private String contents;
-    private String coverUrl;
-
-    public Article(String title, String contents, String coverUrl) {
-        this.title = title;
-        this.contents = contents;
-        this.coverUrl = coverUrl;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public String getCoverUrl() {
-        return coverUrl;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Article)) return false;
-        Article article = (Article) o;
-        return Objects.equals(getTitle(), article.getTitle()) &&
-                Objects.equals(getContents(), article.getContents()) &&
-                Objects.equals(getCoverUrl(), article.getCoverUrl());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getTitle(), getContents(), getCoverUrl());
-    }
 }
