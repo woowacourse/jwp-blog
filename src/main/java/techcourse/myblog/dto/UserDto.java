14e1b688b0fdb5404fcb1bebe3a5e883170ec0ab
pkch93 <star_final@naver.com>
2019-07-20 00:53:41 +0900

    refactor: UserDto class
    각 컬럼들이 모두 같은 제약조건을 가지는 데
    중복으로 제약 조건을 줘야하는 상황보다 하나로 합치는 것이 낫다고 판단.
    상황별로 나눈 Dto를 하나로 합침

--- src/main/java/techcourse/myblog/dto/UserDto.java
+++ src/main/java/techcourse/myblog/dto/UserDto.java
@@ -2,27 +2,20 @@ package techcourse.myblog.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
 public class UserDto {
-    @Getter
-    @Setter
-    public static class Create {
-        private String name;
-        private String password;
-        private String email;
-    }
-
-    @Getter
-    @Setter
-    public static class Update {
-        private String name;
-        private String email;
-    }
-
-    @Getter
-    @Setter
-    public static class Login {
-        private String email;
-        private String password;
-    }
+    @Length(min = 2, max = 10)
+    @Pattern(regexp = "[a-zA-Z가-힣]{2,10}")
+    private String name;
+    @Length(min = 8)
+    @Pattern(regexp = "\\w{8,}")
+    private String password;
+    @Email
+    private String email;
 }
