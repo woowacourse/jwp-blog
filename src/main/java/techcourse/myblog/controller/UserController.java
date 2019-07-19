13cc66b977d516712328dc1110a30e3c784e6027
ddu0422 <ddu0422@naver.com>
2019-07-19 21:26:39 +0900

    feat: 회원가입 시 비밀번호 일치 확인 기능 구

--- src/main/java/techcourse/myblog/controller/UserController.java
+++ src/main/java/techcourse/myblog/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.RedirectView;
 import techcourse.myblog.UserGroups;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.dto.UserDto;
+import techcourse.myblog.service.DuplicateEmailException;
 import techcourse.myblog.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,10 +35,16 @@ public class UserController {
 
     @PostMapping("")
     public String createUser(@Validated({Default.class, UserGroups.Edit.class}) UserDto userDto, BindingResult bindingResult) {
+        try {
+            userService.save(userDto);
+        } catch (DuplicateEmailException e) {
+            bindingResult.addError(new FieldError("userDto", "email", e.getMessage()));
+            return "signup";
+        }
+
         if (bindingResult.hasErrors()) {
             return "signup";
         }
-        userService.save(userDto);
         return "login";
     }
 
