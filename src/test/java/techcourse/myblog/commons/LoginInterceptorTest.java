07aa7d234600f7e424a0b233c7339b7ee99eb4cf
DaejunBae <dpudpu11@gmail.com>
2019-07-20 00:06:59 +0900

    test: LoginInterceptor 세션 테스트 추가

---
+++ src/test/java/techcourse/myblog/commons/LoginInterceptorTest.java
@@ -0,0 +1,57 @@
+package techcourse.myblog.commons;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import techcourse.myblog.users.UserDto;
+
+@AutoConfigureWebTestClient
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LoginInterceptorTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        webTestClient = WebTestClient.bindToWebHandler(exchange -> {
+            String path = exchange.getRequest().getURI().getPath();
+            if ("/users".equals(path) || "/users/1".equals(path) || "/users/1/edit".equals(path)) {
+                return exchange.getSession()
+                        .doOnNext(webSession ->
+                                webSession.getAttributes().put("user", new UserDto.Response(1L,"asd","asd")))
+                        .then();
+            }
+            return null;
+        }).build();
+    }
+
+    @Test
+    void 로그인후_userlist_페이지_접근() {
+        webTestClient.get().uri("/users")
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @Test
+    void 로그인후_mypage_페이지_접근() {
+        webTestClient.get().uri("/users/{id}",1)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @Test
+    void 로그인후_mypage_edit_페이지_접근() {
+        webTestClient.get().uri("/users/{id}/edit",1)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+}
