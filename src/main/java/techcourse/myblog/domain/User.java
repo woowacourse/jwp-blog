6a3f1aae5957acb22679b1d23196be093830d061
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-19 22:42:58 +0900

    feat(User): add User with field, constructor

---
+++ src/main/java/techcourse/myblog/domain/User.java
@@ -0,0 +1,83 @@
+package techcourse.myblog.domain;
+
+import techcourse.myblog.web.dto.UserDto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static User of(UserDto userDto) {
+        return new User(userDto.getName(), userDto.getEmail(), userDto.getPassword());
+    }
+
+    public User update(UserDto userDto) {
+        this.name = userDto.getName();
+        return this;
+    }
+
+    public boolean equalPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, name, password);
+    }
+}
