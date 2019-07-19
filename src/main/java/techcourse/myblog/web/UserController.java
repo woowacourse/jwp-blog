c02f521d944f27641196d2896a4801e9cda0f7a6
hyojaekim <hj00686@naver.com>
2019-07-19 22:07:29 +0900

    [ADD] 회원가입 요청시 유효성 체크하는 기능을 추가하였습니다.
    - 중복되는 Email을 입력하는 경우 예외를 던집니다.
    - 두 개의 비밀번호가 같지 않은 경우 예외를 던집니다.

--- src/main/java/techcourse/myblog/web/UserController.java
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -1,6 +1,8 @@
 package techcourse.myblog.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.domain.UserRepository;
@@ -18,22 +20,38 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String sighUpUser(@Valid UserDto userDto, HttpSession session) {
+    public String sighUpUser(@Valid UserDto userDto, BindingResult bindingResult, Model model, HttpSession session) {
         try {
             if (session.getAttribute("user") != null) {
                 return "redirect:/";
             }
-            checkDuplicateEmail(userDto.getEmail());
+            checkUserDataError(bindingResult);
+            checkSamePasswords(userDto);
+            checkDuplicateEmail(userDto);
             User user = User.of(userDto.getName(), userDto.getEmail(), userDto.getPassword());
             userRepository.save(user);
             return "redirect:/login";
         }catch (InvalidSighUpException e) {
+            model.addAttribute("error", true);
+            model.addAttribute("message", e.getMessage());
             return "sighup";
         }
     }
 
-    private void checkDuplicateEmail(String email) {
-        if (userRepository.findByEmail(email).isPresent()) {
+    private void checkUserDataError(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidSighUpException("올바르지 않은 입력 값 입니다.");
+        }
+    }
+
+    private void checkSamePasswords(UserDto userData) {
+        if (!userData.getPassword().equals(userData.getPasswordConfirm())) {
+            throw new InvalidSighUpException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    private void checkDuplicateEmail(UserDto userData) {
+        if (userRepository.findByEmail(userData.getEmail()).isPresent()) {
             throw new InvalidSighUpException("해당 이메일은 존재하는 이메일 입니다.");
         }
     }
