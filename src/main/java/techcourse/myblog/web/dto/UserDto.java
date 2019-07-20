ef5694a4726ae263539046890f9d6ff6578feeff
hyojaekim <hj00686@naver.com>
2019-07-20 12:44:52 +0900

    [REFACTOR] 유효성 검사를 위한 패턴을 변경하고 리팩토링 하였습니다.
    - 패턴, 사이즈, 오류 메세지를 상수화 하였습니다.

--- src/main/java/techcourse/myblog/web/dto/UserDto.java
+++ src/main/java/techcourse/myblog/web/dto/UserDto.java
@@ -1,26 +1,30 @@
 package techcourse.myblog.web.dto;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class UserDto {
-    private static final int MIN_USER_NAME_SIZE = 2;
-    private static final int MAX_USER_NAME_SIZE = 10;
-    private static final String USER_NAME_PATTERN = "^[a-zA-Zㄱ-ㅎ가-힣]*$";
-    private static final String USER_PASSWORD_PATTERN = "(?=^.{8,}$)((?=.*\\\\d)(?=.*\\\\W+))(?![.\\\\n])(?=.*[A-Z])(?=.*[a-z]).*$";
+    private static final int MIN_NAME_SIZE = 2;
+    private static final int MAX_NAME_SIZE = 10;
+    private static final String NAME_PATTERN = "^[가-힣a-zA-Z]+$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,}$";
+    private static final String ERROR_MESSAGE_ABOUT_NAME_SIZE = "이름은 2글자 이상 10글자 이하이어야 합니다.";
+    private static final String ERROR_MESSAGE_ABOUT_NAME_PATTERN = "이름은 한글 또는 영어만 입력이 가능합니다.";
+    private static final String ERROR_MESSAGE_ABOUT_EMAIL = "이메일 형식이 아닙니다.";
+    private static final String ERROR_MESSAGE_ABOUT_PASSWORD_PATTERN = "비밀번호는 8글자 이상으로 특수문자, 숫자, 소문자, 대문자가 포함되어야 합니다.";
 
-    @Min(value = MIN_USER_NAME_SIZE)
-    @Max(value = MAX_USER_NAME_SIZE)
-    @Pattern(regexp = USER_NAME_PATTERN)
+    @Size(min = MIN_NAME_SIZE, max = MAX_NAME_SIZE, message = ERROR_MESSAGE_ABOUT_NAME_SIZE)
+    @Pattern(regexp = NAME_PATTERN, message = ERROR_MESSAGE_ABOUT_NAME_PATTERN)
     private String name;
 
-    @Email
+    @Email(message = ERROR_MESSAGE_ABOUT_EMAIL)
     private String email;
 
-    @Pattern(regexp = USER_PASSWORD_PATTERN)
+    @Pattern(regexp = PASSWORD_PATTERN, message = ERROR_MESSAGE_ABOUT_PASSWORD_PATTERN)
     private String password;
+
+    @Pattern(regexp = PASSWORD_PATTERN, message = ERROR_MESSAGE_ABOUT_PASSWORD_PATTERN)
     private String passwordConfirm;
 
     public String getName() {
