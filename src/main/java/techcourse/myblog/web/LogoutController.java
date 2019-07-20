c19e141145d4b7249809643284798680da06149b
yejilee <yjll1019@naver.com>
2019-07-20 12:57:12 +0900

    refactor: 로그아웃 시 세션 처리 방법 수정

--- src/main/java/techcourse/myblog/web/LogoutController.java
+++ src/main/java/techcourse/myblog/web/LogoutController.java
@@ -1,14 +1,15 @@
 package techcourse.myblog.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 public class LogoutController {
 	@GetMapping("/logout")
-	public String logout(SessionStatus sessionStatus) {
-		sessionStatus.setComplete();
+	public String logout(HttpSession httpSession) {
+		httpSession.invalidate();
 		return "redirect:/";
 	}
 }
