46eb0810416255e00b252130dd8b968cc306ce7f
hyojaekim <hj00686@naver.com>
2019-07-20 21:38:09 +0900

    [ADD] 로그인 기능을 추가하였습니다.
    - 존재하지 않는 이메일 입력 시 에러 메세지를 띄웁니다.
    - 비밀번호가 맞지 않는 경우 에러 메세지를 띄웁니다.

---
+++ src/main/java/techcourse/myblog/service/LoginService.java
@@ -0,0 +1,42 @@
+package techcourse.myblog.service;
+
+import org.springframework.stereotype.Service;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.dto.LoginDto;
+import techcourse.myblog.repository.UserRepository;
+import techcourse.myblog.service.exception.NotFindUserEmailException;
+import techcourse.myblog.service.exception.UnequalPasswordException;
+
+import java.util.Optional;
+
+@Service
+public class LoginService {
+    private final UserRepository userRepository;
+
+    public LoginService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findUser(LoginDto loginDto) {
+        Optional<User> findUser = userRepository.findByEmail(loginDto.getEmail());
+
+        checkExistEmail(findUser);
+        checkEqualPassword(findUser, loginDto);
+
+        return findUser.get();
+    }
+
+    private void checkExistEmail(Optional<User> findUser) {
+        if (!findUser.isPresent()) {
+            throw new NotFindUserEmailException();
+        }
+    }
+
+    private void checkEqualPassword(Optional<User> findUser, LoginDto loginDto) {
+        String userPassword = findUser.get().getPassword();
+
+        if (!userPassword.equals(loginDto.getPassword())) {
+            throw new UnequalPasswordException();
+        }
+    }
+}
