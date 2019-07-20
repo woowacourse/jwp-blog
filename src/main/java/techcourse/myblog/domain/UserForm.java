44af63069c77957cba3d3d8ed80206fed7dec83e
heebong <heebg2@naver.com>
2019-07-20 23:36:36 +0900

    feat: 회원가입 페이지에서 비밀번호 확인 기능 추가

--- src/main/java/techcourse/myblog/domain/UserForm.java
+++ src/main/java/techcourse/myblog/domain/UserForm.java
@@ -14,7 +14,6 @@ public class UserForm {
     @Pattern(regexp = "^[a-zA-Z가-힣]{2,10}$", message = "2글자 이상, 10글자 이하로 입력하세요. 숫자와 특수문자는 입력할 수 없습니다.")
     private String name;
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~`!@#$%\\^&*()-]).{8,}$", message = "소문자, 대문자, 숫자, 특수문자가 조합된 8글자 이상을 입력하세요 ")
-    @ColumnTransformer(write = "SHA2(?,224)")
     private String password;
     @Email(message = "Wrong Email")
     @Column(unique=true)
