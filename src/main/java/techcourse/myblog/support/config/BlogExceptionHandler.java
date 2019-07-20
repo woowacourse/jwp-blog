ddc9caa80684c06e32a9038da31dfe073713d6a2
CODEMCD <psbum1609@gmail.com>
2019-07-20 15:35:57 +0900

    refactor: 클래스 이름 변경

--- src/main/java/techcourse/myblog/support/config/BlogExceptionHandler.java
+++ src/main/java/techcourse/myblog/support/config/BlogExceptionHandler.java
@@ -2,28 +2,29 @@ package techcourse.myblog.support.config;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import techcourse.myblog.service.exception.EditException;
 import techcourse.myblog.service.exception.ErrorMessage;
 import techcourse.myblog.service.exception.LoginException;
 import techcourse.myblog.service.exception.SignUpException;
 
 @ControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(LoginException.class)
+public class BlogExceptionHandler {
+    @ExceptionHandler(LoginException.class)
     public String handleLoginException(LoginException e, Model model) {
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         model.addAttribute("error", errorMessage);
         return "login";
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(SignUpException.class)
+    @ExceptionHandler(SignUpException.class)
     public String handleSignUpException(SignUpException e, Model model) {
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         model.addAttribute("error", errorMessage);
         return "signup";
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(EditException.class)
+    @ExceptionHandler(EditException.class)
     public String handleEditException(EditException e, Model model) {
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         model.addAttribute("error", errorMessage);
