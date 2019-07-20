f4068809c4a81c0b40bf094c1162f76d3dd04631
wbluke <pwb0127@naver.com>
2019-07-20 15:25:27 +0900

    [ADD] '/login' 이동 기능 추가

---
+++ src/main/java/techcourse/myblog/web/UserController.java
@@ -0,0 +1,13 @@
+package techcourse.myblog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
---
+++ src/test/java/techcourse/myblog/web/UserControllerTests.java
@@ -0,0 +1,24 @@
+package techcourse.myblog.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class UserControllerTests {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void login() {
+        webTestClient.get().uri("/login")
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
