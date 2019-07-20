ee00f7797ab51ae05196f7edc2353dc5df7cc2f4
DaejunBae <dpudpu11@gmail.com>
2019-07-20 17:59:49 +0900

    test: UserServiceTest

--- src/main/java/techcourse/myblog/users/ValidSingupException.java
+++ src/main/java/techcourse/myblog/users/ValidSingupException.java
@@ -1,14 +1,19 @@
 package techcourse.myblog.users;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ValidSingupException extends RuntimeException {
+    private static final Logger log = LoggerFactory.getLogger(ValidSingupException.class);
+
     private Error[] errors;
 
     public ValidSingupException(String defaultMessage, String field) {
         this.errors = new Error[]{new Error(defaultMessage, field)};
+        log.debug("field : {}, message : {}", field, defaultMessage);
     }
 
     public ValidSingupException(Error[] errors) {
