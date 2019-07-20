284e1d48a8191102d3cd571f1c35d597c948bb73
hyun <jhs695@naver.com>
2019-07-20 23:59:55 +0900

    refactor

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,12 +1,55 @@
 package techcourse.myblog.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Article {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String title;
     private String coverUrl;
     private String contents;
+
+    private Article() {
+
+    }
+
+    public static class ArticleBuilder {
+        private String title;
+        private String coverUrl;
+        private String contents;
+
+        public ArticleBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public ArticleBuilder coverUrl(String coverUrl) {
+            this.coverUrl = coverUrl;
+            return this;
+        }
+
+        public ArticleBuilder contents(String contents) {
+            this.contents = contents;
+            return this;
+        }
+
+        public Article build() {
+            return new Article(this);
+        }
+    }
+
+    public Article(ArticleBuilder articleBuilder) {
+        this.title = articleBuilder.title;
+        this.coverUrl = articleBuilder.coverUrl;
+        this.contents = articleBuilder.contents;
+    }
 
     public Article(String title, String coverUrl, String contents) {
         this.title = title;
@@ -14,21 +57,19 @@ public class Article {
         this.contents = contents;
     }
 
-    public boolean checkId(int id) {
-        return this.id == id;
+    public void changeTitle(String title) {
+        this.title = title;
     }
 
-    public void update(Article article) {
-        this.title = article.title;
-        this.coverUrl = article.coverUrl;
-        this.contents = article.contents;
+    public void changeCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void changeContents(String contents) {
+        this.contents = contents;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
@@ -45,16 +86,6 @@ public class Article {
     }
 
     @Override
-    public String toString() {
-        return "Article{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", coverUrl='" + coverUrl + '\'' +
-                ", contents='" + contents + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -68,5 +99,15 @@ public class Article {
     @Override
     public int hashCode() {
         return Objects.hash(id, title, coverUrl, contents);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", coverUrl='" + coverUrl + '\'' +
+                ", contents='" + contents + '\'' +
+                '}';
     }
 }
--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -1,33 +1,7 @@
 package techcourse.myblog.domain;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 
-@Repository
-public class ArticleRepository {
-    private List<Article> articles = new ArrayList<>();
-    private int id = 0;
-
-    public List<Article> findAll() {
-        return new ArrayList<>(articles);
-    }
-
-    public void save(Article article) {
-        id = id + 1;
-        article.setId(id);
-        articles.add(article);
-    }
-
-    public Article findArticleById(int id) {
-        return articles.stream()
-                .filter(x -> x.checkId(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("해당하는 ID의 기사가 없습니다."));
-    }
-
-    public void removeById(int id) {
-        articles.remove(findArticleById(id));
-    }
 }
