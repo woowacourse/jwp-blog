39da7940c8586392c755676f0611719c336ce63b
pkch93 <star_final@naver.com>
2019-07-20 09:05:19 +0900

    test: UserController class
    login 정상 흐름 테스트 추가

--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -7,6 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
     @Autowired
@@ -14,38 +18,62 @@ class UserControllerTest {
 
     @Test
     void 회원가입_페이지_접근_테스트() {
-        webTestClient.get().uri("/signup")
-                            .exchange()
-                            .expectStatus()
-                            .isOk();
+        webTestClient.get().uri("/auth/signup")
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 
     @Test
     void 회원_등록_테스트() {
-        webTestClient.post().uri("/users")
-                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                            .body(
-                                    BodyInserters.fromFormData("name", "donut")
-                                                .with("email", "donut@woowa.com")
-                                                .with("password", "qwer1234")
-                            ).exchange()
-                            .expectStatus()
-                            .is3xxRedirection();
+        회원_등록().expectStatus()
+                .is3xxRedirection();
+    }
+
+    private WebTestClient.ResponseSpec 회원_등록() {
+        return webTestClient.post().uri("/auth/users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(
+                        BodyInserters.fromFormData("name", "pkch")
+                                .with("email", "pkch@woowa.com")
+                                .with("password", "qwerqwer")
+                ).exchange();
     }
 
     @Test
     void 로그인_페이지_접근_테스트() {
-        webTestClient.get().uri("/login")
-                            .exchange()
-                            .expectStatus()
-                            .isOk();
+        webTestClient.get().uri("/auth/login")
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 
     @Test
-    void 회원_목록_페이지_접근_테스트() {
-        webTestClient.get().uri("/users")
-                            .exchange()
-                            .expectStatus()
-                            .isOk();
+    void 로그인_안된_경우_회원_목록_페이지_접근_테스트() {
+        webTestClient.get().uri("/auth/users")
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody()
+                .consumeWith(res -> {
+                    URI redirectUri = res.getResponseHeaders().getLocation();
+                    assertThat(redirectUri.getPath()).isEqualTo("/auth/login");
+                });
+    }
+
+    @Test
+    void 로그인_정상_흐름_테스트() {
+        회원_등록().expectStatus().is3xxRedirection();
+        webTestClient.post().uri("/auth/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(
+                        BodyInserters.fromFormData("email", "pkch@woowa.com")
+                                .with("password", "qwerqwer")
+                ).exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody()
+                .consumeWith(redirectRes -> {
+                    URI redirectUri = redirectRes.getResponseHeaders().getLocation();
+                    assertThat(redirectUri.getPath()).isEqualTo(URI.create("/"));
+                });
     }
 }
