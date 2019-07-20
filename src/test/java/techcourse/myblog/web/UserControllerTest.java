5caa1b5010cd1ebf0c0a7409d76b96521e827b43
hyojaekim <hj00686@naver.com>
2019-07-20 22:26:48 +0900

    [TEST] 메소드 분리 및 실패하는 테스트를 추가하였습니다.

--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -31,30 +31,57 @@ class UserControllerTest {
         userDto.setPasswordConfirm("PassW0rd@");
     }
 
-    private StatusAssertions postUser(UserDto userDto) {
-        return webTestClient.post()
-                .uri("/users")
-                .body(BodyInserters.fromFormData("name", userDto.getName())
-                        .with("email", userDto.getEmail())
-                        .with("password", userDto.getPassword())
-                        .with("passwordConfirm", userDto.getPasswordConfirm()))
-                .exchange()
-                .expectStatus();
-    }
-
     @Test
-    @DisplayName("회원 가입 요청시 로그인 페이지로 이동한다.")
+    @DisplayName("회원 가입 후 로그인 페이지로 이동한다.")
     void signUpTest() {
+        userDto.setName("signup");
+        userDto.setEmail("new@test.com");
+        userDto.setPassword("PassW0rd@");
+        userDto.setPasswordConfirm("PassW0rd@");
+
         postUser(userDto).is3xxRedirection();
     }
 
     @Test
+    @DisplayName("회원 가입 요청시 잘못된 이름 패턴으로 인해 실패한다.")
+    void notSignUpNameTest() {
+        userDto.setName("test1");
+        userDto.setEmail("test@test.com");
+        userDto.setPassword("PassW0rd@");
+        userDto.setPasswordConfirm("PassW0rd@");
+
+        postUser(userDto).isOk();
+    }
+
+    @Test
+    @DisplayName("회원 가입 요청시 잘못된 이메일 패턴으로 인해 실패한다.")
+    void notSignUpByEmailTest() {
+        userDto.setName("test");
+        userDto.setEmail("test.com");
+        userDto.setPassword("PassW0rd@");
+        userDto.setPasswordConfirm("PassW0rd@");
+
+        postUser(userDto).isOk();
+    }
+
+    @Test
     @DisplayName("회원 가입 요청시 잘못된 패스워드 패턴으로 인해 실패한다.")
-    void notSignUpTest() {
+    void notSignUpByPasswordTest() {
         userDto.setName("test");
         userDto.setEmail("test@test.com");
         userDto.setPassword("PassWord");
         userDto.setPasswordConfirm("PassWord");
+
+        postUser(userDto).isOk();
+    }
+
+    @Test
+    @DisplayName("회원 가입 요청시 두개의 패스워드가 다른 이유로 인해 실패한다.")
+    void notSignUpByUnequalPasswordTest() {
+        userDto.setName("test");
+        userDto.setEmail("test@test.com");
+        userDto.setPassword("PassW0rd@");
+        userDto.setPasswordConfirm("PassWord@");
 
         postUser(userDto).isOk();
     }
@@ -66,17 +93,28 @@ class UserControllerTest {
         userDto.setEmail("user@test.com");
         userDto.setPassword("PassW0rd@");
         userDto.setPasswordConfirm("PassW0rd@");
+
         postUser(userDto).is3xxRedirection()
-                .expectBody()
-                .consumeWith(postResponse -> {
-                   webTestClient.get().uri("/users")
-                           .exchange()
-                           .expectStatus().isOk()
-                           .expectBody().consumeWith(result -> {
-                      assertThat(new String(result.getResponseBody()))
-                              .contains(userDto.getName())
-                              .contains(userDto.getEmail());
-                   });
-                });
+                .expectBody().consumeWith(postUserResponse ->
+                webTestClient.get()
+                        .uri("/users")
+                        .exchange()
+                        .expectStatus().isOk()
+                        .expectBody()
+                        .consumeWith(result -> assertThat(new String(result.getResponseBody()))
+                                .contains(userDto.getName())
+                                .contains(userDto.getEmail())
+                        ));
+    }
+
+    private StatusAssertions postUser(UserDto userDto) {
+        return webTestClient.post()
+                .uri("/users")
+                .body(BodyInserters.fromFormData("name", userDto.getName())
+                        .with("email", userDto.getEmail())
+                        .with("password", userDto.getPassword())
+                        .with("passwordConfirm", userDto.getPasswordConfirm()))
+                .exchange()
+                .expectStatus();
     }
 }
