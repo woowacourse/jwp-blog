e4d8f6db61f6e6ae2a20856d302c0b234bb75b66
Seongmo Jeon <oeeen3@gmail.com>
2019-07-20 17:54:53 +0900

    refactor: re-locate package

--- src/main/java/techcourse/myblog/config/AuthenticationConfig.java
+++ src/main/java/techcourse/myblog/config/AuthenticationConfig.java
@@ -1,4 +1,4 @@
-package techcourse.myblog.web;
+package techcourse.myblog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
