62767cb2b4017aa114305265c136385a8e2b8e31
ddu0422 <ddu0422@naver.com>
2019-07-20 15:55:15 +0900

    feat: Interceptor 기능 구현

---
+++ src/main/java/techcourse/myblog/support/WebConfig.java
@@ -0,0 +1,22 @@
+package techcourse.myblog.support;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.myblog.controller.CertificationInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final CertificationInterceptor certificationInterceptor;
+
+    public WebConfig(CertificationInterceptor certificationInterceptor) {
+        this.certificationInterceptor = certificationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(certificationInterceptor)
+                .addPathPatterns("/mypage/**")
+                .addPathPatterns("/logout");
+    }
+}
