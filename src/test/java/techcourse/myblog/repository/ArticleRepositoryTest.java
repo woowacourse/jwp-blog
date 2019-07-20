82efb65bab9db12f9716315b69162a399a338780
hyojaekim <hj00686@naver.com>
2019-07-20 22:07:43 +0900

    [REFACTOR] ArticleRepository 패키지를 이동하였습니다.

--- src/test/java/techcourse/myblog/repository/ArticleRepositoryTest.java
+++ src/test/java/techcourse/myblog/repository/ArticleRepositoryTest.java
@@ -1,4 +1,4 @@
-package techcourse.myblog.domain;
+package techcourse.myblog.repository;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import techcourse.myblog.repository.ArticleRepository;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.dto.ArticleDto;
 import techcourse.myblog.service.exception.CouldNotFindArticleIdException;
 
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ArticleRepositoryTest {
