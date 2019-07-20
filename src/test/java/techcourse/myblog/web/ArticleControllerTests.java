bd02710d1049aba84dadec41ced1ac83a324e891
hyun <jhs695@naver.com>
2019-07-20 21:38:29 +0900

    Revert "[게시글 생성/조회/수정/삭제] 지노 미션 제출합니다. (#45)"
    This reverts commit 72e0dc065eb968f0299090f558f2cc6859792626.

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -6,15 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
-
     @Autowired
     private WebTestClient webTestClient;
-    private WebTestClient.ResponseSpec responseSpec;
 
     @Test
     void index() {
@@ -24,47 +21,10 @@ public class ArticleControllerTests {
     }
 
     @Test
-    void writeForm_test() {
-        webTestClient.get().uri("/writing")
+    void articleForm() {
+        webTestClient.get().uri("/articles/new")
                 .exchange()
                 .expectStatus().isOk();
     }
 
-    @Test
-    void save_test() {
-        insertArticle();
-
-        responseSpec
-                .expectStatus()
-                .isOk();
-    }
-
-    @Test
-    void update_test() {
-        insertArticle();
-
-        webTestClient.put().uri("/articles/1")
-                .body(BodyInserters.fromFormData("title", "title")
-                        .with("coverUrl", "coverUrl").with("contents", "contents"))
-                .exchange()
-                .expectHeader().valueMatches("Location","http://localhost:[0-9]+/$")
-                .expectStatus().is3xxRedirection();
-    }
-
-    @Test
-    void delete_test() {
-        insertArticle();
-
-        webTestClient.delete().uri("/articles/1")
-                .exchange()
-                .expectStatus()
-                .is3xxRedirection();
-    }
-
-    private void insertArticle() {
-        responseSpec = webTestClient.post().uri("/articles")
-                .body(BodyInserters.fromFormData("title", "title")
-                        .with("coverUrl", "coverUrl").with("contents", "contents"))
-                .exchange();
-    }
 }
