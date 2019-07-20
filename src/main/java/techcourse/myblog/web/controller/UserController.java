727da41f5fff3928093838f95f47281b62368c9d
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-20 20:18:39 +0900

    style(UserController): remove unnecessary code

--- src/main/java/techcourse/myblog/web/controller/UserController.java
+++ src/main/java/techcourse/myblog/web/controller/UserController.java
@@ -1,7 +1,5 @@
 package techcourse.myblog.web.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
