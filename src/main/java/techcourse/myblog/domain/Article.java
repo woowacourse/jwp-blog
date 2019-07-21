2dce0cb1e4c01f5bae9081e3b217b13db0bf97b5
eunsukko <47235893+eunsukko@users.noreply.github.com>
2019-07-21 14:13:29 +0900

    [게시글 생성/조회/수정/삭제] 미스터코 미션 제출합니다. (#44)
    * [DOCS] Add README.md
    
    * [ADD] Add dependencies
    
    * [ADD] Add properties
    
    * [MOVE] Move views to templates directory
    
    * [ADD] Add properties
    
    * [FEATURE] Implement redirection to article writing page
    
    * [FEATURE] Implement methods for read and create article
    
    * [FEATURE] Implement methods for read article edit page
    
    * [FEATURE] Implement methods for update article
    
    * [FEATURE] implement methods for delete article
    
    * [FEATURE] Implement methods for read all articles
    
    * [FEATURE] Apply template to article page
    
    * [REFACTOR] adjust put method to article edit page
    
    - 메소드 타입에 따른 해당 메소드 뷰 적용
    
    * [REFACTOR] replace response from rendering to redirection
    
    * [ADD] Update css and html for bug fix
    
    * [FEATURE] Apply delete specific article
    
    * [REFACTOR] management role from Article to ArticleRepository
    
    * [REFACTOR] update for redirection tests
    
    * [REFACTOR] remove duplicated html
    
    * [REMOVE] unnecessary class and test
    
    * [REFACTOR] Replace AutoWired to Constructor DI
    
    * [REFACTOR] Fix ArticleRepository for concurrent access
    
    * [REFACTOR] Use ArticleController through Dto
    
    * [REFACTOR] remove duplicated code in ArticleControllerTests
    
    * [REFACTOR] rename one of Article.class constructor
    
    * [RAFACTOR] remove repository from ArticleControllerTests
    
    * [REFACTOR] gather duplicated part from article related html

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,4 +1,75 @@
 package techcourse.myblog.domain;
 
+import java.util.Objects;
+
 public class Article {
+    private static final int EMPTY_ID = -1;
+
+    private final int id;
+    private final String title;
+    private final String coverUrl;
+    private final String contents;
+
+    public Article(int id, String title, String coverUrl, String contents) {
+        this.id = id;
+        this.title = title;
+        this.coverUrl = coverUrl;
+        this.contents = contents;
+    }
+
+    public static Article createWithoutId(String title, String coverUrl, String contents) {
+        return new Article(EMPTY_ID, title, coverUrl, contents);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id == article.id &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(coverUrl, article.coverUrl) &&
+                Objects.equals(contents, article.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, coverUrl, contents);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", coverUrl='" + coverUrl + '\'' +
+                ", contents='" + contents + '\'' +
+                '}';
+    }
+
+    public Article replaceId(int id) {
+        return new Article(id, title, coverUrl, contents);
+    }
+
+    public boolean isEqualToExceptId(Article article) {
+        return Objects.equals(title, article.title) &&
+                Objects.equals(coverUrl, article.coverUrl) &&
+                Objects.equals(contents, article.contents);
+    }
 }
