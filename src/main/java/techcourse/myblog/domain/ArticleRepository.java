91fd3988d6a2a14ce091fb36758ccd5b4e72d394
ddu0422 <ddu0422@naver.com>
2019-07-21 17:12:53 +0900

    refactor: AricleController 테스트 수정

--- src/main/java/techcourse/myblog/domain/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/ArticleRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 import techcourse.myblog.service.dto.ArticleDto;
 
+import java.util.Optional;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     @Transactional
-    default void update(Long id, ArticleDto articleDto) {
-        Article article = findById(id).get();
-        article.update(articleDto.toArticle());
+    default Optional<Article> update(Long id, ArticleDto articleDto) {
+        Optional<Article> articleOptional = findById(id);
+        articleOptional.ifPresent(article -> article.update(articleDto.toArticle()));
+        return findById(id);
     }
 }
--- src/test/java/techcourse/myblog/controller/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/controller/ArticleControllerTests.java
@@ -1,81 +1,57 @@
-//package techcourse.myblog.controller;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.reactive.server.WebTestClient;
-//
-//import static org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
-//
-//@AutoConfigureWebTestClient
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class ArticleControllerTests {
-//    private static final int ARTICLE_ID = 1;
-//    private static final String LOCATION = "location";
-//
-//    @Autowired
-//    private WebTestClient webTestClient;
-//
-//    @BeforeEach
-//    void 게시글_작성_확인() {
-//        checkResponseHeaderLocation(HttpMethod.POST, "/articles", HttpStatus.FOUND,
-//                LOCATION, ".*articles.*");
-//    }
-//
-//    @Test
-//    void index_페이지_조회() {
-//        checkResponseStatus(HttpMethod.GET, "/", HttpStatus.OK);
-//    }
-//
-//    @Test
-//    void 게시글_작성_페이지_확인() {
-//        checkResponseStatus(HttpMethod.GET, "/writing", HttpStatus.OK);
-//    }
-//
-//    @Test
-//    void 게시글_조회() {
-//        checkResponseStatus(HttpMethod.GET, "/articles/" + ARTICLE_ID, HttpStatus.OK);
-//    }
-//
-//    @Test
-//    void 게시글_수정_페이지_확인() {
-//        checkResponseStatus(HttpMethod.GET, "/articles/" + ARTICLE_ID + "/update", HttpStatus.OK);
-//    }
-//
-//    @Test
-//    void 게시글_수정_확인() {
-//        checkResponseHeaderLocation(HttpMethod.PUT, "/articles/" + ARTICLE_ID, HttpStatus.FOUND,
-//                LOCATION, ".*articles.*");
-//    }
-//
-//    @AfterEach
-//    void 게시글_삭제_확인() {
-//        checkResponseHeaderLocation(HttpMethod.DELETE, "/articles/" + ARTICLE_ID, HttpStatus.FOUND,
-//                LOCATION, ".*/");
-//    }
-//
-//    private ResponseSpec check(HttpMethod httpMethod, String uri) {
-//        return webTestClient.method(httpMethod)
-//                .uri(uri)
-//                .exchange();
-//    }
-//
-//    private ResponseSpec checkResponseStatus(HttpMethod httpMethod, String uri, HttpStatus httpStatus) {
-//        return check(httpMethod, uri)
-//                .expectStatus().isEqualTo(httpStatus);
-//    }
-//
-//    private ResponseSpec checkResponseHeaderLocation(HttpMethod httpMethod, String uri,
-//                                                     HttpStatus httpStatus, String name, String pattern) {
-//        return checkResponseStatus(httpMethod, uri, httpStatus)
-//                .expectHeader().valueMatches(name, pattern);
-//    }
-//}
+package techcourse.myblog.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import techcourse.myblog.controller.template.ControllerTestTemplate;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleRepository;
+import techcourse.myblog.service.dto.ArticleDto;
+
+public class ArticleControllerTests extends ControllerTestTemplate {
+    @Autowired
+    private ArticleRepository articleRepository;
+    private Article article;
+
+    @BeforeEach
+    void 게시글_작성_확인() {
+        article = new ArticleDto("1", "https://www.naver.com", "2").toArticle();
+        articleRepository.save(article);
+        checkStatusAndHeaderLocation(response(HttpMethod.POST, "/articles"), HttpStatus.FOUND, ".*articles.*");
+    }
+
+    @Test
+    void index_페이지_조회() {
+        checkStatus(HttpMethod.GET, "/", HttpStatus.OK);
+    }
+
+    @Test
+    void 게시글_작성_페이지_확인() {
+        checkStatus(HttpMethod.GET, "/writing", HttpStatus.OK);
+    }
+
+    @Test
+    void 게시글_조회() {
+        checkStatus(HttpMethod.GET, "/articles/" + article.getId(), HttpStatus.OK);
+    }
+
+    @Test
+    void 게시글_수정_페이지_확인() {
+        checkStatus(HttpMethod.GET, "/articles/" + article.getId() + "/edit", HttpStatus.OK);
+    }
+
+    @Test
+    void 게시글_수정_확인() {
+        checkStatusAndHeaderLocation(response(HttpMethod.PUT, "/articles/" + article.getId()),
+                HttpStatus.FOUND,".*articles.*");
+    }
+
+    @AfterEach
+    void 게시글_삭제_확인() {
+        checkStatusAndHeaderLocation(response(HttpMethod.DELETE, "/articles/" + article.getId())
+                , HttpStatus.FOUND, ".*/");
+    }
+}
