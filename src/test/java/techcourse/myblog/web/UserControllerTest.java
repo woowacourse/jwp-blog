2f8521c08063aa426e77f877618d71b3d5a4e296
박효진 <hgenie14@gmail.com>
2019-07-21 16:34:18 +0900

    test: UserController에 대한 테스트 추가

--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -6,24 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.function.Consumer;
+
+// TODO 회원가입을 테스트해야 하는데 메서드가 무작위로 실행돼서 회원이 이미 등록된 상태에서 실행되는 문제는 어떡한담
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
+    private static final String CUSTOM_USER_ID = "1";
+    private static final String VALID_NAME = "유효한이름";
+    private static final String VALID_EMAIL = "valid@email.com";
+    private static final String VALID_PASSWORD = "ValidPassword!123";
+
+    private BodyInserters.FormInserter<String> validUserData = getBodyInserters(VALID_NAME, VALID_EMAIL, VALID_PASSWORD);
+
     @Autowired
     private WebTestClient webTestClient;
-
-    private String VALID_NAME = "유효한이름";
-    private String VALID_EMAIL = "valid@email.com";
-    private String VALID_PASSWORD = "ValidPassword!123";
-
-    private String INVALID_NAME = "1nva1id";
-    private String INVALID_EMAIL = "invalidemail";
-    private String INVALID_PASSWORD = "invalidpw";
 
     @Test
     public void 회원가입_페이지_테스트() {
@@ -33,91 +35,156 @@ class UserControllerTest {
     }
 
     @Test
-    public void 회원가입_테스트() {
-        webTestClient.post()
-                .uri("/users")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", VALID_NAME)
-                        .with("email", VALID_EMAIL)
-                        .with("password", VALID_PASSWORD))
-                .exchange()
-                .expectStatus().isFound()
-                .expectBody()
-                .consumeWith(response -> {
-                    webTestClient.get()
-                            .uri(response.getResponseHeaders().getLocation())
-                            .exchange()
-                            .expectStatus()
-                            .isOk();
-                });
+    public void 유효한_정보로_회원가입_하는_경우_테스트() {
+        postSignup(validUserData, response ->
+                webTestClient.get()
+                        .uri(response.getResponseHeaders().getLocation())
+                        .exchange()
+                        .expectStatus()
+                        .isOk()
+        );
     }
 
     @Test
     public void 유효햐지_않은_이름으로_회원가입_하는_경우_예외처리() {
+        String invalidName = "1nva1id";
+
         webTestClient.post()
                 .uri("/users")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", INVALID_NAME)
-                        .with("email", VALID_EMAIL)
-                        .with("password", VALID_PASSWORD))
+                .body(getBodyInserters(invalidName, VALID_EMAIL, VALID_PASSWORD))
                 .exchange()
                 .expectStatus().isBadRequest();
     }
 
     @Test
     public void 유효하지_않은_이메일로_회원가입_하는_경우_예외처리() {
+        String invalidEmail = "invalidemail";
+
         webTestClient.post()
                 .uri("/users")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", VALID_NAME)
-                        .with("email", INVALID_EMAIL)
-                        .with("password", VALID_PASSWORD))
+                .body(getBodyInserters(VALID_NAME, invalidEmail, VALID_PASSWORD))
                 .exchange()
                 .expectStatus().isBadRequest();
     }
 
     @Test
     public void 유효하지_않은_비밀번호로_회원가입_하는_경우_예외처리() {
+        String invalidPassword = "invalidpw";
+
         webTestClient.post()
                 .uri("/users")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", VALID_NAME)
-                        .with("email", VALID_EMAIL)
-                        .with("password", INVALID_PASSWORD))
+                .body(getBodyInserters(VALID_NAME, VALID_EMAIL, invalidPassword))
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+//    @Test
+//    public void 회원수정_페이지가_잘_출력되는지_테스트() {
+//        /** 회원가입 **/
+//        postSignup(validUserData, response ->
+//                /** TODO 로그인 **/
+//                /** 회원수정 **/
+//                webTestClient.get()
+//                        .uri("/mypage/edit")
+//                        .exchange()
+//                        .expectStatus().isOk()
+//                        .expectBody()
+//                        .consumeWith(res -> {
+//                            String body = new String(res.getResponseBody());
+//                            assertThat(body.contains(VALID_NAME)).isTrue();
+//                            assertThat(body.contains(VALID_EMAIL)).isTrue();
+//                        })
+//        );
+//    }
+
+    @Test
+    public void 로그인이_안된_상태에서_회원수정_페이지에_접속하는_경우_예외처리() {
+        webTestClient.get()
+                .uri("/mypage/edit")
                 .exchange()
                 .expectStatus().isBadRequest();
     }
 
     @Test
-    public void 회원조회_페이지_테스트() {
+    public void 회원정보_수정_테스트() {
+        String name = "효진쓰";
+        String password = "@Password123";
+
+        /** 회원가입 **/
+        postSignup(validUserData, response ->
+                /** TODO 로그인 **/
+                /** 회원정보 수정 **/
+                webTestClient.put()
+                        .uri("/mypage/edit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .body(getBodyInserters(name, VALID_EMAIL, password))
+                        .exchange()
+                        .expectStatus().isFound()
+        );
+    }
+
+//    @Test
+//    public void 회원조회_페이지가_잘_출력되는지_테스트() {
+//        /** 회원가입 **/
+//        postSignup(validUserData, response ->
+//                /** TODO 로그인 **/
+//                /** 회원조회 **/
+//                webTestClient.get()
+//                        .uri("/users")
+//                        .exchange()
+//                        .expectStatus()
+//                        .isOk()
+//                        .expectBody()
+//                        .consumeWith(
+//                                res -> {
+//                                    String body = new String(res.getResponseBody());
+//                                    assertThat(body.contains(VALID_NAME)).isTrue();
+//                                    assertThat(body.contains(VALID_EMAIL)).isTrue();
+//                                }
+//                        )
+//        );
+//    }
+
+    @Test
+    public void 로그인이_안된_상태에서_회원조회_페이지에_접속하는_경우_예외처리() {
+        webTestClient.get()
+                .uri("/users")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    public void 회원탈퇴_테스트() {
+        /** 회원가입 **/
+        postSignup(validUserData, response ->
+                /** TODO 로그인 **/
+                /** 회원탈퇴 **/
+                webTestClient.delete()
+                        .uri("/mypage/edit/{userId}", CUSTOM_USER_ID)
+                        .attribute("", VALID_EMAIL)
+                        .exchange()
+                        .expectStatus().isFound()
+        );
+    }
+
+    private void postSignup(BodyInserters.FormInserter<String> userData, Consumer<EntityExchangeResult<byte[]>> consumer) {
         webTestClient.post()
                 .uri("/users")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", "코니코니")
-                        .with("email", "이메일이메일")
-                        .with("password", "비번비번"))
+                .body(userData)
                 .exchange()
                 .expectStatus().isFound()
                 .expectBody()
-                .consumeWith(response -> {
-                    webTestClient.get()
-                            .uri("/users")
-                            .exchange()
-                            .expectStatus()
-                            .isOk()
-                            .expectBody()
-                            .consumeWith(
-                                    res -> {
-                                        String body = new String(res.getResponseBody());
-                                        assertThat(body.contains("코니코니")).isTrue();
-                                        assertThat(body.contains("이메일이메일")).isTrue();
-                                    }
-                            );
-                });
+                .consumeWith(consumer);
+    }
+
+    private BodyInserters.FormInserter<String> getBodyInserters(String name, String email, String password) {
+        return BodyInserters.fromFormData("userId", CUSTOM_USER_ID)
+                .with("name", name)
+                .with("email", email)
+                .with("password", password);
     }
 }
