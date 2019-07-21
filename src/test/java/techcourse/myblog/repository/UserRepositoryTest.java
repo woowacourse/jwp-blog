df584d320c22e60145e4e4123d13abbb4388233e
박효진 <hgenie14@gmail.com>
2019-07-21 20:02:56 +0900

    test: UserRepository 테스트에 추가된 메서드에 대한 테스트 추가

--- src/test/java/techcourse/myblog/repository/UserRepositoryTest.java
+++ src/test/java/techcourse/myblog/repository/UserRepositoryTest.java
@@ -1,46 +1,45 @@
 package techcourse.myblog.repository;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import techcourse.myblog.domain.User;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserRepositoryTest {
+    private static final long DEFAULT_USER_ID = 0L;
+    private static final String NAME = "유효한이름";
+    private static final String EMAIL = "valid@email.com";
+    private static final String PASSWORD = "ValidPassword!123";
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User(DEFAULT_USER_ID, NAME, EMAIL, PASSWORD);
+        userRepository.save(user);
+    }
+
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    public void 회원_등록_테스트() {
-        User user = new User(0L, "코니코니", "cony@naver.com", "@Password1234");
-        userRepository.save(user);
-
-        assertThat(userRepository.findAll()).contains(user);
+    public void 등록된_이메일로_회원이_조회되는지_테스트() {
+        assertThat(userRepository.findUserByEmail(EMAIL)).isEqualTo(user);
     }
 
     @Test
-    public void 모든_회원_조회_테스트() {
-        User user1 = new User(0L, "에헴", "asda@happy.com", "@Password1234");
-        User user2 = new User(0L, "코니", "cony@happy.com", "@Password1234");
-        List<User> users = Arrays.asList(user1, user2);
-        userRepository.saveAll(users);
-
-        assertThat(userRepository.findAll()).isEqualTo(users);
+    public void 등록된_이메일에_매칭되는_User객체수가_조회되는지_테스트() {
+        assertThat(userRepository.countByEmail(EMAIL)).isEqualTo(1);
     }
 
     @Test
-    public void 회원_삭제_테스트() {
-        User user = new User(0L, "에헴", "asd@happy.com", "@Password1234");
-        userRepository.save(user);
-        userRepository.deleteById(user.getUserId());
-
-        assertThat(userRepository.findAll()).doesNotContain(user);
+    public void 등록된_이메일과_비밀번호를_입력했을때_알맞은__User객체가_조회되는지_테스트() {
+        assertThat(userRepository.findUserByEmailAndPassword(EMAIL, PASSWORD)).isEqualTo(user);
     }
 
     @AfterEach
