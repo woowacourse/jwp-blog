562a0aaba2ebda3dfef867c2013e97399f79ab1c
박효진 <hgenie14@gmail.com>
2019-07-21 19:33:54 +0900

    test: LoginContoller 테스트 메서드에서 중복되는 코드를 메서드로 추출

--- src/test/java/techcourse/myblog/web/LoginControllerTest.java
+++ src/test/java/techcourse/myblog/web/LoginControllerTest.java
@@ -1,14 +1,32 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginControllerTest {
+    private static final String CUSTOM_USER_ID = "1";
+    private static final String NAME = "유효한이름";
+    private static final String EMAIL = "valid@email.com";
+    private static final String PASSWORD = "ValidPassword!123";
+
+    private BodyInserters.FormInserter<String> validUserData = getBodyInserters(NAME, EMAIL, PASSWORD);
+
     @Autowired
     private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        postSignup(validUserData);
+    }
 
     @Test
     public void 로그인_페이지_테스트() {
@@ -19,36 +37,58 @@ public class LoginControllerTest {
 
     @Test
     public void 로그인_테스트() {
-        /** 회원 가입 **/
         webTestClient.post()
-                .uri("/users")
+                .uri("/login")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", "코니코니")
-                        .with("email", "이메일")
-                        .with("password", "비번비번"))
+                .body(getBodyInserters(EMAIL, PASSWORD))
                 .exchange()
                 .expectStatus().isFound()
                 .expectBody()
-                .consumeWith(response -> {
-                    /** 로그인 **/
-                    webTestClient.post()
-                            .uri("/login")
-                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                            .body(BodyInserters
-                                    .fromFormData("email", "이메일이메일")
-                                    .with("password", "비번비번"))
+                .consumeWith(res -> {
+                    /** 인덱스 페이지 이동 **/
+                    webTestClient.get()
+                            .uri(res.getRequestHeaders().getLocation())
                             .exchange()
-                            .expectStatus().isFound()
-                            .expectBody()
-                            .consumeWith(r -> {
-                                /** 인덱스 페이지 이동 **/
-                                webTestClient.get()
-                                        .uri(r.getRequestHeaders().getLocation())
-                                        .exchange()
-                                        .expectStatus()
-                                        .isOk();
-                            });
+                            .expectStatus()
+                            .isOk();
                 });
+    }
+
+    @Test
+    public void 로그아웃_테스트() {
+        webTestClient.post()
+                .uri("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(getBodyInserters(EMAIL, PASSWORD))
+                .exchange()
+                .expectStatus().isFound()
+                .expectBody()
+                .consumeWith(r -> {
+                    /** 로그아웃 **/
+                    webTestClient.get()
+                            .uri("/logout")
+                            .exchange()
+                            .expectStatus().isFound();
+                });
+    }
+
+    private void postSignup(BodyInserters.FormInserter<String> userData) {
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(userData)
+                .exchange();
+    }
+
+    private BodyInserters.FormInserter<String> getBodyInserters(String email, String password) {
+        return BodyInserters.fromFormData("email", email)
+                .with("password", password);
+    }
+
+    private BodyInserters.FormInserter<String> getBodyInserters(String name, String email, String password) {
+        return BodyInserters.fromFormData("userId", CUSTOM_USER_ID)
+                .with("name", name)
+                .with("email", email)
+                .with("password", password);
     }
 }
