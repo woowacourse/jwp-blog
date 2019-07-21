9a6fec0f7ee7f591077689ee9dfd4ffddedb949d
ddu0422 <ddu0422@naver.com>
2019-07-22 03:24:45 +0900

    refactor: fix article tests

--- src/test/java/techcourse/myblog/controller/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/controller/ArticleControllerTests.java
@@ -6,21 +6,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import techcourse.myblog.controller.template.ControllerTestTemplate;
 import techcourse.myblog.domain.Article;
-import techcourse.myblog.domain.ArticleRepository;
+import techcourse.myblog.service.ArticleWriteService;
 import techcourse.myblog.service.dto.ArticleDto;
 
 public class ArticleControllerTests extends ControllerTestTemplate {
     @Autowired
-    private ArticleRepository articleRepository;
+    private ArticleWriteService articleWriteService;
+    private ArticleDto articleDto;
     private Article article;
 
     @BeforeEach
     void 게시글_작성_확인() {
-        article = new ArticleDto("1", "https://www.naver.com", "2").toArticle();
-        articleRepository.save(article);
-        checkStatusAndHeaderLocation(response(HttpMethod.POST, "/articles"), HttpStatus.FOUND, ".*articles.*");
+        articleDto = new ArticleDto("1", "https://www.naver.com", "2");
+        article = articleWriteService.save(articleDto);
+        checkStatusAndHeaderLocation(responseWithData(HttpMethod.POST, "/articles", bodyInsert(articleDto)), HttpStatus.FOUND, ".*articles.*");
     }
 
     @Test
@@ -45,7 +48,7 @@ public class ArticleControllerTests extends ControllerTestTemplate {
 
     @Test
     void 게시글_수정_확인() {
-        checkStatusAndHeaderLocation(response(HttpMethod.PUT, "/articles/" + article.getId()),
+        checkStatusAndHeaderLocation(responseWithData(HttpMethod.PUT, "/articles/" + article.getId(), bodyInsert(articleDto)),
                 HttpStatus.FOUND,".*articles.*");
     }
 
@@ -53,5 +56,13 @@ public class ArticleControllerTests extends ControllerTestTemplate {
     void 게시글_삭제_확인() {
         checkStatusAndHeaderLocation(response(HttpMethod.DELETE, "/articles/" + article.getId())
                 , HttpStatus.FOUND, ".*/");
+    }
+    
+    protected MultiValueMap<String, String> bodyInsert(ArticleDto articleDto) {
+        MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add("title", articleDto.getTitle());
+        multiValueMap.add("coverUrl", articleDto.getCoverUrl());
+        multiValueMap.add("contents", articleDto.getContents());
+        return multiValueMap;
     }
 }
