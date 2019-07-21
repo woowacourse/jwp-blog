de19d151fb29d56c3b94e5286b49502a0d8e6697
hyojaekim <hj00686@naver.com>
2019-07-21 12:34:38 +0900

    [TEST] 회원 정보 페이지에 대한 테스트를 적용하였습니다.
    - 해당 페이지에 유저의 이름과 이메일을 확인합니다.

--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -7,28 +7,64 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.StatusAssertions;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.dto.LoginDto;
 import techcourse.myblog.dto.UserDto;
 
+import java.util.function.Consumer;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static techcourse.myblog.web.LoginControllerTest.getSessionId;
+import static techcourse.myblog.web.LoginControllerTest.postLogin;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
     private UserDto userDto;
+    private LoginDto loginDto;
 
     @Autowired
     private WebTestClient webTestClient;
 
+    public static void postUser(WebTestClient webTestClient, UserDto userDto, Consumer<EntityExchangeResult<byte[]>> consumer) {
+        webTestClient.post()
+                .uri("/users")
+                .body(BodyInserters.fromFormData("name", userDto.getName())
+                        .with("email", userDto.getEmail())
+                        .with("password", userDto.getPassword())
+                        .with("passwordConfirm", userDto.getPasswordConfirm()))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody()
+                .consumeWith(consumer);
+    }
+
     @BeforeEach
     void setUp() {
         userDto = new UserDto();
+        loginDto = new LoginDto();
+
         userDto.setName("test");
         userDto.setEmail("test@test.com");
         userDto.setPassword("PassW0rd@");
         userDto.setPasswordConfirm("PassW0rd@");
+
+        loginDto.setEmail("test@test.com");
+        loginDto.setPassword("PassW0rd@");
+    }
+
+    private StatusAssertions postUser(UserDto userDto) {
+        return webTestClient.post()
+                .uri("/users")
+                .body(BodyInserters.fromFormData("name", userDto.getName())
+                        .with("email", userDto.getEmail())
+                        .with("password", userDto.getPassword())
+                        .with("passwordConfirm", userDto.getPasswordConfirm()))
+                .exchange()
+                .expectStatus();
     }
 
     @Test
@@ -94,27 +130,34 @@ class UserControllerTest {
         userDto.setPassword("PassW0rd@");
         userDto.setPasswordConfirm("PassW0rd@");
 
-        postUser(userDto).is3xxRedirection()
-                .expectBody().consumeWith(postUserResponse ->
+        postUser(webTestClient, userDto, postUserResponse -> {
+            webTestClient.get()
+                    .uri("/users")
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody()
+                    .consumeWith(result -> assertThat(new String(result.getResponseBody()))
+                            .contains(userDto.getName())
+                            .contains(userDto.getEmail()));
+        });
+    }
+
+    @Test
+    @DisplayName("로그인을 유지하고 회원 정보 페이지에서 유저의 이름과 이메일을 확인한다.")
+    void myPageViewTest() {
+        postUser(webTestClient, userDto, postUserResponse -> {
+            String sessionId = getSessionId(postUserResponse);
+            postLogin(webTestClient, loginDto, sessionId, postLoginResponse -> {
                 webTestClient.get()
-                        .uri("/users")
+                        .uri("/mypage" + sessionId)
                         .exchange()
                         .expectStatus().isOk()
                         .expectBody()
-                        .consumeWith(result -> assertThat(new String(result.getResponseBody()))
-                                .contains(userDto.getName())
-                                .contains(userDto.getEmail())
-                        ));
-    }
-
-    private StatusAssertions postUser(UserDto userDto) {
-        return webTestClient.post()
-                .uri("/users")
-                .body(BodyInserters.fromFormData("name", userDto.getName())
-                        .with("email", userDto.getEmail())
-                        .with("password", userDto.getPassword())
-                        .with("passwordConfirm", userDto.getPasswordConfirm()))
-                .exchange()
-                .expectStatus();
+                        .consumeWith(myPage ->
+                                assertThat(new String(myPage.getResponseBody()))
+                                        .contains("test")
+                                        .contains("test@test.com"));
+            });
+        });
     }
 }
