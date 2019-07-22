b9f315cf7d901fba0049feb720099026a29eda37
green4469 <green4469@gmail.com>
2019-07-22 16:13:28 +0900

    refactor: remove magic number

--- src/main/java/techcourse/myblog/domain/User/UserForm.java
+++ src/main/java/techcourse/myblog/domain/User/UserForm.java
@@ -13,6 +13,7 @@ public class UserForm {
     public static final String NAME_ERROR_MSG = "2글자 이상, 10글자 이하로 입력하세요. 숫자와 특수문자는 입력할 수 없습니다.";
     public static final String PASSWORD_ERROR_MSG = "소문자, 대문자, 숫자, 특수문자가 조합된 8글자 이상을 입력하세요.";
     public static final String EMAIL_ERROR_MSG = "이메일의 형식에 맞춰 입력하세요.";
+    private static final long BLANK_USER_ID = 0;
 
     private long id;
     @Pattern(regexp = "^[a-zA-Z가-힣]{2,10}$", message = NAME_ERROR_MSG)
@@ -31,7 +32,7 @@ public class UserForm {
     }
 
     public User toUser() {
-        if (id != 0) {
+        if (id != BLANK_USER_ID) {
             return new User(id, name, password, email);
         }
         return new User(name, password, email);
