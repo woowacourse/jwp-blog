ff5aa3f596f6b88890dc55291d77524262d1bc07
yk1028 <wongeunsong94@gmail.com>
2019-07-22 15:06:46 +0900

    refactor: User의 이름, 비밀번호에 대한 유효성 검사를 service에서 domain으로 변경

--- src/main/java/techcourse/myblog/domain/User.java
+++ src/main/java/techcourse/myblog/domain/User.java
@@ -1,12 +1,28 @@
 package techcourse.myblog.domain;
 
+import techcourse.myblog.service.exception.UserArgumentException;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static techcourse.myblog.service.exception.UserArgumentException.*;
 
 @Entity
 public class User {
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 10;
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    private static final String KOREAN_ENGLISH_REGEX = "^[(ㄱ-ㅎ가-힣a-zA-Z)]+$";
+    private static final String LOWER_CASE_REGEX = "[(a-z)]+";
+    private static final String UPPER_CASE_REGEX = "[(A-Z)]+";
+    private static final String NUMBER_REGEX = "[(0-9)]+";
+    private static final String SPECIAL_CHARACTER_REGEX = "[ `~!@#[$]%\\^&[*]\\(\\)_-[+]=\\{\\}\\[\\][|]'\":;,.<>/?]+";
+    private static final String KOREAN_REGEX = "[(ㄱ-ㅎ가-힣)]+";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,9 +34,57 @@ public class User {
     }
 
     public User(String name, String email, String password) {
+        checkValidName(name);
+        checkValidPassword(password);
+
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkValidName(String name) {
+        checkValidNameLength(name);
+        checkValidNameCharacters(name);
+    }
+
+    private void checkValidNameLength(String name) {
+        int nameLength = name.length();
+        if (nameLength < MIN_NAME_LENGTH || nameLength > MAX_NAME_LENGTH) {
+            throw new UserArgumentException(INVALID_NAME_LENGTH_MESSAGE);
+        }
+    }
+
+    private void checkValidNameCharacters(String name) {
+        if (!matchRegex(name, KOREAN_ENGLISH_REGEX)) {
+            throw new UserArgumentException(NAME_INCLUDE_INVALID_CHARACTERS_MESSAGE);
+        }
+    }
+
+    private void checkValidPassword(String password) {
+        checkValidPasswordLength(password);
+        checkValidPasswordCharacters(password);
+    }
+
+    private void checkValidPasswordLength(String password) {
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            throw new UserArgumentException(INVALID_PASSWORD_LENGTH_MESSAGE);
+        }
+    }
+
+    private void checkValidPasswordCharacters(String password) {
+        if (!matchRegex(password, LOWER_CASE_REGEX)
+                || !matchRegex(password, UPPER_CASE_REGEX)
+                || !matchRegex(password, NUMBER_REGEX)
+                || !matchRegex(password, SPECIAL_CHARACTER_REGEX)
+                || matchRegex(password, KOREAN_REGEX)) {
+            throw new UserArgumentException(INVALID_PASSWORD_MESSAGE);
+        }
+    }
+
+    private boolean matchRegex(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
     }
 
     public Long getId() {
@@ -40,6 +104,7 @@ public class User {
     }
 
     public void setName(String name) {
+        checkValidName(name);
         this.name = name;
     }
 }
--- src/main/java/techcourse/myblog/service/UserService.java
+++ src/main/java/techcourse/myblog/service/UserService.java
@@ -13,23 +13,12 @@ import techcourse.myblog.service.exception.UserUpdateException;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static techcourse.myblog.service.exception.UserArgumentException.*;
+import static techcourse.myblog.service.exception.UserArgumentException.EMAIL_DUPLICATION_MESSAGE;
+import static techcourse.myblog.service.exception.UserArgumentException.PASSWORD_CONFIRM_FAIL_MESSAGE;
 
 @Service
 public class UserService {
-    private static final int MIN_NAME_LENGTH = 2;
-    private static final int MAX_NAME_LENGTH = 10;
-    private static final int MIN_PASSWORD_LENGTH = 8;
-    private static final String KOREAN_ENGLISH_REGEX = "^[(ㄱ-ㅎ가-힣a-zA-Z)]+$";
-    private static final String LOWER_CASE_REGEX = "[(a-z)]+";
-    private static final String UPPER_CASE_REGEX = "[(A-Z)]+";
-    private static final String NUMBER_REGEX = "[(0-9)]+";
-    private static final String SPECIAL_CHARACTER_REGEX = "[ `~!@#[$]%\\^&[*]\\(\\)_-[+]=\\{\\}\\[\\][|]'\":;,.<>/?]+";
-    private static final String KOREAN_REGEX = "[(ㄱ-ㅎ가-힣)]+";
-
     private UserRepository userRepository;
     private ArticleRepository articleRepository;
 
@@ -53,29 +42,12 @@ public class UserService {
 
     private void validate(UserDto userDto) {
         checkDuplicatedEmail(userDto.getEmail());
-        checkValidNameLength(userDto.getName());
-        checkValidName(userDto.getName());
         checkPasswordConfirm(userDto);
-        checkValidPasswordLength(userDto.getPassword());
-        checkValidPassword(userDto.getPassword());
     }
 
     private void checkDuplicatedEmail(String email) {
         if (userRepository.findByEmail(email).isPresent()) {
             throw new UserArgumentException(EMAIL_DUPLICATION_MESSAGE);
-        }
-    }
-
-    private void checkValidNameLength(String name) {
-        int nameLength = name.length();
-        if (nameLength < MIN_NAME_LENGTH || nameLength > MAX_NAME_LENGTH) {
-            throw new UserArgumentException(INVALID_NAME_LENGTH_MESSAGE);
-        }
-    }
-
-    private void checkValidName(String name) {
-        if (!matchRegex(name, KOREAN_ENGLISH_REGEX)) {
-            throw new UserArgumentException(NAME_INCLUDE_INVALID_CHARACTERS_MESSAGE);
         }
     }
 
@@ -85,35 +57,10 @@ public class UserService {
         }
     }
 
-    private void checkValidPasswordLength(String password) {
-        if (password.length() < MIN_PASSWORD_LENGTH) {
-            throw new UserArgumentException(INVALID_PASSWORD_LENGTH_MESSAGE);
-        }
-    }
-
-    private void checkValidPassword(String password) {
-        if (!matchRegex(password, LOWER_CASE_REGEX)
-                || !matchRegex(password, UPPER_CASE_REGEX)
-                || !matchRegex(password, NUMBER_REGEX)
-                || !matchRegex(password, SPECIAL_CHARACTER_REGEX)
-                || matchRegex(password, KOREAN_REGEX)) {
-            throw new UserArgumentException(INVALID_PASSWORD_MESSAGE);
-        }
-    }
-
-    private boolean matchRegex(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        return matcher.find();
-    }
-
     public void update(UserPublicInfoDto userPublicInfoDto) {
         try {
             User user = userRepository.findByEmail(userPublicInfoDto.getEmail())
                     .orElseThrow(NotFoundUserException::new);
-            String name = userPublicInfoDto.getName();
-            checkValidNameLength(name);
-            checkValidName(name);
             user.setName(userPublicInfoDto.getName());
             userRepository.save(user);
         } catch (Exception e) {
