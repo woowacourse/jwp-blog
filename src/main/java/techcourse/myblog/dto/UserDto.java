8d699f9e3c072cc34b3604f39f7871ea4c0dea94
starkim06 <starkim06@naver.com>
2019-07-22 19:37:56 +0900

    refactor: toEntity 메서드 삭제

--- src/main/java/techcourse/myblog/dto/UserDto.java
+++ src/main/java/techcourse/myblog/dto/UserDto.java
@@ -2,7 +2,6 @@ package techcourse.myblog.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import techcourse.myblog.domain.User;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
@@ -12,12 +11,10 @@ import javax.validation.constraints.Pattern;
 public class UserDto {
     @Pattern(regexp = "^([a-zA-Z]){2,10}$", message = "올바른 이름을 입력하세요")
     private String name;
+
     @Email(message = "올바른 이메일을 입력하세요")
     private String email;
+
     @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,20}$", message = "올바른 비밀번호를 입력하세요")
     private String password;
-
-    public User toEntity() {
-        return new User(name, email, password);
-    }
 }
