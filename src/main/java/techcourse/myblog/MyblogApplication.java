5c27961012648e45b371d6da90ac0ae504b93a9f
DaejunBae <dpudpu11@gmail.com>
2019-07-22 14:51:34 +0900

    style: 포맷팅

--- src/main/java/techcourse/myblog/MyblogApplication.java
+++ src/main/java/techcourse/myblog/MyblogApplication.java
@@ -2,13 +2,6 @@ package techcourse.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.request.WebRequest;
-import techcourse.myblog.users.ValidUserException;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class MyblogApplication {
