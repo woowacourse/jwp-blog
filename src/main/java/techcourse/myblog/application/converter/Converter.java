68f85cedb7f0aade89b281d8fbaecd1e7e701ae2
skygl <jupiter9501@gmal.com>
2019-07-22 15:58:56 +0900

    [REFACTOR] Coverter 클래스 Abstract Class 로 수정

--- src/main/java/techcourse/myblog/application/converter/Converter.java
+++ src/main/java/techcourse/myblog/application/converter/Converter.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Converter<T, U> {
+public abstract class Converter<T, U> {
     private final Function<T, U> fromDto;
     private final Function<U, T> fromEntity;
 
