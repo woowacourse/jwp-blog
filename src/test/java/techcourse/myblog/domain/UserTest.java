e790934d541870c3b68ab2d1fc46b15c58badc6c
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-22 17:45:15 +0900

    test(UserTest): rename method

--- src/test/java/techcourse/myblog/domain/UserTest.java
+++ src/test/java/techcourse/myblog/domain/UserTest.java
@@ -7,7 +7,8 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class UserTest {
     @Test
     void equalPassword() {
+        String email = "email@gmail.com";
         String password = "qweqwe";
-        assertThat(new User("name", "email@gmail.com", password).equalPassword(password)).isTrue();
+        assertThat(new User("name", email, password).authenticate(email, password)).isTrue();
     }
 }
