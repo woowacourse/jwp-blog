9a2e5cd5dc1f5c16a307e7cea9a394774553afff
starkim06 <starkim06@naver.com>
2019-07-22 14:25:57 +0900

    refactor: DTO에 Bean validation 추가

--- src/main/java/techcourse/myblog/dto/UserDto.java
+++ src/main/java/techcourse/myblog/dto/UserDto.java
@@ -4,11 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import techcourse.myblog.domain.User;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 public class UserDto {
+    @Pattern(regexp = "^([a-zA-Z]){2,10}$")
     private String name;
+    @Email
     private String email;
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,20}$")
     private String password;
 
     public User toEntity() {
--- src/main/java/techcourse/myblog/dto/UserLoginDto.java
+++ src/main/java/techcourse/myblog/dto/UserLoginDto.java
@@ -3,9 +3,14 @@ package techcourse.myblog.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 public class UserLoginDto {
+    @Email
     private String email;
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,20}$")
     private String password;
 }
