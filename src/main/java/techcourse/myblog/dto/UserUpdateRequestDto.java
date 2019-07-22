6d92c31f856f0aac588c509d8f44197c0d35ea1b
Seongmo Jeon <oeeen3@gmail.com>
2019-07-22 23:15:42 +0900

    refactor: default message 수정(원하는 형식 요구)

--- src/main/java/techcourse/myblog/dto/UserUpdateRequestDto.java
+++ src/main/java/techcourse/myblog/dto/UserUpdateRequestDto.java
@@ -10,6 +10,6 @@ import javax.validation.constraints.Pattern;
 @Setter
 @NoArgsConstructor
 public class UserUpdateRequestDto {
-    @Pattern(regexp = "^[a-zA-Z]{2,10}$", message = "형식에 맞는 이름이 아닙니다.")
+    @Pattern(regexp = "^[a-zA-Z]{2,10}$", message = "형식에 맞는 이름이 아닙니다. 이름은 2~10자의 영어 대소문자만 허용합니다.")
     private String userName;
 }
