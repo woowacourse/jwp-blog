987a3e2a696bcff94c34b5a035d112dec51619e4
starkim06 <starkim06@naver.com>
2019-07-22 14:38:55 +0900

    refactor: Bean validation 메세지 추가

--- src/main/java/techcourse/myblog/dto/UserLoginDto.java
+++ src/main/java/techcourse/myblog/dto/UserLoginDto.java
@@ -9,8 +9,8 @@ import javax.validation.constraints.Pattern;
 @Getter
 @Setter
 public class UserLoginDto {
-    @Email
+    @Email(message = "올바른 이메일을 입력하세요")
     private String email;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,20}$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,20}$",message = "올바른 비밀번호를 입력하세요")
     private String password;
 }
