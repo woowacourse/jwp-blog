5dd03803083b5a1cc88bc92640b2b3d007562d9a
codeanddonuts <kkw200@naver.com>
2019-07-22 15:59:15 +0900

    test: Article controller tests re-written.

--- src/test/java/techcourse/myblog/web/ArticleControllerTest.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTest.java
@@ -1,113 +1,143 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.ArticleRepository;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ArticleControllerTest {
-    static final String title = "제목";
-    static final String coverUrl = "이미지";
-    static final String contents = "내용";
+    static final String testTitle = "Jemok";
+    static final String testCoverUrl = "Baegyung";
+    static final String testContents = "Naeyong";
+    static final Article testArticle = new Article(testTitle, testCoverUrl, testContents);
 
     @Autowired
-    private WebTestClient webTestClient;
+    private ArticleRepository articleRepository;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
 
-    @Test
-    void indexTest() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        articleRepository.deleteAll();
     }
 
     @Test
-    void showWritingPageTest() {
-        webTestClient.get().uri("/writing")
-                    .exchange()
-                    .expectStatus().isOk();
+    void indexTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void articleCreateReadTest() {
-        webTestClient.post().uri("/articles")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(
-                        BodyInserters.fromFormData("title", title)
-                                .with("coverUrl", coverUrl)
-                                .with("contents", contents)
-                )
-                .exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody()
-                .consumeWith(res ->
-                        webTestClient.get().uri(res.getResponseHeaders().getLocation())
-                                .exchange()
-                                .expectBody()
-                                .consumeWith(_res -> {
-                                    String body = new String(_res.getResponseBody());
-                                    assertTrue(body.contains(title));
-                                    assertTrue(body.contains(coverUrl));
-                                    assertTrue(body.contains(contents));
-                                })
-                );
+    void showWritingFormTest() throws Exception {
+        mockMvc.perform(get("/writing"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void articleUpdateTest() {
-        final String updateTitle = "제목2";
-        final String updateImage = "이미지2";
-        final String updateContents = "내용2";
-
-        webTestClient.post().uri("/articles")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(
-                        BodyInserters.fromFormData("title", title)
-                                    .with("coverUrl", coverUrl)
-                                    .with("contents", contents)
-                )
-                .exchange()
-                .expectBody()
-                .consumeWith(res ->
-                        webTestClient.put().uri(res.getResponseHeaders().getLocation())
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                                .body(BodyInserters.fromFormData("title", updateTitle)
-                                        .with("coverUrl", updateImage)
-                                        .with("contents", updateContents))
-                                .exchange()
-                                .expectStatus().is3xxRedirection()
-                                .expectBody()
-                                .consumeWith(_res ->
-                                        webTestClient.get().uri(res.getResponseHeaders().getLocation())
-                                                .exchange()
-                                                .expectBody()
-                                                .consumeWith(__res -> {
-                                                    String body = new String(__res.getResponseBody());
-                                                    assertTrue(body.contains(title));
-                                                    assertTrue(body.contains(coverUrl));
-                                                    assertTrue(body.contains(contents));
-                                                }))
-                );
+    void writeTest() throws Exception {
+        mockMvc.perform(
+                post("/articles").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .param("title", testTitle)
+                                .param("coverUrl", testCoverUrl)
+                                .param("contents", testContents)
+        ).andDo(print())
+        .andExpect(status().is3xxRedirection());
+        assertThat(articleRepository.count() != 0).isTrue();
+        boolean[] isWritten = { false };
+        articleRepository.findAll().forEach(article -> {
+            isWritten[0] |= article.getTitle().equals(testTitle)
+                    & article.getCoverUrl().equals(testCoverUrl)
+                    & article.getContents().equals(testContents);
+        });
+        assertThat(isWritten[0]).isTrue();
     }
 
     @Test
-    void articleDelete() {
-        webTestClient.post().uri("/articles")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(
-                        BodyInserters.fromFormData("title", title)
-                                .with("coverUrl", coverUrl)
-                                .with("contents", contents)
-                )
-                .exchange()
-                .expectBody()
-                .consumeWith(res -> webTestClient.delete().uri(res.getResponseHeaders().getLocation())
-                        .exchange()
-                        .expectStatus().is3xxRedirection());
+    void readTest() throws Exception {
+        final Article written = articleRepository.save(testArticle);
+        mockMvc.perform(get("/articles/0"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/"));
+        String body = mockMvc.perform(get("/articles/" + written.getId()))
+                            .andDo(print())
+                            .andExpect(status().isOk())
+                            .andReturn()
+                            .getResponse()
+                            .getContentAsString();
+        assertThat(body.contains(testTitle)).isTrue();
+        assertThat(body.contains(testCoverUrl)).isTrue();
+        assertThat(body.contains(testContents)).isTrue();
+    }
+
+    @Test
+    void updateFormTest() throws Exception {
+        final Article written = articleRepository.save(testArticle);
+        mockMvc.perform(get("/articles/0/edit"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/"));
+        final String body = mockMvc.perform(get("/articles/" + written.getId() + "/edit"))
+                                    .andDo(print())
+                                    .andExpect(status().isOk())
+                                    .andReturn()
+                                    .getResponse()
+                                    .getContentAsString();
+        assertThat(body.contains(testTitle)).isTrue();
+        assertThat(body.contains(testCoverUrl)).isTrue();
+        assertThat(body.contains(testContents)).isTrue();
+    }
+
+    @Test
+    void updateTest() throws Exception {
+        final String updatedTitle = "제목쓰";
+        final String updatedCoverUrl = "배경쓰";
+        final String updatedContents = "내용쓰";
+        final Article written = articleRepository.save(testArticle);
+        mockMvc.perform(put("/articles/0/"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/"));
+        mockMvc.perform(
+                put("/articles/" + written.getId()).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                                    .param("title", updatedTitle)
+                                                    .param("coverUrl", updatedCoverUrl)
+                                                    .param("contents", updatedContents)
+        ).andDo(print())
+        .andExpect(status().is3xxRedirection());
+        final String body = mockMvc.perform(get("/articles/" + written.getId()))
+                                    .andDo(print())
+                                    .andExpect(status().isOk())
+                                    .andReturn()
+                                    .getResponse()
+                                    .getContentAsString();
+        assertThat(body.contains(updatedTitle)).isTrue();
+        assertThat(body.contains(updatedCoverUrl)).isTrue();
+        assertThat(body.contains(updatedContents)).isTrue();
+    }
+
+    @Test
+    void articleDeleteTest() throws Exception {
+        final Article written = articleRepository.save(testArticle);
+        mockMvc.perform(delete("/articles/" + written.getId()));
+        assertThat(articleRepository.count()).isEqualTo(0L);
     }
 }
