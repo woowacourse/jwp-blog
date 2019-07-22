b17d7838734f86a24d695d4f85f5c448aea1d00e
ep1stas1s <masloz@konkuk.ac.kr>
2019-07-22 20:33:01 +0900

    style: remove unnecessary code

--- src/test/java/techcourse/myblog/web/controller/ArticleControllerTest.java
+++ src/test/java/techcourse/myblog/web/controller/ArticleControllerTest.java
@@ -3,7 +3,6 @@ package techcourse.myblog.web.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
--- src/test/java/techcourse/myblog/web/controller/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/controller/UserControllerTest.java
@@ -3,7 +3,6 @@ package techcourse.myblog.web.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseCookie;
 import org.springframework.test.web.reactive.server.WebTestClient;
