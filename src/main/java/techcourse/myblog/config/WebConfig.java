bd29c7f12c7486fd09b5d2add48ddd9780410063
pkch93 <star_final@naver.com>
2019-07-23 16:32:06 +0900

    fix: WebConfig class
    비로그인 상태에서 로그아웃 시 에러 수정

--- src/main/java/techcourse/myblog/config/WebConfig.java
+++ src/main/java/techcourse/myblog/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/auth/logout");
 
         registry.addInterceptor(new NeedAuthInterceptor())
-                .addPathPatterns("/auth/login", "/users/signup");
+                .addPathPatterns("/auth/login", "/auth/logout", "/users/signup");
     }
 }
