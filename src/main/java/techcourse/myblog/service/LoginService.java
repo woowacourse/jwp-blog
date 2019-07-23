7d62dc76948f1f9f143e11a958d48c3421a66e43
강연욱 <fiasboc23@gmail.com>
2019-07-23 10:41:40 +0900

    refactor: unable to reallocate userRepository

--- src/main/java/techcourse/myblog/service/LoginService.java
+++ src/main/java/techcourse/myblog/service/LoginService.java
@@ -11,7 +11,7 @@ public class LoginService {
     private static final String NOT_EXIST_USER = "등록된 이메일이 없습니다.";
     private static final String INCORRECT_PASSWORD = "비밀번호가 일치하지 않습니다.";
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public LoginService(UserRepository userRepository) {
         this.userRepository = userRepository;
