3a1149b7e3434864357b0461f1bac07a620ecda6
pkch93 <star_final@naver.com>
2019-07-23 17:27:13 +0900

    refactor: ArticleRepository, UserRepository
    CrudRepository -> JpaRepository

--- src/main/java/techcourse/myblog/repository/UserRepository.java
+++ src/main/java/techcourse/myblog/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package techcourse.myblog.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import techcourse.myblog.domain.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 }
