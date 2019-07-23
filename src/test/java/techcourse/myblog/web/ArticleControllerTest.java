5bce012a0cb12d38f304ce274c5eeedf780cd4e1
hyperpace <cnk9109@gmail.com>
2019-07-23 18:51:41 +0900

    test(ArticleControllerTest): test ArticleController

--- src/test/java/techcourse/myblog/web/ArticleControllerTest.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTest.java
@@ -4,130 +4,76 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTest {
-    @Autowired
-    private WebTestClient webTestClient;
+	@Autowired
+	private WebTestClient webTestClient;
 
-    @Test
-    void 메인화면_조회() {
-        webTestClient.get().uri("/")
-                .exchange()
-                .expectStatus().isOk();
-    }
+	@Test
+	void 메인화면_조회() {
+		webTestClient.get().uri("/")
+				.exchange()
+				.expectStatus().isOk();
+	}
 
-    @Test
-    void 글쓰기_폼_열기() {
-        webTestClient.get().uri("/articles")
-                .exchange()
-                .expectStatus().isOk();
-    }
+	@Test
+	void 글쓰기_폼_열기() {
+		webTestClient.get().uri("/articles")
+				.exchange()
+				.expectStatus().isOk();
+	}
 
-    @Test
-    void 게시물_생성() {
-        String title = "제목";
-        String coverUrl = "https://i.pinimg.com/736x/78/28/39/7828390ef4efbe704e480440f3bd3875.jpg";
-        String contents = "CONTENTS";
+	@Test
+	void 게시물_생성() {
+		createArticle();
+	}
 
-        webTestClient.post().uri("/articles")
-                .body(
-                        BodyInserters
-                                .fromFormData("title", title)
-                                .with("coverUrl", coverUrl)
-                                .with("contents", contents)
-                ).exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody()
-                .consumeWith(
-                        response -> {
-                            String url = response.getResponseHeaders().get("Location").get(0);
-                            webTestClient.get().uri(url)
-                                    .exchange()
-                                    .expectStatus().isOk()
-                                    .expectBody()
-                                    .consumeWith(
-                                            redirectResponse -> {
-                                                String body = new String(redirectResponse.getResponseBody());
-                                                assertThat(body.contains(title)).isTrue();
-                                                assertThat(body.contains(coverUrl)).isTrue();
-                                                assertThat(body.contains(contents)).isTrue();
-                                            }
-                                    );
-                        }
-                );
-    }
+	private EntityExchangeResult<byte[]> createArticle() {
+		String title = "제목";
+		String coverUrl = "https://i.pinimg.com/736x/78/28/39/7828390ef4efbe704e480440f3bd3875.jpg";
+		String contents = "CONTENTS";
 
-    @Test
-    void 게시글_수정() {
-        String title = "제목";
-        String coverUrl = "https://i.pinimg.com/736x/78/28/39/7828390ef4efbe704e480440f3bd3875.jpg";
-        String contents = "CONTENTS";
+		return webTestClient.post().uri("/articles")
+				.body(fromFormData("title", title)
+						.with("coverUrl", coverUrl)
+						.with("contents", contents))
+				.exchange()
+				.expectStatus().is3xxRedirection()
+				.expectHeader().valueMatches("Location", ".*/articles/[0-9]*")
+				.expectBody()
+				.returnResult();
+	}
 
-        webTestClient.post().uri("/articles")
-                .body(
-                        BodyInserters
-                                .fromFormData("title", "잘가")
-                                .with("coverUrl", "")
-                                .with("contents", "Bye")
-                ).exchange()
-                .expectBody()
-                .consumeWith(
-                        response -> {
-                            Long id = Long.parseLong(response.getResponseHeaders().get("Location").get(0).split("/")[4]);
-                            webTestClient.put().uri("/articles/" + id)
-                                    .body(
-                                            BodyInserters
-                                                    .fromFormData("title", title)
-                                                    .with("coverUrl", coverUrl)
-                                                    .with("contents", contents)
-                                    ).exchange()
-                                    .expectStatus().is3xxRedirection()
-                                    .expectBody()
-                                    .consumeWith(
-                                            response1 -> {
-                                                String url = response1.getResponseHeaders().get("Location").get(0);
-                                                webTestClient.get().uri(url)
-                                                        .exchange()
-                                                        .expectStatus().isOk()
-                                                        .expectBody()
-                                                        .consumeWith(
-                                                                redirectResponse -> {
-                                                                    String body = new String(redirectResponse.getResponseBody());
-                                                                    assertThat(body.contains(title)).isTrue();
-                                                                    assertThat(body.contains(coverUrl)).isTrue();
-                                                                    assertThat(body.contains(contents)).isTrue();
-                                                                }
-                                                        );
-                                            }
-                                    );
-                        }
-                );
+	@Test
+	void 게시글_수정() {
+		String title = "제목";
+		String coverUrl = "https://i.pinimg.com/736x/78/28/39/7828390ef4efbe704e480440f3bd3875.jpg";
+		String contents = "CONTENTS";
 
-    }
+		String updateUrl = createArticle().getResponseHeaders()
+				.getLocation().getPath();
+		webTestClient.put().uri(updateUrl)
+				.body(fromFormData("title", title)
+						.with("coverUrl", coverUrl)
+						.with("contents", contents))
+				.exchange()
+				.expectStatus().is3xxRedirection()
+				.expectHeader().valueMatches("Location", ".*/articles/[0-9]*");
+	}
 
-    @Test
-    void 게시글_삭제() {
-        webTestClient.post().uri("/articles")
-                .body(
-                        BodyInserters
-                                .fromFormData("title", "잘가")
-                                .with("coverUrl", "")
-                                .with("contents", "Bye")
-                ).exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody()
-                .consumeWith(response -> {
-                    Long id = Long.parseLong(response.getResponseHeaders().get("Location").get(0).split("/")[4]);
-                    webTestClient.delete().uri("/articles/" + id)
-                            .exchange()
-                            .expectStatus().is3xxRedirection();
-                });
-
-    }
+	@Test
+	void 게시글_삭제() {
+		String articleId = createArticle().getResponseHeaders()
+				.getLocation().getPath().split("/")[2];
+		webTestClient.delete().uri("/articles/" + articleId)
+				.exchange()
+				.expectStatus().is3xxRedirection()
+				.expectHeader().valueMatches("Location", ".*/");
+	}
 }
