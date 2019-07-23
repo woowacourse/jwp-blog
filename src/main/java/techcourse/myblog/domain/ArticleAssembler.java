65d978884d4a5786f5db5cdbd62c172f2cb14fa7
starkim06 <starkim06@naver.com>
2019-07-23 15:32:02 +0900

    refactor: 코드 컨벤션 수정

--- src/main/java/techcourse/myblog/domain/ArticleAssembler.java
+++ src/main/java/techcourse/myblog/domain/ArticleAssembler.java
@@ -4,6 +4,6 @@ import techcourse.myblog.dto.ArticleWriteDto;
 
 public class ArticleAssembler {
     public static Article writeArticle(ArticleWriteDto articleWriteDto) {
-        return new Article(articleWriteDto.getTitle(),articleWriteDto.getContents(),articleWriteDto.getCoverUrl());
+        return new Article(articleWriteDto.getTitle(), articleWriteDto.getContents(), articleWriteDto.getCoverUrl());
     }
 }
