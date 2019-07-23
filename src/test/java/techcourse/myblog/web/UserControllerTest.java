7e897acd7e99fe408d4af2c319eef3bcfeb64f7f
starkim06 <starkim06@naver.com>
2019-07-23 21:24:43 +0900

    refactor: Session을 활용한 테스트 케이스 추가

--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -8,28 +8,74 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.reactive.function.BodyInserters;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
+    private static final String NAME = "name";
+    private static final String EMAIL = "email";
+    private static final String PASSWORD = "password";
+    private static final String DEFAULT_EMAIL = "starkim06@naver.com";
+    private static final String DEFAULT_PASSWORD = "aA1231!!";
+
+    private String name;
+    private String email;
+    private String password;
 
     @Autowired
     WebTestClient webTestClient;
 
     @Test
-    void 회원등록_오류_테스트() {
+    void 로그인상태_로그인요청() {
+        signUp("alswns", DEFAULT_EMAIL, DEFAULT_PASSWORD);
+        webTestClient.get().uri("/login/page")
+                .header("Authorization", "Basic " + Base64Utils
+                        .encodeToString(("starkim06@naver.com:aA1231!!").getBytes(UTF_8)))
+                .exchange()
+                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/;jsessionid=([0-9A-Z])+")
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void 로그아웃상태_로그인요청() {
+        signUp("alswns", DEFAULT_EMAIL, DEFAULT_PASSWORD);
+        webTestClient.get().uri("/login/page")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void 로그인_테스트() {
         String name = "name";
-        String email = "email";
-        String password = "password";
-        webTestClient.post()
-                .uri("/join")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        String email = "ILovePobi@naver.com";
+        String password = "aA1231!@";
+        signUp(name, email, password);
+
+        webTestClient.post().uri("/login")
                 .body(BodyInserters
-                        .fromFormData("name", name)
-                        .with("email", email)
-                        .with("password", password))
+                        .fromFormData(PASSWORD, password)
+                        .with(EMAIL, email))
+                .exchange()
+                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/;jsessionid=([0-9A-Z])+")
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void 로그인_실패_테스트() {
+        String name = "name";
+        String email = "ILovePobi@naver.com";
+        String password = "aA1231!@";
+        signUp(name, DEFAULT_EMAIL, DEFAULT_PASSWORD);
+
+        webTestClient.post().uri("/login")
+                .body(BodyInserters
+                        .fromFormData(PASSWORD, password)
+                        .with(EMAIL, email))
                 .exchange()
                 .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/err;jsessionid=([0-9A-Z])+")
                 .expectStatus().is3xxRedirection();
@@ -37,18 +83,21 @@ class UserControllerTest {
 
     @Test
     void 회원등록_테스트() {
-        String name = "name";
-        String email = "pobi@naver.com";
-        String password = "aA1231!@";
-        webTestClient.post()
-                .uri("/join")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("name", name)
-                        .with("email", email)
-                        .with("password", password))
-                .exchange()
-                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/login")
+        name = "name";
+        email = "pobi@naver.com";
+        password = "aA1231!@";
+        WebTestClient.ResponseSpec result = signUp(name, email, password);
+        result.expectHeader().valueMatches("Location", "http://localhost:[0-9]+/login")
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void 회원등록_실패_테스트() {
+        name = "name";
+        email = "pobi@naver.com";
+        password = "aA123";
+        WebTestClient.ResponseSpec result = signUp(name, email, password);
+        result.expectHeader().valueMatches("Location", "http://localhost:[0-9]+/err;jsessionid=([0-9A-Z])+")
                 .expectStatus().is3xxRedirection();
     }
 
@@ -60,25 +109,43 @@ class UserControllerTest {
     }
 
     @Test
-    void 로그인_테스트() {
-        String name = "name";
-        String email = "ILovePobi@naver.com";
-        String password = "aA1231!@";
-        webTestClient.post()
+    void 세션_테스트() {
+        signUp("alswns", DEFAULT_EMAIL, DEFAULT_PASSWORD);
+        webTestClient.get().uri("/mypage")
+                .header("Authorization", "Basic " + Base64Utils
+                        .encodeToString(("starkim06@naver.com:aA1231!!").getBytes(UTF_8)))
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void 회원등록_오류_테스트() {
+        name = "name";
+        email = "email";
+        password = "password";
+        WebTestClient.ResponseSpec result = signUp(name, email, password);
+        result.expectHeader().valueMatches("Location", "http://localhost:[0-9]+/err;jsessionid=([0-9A-Z])+")
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void 회원정보_수정_확인() {
+        signUp("alswns", DEFAULT_EMAIL, DEFAULT_PASSWORD);
+        webTestClient.get().uri("/mypage/edit")
+                .header("Authorization", "Basic " + Base64Utils
+                        .encodeToString(("starkim06@naver.com:aA1231!!").getBytes(UTF_8)))
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    private WebTestClient.ResponseSpec signUp(String name, String email, String password) {
+        return webTestClient.post()
                 .uri("/join")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters
-                        .fromFormData("name", name)
-                        .with("email", email)
-                        .with("password", password))
+                        .fromFormData(NAME, name)
+                        .with(EMAIL, email)
+                        .with(PASSWORD, password))
                 .exchange();
-
-        webTestClient.post().uri("/login")
-                .body(BodyInserters
-                        .fromFormData("password", password)
-                        .with("email", email))
-                .exchange()
-                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/;jsessionid=([0-9A-Z])+")
-                .expectStatus().is3xxRedirection();
     }
 }
