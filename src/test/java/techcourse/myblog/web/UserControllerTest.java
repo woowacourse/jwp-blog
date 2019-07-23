db6ee7c75fb8958287edab511c3aa7bf40228c50
pkch93 <star_final@naver.com>
2019-07-23 16:33:02 +0900

    test: AuthController, UserController class
    인증 관련 요청 및 유저 관련 요청 인수 테스트

--- src/test/java/techcourse/myblog/web/UserControllerTest.java
+++ src/test/java/techcourse/myblog/web/UserControllerTest.java
@@ -1,79 +1,187 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.dto.UserDto;
 
-import java.net.URI;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static techcourse.myblog.web.AuthControllerTest.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class UserControllerTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    private UserDto testUserDto;
+
+    @BeforeEach
+    void setUp() {
+        testUserDto = new UserDto();
+        testUserDto.setName("pkch");
+        testUserDto.setEmail("pkch@woowa.com");
+        testUserDto.setPassword("qwerqwer");
+    }
+
     @Test
     void 회원가입_페이지_접근_테스트() {
-        webTestClient.get().uri("/auth/signup")
+        webTestClient.get().uri("/users/signup")
                 .exchange()
                 .expectStatus()
                 .isOk();
     }
 
     @Test
-    void 회원_등록_테스트() {
-        회원_등록().expectStatus()
-                .is3xxRedirection();
+    void 회원_목록_페이지_접근_테스트() {
+        webTestClient.get().uri("/users")
+                .exchange()
+                .expectStatus().isOk();
     }
 
-    private WebTestClient.ResponseSpec 회원_등록() {
-        return webTestClient.post().uri("/auth/users")
+    @Test
+    void 같은_이메일의_회원이_등록되는_경우_회원가입_페이지_리다이렉팅_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        회원_등록(webTestClient, testUserDto).expectStatus().isFound()
+                .expectHeader()
+                .valueMatches("Location", ".*/users/signup");
+    }
+
+    @Test
+    void 회원가입시_이름이_10자_넘어가는_경우_회원가입_페이지_리다이렉팅_테스트() {
+        testUserDto.setName("내이름은열글자가넘어요");
+        회원_등록(webTestClient, testUserDto)
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/users/signup");
+    }
+
+    @Test
+    void 회원가입시_비밀번호가_8자_이하인_경우_회원가입_페이지_리다이렉팅_테스트() {
+        testUserDto.setPassword("hellopk");
+        회원_등록(webTestClient, testUserDto)
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/users/signup");
+    }
+
+    @Test
+    void 로그인_된_상태에서_회원가입_페이지_접근시_메인_페이지_리다이텍팅_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String sessionId = 로그인_세션_ID(webTestClient, testEmail, testPassword);
+        webTestClient.get().uri("/users/signup")
+                .header("cookie", sessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/");
+    }
+
+    @Test
+    void 로그인_상태에서_마이페이지_접근_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String jSessionId = 로그인_세션_ID(webTestClient, "pkch@woowa.com", "qwerqwer");
+        webTestClient.get().uri("/users/pkch@woowa.com")
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void 비로그인_상태에서_임의의_email로_마이페이지_접근시_로그인페이지_리다이렉팅_테스트() {
+        webTestClient.get().uri("/users/pkch@woowa.com")
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/auth/login");
+    }
+
+    @Test
+    void 로그인_상태에서_로그인_된_이메일이_아닌_다른_이메일uri로_접근시_메인페이지_리다이렉팅_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String jSessionId = 로그인_세션_ID(webTestClient, "pkch@woowa.com", "qwerqwer");
+        webTestClient.get().uri("/users/park@woowa.com")
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/");
+    }
+
+    @Test
+    void 로그인_상태에서_개인정보_수정페이지_접근_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String jSessionId = 로그인_세션_ID(webTestClient, "pkch@woowa.com", "qwerqwer");
+        webTestClient.get().uri("/users/pkch@woowa.com/edit")
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void 비로그인_상태에서_임의의_email로_개인정보_수정페이지_접근시_로그인페이지_리다이렉팅_테스트() {
+        webTestClient.get().uri("/users/pkch@woowa.com/edit")
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/auth/login");
+    }
+
+    @Test
+    void 로그인_상태에서_로그인_된_이메일이_아닌_다른_이메일uri로_수정페이지_접근시_메인페이지_리다이렉팅_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String jSessionId = 로그인_세션_ID(webTestClient, "pkch@woowa.com", "qwerqwer");
+        webTestClient.get().uri("/users/park@woowa.com/edit")
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/");
+    }
+
+    @Test
+    void 수정_정상_흐름_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String jSessionId = 로그인_세션_ID(webTestClient, "pkch@woowa.com", "qwerqwer");
+        webTestClient.put().uri("/users/pkch@woowa.com")
+                .header("cookie", jSessionId)
+                .body(BodyInserters.fromFormData("name", "park"))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/users/pkch@woowa.com");
+    }
+
+    @Test
+    void 비로그인_상태에서_수정_요청시_로그인_페이지_리다이렉팅_테스트() {
+        webTestClient.put().uri("/users/park@woowa.com")
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/auth/login");
+    }
+
+    @Test
+    void 회원_탈퇴_정상_흐름_테스트() {
+        회원_등록(webTestClient, testUserDto);
+        String jSessionId = 로그인_세션_ID(webTestClient, "pkch@woowa.com", "qwerqwer");
+        webTestClient.delete().uri("/users/pkch@woowa.com")
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/");
+    }
+
+    @Test
+    void 비로그인_상테에서_회원_탈퇴시_로그인_페이지_리다이렉팅_테스트() {
+        webTestClient.delete().uri("/users/pkch@woowa.com")
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/auth/login");
+    }
+
+    static WebTestClient.ResponseSpec 회원_등록(WebTestClient webTestClient, UserDto userDto) {
+        return webTestClient.post().uri("/users")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(
-                        BodyInserters.fromFormData("name", "pkch")
-                                .with("email", "pkch@woowa.com")
-                                .with("password", "qwerqwer")
+                        BodyInserters.fromFormData("name", userDto.getName())
+                                .with("email", userDto.getEmail())
+                                .with("password", userDto.getPassword())
                 ).exchange();
-    }
-
-    @Test
-    void 로그인_페이지_접근_테스트() {
-        webTestClient.get().uri("/auth/login")
-                .exchange()
-                .expectStatus()
-                .isOk();
-    }
-
-    @Test
-    void 로그인_안된_경우_회원_목록_페이지_접근_테스트() {
-        webTestClient.get().uri("/auth/users")
-                .exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody()
-                .consumeWith(res -> {
-                    URI redirectUri = res.getResponseHeaders().getLocation();
-                    assertThat(redirectUri.getPath()).isEqualTo("/auth/login");
-                });
-    }
-
-    @Test
-    void 로그인_정상_흐름_테스트() {
-        회원_등록().expectStatus().is3xxRedirection();
-        webTestClient.post().uri("/auth/login")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(
-                        BodyInserters.fromFormData("email", "pkch@woowa.com")
-                                .with("password", "qwerqwer")
-                ).exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody()
-                .consumeWith(redirectRes -> {
-                    URI redirectUri = redirectRes.getResponseHeaders().getLocation();
-                    assertThat(redirectUri.getPath()).isEqualTo(URI.create("/"));
-                });
     }
 }
