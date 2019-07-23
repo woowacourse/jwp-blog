acb0d124517248193b2a28994932ec83bbf68cba
pkch93 <star_final@naver.com>
2019-07-23 17:39:54 +0900

    refactor: NotMatchPasswordException class
    현재 인증을 이메일과 비밀번호를 받아서 하고 있으므로 이에 맞게
    NotMatchAuthenticatedException으로 변경

--- src/main/java/techcourse/myblog/exception/NotMatchPasswordException.java
+++
@@ -1,7 +0,0 @@
-package techcourse.myblog.exception;
-
-public class NotMatchPasswordException extends RuntimeException {
-    public NotMatchPasswordException(final String message) {
-        super(message);
-    }
-}
