0a1cfa6577549128f87d615ded3fcafc89ce0c56
ddu0422 <ddu0422@naver.com>
2019-07-23 20:05:56 +0900

    refactor: add validation to User class

--- src/main/java/techcourse/myblog/domain/User.java
+++ src/main/java/techcourse/myblog/domain/User.java
@@ -3,8 +3,11 @@ package techcourse.myblog.domain;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import techcourse.myblog.support.validation.UserGroups;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @NoArgsConstructor
@@ -15,9 +18,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Pattern(regexp = "^[a-zA-Z가-힣]{2,10}$",
+            message = "이름은 2~10자로 제한하며 숫자나 특수문자가 포함될 수 없습니다.",
+            groups = {UserGroups.Edit.class, UserGroups.All.class})
     private String name;
+
     @Column(unique = true)
+    @Email
     private String email;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "비밀번호는 8자 이상의 소문자, 대문자, 숫자, 특수문자의 조합입니다.")
     private String password;
 
     private User(String name, String email, String password) {
--- src/test/java/techcourse/myblog/service/dto/UserDtoTest.java
+++ src/test/java/techcourse/myblog/service/dto/UserDtoTest.java
@@ -1,57 +1,35 @@
 package techcourse.myblog.service.dto;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import techcourse.myblog.support.ValidatorTests;
 import techcourse.myblog.support.validation.UserGroups.*;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-public class UserDtoTest {
-
-    private Validator validator;
-
-    @BeforeEach
-    void setUp() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
-    }
-
+public class UserDtoTest extends ValidatorTests {
     @Test
     void 올바른_이름_테스트() {
         UserDto userDto = new UserDto("abc", "abcd@naver.com", "aA!123456789");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertTrue(violations.isEmpty());
+        checkValidate(userDto, true);
     }
 
     @Test
     void 올바르지_않은_문자가들어간_이름_테스트() {
         UserDto userDto = new UserDto("aaa!!", "abcd@naver.com", "aA!123456789");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto, Edit.class);
-        assertFalse(violations.isEmpty());
+        checkValidate(userDto, false, Edit.class);
     }
 
     @Test
     void 두_글자_미만의_이름_테스트() {
         UserDto userDto = new UserDto("a", "abcd@naver.com", "aA!123456789");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto, Edit.class);
-        assertFalse(violations.isEmpty());
+        checkValidate(userDto, false, Edit.class);
     }
 
     @Test
     void 열_글자_초과의_이름_테스트() {
         UserDto userDto = new UserDto("abcdfabcdfe", "abcd@naver.com", "aA!123456789");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto, Edit.class);
-        assertFalse(violations.isEmpty());
+        checkValidate(userDto, false, Edit.class);
     }
 
     /**
@@ -65,14 +43,12 @@ public class UserDtoTest {
     @CsvSource(value = {"12345678", "1234aA!", "12345678A!", "12345678a!", "12345678aA"})
     void 올바르지_않은_비밀번호_테스트(String password) {
         UserDto userDto = new UserDto("abcd", "abcd@naver.com", password);
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertFalse(violations.isEmpty());
+        checkValidate(userDto, false);
     }
 
     @Test
     void 올바르지_않은_이메일_테스트() {
         UserDto userDto = new UserDto("abcd", "abcdnaver.com", "aA!123456789");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertFalse(violations.isEmpty());
+        checkValidate(userDto, false);
     }
 }
