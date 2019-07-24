dd3d539b5f046a34006b73fce7ccfd027f9fa2c4
이영현 <leey9494@icloud.com>
2019-07-24 17:31:18 +0900

    MOD : @Column값 수정

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -16,12 +16,12 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "title", length = 10, nullable = false)
+    @Column(name = "title", length = 500, nullable = false)
     private String title;
 
-    @Column(name = "contents", length = 100, nullable = false)
+    @Column(name = "contents", length = 6000, nullable = false)
     private String contents;
 
-    @Column(name = "coverUrl", length = 60, nullable = false)
+    @Column(name = "coverUrl", length = 1000, nullable = false)
     private String coverUrl;
 }
