7c5c69008481bc60f96bc4ca8cda8d4971b97497
Soojin Roh <sooreal91@gmail.com>
2019-07-24 19:53:24 +0900

    Refactor : ERROR MESSAGE 별도의 클래스로 분리

--- src/main/java/techcourse/myblog/service/dto/UserRequestDto.java
+++ src/main/java/techcourse/myblog/service/dto/UserRequestDto.java
@@ -7,14 +7,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import static techcourse.myblog.service.utils.Messages.*;
+
 public class UserRequestDto {
-    public static final String NAME_LENGTH_ERROR = "2자이상 10자미만으로 작성해야 합니다.";
-    public static final String NAME_FORMAT_ERROR = "숫자나 특수문자가 포함되었습니다.";
-    public static final String NAME_BLANK_ERROR = "이름을 작성해주세요.";
-    public static final String EMAIL_FORMAT_ERROR = "메일의 양식을 지켜주세요.";
-    public static final String EMAIL_BLANK_ERROR = "메일을 작성해주세요.";
-    public static final String PASSWORD_FORMAT_ERROR = "비밀번호는 8자 이상의 소문자, 대문자, 숫자, 특수문자로 이루어져야 합니다.";
-    public static final String PASSWORD_BLANK_ERROR = "비밀번호를 작성해주세요.";
+
     private static final String PASSWORD_REGEXP = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}";
     private static final String NAME_REGEXP = "[a-zA-Z가-힣]{2,10}";
 
