495e5dd663e2e8f96bba520cb02e83060c608837
gch01410 <gch01410@naver.com>
2019-07-25 02:03:25 +0900

    test: Remove duplicated block

--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -1,23 +1,27 @@
 package techcourse.myblog.web;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.domain.article.Article;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTests {
     @Autowired
     private WebTestClient webTestClient;
+
+    private FluxExchangeResult fluxExchangeResult;
     private String title;
     private String coverUrl;
     private String contents;
@@ -28,33 +32,41 @@ public class ArticleControllerTests {
         coverUrl = "";
         contents = "contents";
 
-        webTestClient.post().uri("/articles")
+        fluxExchangeResult = webTestClient.post().uri("/articles")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters
                         .fromFormData("title", title)
                         .with("coverUrl", coverUrl)
                         .with("contents", contents))
                 .exchange()
-                .expectStatus().is3xxRedirection();
+                .returnResult(Article.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        webTestClient.delete().uri(fluxExchangeResult.getResponseHeaders().getLocation())
+                .exchange();
     }
 
     @Test
     void 메인화면() {
         webTestClient.get().uri("/")
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus()
+                .isOk();
     }
 
     @Test
     void 게시글_생성_페이지_이동() {
         webTestClient.get().uri("/writing")
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus()
+                .isOk();
     }
 
     @Test
     void 게시글_생성() {
-        webTestClient.post().uri("/articles")
+        EntityExchangeResult result = webTestClient.post().uri("/articles")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters
                         .fromFormData("title", title)
@@ -63,108 +75,95 @@ public class ArticleControllerTests {
                 .exchange()
                 .expectStatus()
                 .is3xxRedirection()
+                .expectHeader()
+                .valueMatches("location", ".*/articles/.")
+                .expectBody()
+                .returnResult();
+
+        webTestClient.get().uri(result.getResponseHeaders().getLocation())
+                .exchange()
                 .expectBody()
                 .consumeWith(response -> {
-                    webTestClient.get()
-                            .uri("/articles/2")
-                            .exchange()
-                            .expectStatus().isOk()
-                            .expectBody()
-                            .consumeWith(response2 -> {
-                                String body = new String(response2.getResponseBody());
-                                assertThat(body.contains(title)).isTrue();
-                                assertThat(body.contains(coverUrl)).isTrue();
-                                assertThat(body.contains(contents)).isTrue();
-                            });
+                    String body = new String(Objects.requireNonNull(response.getResponseBody()));
+                    assertThat(body).contains(title);
+                    assertThat(body).contains(coverUrl);
+                    assertThat(body).contains(contents);
                 });
+
+        webTestClient.delete().uri(result.getResponseHeaders().getLocation())
+                .exchange();
     }
 
     @Test
     void 게시글_조회() {
-        title = "title";
-        coverUrl = "";
-        contents = "contents";
-
-        EntityExchangeResult result = webTestClient.post().uri("/articles")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("title", title)
-                        .with("coverUrl", coverUrl)
-                        .with("contents", contents))
+        webTestClient.get().uri(fluxExchangeResult.getResponseHeaders().getLocation())
                 .exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody().returnResult();
-
-        webTestClient.get().uri(result.getResponseHeaders().getLocation())
-                .exchange()
-                .expectStatus().isOk()
+                .expectStatus()
+                .isOk()
                 .expectBody()
                 .consumeWith(response -> {
-                    String body = new String(response.getResponseBody());
-                    assertThat(body.contains("title")).isTrue();
-                    assertThat(body.contains("")).isTrue();
-                    assertThat(body.contains("contents")).isTrue();
+                    String body = new String(Objects.requireNonNull(response.getResponseBody()));
+                    assertThat(body).contains(title);
+                    assertThat(body).contains(coverUrl);
+                    assertThat(body).contains(contents);
                 });
     }
 
     @Test
     void 게시글_수정() {
-
-        EntityExchangeResult result = webTestClient.post().uri("/articles")
+        FluxExchangeResult result = webTestClient.put().uri(fluxExchangeResult.getResponseHeaders().getLocation())
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters
-                        .fromFormData("title", title)
+                        .fromFormData("title", "newTitle")
                         .with("coverUrl", coverUrl)
-                        .with("contents", contents))
-                .exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody().returnResult();
-
-        String title = "newTitle";
-        String coverUrl = "";
-        String contents = "contents";
-
-        webTestClient.put().uri(result.getResponseHeaders().getLocation())
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("title", title)
-                        .with("coverUrl", coverUrl)
-                        .with("contents", contents))
+                        .with("contents", "newContents"))
                 .exchange()
                 .expectStatus()
                 .is3xxRedirection()
+                .expectHeader()
+                .valueMatches("location", ".*/articles/.")
+                .returnResult(Article.class);
+
+        webTestClient.get().uri(result.getResponseHeaders().getLocation())
+                .exchange()
                 .expectBody()
                 .consumeWith(response -> {
-                    webTestClient.get()
-                            .uri(response.getResponseHeaders().getLocation())
-                            .exchange()
-                            .expectStatus().isOk()
-                            .expectBody()
-                            .consumeWith(response2 -> {
-                                String body = new String(response2.getResponseBody());
-                                assertThat(body.contains(title)).isTrue();
-                                assertThat(body.contains(coverUrl)).isTrue();
-                                assertThat(body.contains(contents)).isTrue();
-                            });
+                    String body = new String(Objects.requireNonNull(response.getResponseBody()));
+                    assertThat(body).contains("newTitle");
+                    assertThat(body).contains(coverUrl);
+                    assertThat(body).contains("newContents");
                 });
-
     }
 
     @Test
     void 게시글_삭제() {
-        webTestClient.delete().uri("/articles/1")
+        FluxExchangeResult result = webTestClient.post().uri("/articles")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("contents", contents))
                 .exchange()
-                .expectStatus().is3xxRedirection();
+                .returnResult(Article.class);
 
-        webTestClient.get().uri("/articles/1")
+        webTestClient.delete().uri(result.getResponseHeaders().getLocation())
                 .exchange()
-                .expectStatus().isNoContent();
+                .expectStatus()
+                .is3xxRedirection()
+                .expectHeader()
+                .valueMatches("location", ".*/");
+
+        webTestClient.get().uri(result.getResponseHeaders().getLocation())
+                .exchange()
+                .expectStatus()
+                .isNoContent();
     }
 
     @Test
     void 게시글_수정_페이지_이동() {
-        webTestClient.get().uri("/articles/1/edit")
+        webTestClient.get().uri(fluxExchangeResult.getResponseHeaders().getLocation() + "/edit")
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus()
+                .isOk();
     }
 }
