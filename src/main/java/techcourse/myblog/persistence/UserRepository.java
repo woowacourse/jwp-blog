878916752b4e9ec1361eea71a30f9b03bae60ca1
Soojin Roh <sooreal91@gmail.com>
2019-07-24 19:50:57 +0900

    Refactor : 사용하지 않는 method 삭제

--- src/main/java/techcourse/myblog/persistence/UserRepository.java
+++ src/main/java/techcourse/myblog/persistence/UserRepository.java
@@ -3,11 +3,7 @@ package techcourse.myblog.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import techcourse.myblog.domain.User;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findUsersByEmail(String email);
-
     User findUserByEmail(String email);
 
     User findUserById(long id);
