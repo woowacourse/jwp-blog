bad307e0d284cf6210fb379af56804f8382f0c23
강연욱 <fiasboc23@gmail.com>
2019-07-24 18:13:25 +0900

    feat: create PageRequest class and validate page request data

--- src/main/java/techcourse/myblog/service/ArticleService.java
+++ src/main/java/techcourse/myblog/service/ArticleService.java
@@ -3,7 +3,6 @@ package techcourse.myblog.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,8 +31,8 @@ public class ArticleService {
 
     @Transactional(readOnly = true)
     public List<Article> findAll() {
-        PageRequest request = PageRequest.of(START_PAGE, VIEW_ARTICLE_COUNT, Sort.Direction.DESC, ID);
-        Page<Article> page = articleRepository.findAll(request);
+        PageRequest pageRequest = new PageRequest(START_PAGE, VIEW_ARTICLE_COUNT, Sort.Direction.DESC, ID);
+        Page<Article> page = articleRepository.findAll(pageRequest.of());
         log.info("page : {} ", page.getContent());
         return page.getContent();
     }
