f5e1a64fa5690de63d6338f4ec33245e1e36e733
이영현 <leey9494@icloud.com>
2019-07-26 00:55:15 +0900

    MOD : NotBlank어노테이션 추가

--- src/main/java/techcourse/myblog/dto/ArticleDto.java
+++ src/main/java/techcourse/myblog/dto/ArticleDto.java
@@ -3,10 +3,17 @@ package techcourse.myblog.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Setter
 @Getter
 public class ArticleDto {
+    @NotBlank(message = "제목을 작성해주세요.")
     private String title;
+
+    @NotBlank(message = "내용을 작성해주세요.")
     private String contents;
+
+    @NotBlank(message = "url을 작성해주세요.")
     private String coverUrl;
 }
