2620ab848fc2f06aa0f27a1b4978eb3f1b7a3533
backlo <hjun4512@naver.com>
2019-07-25 16:08:27 +0900

    feat: init project

--- src/main/java/techcourse/myblog/controller/ArticleController.java
+++ src/main/java/techcourse/myblog/controller/ArticleController.java
@@ -1,8 +1,8 @@
-package techcourse.myblog.web;
+package techcourse.myblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import techcourse.myblog.domain.ArticleRepository;
+import techcourse.myblog.repository.ArticleRepository;
 
 @Controller
 public class ArticleController {
---
+++ src/main/java/techcourse/myblog/model/Article.java
@@ -0,0 +1,4 @@
+package techcourse.myblog.model;
+
+public class Article {
+}
