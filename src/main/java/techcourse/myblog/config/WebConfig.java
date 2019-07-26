8935683f2ad909cd1e6a809c7fe68dff5e21fd14
wbluke <pwb0127@naver.com>
2019-07-26 12:07:16 +0900

    [FIX] 로그인하지 않은 User가 게시글을 볼 수 없는 문제 수정

--- src/main/java/techcourse/myblog/config/WebConfig.java
+++ src/main/java/techcourse/myblog/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/mypage/**", "/articles/**");
+                .addPathPatterns("/mypage/**", "/articles/**")
+                .excludePathPatterns("/articles/*");
     }
 }
