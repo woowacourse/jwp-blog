dba35848d7fbaa2ea7b7333d954353cf8e65699c
green4469 <green4469@gmail.com>
2019-07-26 17:25:46 +0900

    chore: add data initialization file for test

---
+++ src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO User (name, password, email) VALUES ('abcdeFGHI', 'abcdEFGH123!@#', 'abcd@hi.com');
+INSERT INTO Article (title, contents, cover_url) VALUES ('testTitle', 'testContents', 'testCoverUrl');
