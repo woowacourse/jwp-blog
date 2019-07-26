29b515a635ce528fca1c719145a6c0ea84e2ff41
codeanddonuts <kkw200@naver.com>
2019-07-26 16:25:13 +0900

    feat: Comment class

---
+++ src/main/java/techcourse/myblog/domain/Comment.java
@@ -0,0 +1,40 @@
+package techcourse.myblog.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDateTime date;
+    private User author;
+    private String contents;
+
+    public Comment() {}
+
+    public Comment(User author, String contents) {
+        this.author = Optional.ofNullable(author).orElseThrow(IllegalArgumentException::new);
+        this.contents = Optional.ofNullable(contents).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public LocalDateTime getDate() {
+        return this.date;
+    }
+
+    public User getAuthor() {
+        return this.author;
+    }
+    public String getContents() {
+        return this.contents;
+    }
+}
