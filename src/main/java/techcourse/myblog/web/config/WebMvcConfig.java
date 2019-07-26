bd4e919e36ec5c05711c9467c8909817ecd9ce7b
school0bhy <house1217@ajou.ac.kr>
2019-07-26 12:01:49 +0900

    refactor: ArgumentResolver 구현

--- src/main/java/techcourse/myblog/web/config/WebMvcConfig.java
+++ src/main/java/techcourse/myblog/web/config/WebMvcConfig.java
@@ -1,11 +1,16 @@
-package techcourse.myblog.web;
+package techcourse.myblog.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.myblog.web.support.LoginUserHandlerMethodArgumentResolver;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -17,5 +22,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Bean
+    public LoginUserHandlerMethodArgumentResolver loginUserMethodArgumentResolver() {
+        LoginUserHandlerMethodArgumentResolver bean = new LoginUserHandlerMethodArgumentResolver();
+        return bean;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserMethodArgumentResolver());
     }
 }
---
+++ src/main/java/techcourse/myblog/web/support/LoginUserHandlerMethodArgumentResolver.java
@@ -0,0 +1,29 @@
+package techcourse.myblog.web.support;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.web.LoginUser;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LoginUserHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType() == LoginUser.class;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest req = (HttpServletRequest) webRequest.getNativeRequest();
+
+        LoginUser loginUser = new LoginUser();
+        User user = (User) req.getSession().getAttribute("user");
+        loginUser.setUser(user);
+
+        return loginUser;
+    }
+}
