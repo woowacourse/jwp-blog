7b9fdf8f816ec9dc68069a85200863c08f2303a4
박효진 <hgenie14@gmail.com>
2019-07-26 16:54:18 +0900

    feat: 뷰 세션에서 User 객체가 아닌 UserResponseDto 객체를 갖도록 수정
    1. UserResponseDto 클래스를 추가한다
    2. UserController 클래스에서 세션에 User 객체 대신 UserResponseDto 객체를 사용하도록 수정한다

--- src/main/java/techcourse/myblog/utils/converter/DtoConverter.java
+++ src/main/java/techcourse/myblog/utils/converter/DtoConverter.java
@@ -1,16 +1,18 @@
 package techcourse.myblog.utils.converter;
-
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.dto.ArticleRequestDto;
 import techcourse.myblog.dto.UserRequestDto;
+import techcourse.myblog.dto.UserResponseDto;
 
 public class DtoConverter {
     public static User convert(UserRequestDto dto) {
         return new User(dto.getName(), dto.getPassword(), dto.getEmail());
     }
-
     public static Article convert(ArticleRequestDto dto) {
         return new Article(dto.getTitle(), dto.getContents(), dto.getCoverUrl());
+    }
+    public static UserResponseDto convert(User user) {
+        return new UserResponseDto(user.getName(), user.getEmail());
     }
 }
