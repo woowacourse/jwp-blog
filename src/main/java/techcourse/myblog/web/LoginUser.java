0d282616446fb996aed648cf6616f56ea1f863f5
school0bhy <house1217@ajou.ac.kr>
2019-07-26 12:01:49 +0900

    refactor: ArgumentResolver 구현

---
+++ src/main/java/techcourse/myblog/web/LoginUser.java
@@ -0,0 +1,15 @@
+package techcourse.myblog.web;
+
+import techcourse.myblog.domain.User;
+
+public class LoginUser {
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
