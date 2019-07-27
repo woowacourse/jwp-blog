5896d7230f7c3869139f4a8ba889714ecd90b2a5
DaejunBae <dpudpu11@gmail.com>
2019-07-27 18:06:17 +0900

    Revert "remove : 모두 삭제하고 처음부터 시작"
    This reverts commit 41598eec13e0db015b2d39b73e306189f6975a7c.

---
+++ src/test/java/techcourse/myblog/articles/ArticleTest.java
@@ -0,0 +1,18 @@
+package techcourse.myblog.articles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArticleTest {
+    @Test
+    void builder() {
+        Article article = Article.builder()
+                .title("title")
+                .contents("contents")
+                .coverUrl("coverUrl")
+                .build();
+
+        assertThat(article).isNotNull();
+    }
+}
