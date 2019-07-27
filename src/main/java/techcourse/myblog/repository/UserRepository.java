9d7f4323dbccd7f5a44b22f5d8c200162ca66bb3
starkim06 <starkim06@naver.com>
2019-07-27 17:17:02 +0900

    refactor: JpaRepository를 상속받도록 수정

--- src/main/java/techcourse/myblog/repository/UserRepository.java
+++ src/main/java/techcourse/myblog/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package techcourse.myblog.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import techcourse.myblog.domain.user.User;
 import techcourse.myblog.domain.user.UserEmail;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(UserEmail email);
 }
+
