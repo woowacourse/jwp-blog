de826f911766c0e846c6b252e8a2bc45cddc813d
andole87 <andole87@gmail.com>
2019-07-28 21:09:10 +0900

    test: 댓글 기능 테스트
    1. CommnetRepositoryTest
    2. CommnetServiceTest

--- src/test/java/techcourse/myblog/service/UserServiceTest.java
+++ src/test/java/techcourse/myblog/service/UserServiceTest.java
@@ -1,10 +1,10 @@
 package techcourse.myblog.service;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import techcourse.myblog.domain.user.User;
 import techcourse.myblog.dto.UserDto;
 import techcourse.myblog.repository.UserRepository;
@@ -21,13 +21,10 @@ class UserServiceTest {
     private UserRepository userRepository;
 
     @BeforeEach
+    @Transactional
     void setUp() {
-        userRepository.save(new User("andole", "A!1bcdefg", "andole@gmail.com"));
-    }
-
-    @AfterEach
-    void tearDown() {
         userRepository.deleteAll();
+        userRepository.saveAndFlush(new User("andole", "A!1bcdefg", "andole@gmail.com"));
     }
 
     @Test
