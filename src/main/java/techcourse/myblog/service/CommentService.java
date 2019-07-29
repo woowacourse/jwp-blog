e577fc6d85e169c2b0f5c539fc1445b2c5062216
pkch93 <star_final@naver.com>
2019-07-29 17:47:51 +0900

    feat : 댓글 수정 기능 구현

--- src/main/java/techcourse/myblog/service/CommentService.java
+++ src/main/java/techcourse/myblog/service/CommentService.java
@@ -10,9 +10,11 @@ import techcourse.myblog.exception.NotMatchAuthenticationException;
 import techcourse.myblog.repository.ArticleRepository;
 import techcourse.myblog.repository.CommentRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class CommentService {
 
     private final ArticleRepository articleRepository;
@@ -35,11 +37,21 @@ public class CommentService {
         return commentRepository.findAllByArticleId(articleId);
     }
 
+    public void update(final String content, final Long commentId, final User user) {
+        Comment comment = findById(commentId, user);
+        comment.update(content);
+    }
+
     public void delete(final Long commentId, final User user) {
+        Comment comment = findById(commentId, user);
+        commentRepository.delete(comment);
+    }
+
+    private Comment findById(final Long commentId, final User user) {
         Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new CommentNotFoundException("해당 댓글을 찾을 수 없습니다"));
         if (!comment.getUser().equals(user)) {
             throw new NotMatchAuthenticationException("권한이 없습니다.");
         }
-        commentRepository.delete(comment);
+        return comment;
     }
 }
