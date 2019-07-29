da5c8c1b422d93be4996a002e6b7d9745b9d7e94
DaejunBae <dpudpu11@gmail.com>
2019-07-30 01:09:03 +0900

    refactor: ExceptionAdvice.handleAuthException() 수정

--- src/main/java/techcourse/myblog/advice/ExceptionAdvice.java
+++ src/main/java/techcourse/myblog/advice/ExceptionAdvice.java
@@ -3,10 +3,10 @@ package techcourse.myblog.advice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 import techcourse.myblog.exception.AuthException;
 
 @ControllerAdvice
@@ -15,13 +15,12 @@ public class ExceptionAdvice {
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AuthException.class)
-    public String handleAuthException(Exception e) {
+    public String handleAuthException(Exception e, Model model) {
 
         log.info(e.toString());
 
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", e);  //예외를 뷰에 던져서 주자.
-
-        return "redirect:/";
+        model.addAttribute("errorMessage", e.getMessage());
+        model.addAttribute("path", "/");
+        return "error";
     }
 }
