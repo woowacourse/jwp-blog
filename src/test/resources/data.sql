922158f2c4e5ad6d728c2b9b673643c1c50cc61d
green4469 <green4469@gmail.com>
2019-07-29 16:16:31 +0900

    feat: add CommentRepository

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO User (name, password, email) VALUES ('abcdeFGHI', 'abcdEFGH123!@#', 'abc@hi.com');
-INSERT INTO Article (title, contents, cover_url) VALUES ('testTitle', 'testContents', 'testCoverUrl');
+INSERT INTO Article (title, contents, cover_url, author) VALUES ('testTitle', 'testContents', 'testCoverUrl', 1);
+
+INSERT INTO Comment (comment, commenter, article, created_at) VALUES ('second', 1, 1, '2012-10-19 10:31:38');
+INSERT INTO Comment (comment, commenter, article, created_at) VALUES ('third', 1, 1, '2012-10-29 10:31:38');
+INSERT INTO Comment (comment, commenter, article, created_at) VALUES ('first', 1, 1, '2012-09-19 10:31:38');
