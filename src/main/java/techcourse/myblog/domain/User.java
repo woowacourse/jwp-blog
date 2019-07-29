40ea81fdfed78948e98003d6f412d4fee909179f
pkch93 <star_final@naver.com>
2019-07-29 16:43:19 +0900

    style: 전체 소스코드 포메팅

--- src/main/java/techcourse/myblog/domain/User.java
+++ src/main/java/techcourse/myblog/domain/User.java
@@ -1,13 +1,10 @@
 package techcourse.myblog.domain;
 
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
