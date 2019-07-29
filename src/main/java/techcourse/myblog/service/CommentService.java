0660d1b196d006dae4e515ab252236bed9f46b5f
aiden <cnk9109@gmail.com>
2019-07-29 18:28:18 +0900

    feat(CommentService): add findByArticle method

--- src/main/java/techcourse/myblog/service/CommentService.java
+++ src/main/java/techcourse/myblog/service/CommentService.java
@@ -1,9 +1,12 @@
 package techcourse.myblog.service;
 
 import org.springframework.stereotype.Service;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
 import techcourse.myblog.repository.CommentRepository;
 import techcourse.myblog.web.dto.CommentDto;
+
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -16,6 +19,10 @@ public class CommentService {
     public Comment findById(Long commentId) {
         return commentRepository.findById(commentId)
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public List<Comment> findByArticle(Article article) {
+        return commentRepository.findByArticle(article);
     }
 
     public Comment save(CommentDto commentDto) {
