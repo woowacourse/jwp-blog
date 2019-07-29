bddcd4e32d98ccaac9b7e3d3dbafd41c43056080
김성훈 <seonghun127@gmail.com>
2019-07-29 22:05:54 +0900

    feat: 댓글 수정 기능 구현

--- src/main/java/techcourse/myblog/comment/controller/CommentController.java
+++ src/main/java/techcourse/myblog/comment/controller/CommentController.java
@@ -2,11 +2,9 @@ package techcourse.myblog.comment.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import techcourse.myblog.argumentresolver.UserSession;
 import techcourse.myblog.comment.dto.CommentDto;
@@ -21,6 +19,12 @@ import javax.validation.Valid;
 public class CommentController {
 
     private final CommentService commentService;
+
+    @GetMapping("/comments/{commentId}/edit")
+    public String renderCommentEditPage(@PathVariable long commentId, Model model) {
+        model.addAttribute("comments", commentService.findById(commentId));
+        return "comment-edit";
+    }
 
     @PostMapping("/articles/{articleId}/comments")
     public RedirectView createComment(@PathVariable long articleId, UserSession userSession, CommentDto.Creation commentDto) {
