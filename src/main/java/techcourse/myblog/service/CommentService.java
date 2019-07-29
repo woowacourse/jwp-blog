f932cff6f848bd34ce797e333a68240bf22b2794
pkch93 <star_final@naver.com>
2019-07-29 16:42:22 +0900

    feat: ArticleController, CommentService class
    댓글 추가 기능 구현

---
+++ src/main/java/techcourse/myblog/service/CommentService.java
@@ -0,0 +1,36 @@
+package techcourse.myblog.service;
+
+import org.aspectj.weaver.ast.Not;
+import org.springframework.stereotype.Service;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.Comment;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.exception.ArticleNotFoundException;
+import techcourse.myblog.repository.ArticleRepository;
+import techcourse.myblog.repository.CommentRepository;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+
+    public CommentService(final ArticleRepository articleRepository, final CommentRepository commentRepository) {
+        this.articleRepository = articleRepository;
+        this.commentRepository = commentRepository;
+    }
+
+
+    public Comment create(final Long articleId, final User user, final Comment comment) {
+        Article article = articleRepository.findById(articleId).orElseThrow(() -> new ArticleNotFoundException("해당 게시물이 없습니다."));
+
+        comment.initialize(user, article);
+        return commentRepository.save(comment);
+    }
+
+    public List<Comment> findAllByArticleId(Long articleId) {
+        return commentRepository.findAllByArticleId(articleId);
+    }
+}
