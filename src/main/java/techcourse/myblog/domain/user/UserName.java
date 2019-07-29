ac63f68dbecc305682e018d757adbe5adc63fe93
starkim06 <starkim06@naver.com>
2019-07-29 20:26:03 +0900

    refactor: 코딩 컨벤션 수정

--- src/main/java/techcourse/myblog/domain/user/UserName.java
+++ src/main/java/techcourse/myblog/domain/user/UserName.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class UserName {
     private static final Pattern NAME_PATTERN = Pattern.compile("^[ㄱ-ㅎ가-힣a-zA-Z]{2,10}$");
     private static final String NAME_ERROR = "이름은 2자에서 10자 사이여야 합니다.";
+
     private String userName;
 
     public UserName(String userName) {
