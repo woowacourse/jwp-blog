fc61a820ccfffc59594aece9c9f7acc24e006c7d
DaejunBae <dpudpu11@gmail.com>
2019-07-29 20:49:28 +0900

    feat: Comment entity

---
+++ src/main/java/techcourse/myblog/comments/Comment.java
@@ -0,0 +1,46 @@
+package techcourse.myblog.comments;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import techcourse.myblog.articles.Article;
+import techcourse.myblog.users.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = "id")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String contents;
+
+    @CreatedDate
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime modifiedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_comment_to_user"), nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="article_id",foreignKey = @ForeignKey(name = "fk_comment_to_article"), nullable = false)
+    private Article article;
+
+    @Builder
+    public Comment(final String contents, final User user, final Article article) {
+        this.contents = contents;
+        this.user = user;
+        this.article = article;
+    }
+}
