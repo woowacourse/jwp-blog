18322716e3d347da5158899c9d1eaac140333833
DaejunBae <dpudpu11@gmail.com>
2019-07-30 16:21:56 +0900

    Revert "feat: CommentDto.Update 추가"
    This reverts commit 8b0bea5deb64a3c6359273d603c67dbc85f371a6.

--- src/main/java/techcourse/myblog/articles/comments/CommentDto.java
+++ src/main/java/techcourse/myblog/articles/comments/CommentDto.java
@@ -6,8 +6,6 @@ import lombok.Setter;
 import techcourse.myblog.articles.Article;
 import techcourse.myblog.users.User;
 
-import java.time.LocalDateTime;
-
 public class CommentDto {
 
     @Getter
@@ -38,24 +36,6 @@ public class CommentDto {
             return Comment.builder()
                     .contents(contents)
                     .build();
-        }
-    }
-
-    @Getter
-    @NoArgsConstructor
-    public static class Response {
-        private Long id;
-        private String contents;
-        private LocalDateTime modifiedDate;
-
-        Response(Comment comment) {
-            this.id = comment.getId();
-            this.contents = comment.getContents();
-            this.modifiedDate = comment.getModifiedDate();
-        }
-
-        public static Response createByComment(Comment comment) {
-            return new Response(comment);
         }
     }
 }
