53210e9a33ac41f5dbf8b932acdbd5b795556ed5
hyun <jhs695@naver.com>
2019-07-30 13:45:52 +0900

    add comment 추가 삭제 수정 기능 추가

--- src/main/java/techcourse/myblog/application/dto/CommentDto.java
+++ src/main/java/techcourse/myblog/application/dto/CommentDto.java
@@ -1,7 +1,22 @@
 package techcourse.myblog.application.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class CommentDto {
+    private long id;
     private String contents;
+    private String userName;
+    private LocalDate localDate;
+    private LocalTime localTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getContents() {
         return contents;
@@ -9,5 +24,29 @@ public class CommentDto {
 
     public void setContents(String contents) {
         this.contents = contents;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
     }
 }
--- src/main/java/techcourse/myblog/application/service/CommentService.java
+++ src/main/java/techcourse/myblog/application/service/CommentService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import techcourse.myblog.application.dto.CommentDto;
 import techcourse.myblog.domain.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CommentService {
 
@@ -31,4 +34,36 @@ public class CommentService {
     }
 
 
+    public List<CommentDto> findAllByArticleId(Long articleId) {
+        List<Comment> comments = commentRepository.findAllByArticleId(articleId);
+        return comments.stream()
+                .map(comment -> {
+                    CommentDto commentDto = new CommentDto();
+                    commentDto.setId(comment.getId());
+                    commentDto.setUserName(comment.getUser().getName());
+                    commentDto.setContents(comment.getContents());
+                    commentDto.setLocalDate(comment.getLocalDate());
+                    commentDto.setLocalTime(comment.getLocalTime());
+                    return commentDto;
+                }).collect(Collectors.toList());
+    }
+
+    public void update(CommentDto commentDto) {
+        Comment comment = commentRepository.findById(commentDto.getId())
+                .orElseThrow(IllegalArgumentException::new);
+
+        comment.modify(commentDto.getContents());
+    }
+
+    public void delete(Long commentId) {
+        commentRepository.deleteById(commentId);
+    }
+
+    public void findCommentWrtier(Long commentId, String email) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(IllegalArgumentException::new);
+        if (comment.getUser().isDifferentEmail(email)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
