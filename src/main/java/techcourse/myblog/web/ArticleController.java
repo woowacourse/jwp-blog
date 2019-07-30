13401cedfb6aa012bdee929cf91e94cc2b54ac27
SunHo Van <vsh123@naver.com>
2019-07-30 19:57:44 +0900

    Revert "[게시글 생성/조회] 에단 미션 제출합니다. (#2)"
    This reverts commit 095864c41b1d37f03f079597b55ed8ddfeec2334.

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,67 +2,10 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import techcourse.myblog.domain.Article;
-import techcourse.myblog.service.ArticleService;
-
-import java.util.List;
+import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
-    private final ArticleService articleService;
-
     @Autowired
-    public ArticleController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
-
-    @GetMapping("/")
-    public String indexView(Model model) {
-        List<Article> articles = articleService.findAll();
-        model.addAttribute("articles", articles);
-        return "index";
-    }
-
-    @GetMapping("/writing")
-    public String writeArticleView() {
-        return "article-edit";
-    }
-
-    @PostMapping("/articles")
-    public String publishArticle(ArticleRequestDto article) {
-        articleService.addArticle(Article.from(article));
-        return "redirect:/";
-    }
-
-    @GetMapping("/articles/{articleId}")
-    public String articleView(@PathVariable Long articleId, Model model) {
-        Article article = articleService.findById(articleId);
-        model.addAttribute("article", article);
-        return "article";
-    }
-
-    @GetMapping("/articles/{articleId}/edit")
-    public String editArticleView(@PathVariable Long articleId, Model model) {
-        Article article = articleService.findById(articleId);
-        model.addAttribute("article", article);
-        return "article-edit";
-    }
-
-    @PutMapping("/articles/{articleId}")
-    public String editArticle(@PathVariable Long articleId, ArticleRequestDto reqArticle, Model model) {
-        Article article = Article.from(reqArticle);
-        article.setId(articleId);
-        articleService.update(article);
-        Article articleToShow = articleService.findById(articleId);
-        model.addAttribute("article", articleToShow);
-        return "article";
-    }
-
-    @DeleteMapping("/articles/{articleId}")
-    public String deleteArticle(@PathVariable Long articleId) {
-        articleService.deleteById(articleId);
-        return "redirect:/";
-    }
+    private ArticleRepository articleRepository;
 }
--- src/test/java/techcourse/myblog/web/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTests.java
@@ -4,18 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
-import reactor.core.publisher.Mono;
-import techcourse.myblog.domain.Article;
-import techcourse.myblog.domain.ArticleRepository;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,154 +13,18 @@ public class ArticleControllerTests {
     @Autowired
     private WebTestClient webTestClient;
 
-    @Autowired
-    private ArticleRepository articleRepository;
-
     @Test
     void index() {
         webTestClient.get().uri("/")
-            .exchange()
-            .expectStatus().isOk();
+                .exchange()
+                .expectStatus().isOk();
     }
 
     @Test
     void articleForm() {
-        webTestClient.get().uri("/writing")
-            .exchange()
-            .expectStatus().isOk();
+        webTestClient.get().uri("/articles/new")
+                .exchange()
+                .expectStatus().isOk();
     }
 
-    @Test
-    void createArticlePost() {
-        Map<String, String> requestMap = new HashMap<>();
-        String title = "hello";
-        String backgroundURL = "http://asdf.com";
-        String content = "helloContent";
-
-        requestMap.put("title", title);
-        requestMap.put("backgroundURL", backgroundURL);
-        requestMap.put("content", content);
-        webTestClient.post()
-            .uri("/articles")
-            .body(Mono.just(requestMap), Map.class)
-            .exchange()
-            .expectStatus().is3xxRedirection();
-    }
-
-    @Test
-    void retrieveArticle() {
-        Article newArticle = Article.of("title", "http://background.com", "가나다라마바사");
-        articleRepository.addArticle(newArticle);
-
-        webTestClient.get()
-            .uri("/articles/" + newArticle.getId())
-            .exchange()
-            .expectStatus().isOk();
-    }
-
-    @Test
-    void editArticle() {
-        Article newArticle = Article.of("title", "http://background.com", "가나다라마바사");
-        articleRepository.addArticle(newArticle);
-
-        webTestClient.get()
-            .uri("/articles/" + newArticle.getId() + "/edit")
-            .exchange()
-            .expectStatus().isOk();
-    }
-
-
-    @Test
-    void editArticlePut() {
-        Article newArticle = Article.of("my article", "http://image.com/", "origin contents");
-        ArticleRequestDto changedArticle = ArticleRequestDto.of("changed title", "backgroundURL", "changed contents");
-        articleRepository.addArticle(newArticle);
-
-        webTestClient.put()
-            .uri("/articles/" + newArticle.getId())
-            .body(BodyInserters
-                .fromFormData("title", changedArticle.getTitle())
-                .with("coverUrl", changedArticle.getCoverUrl())
-                .with("contents", changedArticle.getContents()))
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody()
-            .consumeWith(response -> {
-                Article articleFound = articleRepository.findById(newArticle.getId()).get();
-                assertThat(articleFound.getTitle()).isEqualTo(changedArticle.getTitle());
-                assertThat(articleFound.getCoverUrl()).isEqualTo(changedArticle.getCoverUrl());
-                assertThat(articleFound.getContents()).isEqualTo(changedArticle.getContents());
-            });
-    }
-
-    @Test
-    void deleteArticle() {
-        Article newArticle = Article.of("my article", "http://image.com/", "origin contents");
-        articleRepository.addArticle(newArticle);
-
-        webTestClient.delete()
-            .uri("/articles/" + newArticle.getId())
-            .exchange()
-            .expectStatus().is3xxRedirection();
-
-        assertThat(articleRepository.findById(newArticle.getId()).isPresent()).isFalse();
-    }
-
-    @Test
-    void create_article() {
-        String title = "목적의식 있는 연습을 통한 효과적인 학습";
-        String coverUrl = "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/5tdm/image/7OdaODfUPkDqDYIQKXk_ET3pfKo.jpeg";
-        String contents = "나는 우아한형제들에서 우아한테크코스 교육 과정을 진행하고 있다. 우테코를 설계하면서 고민스러웠던 부분 중의 하나는 '선발 과정을 어떻게 하면 의미 있는 시간으로 만들 것인가?'였다.";
-
-        webTestClient.post()
-            .uri("/articles")
-            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            .body(BodyInserters
-                .fromFormData("title", title)
-                .with("coverUrl", coverUrl)
-                .with("contents", contents))
-            .exchange()
-            .expectStatus().is3xxRedirection()
-            .expectBody()
-            .consumeWith(response -> {
-                webTestClient.get().uri(response.getResponseHeaders().get("Location").get(0))
-                    .exchange()
-                    .expectStatus().isOk()
-                    .expectBody()
-                    .consumeWith(res -> {
-                        String body = new String(res.getResponseBody());
-                        assertThat(body.contains(title)).isTrue();
-                        assertThat(body.contains(coverUrl)).isTrue();
-//                        This assertion can fail when length of contents over 100 because of excerpt.
-//                        assertThat(body.contains(contents)).isTrue();
-                    });
-
-            });
-    }
-
-    @Test
-    void delete_article() {
-        String title = "article to delete";
-        String coverUrl = "http://cover-url.com/delete.jpg";
-        String contents = "article contents to delete";
-        Article newArticle = Article.of(title, coverUrl, contents);
-        articleRepository.addArticle(newArticle);
-
-        webTestClient.delete()
-            .uri("/articles/" + newArticle.getId())
-            .exchange()
-            .expectStatus().is3xxRedirection()
-            .expectBody()
-            .consumeWith(response -> {
-                webTestClient.get().uri(response.getResponseHeaders().get("Location").get(0))
-                    .exchange()
-                    .expectStatus().isOk()
-                    .expectBody()
-                    .consumeWith(res -> {
-                        String body = new String(res.getResponseBody());
-                        assertThat(body.contains(title)).isFalse();
-                        assertThat(body.contains(coverUrl)).isFalse();
-                    });
-            });
-    }
 }
