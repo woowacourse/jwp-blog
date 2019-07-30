37af0bf41b610d581d93c7a89a5bb03dbfe5481d
wbluke <pwb0127@naver.com>
2019-07-30 15:14:28 +0900

    [ADD] Article 삭제 시 해당 Comment를 같이 삭제하는 기능 구현

--- src/main/java/techcourse/myblog/repository/CommentRepository.java
+++ src/main/java/techcourse/myblog/repository/CommentRepository.java
@@ -1,11 +1,17 @@
 package techcourse.myblog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import techcourse.myblog.domain.Comment;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByArticleId(Long articleId);
+
+    @Modifying
+    @Transactional
+    void deleteByArticleId(long id);
 }
