bec89b890e0550bb5a63e19b28201aab7fdea49e
yk1028 <wongeunsong94@gmail.com>
2019-07-30 16:58:01 +0900

    refactor: data.sql 제약조건에 맞게 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO user(email, name, password) VALUES ('test@test.test', 'test', 'passWORD1!');
+INSERT INTO user(email, name, password) VALUES ('test@test.test', 'base', 'passWORD1!');
 INSERT INTO user(email, name, password) VALUES ('update@test.test', 'name', 'passWORD1!');
 INSERT INTO user(email, name, password) VALUES ('delete@test.test', 'name', 'passWORD1!');
-INSERT INTO user(email, name, password) VALUES ('test2@test.test', 'test2', 'passWORD1!');
+INSERT INTO user(email, name, password) VALUES ('test2@test.test', 'mismatch', 'passWORD1!');
 
 INSERT INTO article(title, cover_url, contents, author_id) VALUES ('test2@test.test', 'test2', 'passWORD1!', 1);
 
