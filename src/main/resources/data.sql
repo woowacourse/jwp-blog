707554f944d2f3f33732f7ff885f47e4af02d404
imkimheejoo <jujudamdadi@naver.com>
2019-07-30 17:05:04 +0900

    feat: data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO user(user_name, email, password)
+VALUES ('heejoo', 'heejoo@gmail.com', 'Aa12345!');
+
+INSERT INTO user(user_name, email, password)
+VALUES ('cony', 'cony@gmail.com', 'Aa12345!');
+
+INSERT INTO article(title, cover_url, contents, author)
+VALUES ('title', 'coverUrl', 'contents', 1);
+
+INSERT INTO article(title, cover_url, contents, author)
+VALUES ('title', 'coverUrl', 'contents', 1);
+
+INSERT INTO comment(contents, author, article_id)
+VALUES ('sdkasdsaldas', 1, 1);
+
+INSERT INTO comment(contents, author, article_id)
+VALUES ('sssssssss', 1, 1);
