d39473c430f215502bb8daedc47b7f6ffddcc8aa
pkch93 <star_final@naver.com>
2019-07-30 15:58:46 +0900

    test: ArticleController class
    comment 인수 테스트

--- src/test/java/techcourse/myblog/web/ArticleControllerTest.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTest.java
@@ -2,14 +2,23 @@ package techcourse.myblog.web;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import techcourse.myblog.domain.Comment;
+import techcourse.myblog.domain.User;
 import techcourse.myblog.dto.UserDto;
+import techcourse.myblog.service.CommentService;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static techcourse.myblog.web.AuthControllerTest.로그인_세션_ID;
 import static techcourse.myblog.web.UserControllerTest.회원_등록;
 
@@ -26,6 +35,9 @@ class ArticleControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @MockBean
+    private CommentService commentService;
 
     @BeforeEach
     void setUp() {
@@ -133,5 +145,35 @@ class ArticleControllerTest {
                         .header("cookie", jSessionId)
                         .exchange()
                         .expectStatus().is3xxRedirection());
+    }
+
+    @Test
+    void commentCreateTest() {
+        given(commentService.create(1L, null, new Comment())).willReturn(new Comment());
+
+        webTestClient.post().uri("/articles/1/comments")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection();
+    }
+
+    @Test
+    void commentUpdateTest() {
+        webTestClient.put().uri("/articles/1/comments/1")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/articles/1");
+    }
+
+    @Test
+    void commentDeleteTest() {
+        webTestClient.delete().uri("/articles/1/comments/1")
+                .header("cookie", jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", ".*/articles/1");
     }
 }
