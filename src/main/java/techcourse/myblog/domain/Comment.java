506365103ea2b34f80506e1318307a26e060e063
이영현 <leey9494@icloud.com>
2019-07-31 17:45:05 +0900

    ADD : Spring Data Jpa 추가

--- src/main/java/techcourse/myblog/domain/Comment.java
+++ src/main/java/techcourse/myblog/domain/Comment.java
@@ -1,11 +1,17 @@
 package techcourse.myblog.domain;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = false)
 @Entity
-public class Comment {
+@Getter
+@NoArgsConstructor
+@Table(name = "comment")
+public class Comment extends AuditLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -13,9 +19,6 @@ public class Comment {
 
     @Column(name = "contents", nullable = false)
     private String contents;
-
-    @Column(name = "time", nullable = false)
-    private LocalDateTime time;
 
     @ManyToOne
     @JoinColumn(name = "author", foreignKey = @ForeignKey(name = "fk_comment_to_user"), nullable = false)
@@ -25,54 +28,13 @@ public class Comment {
     @JoinColumn(name = "article", foreignKey = @ForeignKey(name = "fk_comment_to_article"), nullable = false)
     private Article article;
 
-    public Comment() {
-    }
-
-    public Comment(String contents, LocalDateTime time, User author, Article article) {
+    public Comment(String contents, User author, Article article) {
         this.contents = contents;
-        this.time = time;
         this.author = author;
         this.article = article;
     }
 
     public void setContents(String contents) {
         this.contents = contents;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public LocalDateTime getTime() {
-        return time;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public Article getArticle() {
-        return article;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Comment comment = (Comment) o;
-        return id == comment.id &&
-                Objects.equals(contents, comment.contents) &&
-                Objects.equals(time, comment.time) &&
-                Objects.equals(author, comment.author) &&
-                Objects.equals(article, comment.article);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, contents, time, author, article);
     }
 }
