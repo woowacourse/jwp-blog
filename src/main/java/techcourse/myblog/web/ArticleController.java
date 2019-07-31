37126c4babb1c1371d9ea1b8c2ab661b53e737f0
SunHo Van <vsh123@naver.com>
2019-07-31 12:07:54 +0900

    Revert "[게시글 생성/조회] 쉐이크반 미션 제출합니다.  (#1)"
    This reverts commit 162857fdcec464f08496655aed4ba2467b20ce20.

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,63 +2,10 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import techcourse.myblog.dto.ArticleRequestDto;
-import techcourse.myblog.service.ArticleService;
-import techcourse.myblog.service.CategoryService;
+import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
-    private final ArticleService articleService;
-    private final CategoryService categoryService;
-
     @Autowired
-    public ArticleController(ArticleService articleService, CategoryService categoryService) {
-        this.articleService = articleService;
-        this.categoryService = categoryService;
-    }
-
-    @GetMapping("/")
-    public String indexView(Model model) {
-        model.addAttribute("articles", articleService.findAll());
-        return "index";
-    }
-
-    @GetMapping("/writing")
-    public String writeArticleView(Model model) {
-        model.addAttribute("categories", categoryService.findAll());
-        return "article-edit";
-    }
-
-    @PostMapping("/articles")
-    public String publishArticle(ArticleRequestDto articleRequestDto) {
-        articleService.addArticle(articleRequestDto);
-        return "redirect:/";
-    }
-
-    @GetMapping("/articles/{articleId}")
-    public String articleView(@PathVariable Long articleId, Model model) {
-        model.addAttribute("article", articleService.findById(articleId));
-        return "article";
-    }
-
-    @GetMapping("/articles/{articleId}/edit")
-    public String editArticleView(@PathVariable Long articleId, Model model) {
-        model.addAttribute("article", articleService.findById(articleId));
-        return "article-edit";
-    }
-
-    @PutMapping("/articles/{articleId}")
-    public String editArticle(@PathVariable Long articleId, ArticleRequestDto articleRequestDto, Model model) {
-        articleService.editArticle(articleId, articleRequestDto);
-        model.addAttribute("article", articleService.findById(articleId));
-        return "article";
-    }
-
-    @DeleteMapping("/articles/{articleId}")
-    public String deleteArticle(@PathVariable Long articleId) {
-        articleService.deleteById(articleId);
-        return "redirect:/";
-    }
+    private ArticleRepository articleRepository;
 }
