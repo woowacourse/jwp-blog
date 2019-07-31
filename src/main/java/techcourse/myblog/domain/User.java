0d5920156c29a82fa911f5a0cc20152d65a3d5aa
heebong <heebg2@naver.com>
2019-07-31 18:41:24 +0900

    refactor: User entity에 validation 추가
    - 비밀번호, 이메일 추가

--- src/main/java/techcourse/myblog/domain/User.java
+++ src/main/java/techcourse/myblog/domain/User.java
@@ -3,6 +3,9 @@ package techcourse.myblog.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -14,12 +17,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Pattern(regexp = "^[a-zA-Z가-힣]{2,10}$")
     @Column(nullable = false)
     @Size(min = 2, max = 10)
     private String name;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~`!@#$%\\^&*()-]).{8,}$")
     @Column(nullable = false)
     @Size(max = 30)
     private String password;
+    @Email
     @Column(nullable = false, unique = true)
     @Size(max = 20)
     private String email;
