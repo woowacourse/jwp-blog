27797c6e8063f1b771b183d201e01bfafe595c62
andole87 <andole87@gmail.com>
2019-07-31 23:02:01 +0900

    refactor: Advice StatusCode 400으로  변경

--- src/main/java/techcourse/myblog/web/support/CommentControllerAdvice.java
+++ src/main/java/techcourse/myblog/web/support/CommentControllerAdvice.java
@@ -7,25 +7,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import techcourse.myblog.domain.article.ArticleException;
 import techcourse.myblog.domain.comment.CommentException;
-import techcourse.myblog.web.ArticleController;
+import techcourse.myblog.web.CommentController;
 
-@ControllerAdvice(assignableTypes = ArticleController.class)
-public class ArticleControllerAdvice {
+@ControllerAdvice(assignableTypes = CommentController.class)
+public class CommentControllerAdvice {
     private static final Logger log = LoggerFactory.getLogger(ArticleControllerAdvice.class);
-
-    @ExceptionHandler(ArticleException.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public String articleHandler(ArticleException e, RedirectAttributes redirectAttributes) {
-        log.debug("article Manipulate FAILED {}", e.getMessage());
-        redirectAttributes.addFlashAttribute("error", e.getMessage());
-        return "redirect:/error";
-    }
 
     @ExceptionHandler(CommentException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public String commentHandler(CommentException e, RedirectAttributes redirectAttributes) {
+        log.debug("Comment Exception : {}", e.getMessage());
         redirectAttributes.addFlashAttribute("error", e.getMessage());
         return "redirect:/error";
     }
