993d439f93ec811593aae552e8c568bc378fb44a
ddu0422 <ddu0422@naver.com>
2019-07-31 11:13:07 +0900

    refactor: 불필요한 파일 삭제

--- src/test/java/techcourse/myblog/domain/UserTest.java
+++ src/test/java/techcourse/myblog/domain/UserTest.java
@@ -11,12 +11,6 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-<<<<<<<HEAD
-=======
-        >>>>>>>upstream/school0bhy
-        <<<<<<<HEAD
-=======
-        >>>>>>>upstream/school0bhy
 
 class UserTest {
     private User user;
@@ -41,26 +35,4 @@ class UserTest {
                 Arguments.of("name", "e@mail.com", "잘못된패스워드", "password")
         );
     }
-<<<<<<< HEAD
-=======
-
-    @ParameterizedTest(name = "{index}: {1}")
-    @MethodSource("userAuthResultStream")
-    void authenticate_fail_test(String email, String password) {
-        assertThrows(AuthenticationFailedException.class, () ->
-                user.authenticate(email, password));
-    }
-
-    static Stream<Arguments> userAuthResultStream() throws Throwable {
-        return Stream.of(
-                Arguments.of("mail@email.com", "pAssw!rd0"),
-                Arguments.of("e@mail.com", "Passw0rd!")
-        );
-    }
-
-    @Test
-    void authenticate_success_test() {
-        assertDoesNotThrow(() -> user.authenticate("e@mail.com", "pAssw!rd0"));
-    }
->>>>>>> upstream/school0bhy
 }
