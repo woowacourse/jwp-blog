87d311ddcf69357c274b4fe347096d567918f345
aiden <cnk9109@gmail.com>
2019-07-31 11:56:10 +0900

    feat(AuthInterceptor): update check session

--- src/main/java/techcourse/myblog/web/interceptor/AuthInterceptor.java
+++ src/main/java/techcourse/myblog/web/interceptor/AuthInterceptor.java
@@ -4,12 +4,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HttpSession session = request.getSession(false);
+        Object session = request.getSession().getAttribute("user");
         if (session == null) {
             response.sendRedirect("/login");
             return false;
