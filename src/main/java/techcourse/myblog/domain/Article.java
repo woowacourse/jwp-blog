5037c75d6f6a72f0a0e02b2f7da5d5067fee8701
ddu0422 <ddu0422@naver.com>
2019-07-31 21:29:11 +0900

    refactor: Article 변경
    lombok 추가
    @Lob 적용
    update 시 작성자 확인

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,33 +1,47 @@
 package techcourse.myblog.domain;
 
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import techcourse.myblog.support.exception.MismatchAuthorException;
 
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@EqualsAndHashCode(of = "id")
+@DynamicUpdate
 public class Article {
+    private static final Integer TITLE_LENGTH = 50;
+    private static final String FK_FIELD_NAME = "author";
+    private static final String FK_NAME = "fk_article_to_user";
+    private static final String MISMATCH_AUTHOR_ERROR_MSG = "작성자가 아닙니다.";
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = TITLE_LENGTH)
     private String title;
 
     @Column(nullable = false)
+    @Lob
     private String coverUrl;
 
-    @Column(nullable = false, length = 500)
+    @Column(nullable = false)
+    @Lob
     private String contents;
 
     @ManyToOne
-    @JoinColumn(name = "author", foreignKey = @ForeignKey(name = "fk_article_to_user"))
+    @JoinColumn(name = FK_FIELD_NAME, foreignKey = @ForeignKey(name = FK_NAME))
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User author;
-
-    private Article() {
-    }
-
+    
     public Article(String title, String coverUrl, String contents, User author) {
         this.title = title;
         this.contents = contents;
@@ -35,41 +49,16 @@ public class Article {
         this.author = author;
     }
 
-    public Article update(Article article) {
+    public void update(Article article) {
+        validateAuthor(article);
         title = article.getTitle();
         coverUrl = article.getCoverUrl();
         contents = article.getContents();
-
-        return this;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCoverUrl() {
-        return coverUrl;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", coverUrl='" + coverUrl + '\'' +
-                ", contents='" + contents + '\'' +
-                '}';
+    
+    private void validateAuthor(Article article) {
+        if (!article.author.equals(this.author)) {
+            throw new MismatchAuthorException(MISMATCH_AUTHOR_ERROR_MSG);
+        }
     }
 }
