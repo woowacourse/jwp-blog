cf91b235f0e45435659c9cfb12a4237e720988e4
choihz <choixhz@gmail.com>
2019-08-01 09:47:55 +0900

    feat: Comment class 구현

---
+++ src/main/java/techcourse/myblog/domain/Comment.java
@@ -0,0 +1,29 @@
+package techcourse.myblog.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author", foreignKey = @ForeignKey(name = "fk_comment_to_user"))
+    private User author;
+
+    @Column(nullable = false)
+    private String contents;
+
+    public Comment() {
+    }
+
+    public Comment(User author, String contents) {
+        this.author = author;
+        this.contents = contents;
+    }
+
+    public void update(Comment updateComment) {
+        this.contents = updateComment.contents;
+    }
+}
