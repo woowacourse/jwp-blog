e88e83d56fa98548e769fc8e80569ab981e4a39f
Soojin Roh <sooreal91@gmail.com>
2019-08-01 17:11:10 +0900

    [REFACTOR] 사용하지 않는 생성자 삭제

--- src/main/java/techcourse/myblog/application/dto/UserResponse.java
+++ src/main/java/techcourse/myblog/application/dto/UserResponse.java
@@ -8,12 +8,6 @@ public class UserResponse {
     private UserResponse() {
     }
 
-    public UserResponse(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
     public Long getId() {
         return id;
     }
