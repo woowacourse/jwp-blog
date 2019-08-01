582db2113f524caed0086639e45d3426bd0fbf0a
green4469 <green4469@gmail.com>
2019-08-01 15:10:16 +0900

    refactor: use "import.sql"

--- src/main/java/techcourse/myblog/web/AuthConfig.java
+++ src/main/java/techcourse/myblog/web/AuthConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class AuthConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor())
