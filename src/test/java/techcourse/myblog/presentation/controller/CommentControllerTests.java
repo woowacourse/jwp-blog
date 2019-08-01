e65fdbd58947f6216e7ee759671803b288a09fba
skygl <jupiter9501@gmal.com>
2019-08-01 09:48:12 +0900

    [TEST] CommentController 테스트 코드 작성

--- src/test/java/techcourse/myblog/presentation/controller/CommentControllerTests.java
+++ src/test/java/techcourse/myblog/presentation/controller/CommentControllerTests.java
@@ -21,34 +21,19 @@ public class CommentControllerTests {
     private static String EMAIL = "hard@gmail.com";
     private static String NAME = "hard";
     private static String PASSWORD = "qwerasdf";
+
+    private static Long commentId = 3L;
+
     @Autowired
     WebTestClient webTestClient;
 
     @Test
     void new_comment_test() {
-        //회원가입
-        webTestClient.post()
-                .uri("/users")
-                .body(fromFormData("name", NAME)
-                        .with("email", EMAIL)
-                        .with("password", PASSWORD))
-                .exchange()
-                .expectStatus().isFound();
-        //로그인
+        register();
         String sessionId = logInAndGetSessionId();
-        //글작성
-        EntityExchangeResult result = webTestClient.post().uri("/articles")
-                .header("Cookie", sessionId)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters
-                        .fromFormData("title", "111")
-                        .with("coverUrl", "222")
-                        .with("contents", "333"))
-                .exchange()
-                .expectStatus().is3xxRedirection()
-                .expectBody().returnResult();
-        //댓글작성
-        String articleUri = result.getResponseHeaders().getLocation().getPath();
+        String articleUri = postArticle(sessionId);
+
+        // 댓글 작성
         webTestClient.post().uri(articleUri + "/comments")
                 .header("Cookie", sessionId)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
@@ -60,6 +45,7 @@ public class CommentControllerTests {
             assertThat(body.getResponseHeaders().getLocation().getPath()).isEqualTo(articleUri);
         });
 
+        // 댓글 조회 (생성 확인)
         webTestClient.get().uri(articleUri)
                 .exchange()
                 .expectBody().consumeWith(response -> {
@@ -68,6 +54,69 @@ public class CommentControllerTests {
             assertThat(body.contains("445")).isFalse();
         });
 
+
+        // 댓글 수정
+        webTestClient.put().uri(articleUri + "/comments/" + commentId)
+                .header("Cookie", sessionId)
+                .body(BodyInserters
+                        .fromFormData("contents", "445"))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody().consumeWith(body -> {
+            assertThat(body.getResponseHeaders().getLocation().getPath()).isEqualTo(articleUri);
+        });
+
+        // 댓글 조회 (수정 확인)
+        webTestClient.get().uri(articleUri)
+                .exchange()
+                .expectBody().consumeWith(response -> {
+            String body = new String(response.getResponseBody());
+            assertThat(body.contains("444")).isFalse();
+            assertThat(body.contains("445")).isTrue();
+        });
+
+        // 댓글 삭제
+        webTestClient.delete().uri(articleUri + "/comments/" + commentId)
+                .header("Cookie", sessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody().consumeWith(body -> {
+            assertThat(body.getResponseHeaders().getLocation().getPath()).isEqualTo(articleUri);
+        });
+
+
+        // 댓글 조회 (삭제 확인)
+        webTestClient.get().uri(articleUri)
+                .exchange()
+                .expectBody().consumeWith(response -> {
+            String body = new String(response.getResponseBody());
+            assertThat(body.contains("444")).isFalse();
+            assertThat(body.contains("445")).isFalse();
+        });
+    }
+
+    private String postArticle(String sessionId) {
+        EntityExchangeResult result = webTestClient.post().uri("/articles")
+                .header("Cookie", sessionId)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters
+                        .fromFormData("title", "111")
+                        .with("coverUrl", "222")
+                        .with("contents", "333"))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectBody().returnResult();
+        return result.getResponseHeaders().getLocation().getPath();
+    }
+
+    private void register() {
+        webTestClient.post()
+                .uri("/users")
+                .body(fromFormData("name", NAME)
+                        .with("email", EMAIL)
+                        .with("password", PASSWORD))
+                .exchange()
+                .expectStatus().isFound();
     }
 
     private String logInAndGetSessionId() {
