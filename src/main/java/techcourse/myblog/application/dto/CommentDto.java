9d7eba0721b0694835e29c80c604bc0f88076cdc
hyun <jhs695@naver.com>
2019-08-01 15:21:41 +0900

    refactor domain model의 필드 변경으로 Dto 필드 수정

--- src/main/java/techcourse/myblog/application/dto/CommentDto.java
+++ src/main/java/techcourse/myblog/application/dto/CommentDto.java
@@ -3,8 +3,7 @@ package techcourse.myblog.application.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -12,6 +11,6 @@ public class CommentDto {
     private long id;
     private String contents;
     private String userName;
-    private LocalDate localDate;
-    private LocalTime localTime;
+    private LocalDateTime createDateTime;
+    private LocalDateTime updateDateTime;
 }
