37695a9664b9e2c443ff986ced37737aa9511db4
deocksoo <dusol92@snu.ac.kr>
2019-08-01 17:44:06 +0900

    test: UserTest와 CommentTest를 추가합니다

---
+++ src/test/java/techcourse/myblog/model/CommentTest.java
@@ -0,0 +1,46 @@
+package techcourse.myblog.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import techcourse.myblog.exception.MisMatchAuthorException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.transaction.Transaction;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CommentTest {
+    private Comment testComment;
+    private User author;
+    private Article article;
+
+    @BeforeEach
+    void setUp() {
+        author = new User("owner", "owner@email.com", "!@QW12qw12");
+        article = new Article();
+        testComment = new Comment("contents", author, article);
+    }
+
+    @Test
+    @DisplayName("댓글을 업데이트 합니다")
+    void update() {
+        Comment updatedComment = new Comment("updated contents", author, article);
+
+        testComment.update(updatedComment);
+        assertThat(testComment.getContents()).isEqualTo(updatedComment.getContents());
+    }
+
+    @Test
+    @DisplayName("임의의 사용자가 댓글을 작성한 사용자인지 확인합니다.")
+    void checkOnwer() {
+        User stranger = new User("stranger", "stranger@email.com", "!@QW12qw");
+
+        assertThatThrownBy(() -> testComment.checkOwner(stranger)).isInstanceOf(MisMatchAuthorException.class);
+        assertThatCode(() -> testComment.checkOwner(author)).doesNotThrowAnyException();
+    }
+}
