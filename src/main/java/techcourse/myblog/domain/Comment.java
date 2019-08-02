b60fe1a7a5e0860b368ecd8ca68e893ff04758fb
green4469 <green4469@gmail.com>
2019-08-02 17:19:08 +0900

    refactor: use Spring JPA Auditing to handle created date of entity 'Comment'

--- src/main/java/techcourse/myblog/domain/Comment.java
+++ src/main/java/techcourse/myblog/domain/Comment.java
@@ -3,6 +3,8 @@ package techcourse.myblog.domain;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import techcourse.myblog.service.dto.CommentRequestDto;
 
 import javax.persistence.*;
@@ -13,6 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 @EqualsAndHashCode(of = {"id"})
 @ToString
+@EntityListeners(value = { AuditingEntityListener.class })
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +29,7 @@ public class Comment {
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(foreignKey = @ForeignKey(name = "FK_ARTICLE"), name = "article")
     private Article article;
+    @CreatedDate
     private LocalDateTime createdAt;
 
     @Builder
@@ -33,11 +37,6 @@ public class Comment {
         this.comment = comment;
         this.commenter = commenter;
         this.article = article;
-    }
-
-    @PrePersist
-    public void createdAt() {
-        this.createdAt = LocalDateTime.now();
     }
 
     public void update(CommentRequestDto commentRequestDto) {
--- src/test/java/techcourse/myblog/persistence/CommentRepositoryTest.java
+++ src/test/java/techcourse/myblog/persistence/CommentRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
 import techcourse.myblog.domain.User;
@@ -12,7 +11,6 @@ import techcourse.myblog.domain.User;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static techcourse.myblog.web.UserControllerTest.testEmail;
 
 @DataJpaTest
 public class CommentRepositoryTest {
@@ -27,8 +25,8 @@ public class CommentRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        user = userRepository.findByEmail(testEmail).get();
-        article = articleRepository.findByAuthor(user).get(0);
+        user = userRepository.save(new User("moomin", "qweQWE123!@#", "moomin@naver.com"));
+        article = articleRepository.save(new Article("moomin's article", "moomin is happy", "moomin's background image"));
     }
 
     @Test
@@ -42,13 +40,5 @@ public class CommentRepositoryTest {
 
         Comment justCreatedComment = commentRepository.findByArticleOrderByCreatedAt(article).get(0);
         assertThat(justCreatedComment.getCreatedAt()).isNotNull();
-    }
-
-    @Test
-    void 댓글_시간순_정렬_테스트() {
-        List<Comment> comments = commentRepository.findByArticleOrderByCreatedAt(article);
-        assertThat(comments.get(0).getComment()).isEqualTo("first");
-        assertThat(comments.get(1).getComment()).isEqualTo("second");
-        assertThat(comments.get(2).getComment()).isEqualTo("third");
     }
 }
