02e85331136658e6ea5c62b82fe99a3579495124
jinwook <jin942002@naver.com>
2019-08-03 08:42:46 +0900

    refactor: use argument resolver for argument @UserFromSession
    parameter type is optional<user>

--- src/main/java/techcourse/myblog/controller/ArticleController.java
+++ src/main/java/techcourse/myblog/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import techcourse.myblog.annotation.UserFromSession;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.exception.NotFoundArticleException;
@@ -17,6 +18,7 @@ import techcourse.myblog.service.dto.ArticleDTO;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -24,13 +26,12 @@ public class ArticleController {
     private final ArticleRepository articleRepository;
     private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
 
-
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
 
     @GetMapping("/{articleId}")
-    public String show(@PathVariable Long articleId, HttpSession session, Model model) {
+    public String show(@PathVariable Long articleId, @UserFromSession Optional<User> user, Model model) {
         Article article = articleRepository
                 .findById(articleId)
                 .orElseThrow(NotFoundArticleException::new);
@@ -38,46 +39,39 @@ public class ArticleController {
         model.addAttribute("article", article);
         model.addAttribute("comments", article.getComments());
 
-        User user = (User) session.getAttribute("user");
-        if (user != null) {
-            model.addAttribute("user", user);
-        }
+        user.ifPresent(value -> model.addAttribute("user", value));
 
         return "article";
     }
 
     @GetMapping("/new")
-    public String createForm(HttpSession session, Model model) {
-        User user = (User) session.getAttribute("user");
-        if (user == null) {
-            throw new UnauthenticatedUserException();
-        }
-        model.addAttribute("user", user);
+    public String createForm(HttpSession session, @UserFromSession Optional<User> user,
+                             Model model) {
+        model.addAttribute("user", user.orElseThrow(UnauthenticatedUserException::new));
         return "article-edit";
     }
 
     @PostMapping
-    public String create(ArticleDTO articleDTO, HttpSession session) {
-        User user = (User) session.getAttribute("user");
-        if (user == null) {
-            throw new UnauthenticatedUserException();
-        }
-
-        Article article = articleDTO.toDomain(user);
+    public String create(ArticleDTO articleDTO, @UserFromSession Optional<User> user) {
+        Article article = articleDTO.toDomain(user.orElseThrow(UnauthenticatedUserException::new));
 
         articleRepository.save(article);
         return "redirect:/articles/" + article.getId();
     }
 
     @GetMapping("/{articleId}/edit")
-    public Object updateForm(@PathVariable Long articleId, HttpSession session, Model model) {
+    public Object updateForm(@PathVariable Long articleId,
+                             @UserFromSession Optional<User> optionalUser,
+                             Model model) {
         Article savedArticle = articleRepository
                 .findById(articleId)
                 .orElseThrow(NotFoundArticleException::new);
 
-        checkAuthor(session, savedArticle);
+        User user = optionalUser.orElseThrow(UnauthenticatedUserException::new);
 
-        model.addAttribute("user", session.getAttribute("user"));
+        checkAuthor(user, savedArticle);
+
+        model.addAttribute("user", user);
         model.addAttribute("article", savedArticle);
         model.addAttribute("articleId", articleId);
         return "article-edit";
@@ -85,34 +79,33 @@ public class ArticleController {
 
     @Transactional
     @PutMapping("/{articleId}")
-    public String update(@PathVariable Long articleId, HttpSession session, ArticleDTO articleDTO) {
+    public String update(@PathVariable Long articleId, @UserFromSession Optional<User> optionalUser,
+                         ArticleDTO articleDTO) {
         Article savedArticle = articleRepository
                 .findById(articleId)
                 .orElseThrow(NotFoundArticleException::new);
 
-        checkAuthor(session, savedArticle);
+        User user = optionalUser.orElseThrow(UnauthenticatedUserException::new);
+
+        checkAuthor(user, savedArticle);
         savedArticle.update(articleDTO.toDomain(savedArticle.getAuthor()));
 
         return "redirect:/articles/" + articleId;
     }
 
-    private void checkAuthor(HttpSession session, Article savedArticle) {
-        User user = (User) session.getAttribute("user");
+    private void checkAuthor(User user, Article savedArticle) {
         if (!user.equals(savedArticle.getAuthor())) {
             throw new UnauthenticatedUserException();
         }
     }
 
     @DeleteMapping("/{articleId}")
-    public String delete(@PathVariable Long articleId, HttpSession session) {
+    public String delete(@PathVariable Long articleId, @UserFromSession Optional<User> user) {
         Article savedArticle = articleRepository
                 .findById(articleId)
                 .orElseThrow(NotFoundArticleException::new);
+        checkAuthor(user.orElseThrow(UnauthenticatedUserException::new), savedArticle);
 
-        User user = (User) session.getAttribute("user");
-        if (!user.isAuthorOf(savedArticle)) {
-            throw new UnauthenticatedUserException();
-        }
 
         articleRepository.deleteById(articleId);
         return "redirect:/";
---
+++ src/main/java/techcourse/myblog/webconfig/WebConfig.java
@@ -0,0 +1,16 @@
+package techcourse.myblog.webconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.myblog.controller.argumentresolver.UserArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserArgumentResolver());
+    }
+}
