987dcb25d4d35a6ddddc90a77875b7d882482bde
aiden <cnk9109@gmail.com>
2019-08-03 02:24:16 +0900

    test(ArticleControllerTest): test to WebTestClient

--- src/test/java/techcourse/myblog/web/ArticleControllerTest.java
+++ src/test/java/techcourse/myblog/web/ArticleControllerTest.java
@@ -1,99 +1,106 @@
 package techcourse.myblog.web;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import techcourse.myblog.domain.User;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ArticleControllerTest {
+public class ArticleControllerTest extends ControllerTest {
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    private MockMvc mockMvc;
-    private MockHttpSession session;
-    private String name = "aiden";
-    private String email = "aiden@gmail.com";
-    private String password = "12Woowa@";
+    private WebTestClient webTestClient;
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        session = new MockHttpSession();
-        session.setAttribute("user", new User(1L, name, email, password));
-    }
-
-    @AfterEach
-    void tearDown() {
-        session.clearAttributes();
-        session = null;
-    }
-
-    private ResultActions signUp() throws Exception {
-        return mockMvc.perform(
-                post("/users").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("name", name)
-                        .param("email", email)
-                        .param("password", password)
-        ).andDo(print());
-    }
-
-    private ResultActions creatArticle() throws Exception {
-        return mockMvc.perform(
-                post("/articles").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("title", "title...")
-                        .param("coverUrl", "coverUrl...")
-                        .param("contents", "contents...")
-                        .session(session)
-        ).andDo(print());
+        jSessionId = login(webTestClient);
     }
 
     @Test
-    void 글쓰기_폼_열기() throws Exception {
-        mockMvc.perform(get("/articles").session(session))
-                .andDo(print())
-                .andExpect(status().isOk());
+    void 게시글_생성_폼_열기() {
+        webTestClient.get().uri("/articles")
+                .cookie(JSESSIONID, jSessionId)
+                .exchange()
+                .expectStatus().isOk()
+        ;
     }
 
     @Test
-    void 게시글_작성() throws Exception {
-        signUp();
-        creatArticle().andExpect(status().is3xxRedirection());
+    void 게시글_수정_폼_열기() {
+        String articleId = "9";
+
+        webTestClient.get().uri("/articles/" + articleId)
+                .cookie(JSESSIONID, jSessionId)
+                .exchange()
+                .expectStatus().isOk()
+        ;
     }
 
     @Test
-    void 게시글_수정() throws Exception {
+    void 게시글_작성() {
+        webTestClient.post().uri("/articles")
+                .cookie(JSESSIONID, jSessionId)
+                .body(BodyInserters
+                        .fromFormData("title", "test title")
+                        .with("coverUrl", "test coverUrl")
+                        .with("contents", "test contents"))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+        ;
+    }
+
+    @Test
+    void 게시글_수정() {
+        String articleId = "9";
         String title = "제목";
         String coverUrl = "https://i.pinimg.com/736x/78/28/39/7828390ef4efbe704e480440f3bd3875.jpg";
         String contents = "CONTENTS";
-        signUp();
-        mockMvc.perform(
-                put(creatArticle().andReturn().getResponse().getRedirectedUrl())
-                        .param("title", title)
-                        .param("coverUrl", coverUrl)
-                        .param("contents", contents)
-                        .session(session)
-        ).andDo(print())
-                .andExpect(status().is3xxRedirection());
+
+        webTestClient.put().uri("/articles/" + articleId)
+                .cookie(JSESSIONID, jSessionId)
+                .body(BodyInserters.fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("contents", contents))
+                .exchange()
+                .expectHeader().valueMatches("Location", ".*/articles/" + articleId)
+        ;
     }
 
     @Test
-    void 게시글_삭제() throws Exception {
-        signUp();
-        mockMvc.perform(delete(creatArticle().andReturn().getResponse().getRedirectedUrl()).session(session))
-                .andDo(print())
-                .andExpect(status().is3xxRedirection());
+    void 게시글_수정_권한없음() {
+        String articleId = "9";
+        String title = "제목";
+        String coverUrl = "https://i.pinimg.com/736x/78/28/39/7828390ef4efbe704e480440f3bd3875.jpg";
+        String contents = "CONTENTS";
+
+        webTestClient.put().uri("/articles/" + articleId)
+                .cookie(JSESSIONID, login(webTestClient, "woowa@woowa.com", "12Woowa@@"))
+                .body(BodyInserters.fromFormData("title", title)
+                        .with("coverUrl", coverUrl)
+                        .with("contents", contents))
+                .exchange()
+                .expectBody()
+                .returnResult().getStatus().is4xxClientError()
+        ;
+    }
+
+    @Test
+    void 게시글_삭제() {
+        String articleId = "9";
+        webTestClient.delete().uri("/articles/" + articleId)
+                .cookie(JSESSIONID, jSessionId)
+                .exchange()
+                .expectStatus().is3xxRedirection()
+        ;
+    }
+
+    @Test
+    void 게시글_삭제_권한없음() {
+        String articleId = "9";
+        webTestClient.delete().uri("/articles/" + articleId)
+                .cookie(JSESSIONID, login(webTestClient, "woowa@woowa.com", "12Woowa@@"))
+                .exchange()
+                .expectBody()
+                .returnResult().getStatus().is4xxClientError()
+        ;
     }
 }
