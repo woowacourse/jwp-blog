370639e08aa6f3e251bba549f1263d042cea6fd5
aiden <cnk9109@gmail.com>
2019-08-03 01:59:53 +0900

    feat(ArticleService): update Authentication and exception

--- src/main/java/techcourse/myblog/service/ArticleService.java
+++ src/main/java/techcourse/myblog/service/ArticleService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.repository.ArticleRepository;
+import techcourse.myblog.service.exception.NoPermissionArticleException;
+import techcourse.myblog.service.exception.NoRowException;
 import techcourse.myblog.web.dto.ArticleDto;
 
 import java.util.List;
@@ -23,19 +25,27 @@ public class ArticleService {
 
     public Article findById(Long articleId) {
         return articleRepository.findById(articleId)
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(NoRowException::new);
     }
 
     public Article save(ArticleDto articleDto, User user) {
         return articleRepository.save(articleDto.create(user));
     }
 
-    public Article update(Long articleId, ArticleDto articleDto) {
+    public Article update(Long articleId, ArticleDto articleDto, User loginUser) {
+        exist(articleId, loginUser);
         Article selectedArticle = findById(articleId);
         return articleRepository.save(selectedArticle.update(articleDto));
     }
 
-    public void delete(Long articleId) {
+    public void delete(Long articleId, User loginUser) {
+        exist(articleId, loginUser);
         articleRepository.deleteById(articleId);
+    }
+
+    public void exist(Long articleId, User author) {
+        if (!articleRepository.existsByIdAndAuthor(articleId, author)) {
+            throw new NoPermissionArticleException("게시글에 대한 권한이 없습니다.");
+        }
     }
 }
