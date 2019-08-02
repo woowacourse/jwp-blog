4796af3b2cd637c7aa202b27d32e34cf5aefec62
green4469 <green4469@gmail.com>
2019-08-02 16:45:36 +0900

    refactor: use @EqualsAndHashcode only for id, not for mutable fields
    Hashcode can change when value of fields changes. (https://kwonnam.pe.kr/wiki/java/lombok/pitfall)

--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 @Getter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = {"id"})
 @ToString
 public class Article {
     @Id
--- src/main/java/techcourse/myblog/domain/Comment.java
+++ src/main/java/techcourse/myblog/domain/Comment.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Getter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = {"id"})
 @ToString
 public class Comment {
     @Id
