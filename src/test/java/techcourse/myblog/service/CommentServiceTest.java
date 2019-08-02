a1db1354a5912d547d23d554eb17c675e57c939e
aiden <cnk9109@gmail.com>
2019-08-03 02:22:00 +0900

    test(CommentServiceTest): test

--- src/test/java/techcourse/myblog/service/CommentServiceTest.java
+++ src/test/java/techcourse/myblog/service/CommentServiceTest.java
@@ -3,19 +3,19 @@ package techcourse.myblog.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
 import techcourse.myblog.domain.User;
+import techcourse.myblog.service.exception.NoPermissionCommentException;
+import techcourse.myblog.service.exception.NoRowException;
 import techcourse.myblog.web.dto.ArticleDto;
 import techcourse.myblog.web.dto.CommentDto;
-import techcourse.myblog.web.dto.UserDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CommentServiceTest {
     private final UserService userService;
@@ -35,13 +35,14 @@ class CommentServiceTest {
 
     @BeforeEach
     void setUp() {
-        author = userService.save(new UserDto("aiden", "aiden1@naver.com", "12Aiden@@"));
+        author = userService.findByEmail("aiden@woowa.com");
         article = articleService.save(new ArticleDto("gogo", "coverUrl", "contents"), author);
         CommentDto commentDto = new CommentDto("brand new comment");
         beforeComment = commentService.save(commentDto, author, article);
     }
 
     @Test
+    @Transactional
     void 저장_조회_테스트() {
         Comment comment = commentService.save(new CommentDto("contents"), author, article);
         assertThat(commentService.findById(comment.getId())).isEqualTo(comment);
@@ -50,7 +51,7 @@ class CommentServiceTest {
     @Test
     void 수정_테스트() {
         CommentDto newCommentDto = new CommentDto("update");
-        Comment newComment = commentService.update(beforeComment.getId(), newCommentDto);
+        Comment newComment = commentService.update(beforeComment.getId(), newCommentDto, author);
         assertThat(newComment.getContents()).isEqualTo("update");
     }
 
@@ -64,9 +65,17 @@ class CommentServiceTest {
     @Test
     void 삭제_테스트() {
         Long beforeCommentId = beforeComment.getId();
-        commentService.delete(beforeCommentId);
+        commentService.delete(beforeCommentId, author);
         assertThatThrownBy(() -> {
             commentService.findById(beforeCommentId);
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(NoRowException.class);
+    }
+
+    @Test
+    void 존재_유무_확인() {
+        User user2 = userService.findByEmail("woowa@woowa.com");
+        assertThatThrownBy(() -> {
+            commentService.exist(19L, user2);
+        }).isInstanceOf(NoPermissionCommentException.class);
     }
 }
