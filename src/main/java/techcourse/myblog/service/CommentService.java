e21580bb6a89b955168376af5d8bebdf8791527d
aiden <cnk9109@gmail.com>
2019-08-03 02:00:32 +0900

    feat(CommentService): update Authentication and exception

--- src/main/java/techcourse/myblog/service/CommentService.java
+++ src/main/java/techcourse/myblog/service/CommentService.java
@@ -5,6 +5,8 @@ import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.repository.CommentRepository;
+import techcourse.myblog.service.exception.NoPermissionCommentException;
+import techcourse.myblog.service.exception.NoRowException;
 import techcourse.myblog.web.dto.CommentDto;
 
 import java.util.List;
@@ -19,7 +21,7 @@ public class CommentService {
 
     public Comment findById(Long commentId) {
         return commentRepository.findById(commentId)
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(NoRowException::new);
     }
 
     public List<Comment> findByArticle(Article article) {
@@ -30,14 +32,22 @@ public class CommentService {
         return commentRepository.save(commentDto.create(author, article));
     }
 
-    public Comment update(Long commentId, CommentDto commentDto) {
-        Comment selectedComment = findById(commentId);
-        return commentRepository.save(selectedComment.update(commentDto));
+    public Comment update(Long commentId, CommentDto commentDto, User loginUser) {
+        exist(commentId, loginUser);
+        Comment comment = findById(commentId);
+        return commentRepository.save(comment.update(commentDto));
     }
 
-    public Long delete(Long commentId) {
+    public Long delete(Long commentId, User loginUser) {
+        exist(commentId, loginUser);
         Long articleId = findById(commentId).getArticleId();
         commentRepository.deleteById(commentId);
         return articleId;
+    }
+
+    public void exist(Long commentId, User loginUser) throws NoPermissionCommentException {
+        if (!commentRepository.existsByIdAndAuthor(commentId, loginUser)) {
+            throw new NoPermissionCommentException("댓글에 대한 권한이 없습니다.");
+        }
     }
 }
