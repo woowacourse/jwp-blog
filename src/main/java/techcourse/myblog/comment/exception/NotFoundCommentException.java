37de289e8dc17411baf541782551e366a9d5c8d3
김성훈 <seonghun127@gmail.com>
2019-08-03 18:33:37 +0900

    style: 코드 포맷팅

--- src/main/java/techcourse/myblog/comment/exception/NotFoundCommentException.java
+++ src/main/java/techcourse/myblog/comment/exception/NotFoundCommentException.java
@@ -2,6 +2,6 @@ package techcourse.myblog.comment.exception;
 
 public class NotFoundCommentException extends RuntimeException {
     public NotFoundCommentException(long commentId) {
-        super("[ " + commentId + "]에 해당하는 댓글을 찾을 수 없습니다.");
+        super("[ " + commentId + " ]에 해당하는 댓글을 찾을 수 없습니다.");
     }
 }
