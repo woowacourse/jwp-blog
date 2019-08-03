06d5dc1a82d44e4f4f12d9c35e3c56c0e2f74361
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-08-03 20:46:16 +0900

    [REFACTOR] Cleanup code

--- src/test/java/techcourse/myblog/presentation/controller/ArticleControllerTests.java
+++ src/test/java/techcourse/myblog/presentation/controller/ArticleControllerTests.java
@@ -1,7 +1,5 @@
 package techcourse.myblog.presentation.controller;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
