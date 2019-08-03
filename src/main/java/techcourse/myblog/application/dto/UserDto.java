3c1d51cd9bd0403c1251d392ff98955db17aa174
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-08-03 20:38:54 +0900

    [REFACTOR] CleanUp UserDto

--- src/main/java/techcourse/myblog/application/dto/UserDto.java
+++ src/main/java/techcourse/myblog/application/dto/UserDto.java
@@ -29,18 +29,6 @@ public class UserDto {
         return email.equals(this.email);
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getEmail() {
         return email;
     }
