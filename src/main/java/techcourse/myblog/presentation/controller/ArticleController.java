fe1f4ad944fbf790910aa79f4efc279fa8156848
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-08-03 17:20:53 +0900

    [REFACTOR] ArticleController 클립업

--- src/main/java/techcourse/myblog/presentation/controller/ArticleController.java
+++ src/main/java/techcourse/myblog/presentation/controller/ArticleController.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import techcourse.myblog.application.dto.ArticleDto;
-import techcourse.myblog.application.dto.CommentDto;
 import techcourse.myblog.application.service.ArticleService;
 import techcourse.myblog.application.service.CommentService;
-import techcourse.myblog.domain.User;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
+import techcourse.myblog.domain.Email;
 
 @Controller
 public class ArticleController {
@@ -27,27 +22,22 @@ public class ArticleController {
     }
 
     @PostMapping("/articles")
-    public RedirectView createArticles(ArticleDto article, HttpSession httpSession) {
-        //todo: argument resolver, 공통 메소드
-        String email = (String) httpSession.getAttribute("email");
-        Long id = articleService.save(article, email);
-
+    public RedirectView createArticles(ArticleDto article, Email email) {
+        Long id = articleService.save(article, email.getEmail());
         return new RedirectView("/articles/" + id);
     }
 
     @GetMapping("/articles/{articleId}")
-    public ModelAndView readArticlePageByArticleId(@PathVariable Long articleId, HttpSession httpSession) {
+    public ModelAndView readArticlePageByArticleId(@PathVariable Long articleId, Email email) {
         ModelAndView modelAndView = new ModelAndView("/article");
         modelAndView.addObject("article", articleService.findById(articleId));
-        String sessionEmail = (String) httpSession.getAttribute("email");
-        List<CommentDto> check = commentService.findAllCommentsByArticleId(articleId, sessionEmail);
-        modelAndView.addObject("comments", commentService.findAllCommentsByArticleId(articleId, sessionEmail));
+        modelAndView.addObject("comments", commentService.findAllCommentsByArticleId(articleId, email.getEmail()));
         return modelAndView;
     }
 
     @GetMapping("/articles/{articleId}/edit")
-    public ModelAndView readArticleEditPage(@PathVariable Long articleId, HttpSession httpSession) {
-        articleService.checkAuthor(articleId, (String)httpSession.getAttribute("email"));
+    public ModelAndView readArticleEditPage(@PathVariable Long articleId, Email email) {
+        articleService.checkAuthor(articleId, email.getEmail());
         ModelAndView modelAndView = new ModelAndView("/article-edit");
         modelAndView.addObject("article", articleService.findById(articleId));
         modelAndView.addObject("method", "put");
@@ -67,12 +57,4 @@ public class ArticleController {
         articleService.removeById(articleId);
         return redirectView;
     }
-
-//    @DeleteMapping("/articles/{articleId}")
-//    public RedirectView deleteArticle(@PathVariable Long articleId) {
-//        RedirectView redirectView = new RedirectView("/");
-//        commentService.removeByArticle(articleId);
-//        articleService.removeById(articleId);
-//        return redirectView;
-//    }
 }
