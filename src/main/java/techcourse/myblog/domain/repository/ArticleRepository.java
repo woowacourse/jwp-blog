098509d129968368e2e52e4a910881629981c099
이영현 <leey9494@icloud.com>
2019-08-04 15:25:45 +0900

    Refactor : @Repository 삭제

--- src/main/java/techcourse/myblog/domain/repository/ArticleRepository.java
+++ src/main/java/techcourse/myblog/domain/repository/ArticleRepository.java
@@ -1,10 +1,8 @@
 package techcourse.myblog.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import techcourse.myblog.domain.Article;
 
-@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Article findArticleById(long id);
 }
