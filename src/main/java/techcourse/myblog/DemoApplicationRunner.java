8a645a4797647ebb128b31be4aceff4de7b0b5d8
CODEMCD <psbum1609@gmail.com>
2019-08-04 19:58:05 +0900

    [CHORE] ApplicationRunner 클래스 추가

---
+++ src/main/java/techcourse/myblog/DemoApplicationRunner.java
@@ -0,0 +1,27 @@
+package techcourse.myblog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.domain.repository.UserRepository;
+import techcourse.myblog.support.encrytor.EncryptHelper;
+
+@Component
+@Profile("!test")
+public class DemoApplicationRunner implements ApplicationRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EncryptHelper encryptHelper;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        User user = new User("BROWN", "user@naver.com", encryptHelper.encrypt("Password123!"));
+        userRepository.save(user);
+    }
+}
--- src/main/java/techcourse/myblog/web/interceptor/AuthInterceptor.java
+++ src/main/java/techcourse/myblog/web/interceptor/AuthInterceptor.java
@@ -9,16 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (request.getRequestURI().equals("/users") && request.getMethod().equals("POST")) {
+        // 회원 가입 통과
+        if ("/users".equals(request.getRequestURI()) && "POST".equals(request.getMethod())) {
             return true;
         }
 
         UserResponse userSession = (UserResponse) request.getSession().getAttribute("user");
 
+        // 로그인 안할 때 통과
         if ("/login".equals(request.getRequestURI()) && userSession == null) {
             return true;
         }
 
+        // 이미 로그인 했다면 통과하지 못함
         if ("/login".equals(request.getRequestURI()) && userSession != null) {
             response.sendRedirect("/");
             return false;
