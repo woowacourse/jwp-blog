dd6e160f810321f089168350e014b0a0522b45b8
ddu0422 <ddu0422@naver.com>
2019-08-04 19:39:44 +0900

    refactor: common interceptor 삭제

--- src/main/java/techcourse/myblog/config/WebMvcConfig.java
+++ src/main/java/techcourse/myblog/config/WebMvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import techcourse.myblog.presentation.interceptor.CommonInterceptor;
 import techcourse.myblog.presentation.interceptor.LogoutInterceptor;
 import techcourse.myblog.presentation.support.LoginUserHandlerMethodArgumentResolver;
 import techcourse.myblog.presentation.interceptor.LoginInterceptor;
@@ -14,14 +13,10 @@ import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    private final HandlerInterceptor commonInterceptor;
     private final HandlerInterceptor loginInterceptor;
     private final HandlerInterceptor logoutInterceptor;
 
-    public WebMvcConfig(final CommonInterceptor commonInterceptor,
-                        final LoginInterceptor loginInterceptor,
-                        final LogoutInterceptor logoutInterceptor) {
-        this.commonInterceptor = commonInterceptor;
+    public WebMvcConfig(final LoginInterceptor loginInterceptor, final LogoutInterceptor logoutInterceptor) {
         this.loginInterceptor = loginInterceptor;
         this.logoutInterceptor = logoutInterceptor;
     }
@@ -37,10 +32,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/login")
                 .addPathPatterns("/signup")
                 .addPathPatterns("/users");
-
-        registry.addInterceptor(commonInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/logout");
     }
 
     @Override
