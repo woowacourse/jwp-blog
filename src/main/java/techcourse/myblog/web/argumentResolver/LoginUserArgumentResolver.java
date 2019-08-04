bc80cb514e69997645730fdc646f59684939db0e
이영현 <leey9494@icloud.com>
2019-08-04 18:19:46 +0900

    Refactor : gradle & convention 수정

--- src/main/java/techcourse/myblog/web/argumentResolver/LoginUserArgumentResolver.java
+++ src/main/java/techcourse/myblog/web/argumentResolver/LoginUserArgumentResolver.java
@@ -6,6 +6,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import techcourse.myblog.web.SessionManager;
 import techcourse.myblog.web.annotation.LoginUser;
 
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
@@ -16,7 +17,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        Object user = webRequest.getAttribute("user", WebRequest.SCOPE_SESSION);
+        Object user = webRequest.getAttribute(SessionManager.USER, WebRequest.SCOPE_SESSION);
         return user;
     }
 }
