f80847dba9f66fc56a8f53f826f3ad71e66bff0b
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-04 15:02:59 +0900

    Update article service test to use mockito

--- src/test/java/techcourse/myblog/service/ArticleServiceTest.java
+++ src/test/java/techcourse/myblog/service/ArticleServiceTest.java
@@ -1,82 +1,135 @@
 package techcourse.myblog.service;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.transaction.annotation.Transactional;
 import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.User;
 import techcourse.myblog.exception.ArticleNotFoundException;
+import techcourse.myblog.repository.ArticleRepository;
+import techcourse.myblog.repository.UserRepository;
 import techcourse.myblog.service.dto.ArticleRequestDto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
 public class ArticleServiceTest {
-    private static final Long DEFAULT_USER_ID = 999L;
-    private static final Long DEFAULT_ARTICLE_ID = 999L;
+    private static final Long DEFAULT_USER_ID = 1L;
+    private static final String DEFAULT_USER_EMAIL = "john123@example.com";
+    private static final String DEFAULT_USER_NAME = "john";
+    private static final String DEFAULT_USER_PASSWORD = "p@ssW0rd";
 
-    @Autowired
+    private static final Long DEFAULT_ARTICLE_ID = 100L;
+    private static final String DEFAULT_ARTICLE_TITLE = "some title";
+    private static final String DEFAULT_ARTICLE_COVER_URL = "https://images.pexels.com/photos/731217/pexels-photo-731217.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+    private static final String DEFAULT_ARTICLE_CONTENTS = "## some cool one";
+
+    private static User defaultUser;
+    private static Article defaultArticle;
+
+    private static UserRepository userRepository;
+    private static ArticleRepository articleRepository;
+
     private ArticleService articleService;
 
-    @Autowired
-    private UserService userService;
+    @BeforeAll
+    static void init() {
+    }
+
+    @BeforeEach
+    void setup() {
+        defaultUser = mock(User.class);
+        when(defaultUser.getId()).thenReturn(DEFAULT_USER_ID);
+        when(defaultUser.getEmail()).thenReturn(DEFAULT_USER_EMAIL);
+        when(defaultUser.getName()).thenReturn(DEFAULT_USER_NAME);
+        when(defaultUser.getPassword()).thenReturn(DEFAULT_USER_PASSWORD);
+
+        defaultArticle = mock(Article.class);
+        when(defaultArticle.getId()).thenReturn(DEFAULT_ARTICLE_ID);
+        when(defaultArticle.getTitle()).thenReturn(DEFAULT_ARTICLE_TITLE);
+        when(defaultArticle.getCoverUrl()).thenReturn(DEFAULT_ARTICLE_COVER_URL);
+        when(defaultArticle.getContents()).thenReturn(DEFAULT_ARTICLE_CONTENTS);
+        when(defaultArticle.getAuthor()).thenReturn(defaultUser);
+
+        articleRepository = mock(ArticleRepository.class);
+        userRepository = mock(UserRepository.class);
+        articleService = new ArticleService(articleRepository, userRepository);
+    }
 
     @Test
     void 게시글_생성_확인() {
-        User retrieveUser = userService.findById(DEFAULT_USER_ID);
-        ArticleRequestDto article = new ArticleRequestDto("some title", "", "some contents");
-        Long articleId = articleService.save(article, retrieveUser.getId());
-        assertThat(articleService.findById(articleId).getTitle()).isEqualTo(article.getTitle());
-        assertThat(articleService.findById(articleId).getContents()).isEqualTo(article.getContents());
+        // Given
+        when(articleRepository.save(any())).thenReturn(defaultArticle);
+        when(articleRepository.findById(DEFAULT_ARTICLE_ID)).thenReturn(Optional.of(defaultArticle));
+        when(userRepository.findById(DEFAULT_USER_ID)).thenReturn(Optional.of(defaultUser));
+        ArticleRequestDto article = new ArticleRequestDto(DEFAULT_ARTICLE_TITLE, DEFAULT_ARTICLE_COVER_URL, DEFAULT_ARTICLE_CONTENTS);
+
+        // When
+        Long articleId = articleService.save(article, DEFAULT_USER_ID);
+
+        // Then
+        assertThat(articleId).isEqualTo(DEFAULT_ARTICLE_ID);
+        assertThat(articleService.findById(articleId)).isEqualTo(defaultArticle);
     }
 
 
     @Test
     void 게시글_조회_확인() {
+        // Given
+        when(articleRepository.findById(DEFAULT_ARTICLE_ID)).thenReturn(Optional.of(defaultArticle));
+
+        // When
         Article retrieveArticleDto = articleService.findById(DEFAULT_ARTICLE_ID);
+
+        // Then
         assertThat(retrieveArticleDto).isEqualTo(articleService.findById(DEFAULT_ARTICLE_ID));
     }
 
     @Test
     void 모든_게시글_조회_확인() {
+        // Given
+        when(articleRepository.findById(DEFAULT_ARTICLE_ID)).thenReturn(Optional.of(defaultArticle));
+        when(articleRepository.findAll()).thenReturn(Collections.singletonList(defaultArticle));
+
+        // When
         List<Article> articleDtos = articleService.findAll();
-        assertThat(articleDtos).hasSize(1).containsExactly(articleService.findById(DEFAULT_ARTICLE_ID));
+
+        // Then
+        assertThat(articleDtos)
+            .hasSize(1)
+            .containsExactly(articleService.findById(DEFAULT_ARTICLE_ID));
     }
 
     @Test
     void 게시글_수정_확인() {
-        ArticleRequestDto updatedArticleDto = new ArticleRequestDto("title", "", "contents");
-        articleService.update(DEFAULT_ARTICLE_ID, updatedArticleDto);
-        Article retrievedArticleDto = articleService.findById(DEFAULT_ARTICLE_ID);
-        assertThat(retrievedArticleDto.getTitle()).isEqualTo(updatedArticleDto.getTitle());
-        assertThat(retrievedArticleDto.getContents()).isEqualTo(updatedArticleDto.getContents());
-    }
+        // Given
+        when(articleRepository.findById(DEFAULT_ARTICLE_ID)).thenReturn(Optional.of(defaultArticle));
+        ArticleRequestDto updateRequestDto = new ArticleRequestDto("title", "", "contents");
 
-    @Test
-    void 게시글_수정_오류확인_게시글이_null일_경우() {
-        assertThatExceptionOfType(NullPointerException.class)
-            .isThrownBy(() -> articleService.update(DEFAULT_ARTICLE_ID, null));
+        // When
+        articleService.update(DEFAULT_ARTICLE_ID, updateRequestDto);
+
+        // Then
+        verify(defaultArticle).update(any());
     }
 
     @Test
     void 게시글_삭제_확인() {
-        User retrieveUser = userService.findById(DEFAULT_USER_ID);
-        ArticleRequestDto article = new ArticleRequestDto("some title", "", "some contents");
-        Long articleId = articleService.save(article, retrieveUser.getId());
-        articleService.delete(articleId);
-        assertThatExceptionOfType(ArticleNotFoundException.class)
-            .isThrownBy(() -> articleService.findById(articleId));
-    }
+        // Given
+        when(articleRepository.findById(DEFAULT_ARTICLE_ID)).thenThrow(ArticleNotFoundException.class);
 
-    @Test
-    void 게시글_삭제_오류확인_없는_게시글_삭제요청할_경우() {
-        assertThatExceptionOfType(EmptyResultDataAccessException.class)
-            .isThrownBy(() -> articleService.delete(DEFAULT_ARTICLE_ID + 1));
+        // When
+        articleService.delete(DEFAULT_ARTICLE_ID);
+
+        // Then
+        assertThatExceptionOfType(ArticleNotFoundException.class)
+            .isThrownBy(() -> articleService.findById(DEFAULT_ARTICLE_ID));
+        verify(articleRepository).deleteById(DEFAULT_ARTICLE_ID);
     }
 }
