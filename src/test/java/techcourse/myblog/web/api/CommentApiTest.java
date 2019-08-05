adafd6567c9109d17895df982fc906368a6517e3
DaejunBae <dpudpu11@gmail.com>
2019-08-05 16:26:54 +0900

    refactor: CommentApiTest ObjectMapper 적용

--- src/test/java/techcourse/myblog/web/api/CommentApiTest.java
+++ src/test/java/techcourse/myblog/web/api/CommentApiTest.java
@@ -1,19 +1,30 @@
 package techcourse.myblog.web.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 import techcourse.myblog.dto.CommentDto;
 import techcourse.myblog.web.controller.BaseControllerTests;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CommentApiTest extends BaseControllerTests {
     @Autowired
     WebTestClient webTestClient;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     private String jSessionId;
 
@@ -27,19 +38,26 @@ class CommentApiTest extends BaseControllerTests {
     }
 
     @Test
-    public void create() {
+    public void create() throws IOException {
 
         final String contents = "changed contents";
         CommentDto.Create commentDto = new CommentDto.Create();
         commentDto.setContents(contents);
 
-        webTestClient.post().uri("/api/articles/1/comments")
+        final EntityExchangeResult<byte[]> entityExchangeResult = webTestClient.post().uri("/api/articles/1/comments")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .cookie(JSESSIONID, jSessionId)
                 .body(Mono.just(commentDto), CommentDto.Create.class)
                 .exchange()
                 .expectStatus().isCreated()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8);
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody().returnResult();
+
+        String body = new String(Objects.requireNonNull(entityExchangeResult.getResponseBody()));
+
+        List<CommentDto.Response> comments = new ObjectMapper().readValue(body, List.class);
+
+        assertThat(comments).isNotEmpty();
     }
 }
