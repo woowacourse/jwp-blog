5b066d6546a89905dda5c6ad9dd122cd103eb2b7
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-05 17:14:43 +0900

    refactor: use primitive type

--- src/main/java/techcourse/myblog/service/dto/CommentRequest.java
+++ src/main/java/techcourse/myblog/service/dto/CommentRequest.java
@@ -3,9 +3,18 @@ package techcourse.myblog.service.dto;
 import javax.validation.constraints.NotBlank;
 
 public class CommentRequest {
+    private long articleId;
+
     @NotBlank(message = "댓글을 입력하세요!")
     private String contents;
-    private Long articleId;
+
+    public CommentRequest() {
+    }
+
+    public CommentRequest(Long articleId, String contents) {
+        this.articleId = articleId;
+        this.contents = contents;
+    }
 
     public String getContents() {
         return contents;
@@ -21,5 +30,13 @@ public class CommentRequest {
 
     public void setArticleId(Long articleId) {
         this.articleId = articleId;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentRequest{" +
+                "contents='" + contents + '\'' +
+                ", articleId=" + articleId +
+                '}';
     }
 }
