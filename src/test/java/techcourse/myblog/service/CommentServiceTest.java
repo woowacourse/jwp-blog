3671a77692d7843533f567d494f09a01bb326301
DaejunBae <dpudpu11@gmail.com>
2019-08-05 23:07:46 +0900

    test: CommentServiceTest slicesTest 로 구현

--- src/test/java/techcourse/myblog/service/CommentServiceTest.java
+++ src/test/java/techcourse/myblog/service/CommentServiceTest.java
@@ -1,71 +1,124 @@
 package techcourse.myblog.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import techcourse.myblog.dto.ArticleDto;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.Comment;
+import techcourse.myblog.domain.User;
 import techcourse.myblog.dto.CommentDto;
-import techcourse.myblog.service.ArticleService;
-import techcourse.myblog.service.CommentService;
-import techcourse.myblog.dto.UserDto;
-import techcourse.myblog.service.UserService;
+import techcourse.myblog.exception.AuthException;
+import techcourse.myblog.repository.CommentRepository;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@SpringBootTest(classes = CommentService.class)
 class CommentServiceTest {
+
+    private static final long USER_ID = 1L;
+    protected static final long ANOTHER_USER_ID = 2L;
 
     @Autowired
     CommentService commentService;
 
-    @Autowired
-    UserService userService;
-
-    @Autowired
+    @MockBean
     ArticleService articleService;
 
-    private Long userId;
-    private Long articleId;
-    private Long commentId;
+    @MockBean
+    UserService userService;
 
-    @BeforeEach
-    void setUp() {
-        UserDto.Register userDto = UserDto.Register.builder()
-                .email("email@gmail.com")
-                .name("name")
-                .password("P@ssw0rd")
-                .confirmPassword("P@ssw0rd")
-                .build();
+    @MockBean
+    CommentRepository commentRepository;
 
-        userId = userService.save(userDto);
+    private User user = new User(USER_ID, "email@gamil.com", "name", "P@ss0wrd");
+    private Article article = new Article("title", "contents", "coverUrl", user);
+    private Comment comment = new Comment("contents", user, article);
+    private Comment anotherComment = new Comment("anotherComment", user, article);
 
-        ArticleDto.Request articleDto = ArticleDto.Request.builder()
-                .title("title")
-                .coverUrl("coverUrl")
-                .contents("contents")
-                .build();
+    @Test
+    void 댓글_저장() {
+        // given
+        CommentDto.Create commentCreate = mock(CommentDto.Create.class);
+        when(userService.findById(USER_ID)).thenReturn(user);
+        when(articleService.findById(any())).thenReturn(article);
+        when(commentRepository.save(any())).thenReturn(comment);
 
-        articleId = articleService.save(userId, articleDto);
+        // when
+        final CommentDto.Response commentResponse = commentService.save(commentCreate, USER_ID);
 
-        CommentDto.Create commentDto = new CommentDto.Create();
-        commentDto.setArticleId(articleId);
-        commentDto.setContents("contents");
-
-        commentId = commentService.save(commentDto, userId).getId();
+        // then
+        verify(userService).findById(USER_ID);
+        verify(articleService).findById(any());
+        verify(commentRepository).save(any());
+        assertThat(commentResponse.getContents()).isEqualTo(comment.getContents());
+        assertThat(commentResponse.getName()).isEqualTo(user.getName());
     }
 
     @Test
-    void update() {
-        final String modifiedContents = "updateContents";
+    void 댓글_수정() {
+        // given
+        CommentDto.Update commentUpdate = mock(CommentDto.Update.class);
+        when(commentUpdate.toComment()).thenReturn(anotherComment);
+        when(commentRepository.findById(any())).thenReturn(Optional.ofNullable(comment));
 
-        CommentDto.Update commentDto = new CommentDto.Update();
-        commentDto.setId(commentId);
-        commentDto.setArticleId(articleId);
-        commentDto.setContents(modifiedContents);
+        // when
+        final CommentDto.Response commentResponse = commentService.update(commentUpdate, USER_ID);
 
-        final CommentDto.Response modifiedComment = commentService.update(commentDto, userId);
-
-        assertThat(modifiedContents).isEqualTo(modifiedComment.getContents());
+        // then
+        verify(commentRepository).findById(any());
+        assertThat(commentResponse.getContents()).isEqualTo(anotherComment.getContents());
+        assertThat(commentResponse.getName()).isEqualTo(user.getName());
     }
+
+    @Test
+    void 다른_작성자_댓글_수정_예외처리() {
+        // given
+        CommentDto.Update commentUpdate = mock(CommentDto.Update.class);
+        when(commentUpdate.toComment()).thenReturn(anotherComment);
+        when(commentRepository.findById(any())).thenReturn(Optional.ofNullable(comment));
+
+        // when & then
+        assertThrows(AuthException.class, () -> commentService.update(commentUpdate, ANOTHER_USER_ID));
+    }
+
+    @Test
+    void 댓글_삭제() {
+        // given
+        when(commentRepository.findById(any())).thenReturn(Optional.ofNullable(comment));
+
+        // when
+        commentService.delete(1L, USER_ID);
+
+        // then
+        verify(commentRepository).delete(comment);
+    }
+    @Test
+    void 다른_작성자_댓글_삭제_예외처리() {
+        // given
+        when(commentRepository.findById(any())).thenReturn(Optional.ofNullable(comment));
+
+        // when & then
+        assertThrows(AuthException.class, () ->commentService.delete(1L, ANOTHER_USER_ID));
+    }
+
+    //
+//    @Test
+//    void update() {
+//        final String modifiedContents = "updateContents";
+//
+//        CommentDto.Update commentDto = new CommentDto.Update();
+//        commentDto.setId(commentId);
+//        commentDto.setArticleId(articleId);
+//        commentDto.setContents(modifiedContents);
+//
+//        final CommentDto.Response modifiedComment = commentService.update(commentDto, userId);
+//
+//        assertThat(modifiedContents).isEqualTo(modifiedComment.getContents());
+//    }
 }
