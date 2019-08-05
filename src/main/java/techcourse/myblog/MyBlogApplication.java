b40229c90a9e37e8c3bc299840556bbbc4cc5648
MrKwon <erased11@gmail.com>
2019-08-05 10:26:00 +0900

    feat: CommandLineRunner 추가

--- src/main/java/techcourse/myblog/MyBlogApplication.java
+++ src/main/java/techcourse/myblog/MyBlogApplication.java
@@ -1,7 +1,16 @@
 package techcourse.myblog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import techcourse.myblog.domain.Article;
+import techcourse.myblog.domain.Comment;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.repository.ArticleRepository;
+import techcourse.myblog.repository.CommentRepository;
+import techcourse.myblog.repository.UserRepository;
 
 @SpringBootApplication
 public class MyBlogApplication {
@@ -10,4 +19,20 @@ public class MyBlogApplication {
         SpringApplication.run(MyBlogApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository, ArticleRepository articleRepository, CommentRepository commentRepository) {
+        return (args -> {
+            User user = userRepository.save(
+                    new User("luffy", "luffy@luffy.com", "@Password12")
+            );
+
+            Article article = articleRepository.save(
+                    new Article("제목", "", "내용", user)
+            );
+
+            commentRepository.save(
+                    new Comment("contents", user, article)
+            );
+        });
+    }
 }
