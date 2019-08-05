6b412abb0b438aea8063e84264169b59a038b84a
MrKwon <erased11@gmail.com>
2019-08-05 12:55:50 +0900

    feat: CommentRestController 댓글 추가 기능 구현

--- src/main/java/techcourse/myblog/controller/CommentRestController.java
+++ src/main/java/techcourse/myblog/controller/CommentRestController.java
@@ -2,6 +2,8 @@ package techcourse.myblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import techcourse.myblog.controller.session.UserSessionManager;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
+import techcourse.myblog.domain.User;
+import techcourse.myblog.dto.CommentRequest;
 import techcourse.myblog.dto.CommentResponse;
 import techcourse.myblog.repository.CommentRepository;
+import techcourse.myblog.service.ArticleService;
 
 @RequestMapping("/comments")
 @RestController
 public class CommentRestController {
+
+    private ArticleService articleService;
+    private CommentRepository commentRepository;
+    private UserSessionManager userSessionManager;
+
     @Autowired
-    CommentRepository commentRepository;
+    public CommentRestController(ArticleService articleService, CommentRepository commentRepository, UserSessionManager userSessionManager/*UserRepository userRepository*/) {
+        this.articleService = articleService;
+        this.commentRepository = commentRepository;
+        this.userSessionManager = userSessionManager;
+    }
 
     @GetMapping
     public List<CommentResponse> showAllComments(@RequestParam Long articleId) {
@@ -32,5 +48,20 @@ public class CommentRestController {
             comments.add(commentResponse);
         });
         return comments;
+    }
+
+    @PostMapping
+    public CommentResponse save(@RequestBody CommentRequest commentRequest) {
+        User user = userSessionManager.getUser();
+        Article article = articleService.select(commentRequest.getArticleId());
+        Comment comment = new Comment(commentRequest.getContents(), user, article);
+        Comment savedComment = commentRepository.save(comment);
+        CommentResponse commentResponse = new CommentResponse(
+                savedComment.getId(),
+                savedComment.getContents(),
+                savedComment.getAuthor().getId(),
+                savedComment.getAuthor().getName()
+        );
+        return commentResponse;
     }
 }
--- src/test/java/techcourse/myblog/controller/CommentRestControllerTest.java
+++ src/test/java/techcourse/myblog/controller/CommentRestControllerTest.java
@@ -3,17 +3,25 @@ package techcourse.myblog.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import io.restassured.response.Response;
+import techcourse.myblog.controller.test.WebClientGenerator;
+import techcourse.myblog.dto.CommentRequest;
 import techcourse.myblog.dto.CommentResponse;
+import techcourse.myblog.dto.UserDto;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CommentRestControllerTest {
+public class CommentRestControllerTest extends WebClientGenerator {
     @LocalServerPort
     private int port;
 
@@ -31,5 +39,28 @@ public class CommentRestControllerTest {
                 .get("");
 
         assertThat(comments.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void Comment_작성하기() {
+        UserDto userDto = new UserDto("", "luffy@luffy.com", "@Password12");
+        MultiValueMap<String, ResponseCookie> loginCookie = getLoginCookie(userDto);
+
+        CommentRequest commentRequest = new CommentRequest(1L, "새로운 댓글입니다.");
+
+        Response response = given()
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .cookie("JSESSIONID", Objects.requireNonNull(loginCookie.getFirst("JSESSIONID")).getValue())
+                .body(commentRequest)
+                .expect()
+                .statusCode(200)
+                .when()
+                .post("http://localhost:" + port + "/comments");
+
+        Map<String, String> comment = response.getBody()
+                .jsonPath()
+                .get("");
+
+        assertThat(comment.get("contents")).isEqualTo(commentRequest.getContents());
     }
 }
