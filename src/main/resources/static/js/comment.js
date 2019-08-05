a0a5f5baf53b3749c451e50791e6e3e7168d894e
SunHo Van <vsh123@naver.com>
2019-08-05 18:59:32 +0900

    refactor : 수정 삭제 script 구현

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -6,6 +6,7 @@ const CommentApp = (function () {
         const deleteComment = function () {
             const list = document.getElementById('comment-list');
             list.addEventListener('click', commentService.remove);
+            list.addEventListener('click',commentService.update);
         };
 
         const init = function () {
@@ -19,25 +20,52 @@ const CommentApp = (function () {
 
     const CommentService = function () {
         const remove = function (event) {
+            console.log("hello")
             const articleId = document.getElementById('article-id').value;
             const btn = event.target;
-            if (btn.id !== 'delete-btn') {
-                return;
+            if (btn.classList.contains('delete-comment')) {
+                deleteComment(event);
             }
-            const form = document.querySelector('form');
-            form.action = '/articles/' + articleId +  '/comment/' + btn.value;
-            const deleteInput = document.createElement('input');
-            deleteInput.type = 'hidden';
-            deleteInput.name = '_method';
-            deleteInput.value = 'delete';
-
-            form.insertAdjacentElement('afterbegin', deleteInput);
-
-            form.submit();
         };
+
+        const deleteComment = function (event) {
+            const articleId = document.getElementById('article-id').value;
+            const btn = event.target;
+            console.log(btn.parentNode)
+            const url = '/articles/' + articleId + '/comment/' + btn.value;
+            const URL = "http://localhost:8080/" + url;
+            console.log(URL);
+            const obj = {
+                method: 'DELETE',
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            };
+
+            fetch(URL, obj)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (json) {
+                    const li = btn.parentNode
+                    const ul = btn.parentNode.parentNode
+                    ul.removeChild(li)
+                });
+        }
+
+        const update = function(event) {
+            const articleId = document.getElementById('article-id').value;
+            const btn = event.target;
+            if(btn.classList.contains("btn-lg")) {
+                const url = 'articles/' + articleId + '/comment/' + btn.value;
+                console.log(url);
+                document.getElementById('comment-update-url').setAttribute('value', url);
+            }
+        }
 
         return {
             remove: remove,
+            update: update
         }
     }
 
