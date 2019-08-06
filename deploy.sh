fd3de2b3a72f40e4b553a12df97250de4fa15238
green4469 <green4469@gmail.com>
2019-08-06 15:39:25 +0900

    chore: update scripts

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 kill $(lsof -t -i:8080)
 git pull
-./gradlew clean build
-java -jar ./build/libs/$(ls ./build/libs) &
+~/jwp-blog/gradlew clean build
+java -jar ~/jwp-blog/build/libs/$(ls ~/jwp-blog/build/libs) &
