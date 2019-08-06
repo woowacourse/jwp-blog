4d851f20a3e47f57e0253e84bb40cac3767bb925
backlo <hjun4512@naver.com>
2019-08-06 13:41:10 +0900

    fix: comment ajax fetch 로 변경

--- src/main/java/techcourse/myblog/controller/CommentController.java
+++ src/main/java/techcourse/myblog/controller/CommentController.java
@@ -2,6 +2,8 @@ package techcourse.myblog.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import techcourse.myblog.controller.dto.RequestCommentDto;
@@ -24,12 +26,6 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    //    @PostMapping
-//    public String createComment(RequestCommentDto commentDto, @ModelAttribute User user) {
-//        Long articleId = commentService.save(commentDto, user);
-//        return "redirect:/articles/" + articleId;
-//    }
-//
     @GetMapping("/{commentId}/edit")
     public String editCommentForm(@PathVariable Long commentId, Model model, @ModelAttribute User user) {
         Comment comment = commentService.findById(commentId, user);
@@ -37,10 +33,10 @@ public class CommentController {
         return "comment-edit";
     }
 
-    @DeleteMapping("/{commentId}")
-    public String deleteComment(@PathVariable Long commentId, @ModelAttribute User user) {
-        return "redirect:/articles/" + commentService.delete(commentId, user).getId();
-    }
+//    @DeleteMapping("/{commentId}")
+//    public String deleteComment(@PathVariable Long commentId, @ModelAttribute User user) {
+//        return "redirect:/articles/" + commentService.delete(commentId, user).getId();
+//    }
 
     @PutMapping("/{commentId}")
     public String updateComment(@PathVariable Long commentId, RequestCommentDto requestCommentDto, @ModelAttribute User user) {
@@ -50,7 +46,14 @@ public class CommentController {
     }
 
     @PostMapping
-    public ResponseCommentDto createComment(@RequestBody RequestCommentDto requestCommentDto, @ModelAttribute User user) {
-        return commentService.save(requestCommentDto, user);
+    public ResponseEntity<ResponseCommentDto> createComment(@RequestBody RequestCommentDto requestCommentDto, @ModelAttribute User user) {
+        return new ResponseEntity<>(commentService.save(requestCommentDto, user), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseCommentDto deleteComment(@PathVariable Long commentId, @ModelAttribute User user) {
+        commentService.delete(commentId, user);
+
+        return new ResponseCommentDto();
     }
 }
