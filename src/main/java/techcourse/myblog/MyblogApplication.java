d5bfed28e5c089547693b4dc599ac27e48c81952
SunHo Van <vsh123@naver.com>
2019-08-06 18:19:27 +0900

    Revert "revert to home"
    This reverts commit 1ee97e3d7aff3d1161d08f942f375d4f15dd0ac8.

--- src/main/java/techcourse/myblog/MyblogApplication.java
+++ src/main/java/techcourse/myblog/MyblogApplication.java
@@ -2,12 +2,12 @@ package techcourse.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class MyblogApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MyblogApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogApplication.class, args);
+    }
+
 }
