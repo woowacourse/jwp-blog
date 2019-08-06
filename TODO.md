0811e242ad2510622a1ce8aaab8074633cfe6e1a
Ko eunsuk <eunsukko.128@gmail.com>
2019-08-06 17:52:48 +0900

    Revert "[게시글 생성/조회/수정/삭제] 미스터코 미션 제출합니다. (#44)"
    This reverts commit 2dce0cb1e4c01f5bae9081e3b217b13db0bf97b5.

--- TODO.md
+++
@@ -1,9 +0,0 @@
-TODO..
-- [x] understand this code.... ()
-    - [x] fix bug (when update mypage then can not access edit page)
-        - after updating, session.user has corrupted id
-
-- [ ] implement HandlerMethodArgumentResolver (strategy pattern for specific arguments)
-
-테스트 보강
-- [ ] test for checking is logged in for editing ()
--- src/main/java/techcourse/myblog/domain/Article.java
+++ src/main/java/techcourse/myblog/domain/Article.java
@@ -1,43 +1,4 @@
 package techcourse.myblog.domain;
 
-import lombok.*;
-import techcourse.myblog.service.dto.ArticleRequestDto;
-
-import javax.persistence.*;
-
-@Entity
-@NoArgsConstructor
-@Getter
-@EqualsAndHashCode
-@ToString
 public class Article {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @Column(nullable = false)
-    private String title;
-    @Column(nullable = false)
-    private String contents;
-    @Column(nullable = false)
-    private String coverUrl;
-
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "FK_AUTHOR"), name = "author")
-    private User author;
-
-    public Article(String title, String contents, String coverUrl) {
-        this.title = title;
-        this.contents = contents;
-        this.coverUrl = coverUrl;
-    }
-
-    public void update(ArticleRequestDto articleRequestDto) {
-        this.title = articleRequestDto.getTitle();
-        this.contents = articleRequestDto.getContents();
-        this.coverUrl = articleRequestDto.getCoverUrl();
-    }
-
-    public void setAuthor(User persistUser) {
-        this.author = persistUser;
-    }
 }
