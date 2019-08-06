bee3eb4e9b3d5921d5933cf1abdf180a7811c4d8
이영현 <leey9494@icloud.com>
2019-08-06 18:07:14 +0900

    setting merget

--- src/main/java/techcourse/myblog/web/annotation/LoginUser 2.java
+++
@@ -1,9 +0,0 @@
-package techcourse.myblog.web.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface LoginUser {
-}
