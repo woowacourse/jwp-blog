22a5c73d44b4582f703120df29cdc16b77aecf67
SunHo Van <vsh123@naver.com>
2019-08-06 16:32:44 +0900

    Revert "revert mission"
    This reverts commit 6034a57b9e86613434b8fd2b474b3762ec125027.

--- src/main/java/techcourse/myblog/application/service/exception/NotExistCommentException.java
+++
@@ -1,8 +0,0 @@
-package techcourse.myblog.application.service.exception;
-
-public class NotExistCommentException extends RuntimeException {
-    public NotExistCommentException(String message) {
-        super(message);
-    }
-}
-
