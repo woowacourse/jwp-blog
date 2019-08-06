a98cb8bcc4c3f54713c5c27b10c4ee95613281aa
green4469 <green4469@gmail.com>
2019-08-06 11:07:39 +0900

    feat: add empty RestCommentController class

---
+++ src/main/java/techcourse/myblog/presentation/RestCommentController.java
@@ -0,0 +1,11 @@
+package techcourse.myblog.presentation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/comments")
+public class RestCommentController {
+}
