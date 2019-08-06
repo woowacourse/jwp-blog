1bdf6c971e1c29e6548bc7e876347714dde8ae55
박효진 <hgenie14@gmail.com>
2019-08-06 17:24:11 +0900

    test: 로그인 및 댓글 등록/수정 기능을 비동기식 통신으로 바꿈에 따른 테스트 수정

--- src/test/java/techcourse/myblog/comment/controller/CommentControllerTest.java
+++ src/test/java/techcourse/myblog/comment/controller/CommentControllerTest.java
@@ -1,16 +1,24 @@
 package techcourse.myblog.comment.controller;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+import techcourse.myblog.dto.CommentRequestDto;
 import techcourse.myblog.template.LoginTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommentControllerTest extends LoginTemplate {
     private static final String CONTENTS = "댓글썼당";
     private static final String SECOND_USER_NAME = "두번째유저";
     private static final String SECOND_USER_EMAIL = "second@user.com";
     private static final String SECOND_USER_PASSWORD = "Second22@";
+
+    private static int commentId = 1;
 
     @BeforeEach
     void setUp() {
@@ -20,43 +28,56 @@ public class CommentControllerTest extends LoginTemplate {
     @Test
     void 댓글이_정상적으로_등록되는지_테스트() {
         String articleId = getNewArticleId();
+        CommentRequestDto commentRequestDto = new CommentRequestDto(CONTENTS);
 
         loggedInPostRequest("/comment/" + articleId)
-                .body(BodyInserters.fromFormData("contents", CONTENTS))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(commentRequestDto), CommentRequestDto.class)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/articles/" + articleId + ".*");
+                .expectStatus().isOk()
+                .expectBody(CommentRequestDto.class)
+                .consumeWith(res -> assertThat(res.getResponseBody().getContents()).isEqualTo(CONTENTS));
     }
 
     @Test
     void 댓글이_정상적으로_수정되는지_테스트() {
         String articleId = getNewArticleId();
-        String commentId = getNewCommentId(articleId);
+        getNewCommentId(articleId);
+
+        CommentRequestDto commentRequestDto = new CommentRequestDto(CONTENTS + 2);
 
         loggedInPutRequest("/comment/" + articleId + "/" + commentId)
-                .body(BodyInserters.fromFormData("contents", CONTENTS))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(commentRequestDto), CommentRequestDto.class)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/articles/" + articleId + ".*");
+                .expectStatus().isOk()
+                .expectBody(CommentRequestDto.class)
+                .consumeWith(res -> {
+                    assertThat(res.getResponseBody().getContents()).isEqualTo(CONTENTS + 2);
+                });
     }
 
     @Test
     void 수정하려는_사용자가_댓글_작성자가_아닌_경우_예외처리() {
+        CommentRequestDto commentRequestDto = new CommentRequestDto(CONTENTS);
         registeredWebTestClient(SECOND_USER_NAME, SECOND_USER_EMAIL, SECOND_USER_PASSWORD);
         String articleId = getNewArticleId();
-        String commentId = getNewCommentId(articleId);
+        getNewCommentId(articleId);
 
         loggedInPutRequest("/comment/" + articleId + "/" + commentId, SECOND_USER_EMAIL, SECOND_USER_PASSWORD)
-                .body(BodyInserters.fromFormData("contents", CONTENTS))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(commentRequestDto), CommentRequestDto.class)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/");
+                .expectStatus().isFound();
     }
 
     @Test
     void 댓글이_정상적으로_삭제되는지_테스트() {
         String articleId = getNewArticleId();
-        String commentId = getNewCommentId(articleId);
+        getNewCommentId(articleId);
 
         loggedInDeleteRequest("/comment/" + articleId + "/" + commentId)
                 .exchange()
@@ -69,12 +90,17 @@ public class CommentControllerTest extends LoginTemplate {
     void 삭제하려는_사용자가_댓글_작성자가_아닌_경우_예외처리() {
         registeredWebTestClient(SECOND_USER_NAME, SECOND_USER_EMAIL, SECOND_USER_PASSWORD);
         String articleId = getNewArticleId();
-        String commentId = getNewCommentId(articleId);
+        getNewCommentId(articleId);
 
         loggedInDeleteRequest("/comment/" + articleId + "/" + commentId, SECOND_USER_EMAIL, SECOND_USER_PASSWORD)
                 .exchange()
                 .expectStatus().isFound()
                 .expectHeader().valueMatches("Location", "http://localhost:[0-9]+/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        commentId++;
     }
 
     private String getNewArticleId() {
@@ -84,15 +110,15 @@ public class CommentControllerTest extends LoginTemplate {
                 .getPath().split("/")[2];
     }
 
-    private String getNewCommentId(String articleId) {
-        return loggedInPostRequest("/comment/" + articleId)
-                .body(BodyInserters.fromFormData("contents", CONTENTS))
+    private void getNewCommentId(String articleId) {
+        CommentRequestDto commentRequestDto = new CommentRequestDto(CONTENTS);
+
+        loggedInPostRequest("/comment/" + articleId)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(commentRequestDto), CommentRequestDto.class)
                 .exchange()
-                .expectStatus().isFound()
-                .expectBody().returnResult()
-                .getResponseHeaders()
-                .getLocation()
-                .getPath().split("/")[2];
+                .expectStatus().isOk();
     }
 
     public EntityExchangeResult<byte[]> articlePostRequest() {
