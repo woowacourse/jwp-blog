1ecf8ba061ce7c1842066c9907586ba842373584
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-06 14:37:29 +0900

    refactor: uri 변경

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -1,5 +1,9 @@
 package techcourse.myblog.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +12,15 @@ import techcourse.myblog.domain.User;
 import techcourse.myblog.service.ArticleService;
 import techcourse.myblog.service.CommentService;
 import techcourse.myblog.service.dto.ArticleRequest;
+import techcourse.myblog.service.dto.CommentsResponse;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/articles")
 public class ArticleController {
+    private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
+
     private ArticleService articleService;
     private CommentService commentService;
 
@@ -23,44 +29,57 @@ public class ArticleController {
         this.commentService = commentService;
     }
 
-    @GetMapping("/edit")
+    @GetMapping("/articles/edit")
     public String formArticle(Model model) {
+        log.debug("begin");
         model.addAttribute("article", null);
         return "article-edit";
     }
 
-    @PostMapping
+    @PostMapping("/articles")
     public String saveArticle(@Valid ArticleRequest articleRequest, Model model, HttpSession httpSession) {
+        log.debug("begin");
         User user = (User) httpSession.getAttribute("user");
         Article article = articleService.save(articleRequest, user);
         model.addAttribute("article", article);
         return "redirect:/articles/" + article.getId();
     }
 
-    @GetMapping("/{articleId}")
-    public String selectArticle(@PathVariable("articleId") long articleId, Model model) {
-        Article article = articleService.findById(articleId);
-        model.addAttribute("article", article);
+    @GetMapping("/articles/{articleId}")
+    public String showArticlePage(@PathVariable("articleId") long articleId, Model model) {
+        model.addAttribute("articleId", articleId);
         return "article";
     }
 
-    @GetMapping("/{articleId}/edit")
+    @GetMapping("/api/articles/{articleId}")
+    @ResponseBody
+    public ResponseEntity<Article> selectArticle(@PathVariable("articleId") long articleId) {
+        return new ResponseEntity<>(articleService.findById(articleId), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/articles/{articleId}/edit")
     public String edit(@PathVariable("articleId") long articleId, Model model, HttpSession httpSession) {
         Article article = articleService.findByIdWithUser(articleId, (User) httpSession.getAttribute("user"));
         model.addAttribute("article", article);
         return "article-edit";
     }
 
-    @PutMapping("/{articleId}")
+    @PutMapping("/articles/{articleId}")
     public String editArticle(@PathVariable("articleId") long articleId, @ModelAttribute ArticleRequest articleRequest, HttpSession httpSession, Model model) {
         Article article = articleService.editArticle(articleRequest, articleId, (User) httpSession.getAttribute("user"));
         model.addAttribute("article", article);
         return "redirect:/articles/" + articleId;
     }
 
-    @DeleteMapping("/{articleId}")
+    @DeleteMapping("/articles/{articleId}")
     public String deleteArticle(@PathVariable("articleId") long articleId, HttpSession httpSession) {
         articleService.deleteById(articleId, (User) httpSession.getAttribute("user"));
         return "redirect:/";
+    }
+
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentsResponse> getComment(@PathVariable("articleId") long articleId) {
+        return new ResponseEntity<>(commentService.findByArticleId(articleId), HttpStatus.OK);
     }
 }
--- src/main/java/techcourse/myblog/web/CommentController.java
+++ src/main/java/techcourse/myblog/web/CommentController.java
@@ -3,38 +3,26 @@ package techcourse.myblog.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.Comment;
 import techcourse.myblog.domain.User;
-import techcourse.myblog.service.ArticleService;
 import techcourse.myblog.service.CommentService;
 import techcourse.myblog.service.dto.CommentRequest;
-import techcourse.myblog.service.dto.CommentsResponse;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/api/comments")
 public class CommentController {
     private CommentService commentService;
-    private ArticleService articleService;
 
-    public CommentController(CommentService commentService, ArticleService articleService) {
+    public CommentController(CommentService commentService) {
         this.commentService = commentService;
-        this.articleService = articleService;
-    }
-
-    @GetMapping("/{articleId}")
-    public ResponseEntity<CommentsResponse> getComment(@PathVariable("articleId") long articleId) {
-        return new ResponseEntity<>(commentService.findByArticleId(articleId), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Comment> saveComment(@RequestBody @Valid CommentRequest commentRequest, HttpSession httpSession) {
-        // TODO: 2019-08-05 remove article service
-        Article article = articleService.findById(commentRequest.getArticleId());
-        Comment comment = commentService.save(commentRequest, article, (User) httpSession.getAttribute("user"));
+        Comment comment = commentService.save(commentRequest, (User) httpSession.getAttribute("user"));
         return new ResponseEntity<>(comment, HttpStatus.OK);
     }
 
