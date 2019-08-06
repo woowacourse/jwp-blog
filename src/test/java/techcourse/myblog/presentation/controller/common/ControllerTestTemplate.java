c7b6ebe17d3606d731b0a519991b386739ea2c4d
ddu0422 <ddu0422@naver.com>
2019-08-06 11:02:57 +0900

    refactor: TestTemplate을 적용해 중복 제거
    * ControllerTestTemplate에 Mono를 인자로 받는 TemplateMethod 추가

--- src/test/java/techcourse/myblog/presentation/controller/common/ControllerTestTemplate.java
+++ src/test/java/techcourse/myblog/presentation/controller/common/ControllerTestTemplate.java
@@ -6,19 +6,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.StatusAssertions;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.BodyContentSpec;
 import org.springframework.test.web.reactive.server.WebTestClient.RequestHeadersSpec;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
+import techcourse.myblog.application.dto.ArticleDto;
 import techcourse.myblog.application.dto.UserDto;
 import techcourse.myblog.domain.user.User;
 import techcourse.myblog.domain.user.UserRepository;
-import techcourse.myblog.application.dto.ArticleDto;
 
 import java.util.Objects;
 
@@ -59,7 +61,7 @@ public class ControllerTestTemplate {
     protected StatusAssertions httpRequestWithData(HttpMethod method, String uri, MultiValueMap<String, String> data) {
         return httpRequest(makeRequestSpecWithData(method, uri, data));
     }
-    
+
     protected StatusAssertions loginAndRequest(HttpMethod method, String path, UserDto userDto) {
         return httpRequest(makeRequestSpec(method, path).cookie(JSESSIONID, getLoginSessionId(userDto)));
     }
@@ -67,7 +69,11 @@ public class ControllerTestTemplate {
     protected StatusAssertions loginAndRequestWithData(HttpMethod method, String path, MultiValueMap<String, String> data, UserDto userDto) {
         return httpRequest(makeRequestSpecWithData(method, path, data).cookie(JSESSIONID, getLoginSessionId(userDto)));
     }
-    
+
+    protected BodyContentSpec loginAndRequestWithMonoData(HttpMethod method, String path, HttpStatus httpStatus, Object object, UserDto userDto) {
+        return httpRequest(makeRequestSpecWithData(method, path, object).cookie(JSESSIONID, getLoginSessionId(userDto)), httpStatus);
+    }
+
     protected StatusAssertions loginAndRequestWriter(HttpMethod method, String path) {
         return loginAndRequest(method, path, savedUserDto);
     }
@@ -84,11 +90,20 @@ public class ControllerTestTemplate {
                 .getFirst(JSESSIONID))
                 .getValue();
     }
-    
+
     private StatusAssertions httpRequest(RequestHeadersSpec requestHeadersSpec) {
         return requestHeadersSpec
                 .exchange()
                 .expectStatus();
+    }
+
+    private BodyContentSpec httpRequest(RequestHeadersSpec requestHeadersSpec, HttpStatus httpStatus) {
+        return requestHeadersSpec
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isEqualTo(httpStatus)
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody();
     }
     
     private RequestBodySpec makeRequestSpec(HttpMethod method, String uri) {
@@ -99,6 +114,12 @@ public class ControllerTestTemplate {
         return makeRequestSpec(method, uri)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters.fromFormData(data));
+    }
+
+    private RequestHeadersSpec<?> makeRequestSpecWithData(HttpMethod method, String uri, Object object) {
+        return makeRequestSpec(method, uri)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(object), Object.class);
     }
 
     protected String getResponseBody(StatusAssertions statusAssertions) {
