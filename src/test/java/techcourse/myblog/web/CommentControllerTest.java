0e2bfa236a8972f8d8eac5d0c6b509dac3e2c254
imkimheejoo <jujudamdadi@naver.com>
2019-08-06 15:27:51 +0900

    test: ajax 댓글 기능 테스트 구현

--- src/test/java/techcourse/myblog/web/CommentControllerTest.java
+++ src/test/java/techcourse/myblog/web/CommentControllerTest.java
@@ -2,12 +2,13 @@ package techcourse.myblog.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+import techcourse.myblog.service.dto.CommentDto;
 
-import java.util.Objects;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static techcourse.myblog.user.UserTest.user;
 import static techcourse.myblog.user.UserTest.user2;
 
@@ -26,40 +27,37 @@ public class CommentControllerTest extends AbstractControllerTest {
     @Test
     void 로그인_후_댓글_생성() {
         String jSessionId = extractJSessionId(login(user));
+        CommentDto commentDto = new CommentDto();
+        commentDto.setContents("댓글입니다.");
+
         webTestClient.post().uri("/articles/1/comment")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
                 .cookie("JSESSIONID", jSessionId)
-                .body(BodyInserters
-                        .fromFormData("contents", "댓글입니다."))
+                .body(Mono.just(commentDto), CommentDto.class)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("location", ".*/articles.*");
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody()
+                .jsonPath("$..contents").value(hasItem("댓글입니다."));
     }
 
     @Test
     void 로그인_후_댓글_수정() {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setContents("수정된 댓글입니다.");
+
         String jSessionId = extractJSessionId(login(user));
-        String updateComment = "수정된 댓글입니다.";
-        WebTestClient.ResponseSpec responseSpec = webTestClient.put().uri("/articles/1/comment/1")
+        webTestClient.put().uri("/articles/1/comment/1")
                 .cookie("JSESSIONID", jSessionId)
-                .body(BodyInserters
-                        .fromFormData("contents", updateComment))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(commentDto),CommentDto.class)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("location", ".*/articles.*");
-
-        responseSpec.expectBody()
-                .consumeWith(response -> {
-                    webTestClient.get().uri(Objects.requireNonNull(response.getResponseHeaders().get("Location")).get(0))
-                            .exchange()
-                            .expectStatus().isOk()
-                            .expectBody()
-                            .consumeWith(res -> {
-                                String body = new String(Objects.requireNonNull(res.getResponseBody()));
-                                assertThat(body.contains(updateComment)).isTrue();
-                            });
-                });
-
-
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody()
+                .jsonPath("$..contents").value(hasItem("수정된 댓글입니다."));
     }
 
     @Test
@@ -68,19 +66,24 @@ public class CommentControllerTest extends AbstractControllerTest {
         webTestClient.delete().uri("/articles/1/comment/2")
                 .cookie("JSESSIONID", jSessionId)
                 .exchange()
-                .expectStatus().isFound()
-                .expectHeader().valueMatches("location", ".*/articles.*");
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody()
+                .jsonPath("$..id").value(not("2"));
     }
 
     @Test
     void 자신이_쓰지_않은_댓글_수정_시도() {
         String jSessionId = extractJSessionId(login(user2));
-        String updateComment = "수정된 댓글입니다.";
+        CommentDto commentDto = new CommentDto();
+        commentDto.setContents("수정된 댓글입니다.");
+
 
         webTestClient.put().uri("/articles/1/comment/1")
                 .cookie("JSESSIONID", jSessionId)
-                .body(BodyInserters
-                        .fromFormData("contents", updateComment))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(commentDto),CommentDto.class)
                 .exchange()
                 .expectStatus().is3xxRedirection()
                 .expectHeader().valueMatches("location", ".*/");
