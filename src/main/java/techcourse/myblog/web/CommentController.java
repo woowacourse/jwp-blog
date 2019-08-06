0e8b83f503d6841319de3cbfb3f06d7cb5dbadcd
gch01410 <gch01410@naver.com>
2019-08-06 15:34:41 +0900

    refactor: Replace function of comment updating with ajax

--- src/main/java/techcourse/myblog/web/CommentController.java
+++ src/main/java/techcourse/myblog/web/CommentController.java
@@ -1,7 +1,5 @@
 package techcourse.myblog.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +13,6 @@ import java.util.List;
 @Controller
 @RequestMapping("/articles/{articleId}/comments")
 public class CommentController {
-    private static final Logger log = LoggerFactory.getLogger(CommentController.class);
-
     private final CommentService commentService;
 
     @Autowired
@@ -27,16 +23,16 @@ public class CommentController {
     @ResponseBody
     @PostMapping("")
     public List<CommentResponse> addComment(@PathVariable final Long articleId, @RequestBody final CommentRequest commentRequest, final AccessUserInfo accessUserInfo) {
-        log.info("Json : " + commentRequest.getComment());
         commentService.save(commentRequest, accessUserInfo.getUser().getId(), articleId);
         return commentService.findByArticleId(articleId);
     }
 
 
+    @ResponseBody
     @PutMapping("/{commentId}")
-    public String updateComment(@PathVariable final Long articleId, @PathVariable final Long commentId, final CommentRequest commentRequest, final AccessUserInfo accessUserInfo) {
+    public List<CommentResponse> updateComment(@PathVariable final Long articleId, @PathVariable final Long commentId, @RequestBody final CommentRequest commentRequest, final AccessUserInfo accessUserInfo) {
         commentService.update(commentRequest, commentId, articleId, accessUserInfo.getUser());
-        return "redirect:/articles/" + articleId;
+        return commentService.findByArticleId(articleId);
     }
 
     @DeleteMapping("/{commentId}")
