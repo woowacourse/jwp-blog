24d54f83e7e2684fbc7ef2ffd2b83b51bf031794
SunHo Van <vsh123@naver.com>
2019-08-07 09:14:00 +0900

    refactor : properties -> yml 변경

--- src/main/java/techcourse/myblog/MyblogApplication.java
+++ src/main/java/techcourse/myblog/MyblogApplication.java
@@ -2,11 +2,18 @@ package techcourse.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MyblogApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/springboot-webservice/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(MyblogApplication.class, args);
+		new SpringApplicationBuilder(MyblogApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
