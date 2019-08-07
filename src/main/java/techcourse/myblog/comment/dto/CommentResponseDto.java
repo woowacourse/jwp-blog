10a2d488cc79c493d81723ecd45bfc8236e93596
Seongmo Jeon <oeeen3@gmail.com>
2019-08-07 23:24:00 +0900

    refactor: 불필요한 컨트롤러 제거, 테스트 제거

--- src/main/java/techcourse/myblog/comment/dto/CommentResponseDto.java
+++ src/main/java/techcourse/myblog/comment/dto/CommentResponseDto.java
@@ -1,9 +1,7 @@
 package techcourse.myblog.comment.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-import techcourse.myblog.article.domain.Article;
 import techcourse.myblog.user.domain.User;
 
 @Getter
@@ -12,7 +10,4 @@ public class CommentResponseDto {
     private long id;
     private String contents;
     private User author;
-
-    @JsonIgnore
-    private Article article;
 }
