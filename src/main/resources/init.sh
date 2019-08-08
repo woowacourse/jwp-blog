230a9115f2802217fb11e4c3eaec0fc04cb0c2bc
codeanddonuts <kkw200@naver.com>
2019-08-08 16:05:43 +0900

    fix: auto deploy script

--- src/main/resources/init.sh
+++ src/main/resources/init.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-cd /home/ubuntu/
 sudo apt-get update
 sudo apt-get install default-jdk
 sudo apt-get install mysql-server
-sudo mysql -uroot << QUERIES
-CREATE DATABASE woowa;
-CREATE USER 'donut'@'localhost' identified by 'qwer1234';
-GRANT ALL PRIVILEGES ON woowa.* to 'donut'@'localhost';
-QUERIES
 sudo apt-get install git
+cd /home/ubuntu/
 git clone -b level4 https://github.com/codeanddonuts/jwp-blog.git
 cd jwp-blog
 ./gradlew clean build
