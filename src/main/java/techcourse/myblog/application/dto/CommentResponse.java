e361924d0701957e36039f847cf631ccc4d00d3d
Soojin Roh <sooreal91@gmail.com>
2019-08-08 10:56:38 +0900

    Refactor : Separate inner class of CommentResponse to CommentDto

--- src/main/java/techcourse/myblog/application/dto/CommentResponse.java
+++ src/main/java/techcourse/myblog/application/dto/CommentResponse.java
@@ -2,8 +2,6 @@ package techcourse.myblog.application.dto;
 
 import techcourse.myblog.domain.Comment;
 
-import java.time.LocalDateTime;
-
 public class CommentResponse extends BaseResponse {
 
     private CommentDto comment;
@@ -29,52 +27,4 @@ public class CommentResponse extends BaseResponse {
         this.comment = comment;
     }
 
-    public static class CommentDto {
-        private Long id;
-        private String contents;
-        private String authorName;
-        private LocalDateTime createdTime;
-
-        public CommentDto() {
-        }
-
-        public CommentDto(Long id, String contents, String authorName, LocalDateTime createdTime) {
-            this.id = id;
-            this.contents = contents;
-            this.authorName = authorName;
-            this.createdTime = createdTime;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getContents() {
-            return contents;
-        }
-
-        public void setContents(String contents) {
-            this.contents = contents;
-        }
-
-        public String getAuthorName() {
-            return authorName;
-        }
-
-        public void setAuthorName(String authorName) {
-            this.authorName = authorName;
-        }
-
-        public LocalDateTime getCreatedTime() {
-            return createdTime;
-        }
-
-        public void setCreatedTime(LocalDateTime createdTime) {
-            this.createdTime = createdTime;
-        }
-    }
 }
