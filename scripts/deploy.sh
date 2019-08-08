2fd4d857ccc3eb3b01437e31a21cd47e8c014491
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-08 09:43:04 +0900

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-sudo docker stop $(sudo docker ps -a -q --filter name=elastic_hypatia)
-sudo docker rm $(sudo docker ps -a -q --filter name=elastic_hypatia)
+container_id_to_stop = $(sudo docker ps -q --filter name=myblog)
+
+if ${#container_id_to_stop}=0 then
+    sudo docker stop $container_id_to_stop
+fi
+
+container_id_to_remove = $(sudo docker ps -a -q --filter name=myblog)
+
+if ${#container_id_to_remove}=0 then
+    sudo docker rm $container_id_to_remove
+fi
+
 docker start -itd \
     --name myblog \
     -p 8080:8080 \
