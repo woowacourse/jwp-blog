2cea2b7ee1d3daf389f3add184d64ac0d7ee1806
DaejunBae <dpudpu11@gmail.com>
2019-08-08 16:40:37 +0900

    refactor: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,26 @@
-BRANCH="dpudpu"
+REPOSITORY="/home/ubuntu/"
+cd $REPOSITORY/jwp-blog
+
+BRANCH="step4"
 echo ">>> GIT PULL ..."
 git pull origin $BRANCH
+
 echo ">>> BUILD START ..."
-gradlew clean build
+./gradlew clean build
 echo ">>> BUILD COMPLETE"
-CURRENT_PID=$(pgrep -f java)
-echo ">>> KILL PROCESS $CURRENT_PID"
-kill -2 $CURRENT_PID
-JAR_NAME=$(ls /home/ubuntu/jwp-blog/build/libs/ | grep 'myblog' | tail -n 1)
-echo ">>> START APPLICATION..."
+
+echo ">>> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f myblog)
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+	echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo ">>> KILL PROCESS $CURRENT_PID"
+	kill -9 $CURRENT_PID
+fi
+
+echo ">>> START NEW APPLICATION..."
+JAR_NAME=$(ls $REPOSITORY/jwp-blog/build/libs/ | grep 'myblog' | tail -n 1)
+echo ">>> JAR NAME: $JAR_NAME"
 nohup java -jar build/libs/$JAR_NAME &
