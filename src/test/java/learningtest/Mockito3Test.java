2e64f3569f0d950d6c137c59fdc1a63c3f21eb13
DaejunBae <dpudpu11@gmail.com>
2019-08-08 13:35:36 +0900

    test: Mockito 학습테스트 추가

---
+++ src/test/java/learningtest/Mockito3Test.java
@@ -0,0 +1,24 @@
+package learningtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import techcourse.myblog.repository.ArticleRepository;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Mockito3Test {
+
+    @Mock
+    ArticleRepository articleRepository;
+
+    @Test
+    public void mockTest() {
+
+        when(articleRepository.findById(1L)).thenReturn(null);
+
+        System.out.println(articleRepository.findById(1L));
+    }
+}
