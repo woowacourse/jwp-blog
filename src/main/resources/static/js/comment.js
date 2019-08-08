32a36b9e2332af47e64e54bff0f9c2245cdd9d74
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-08 13:49:49 +0900

    refactor : 프론트엔드 템플릿 append 방식 -> 백틱 방식으로 수정

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -6,23 +6,25 @@ const CommentApp = (function () {
         const deleteComment = function () {
             const list = document.getElementById('comment-list');
             list.addEventListener('click', commentService.remove);
-            list.addEventListener('click',commentService.update);
+        };
+
+        const updateComment = function () {
+            const list = document.getElementById('comment-list');
+            list.addEventListener('click', commentService.update);
         };
 
         const init = function () {
+            updateComment();
             deleteComment();
         };
 
         return {
             init: init
         }
-    }
+    };
 
     const CommentService = function () {
-        const DOMAIN_URL = location.protocol + "//" + location.host;
-
         const remove = function (event) {
-            console.log("hello")
             const articleId = document.getElementById('article-id').value;
             const btn = event.target;
             if (btn.classList.contains('delete-comment')) {
@@ -33,10 +35,7 @@ const CommentApp = (function () {
         const deleteComment = function (event) {
             const articleId = document.getElementById('article-id').value;
             const btn = event.target;
-            console.log(btn.parentNode)
             const url = '/articles/' + articleId + '/comment/' + btn.value;
-            const URL = DOMAIN_URL + url;
-            console.log(URL);
             const obj = {
                 method: 'DELETE',
                 headers: {
@@ -44,36 +43,35 @@ const CommentApp = (function () {
                 }
             };
 
-            fetch(URL, obj)
+            fetch(url, obj)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (json) {
-                    const li = btn.parentNode
-                    const ul = btn.parentNode.parentNode
-                    ul.removeChild(li)
+                    const li = btn.parentNode;
+                    const ul = btn.parentNode.parentNode;
+                    ul.removeChild(li);
                 });
-        }
+        };
 
-        const update = function(event) {
+        const update = function (event) {
             const articleId = document.getElementById('article-id').value;
             const btn = event.target;
-            if(btn.classList.contains("btn-lg")) {
+            if (btn.classList.contains("btn-lg")) {
                 const url = '/articles/' + articleId + '/comment/' + btn.value;
-                console.log(url);
                 document.getElementById('comment-update-url').setAttribute('value', url);
             }
-        }
+        };
 
         return {
             remove: remove,
-            update: update
+            update: update,
         }
-    }
+    };
 
     const init = function () {
-        const commentController = new CommentController()
-        commentController.init()
+        const commentController = new CommentController();
+        commentController.init();
     };
 
     return {
