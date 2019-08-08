70c6625ddbcbb1c38bc43d7d2b6f2806de10b58d
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-08 09:54:29 +0900

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-container_id_to_stop = $(sudo docker ps -q --filter name=myblog)
+
+container_id_to_stop=$(sudo docker ps -q --filter name=myblog)
 
 if [ ${#container_id_to_stop}=0 ]; then
     sudo docker stop $container_id_to_stop
 fi
 
-container_id_to_remove = $(sudo docker ps -a -q --filter name=myblog)
+container_id_to_remove=$(sudo docker ps -a -q --filter name=myblog)
 
 if [ ${#container_id_to_remove}=0 ]; then
     sudo docker rm $container_id_to_remove
