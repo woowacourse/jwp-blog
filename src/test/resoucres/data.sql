fbd4ab7b1b07c0587af16be6e6c57ed7ccdc844d
DaejunBae <dpudpu11@gmail.com>
2019-08-09 14:58:35 +0900

    feat: 테스트용 샘플 데이터 추가

---
+++ src/test/resoucres/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO user(email, name, password) VALUES ('spring@gmail.com', 'spring','1234');
+INSERT INTO user(email, name, password) VALUES ('brown@gmail.com', 'brown','1234');
+INSERT INTO user(email, name, password) VALUES ('cu@gmail.com', 'CU','1234');
+INSERT INTO user(email, name, password) VALUES ('asd@asd', 'asd','1234');
+
+
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '1','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '2','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '3','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '4','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '5','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '6','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '7','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES  (1, '8','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '9','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '10','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '11','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '12','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '13','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '14','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '15','https://avatars3.githubusercontent.com/u/46308949?s=460&v=4','냉무');
+INSERT INTO article(user_id, title, cover_url, contents) VALUES (1, '16','https://avatars3.githubusercontent.com/u/15669435?s=180&v=4','contents');
+
+INSERT INTO comment(contents, user_id, article_id) VALUES  ('contents', 1, 16);
+INSERT INTO comment(contents, user_id, article_id) VALUES  ('contents', 1, 16);
+INSERT INTO comment(contents, user_id, article_id) VALUES  ('contents', 1, 16);
