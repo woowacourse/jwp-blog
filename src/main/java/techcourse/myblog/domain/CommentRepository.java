e5ff83d22640ecedae46940716005f63a343554f
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-10 19:04:30 +0900

    refactor: remove unnecessary code

--- src/main/java/techcourse/myblog/domain/CommentRepository.java
+++ src/main/java/techcourse/myblog/domain/CommentRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByArticle(Article article);
-
     List<Comment> findByArticleId(Long articleId);
 }
